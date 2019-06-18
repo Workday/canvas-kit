42bee892256e09d85f4cd9ea21660ed156982ca7
manuel carrera <manuel.carrera@workday.com>
2019-06-18 16:41:39 -0600

    fix: update version and remove prop for styles

--- modules/side-panel/react/lib/SidePanel.tsx
+++ modules/side-panel/react/lib/SidePanel.tsx
@@ -48,7 +48,7 @@ const SidePanelContainer = styled('div')<SidePanelProps>(
     boxSizing: 'border-box',
     display: 'flex',
     flexDirection: 'column',
-    transition: 'width 600ms ease',
+    transition: 'width 200ms ease',
     position: 'absolute',
   },
   ({open}) => ({
@@ -92,8 +92,8 @@ const ChildrenContainer = styled('div')<Pick<SidePanelProps, 'openWidth' | 'open
     transition: 'none',
     zIndex: 1, // show above SidePanelFooter when screen is small vertically
   },
-  ({open, openWidth}) => ({
-    width: open ? openWidth : closedWidth,
+  ({openWidth}) => ({
+    width: openWidth,
   })
 );
 
