7aba8743d70d68a43948a4969fe28aca09c7e0e7
byed <dan.bye.0@gmail.com>
2019-06-26 17:45:41 -0600

    build(travis): push to docs

--- utils/storybook/commitDocs.sh
+++ utils/storybook/commitDocs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 git add $TRAVIS_BUILD_DIR/docs
 git diff $TRAVIS_BUILD_DIR --quiet && git diff $TRAVIS_BUILD_DIR --staged --quiet || git commit -m "docs(storybook): Build Storybook [ci skip]"
+git push
