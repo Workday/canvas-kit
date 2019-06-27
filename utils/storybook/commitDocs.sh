9e5c11920c6cf1fb3575ca6692201cd1bf8094c4
byed <dan.bye.0@gmail.com>
2019-06-27 08:33:50 -0600

    build(travis): use pages provider

--- utils/storybook/commitDocs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git add $TRAVIS_BUILD_DIR/docs
-git diff --quiet && git diff --staged --quiet || git commit -m "docs(storybook): Build Storybook [ci skip]"
-git push origin master
