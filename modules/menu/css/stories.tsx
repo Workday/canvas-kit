2be347bcc5ebe20f49fcd1a03c0a9756da5ba5ce
Stephane Rangaya <stephane@rangaya.com>
2019-07-03 17:39:18 +0200

    docs(CSS): Update stories styling to use emotion

--- modules/menu/css/stories.tsx
+++ modules/menu/css/stories.tsx
@@ -2,57 +2,64 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
+import {css} from 'emotion';
 import './index.scss';
-import './stories.scss';
+
+const containerStyle = css({
+  '> *': {
+    margin: '20px !important',
+
+    '&:first-child': {
+      marginLeft: 0,
+    },
+  },
+});
 
 storiesOf('CSS/Menu', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
     <div className="story">
-      <section className="menu-demo-section">
-        <h2>Menus</h2>
-        <div className="wdc-type menu-demo">
-          <div className="wdc-menu">
-            <ul>
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li>Item 2</li>
-            </ul>
-          </div>
-          <div className="wdc-menu wdc-menu-right">
-            <ul>
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li>Item 2</li>
-            </ul>
-          </div>
-          <div className="wdc-menu wdc-menu-left">
-            <ul>
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li>Item 2</li>
-            </ul>
-          </div>
-          <div className="wdc-menu wdc-menu-top">
-            <ul>
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li>Item 2</li>
-            </ul>
-          </div>
-          <div className="wdc-menu wdc-menu-bottom">
-            <ul>
-              <li>
-                <a href="#">Item 1</a>
-              </li>
-              <li>Item 2</li>
-            </ul>
-          </div>
+      <div className={containerStyle + ' wdc-type'}>
+        <div className="wdc-menu">
+          <ul>
+            <li>
+              <a href="#">Item 1</a>
+            </li>
+            <li>Item 2</li>
+          </ul>
         </div>
-      </section>
+        <div className="wdc-menu wdc-menu-right">
+          <ul>
+            <li>
+              <a href="#">Item 1</a>
+            </li>
+            <li>Item 2</li>
+          </ul>
+        </div>
+        <div className="wdc-menu wdc-menu-left">
+          <ul>
+            <li>
+              <a href="#">Item 1</a>
+            </li>
+            <li>Item 2</li>
+          </ul>
+        </div>
+        <div className="wdc-menu wdc-menu-top">
+          <ul>
+            <li>
+              <a href="#">Item 1</a>
+            </li>
+            <li>Item 2</li>
+          </ul>
+        </div>
+        <div className="wdc-menu wdc-menu-bottom">
+          <ul>
+            <li>
+              <a href="#">Item 1</a>
+            </li>
+            <li>Item 2</li>
+          </ul>
+        </div>
+      </div>
     </div>
   ));
--- modules/tooltip/css/stories.tsx
+++ modules/tooltip/css/stories.tsx
@@ -2,35 +2,44 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
+import {css} from 'emotion';
 import './index.scss';
-import './stories.scss';
+
+const containerStyle = css({
+  paddingTop: '20px',
+  '> *': {
+    margin: '20px !important',
+
+    '&:first-child': {
+      marginLeft: 0,
+    },
+  },
+});
 
 storiesOf('CSS/Tooltip', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
     <div className="story">
-      <section className="tooltip-demo-section">
-        <div className="wdc-type tooltip-demo">
-          <div className="wdc-tooltip-container">
-            Right tooltip
-            <div className="wdc-tooltip wdc-tooltip-right">Tooltip text</div>
-          </div>
-
-          <div className="wdc-tooltip-container">
-            Left tooltip
-            <div className="wdc-tooltip wdc-tooltip-left">Tooltip text</div>
-          </div>
-
-          <div className="wdc-tooltip-container">
-            Top tooltip
-            <div className="wdc-tooltip wdc-tooltip-top">Tooltip text</div>
-          </div>
-
-          <div className="wdc-tooltip-container">
-            Bottom tooltip
-            <div className="wdc-tooltip wdc-tooltip-bottom">Tooltip text</div>
-          </div>
+      <div className={containerStyle + ' wdc-type'}>
+        <div className="wdc-tooltip-container">
+          Right tooltip
+          <div className="wdc-tooltip wdc-tooltip-right">Tooltip text</div>
         </div>
-      </section>
+
+        <div className="wdc-tooltip-container">
+          Left tooltip
+          <div className="wdc-tooltip wdc-tooltip-left">Tooltip text</div>
+        </div>
+
+        <div className="wdc-tooltip-container">
+          Top tooltip
+          <div className="wdc-tooltip wdc-tooltip-top">Tooltip text</div>
+        </div>
+
+        <div className="wdc-tooltip-container">
+          Bottom tooltip
+          <div className="wdc-tooltip wdc-tooltip-bottom">Tooltip text</div>
+        </div>
+      </div>
     </div>
   ));
