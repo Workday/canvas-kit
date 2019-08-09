682b57c664fafbd2b5278718f1ac3984f168efa9
James Fan <james@somewhatstrange.com>
2019-08-09 13:40:32 -0700

    refactor(loading-animation): Update CSS styles to match React implementation (#83)
    * feat(loading-animation): update CSS styles to match React
    
    * fix(loading-animation): code clean up
    
    * fix(loading-animation): apply proper container styles and adjust dot positioning accordingly

--- modules/loading-animation/css/stories.tsx
+++ modules/loading-animation/css/stories.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
+import {css} from 'emotion';
 import './index.scss';
+
+const containerStyle = css({
+  paddingTop: '12px',
+});
 
 storiesOf('CSS/Loading Animations', module)
   .addDecorator(withReadme(README))
-  .add('All', () => (
+  .add('Loading Dots', () => (
     <div className="story">
-      <div className="wdc-loading-spinner">
-        <span />
+      <div className={containerStyle}>
+        <div className="wdc-loading-dots">
+          <span />
+        </div>
       </div>
     </div>
   ));
