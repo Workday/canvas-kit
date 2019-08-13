9654a60d3f8cf40119335ee9ab604f9fe47bbed7
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2019-08-13 15:56:24 -0600

    refactor(Card): Update CSS styles to match React implementation (#113)
    * refactor(card): match card story and css to match react
    
    * refactor(card): add full width class and update readme
    
    * refactor(card): remove width css and fix readme
    
    * docs(card): Update URL to text link

--- modules/card/css/stories.tsx
+++ modules/card/css/stories.tsx
@@ -10,53 +10,57 @@ storiesOf('CSS/Card', module)
     <div className="story">
       <h2>Default</h2>
       <section>
+        <div className="wdc-card">
+          <h3 className="wdc-card-header">Card Header</h3>
+          <div className="wdc-card-body">.wdc-card</div>
+        </div>
         <div className="wdc-card-container">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-6">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-6</div>
           </div>
           <div className="wdc-card-4">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-4</div>
           </div>
           <div className="wdc-card-4">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-4</div>
           </div>
           <div className="wdc-card-4">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-4</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-3</div>
           </div>
           <div className="wdc-card-6">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-6</div>
           </div>
           <div className="wdc-card-6">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-6</div>
           </div>
         </div>
@@ -67,71 +71,71 @@ storiesOf('CSS/Card', module)
       <section>
         <div className="wdc-card-container wdc-card-container-around">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-around</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-around</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-around</div>
           </div>
         </div>
         <div className="wdc-card-container wdc-card-container-between">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-between</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-between</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-between</div>
           </div>
         </div>
         <div className="wdc-card-container wdc-card-container-center">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-center</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-center</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-center</div>
           </div>
         </div>
         <div className="wdc-card-container wdc-card-container-end">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-end</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-end</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-end</div>
           </div>
         </div>
         <div className="wdc-card-container wdc-card-container-start">
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-start</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-start</div>
           </div>
           <div className="wdc-card-3">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-container-start</div>
           </div>
         </div>
@@ -142,12 +146,35 @@ storiesOf('CSS/Card', module)
       <section>
         <div className="wdc-card-container">
           <div className="wdc-card-6 wdc-card-no-padding">
-            <div className="wdc-card-header">Card Header</div>
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-6.wdc-card-no-padding</div>
           </div>
-          <div className="wdc-card-6">
-            <div className="wdc-card-header">Card Header</div>
+          <div className="wdc-card-6 wdc-card-no-padding">
+            <h3 className="wdc-card-header">Card Header</h3>
             <div className="wdc-card-body">.wdc-card-6</div>
+          </div>
+        </div>
+      </section>
+
+      <h2>Different Depths</h2>
+
+      <section>
+        <div className="wdc-card-container">
+          <div className="wdc-card-3 wdc-depth-1">
+            <h3 className="wdc-card-header">Card Header</h3>
+            <div className="wdc-card-body">.wdc-depth-1</div>
+          </div>
+          <div className="wdc-card-3 wdc-depth-2">
+            <h3 className="wdc-card-header">Card Header</h3>
+            <div className="wdc-card-body">.wdc-depth-2</div>
+          </div>
+          <div className="wdc-card-3 wdc-depth-3">
+            <h3 className="wdc-card-header">Card Header</h3>
+            <div className="wdc-card-body">.wdc-depth-3</div>
+          </div>
+          <div className="wdc-card-3 wdc-depth-4">
+            <h3 className="wdc-card-header">Card Header</h3>
+            <div className="wdc-card-body">.wdc-depth-4</div>
           </div>
         </div>
       </section>
