5f2dbbdcdb1817a46cb7bbe9af824270771b0c5c
James Fan <james@somewhatstrange.com>
2019-08-13 16:22:07 -0700

    refactor(menu): Update CSS styles to match React implementation

--- modules/menu/css/stories.tsx
+++ modules/menu/css/stories.tsx
@@ -6,13 +6,7 @@ import {css} from 'emotion';
 import './index.scss';
 
 const containerStyle = css({
-  '> *': {
-    margin: '20px !important',
-
-    '&:first-child': {
-      marginLeft: 0,
-    },
-  },
+  paddingTop: '12px',
 });
 
 storiesOf('CSS/Menu', module)
@@ -23,9 +17,15 @@ storiesOf('CSS/Menu', module)
         <div className="wdc-menu">
           <ul>
             <li>
-              <a href="#">Item 1</a>
+              <a href="#">First Item</a>
             </li>
-            <li>Item 2</li>
+            <li>Second Item with really really really long label</li>
+            <li className="is-disabled">Third Item (disabled)</li>
+            <li>
+              <em>
+                Fourth Item (<b>with markup</b>)
+              </em>
+            </li>
           </ul>
         </div>
       </div>
