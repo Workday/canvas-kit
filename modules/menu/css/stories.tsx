7552874658e97343b84c6e8c0ea68d9a0c89467a
James Fan <james@somewhatstrange.com>
2019-08-26 10:24:20 -0700

    fix(Menu): Add Menu example with icons and grow support (#147)
    * fix(Menu): Remove unnecessary container styles
    
    * fix(Menu): Remove font-smoothing overrides
    
    * fix(Menu): Add Menu example with icons and grow support
    
    * fix(Menu): Remove unused import
    
    * fix(Menu): Use CSS icon instead of React SystemIcon
    
    * fix(Menu): Update README and stories

--- modules/menu/css/stories.tsx
+++ modules/menu/css/stories.tsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
-import {css} from 'emotion';
 import './index.scss';
+// @ts-ignore
+import initializeIcons from '../../icon/css/lib/canvas-kit-css-icon';
 
-const containerStyle = css({
-  paddingTop: '12px',
-});
+class WithIconsDemo extends Component {
+  componentDidMount() {
+    initializeIcons();
+  }
+
+  render() {
+    return (
+      <div className="wdc-type">
+        <div className="wdc-menu" style={{width: '250px'}}>
+          <ul>
+            <li className="wdc-menu-item-focused">
+              <i className="wdc-icon" data-icon="uploadCloud" data-category="system" />
+              <span className="wdc-menu-item-label">First Item</span>
+            </li>
+            <li>
+              <i className="wdc-icon" data-icon="setup" data-category="system" />
+              <span className="wdc-menu-item-label">
+                Second Item with really really really long label
+              </span>
+            </li>
+            <li className="wdc-menu-item-disabled">
+              <i className="wdc-icon" data-icon="uploadCloud" data-category="system" />
+              <span className="wdc-menu-item-label">Third Item (disabled)</span>
+              <i className="wdc-icon" data-icon="extLink" data-category="system" />
+            </li>
+            <li>
+              <i className="wdc-icon" data-icon="user" data-category="system" />
+            </li>
+            <hr />
+            <li>
+              <i className="wdc-icon" data-icon="user" data-category="system" />
+              <span className="wdc-menu-item-label">Fifth Item (with divider)</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
 storiesOf('CSS/Menu', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
     <div className="story">
-      <div className={containerStyle + ' wdc-type'}>
-        <div className="wdc-menu">
+      <div className="wdc-type">
+        <div className="wdc-menu wdc-menu-grow">
           <ul>
-            <li>
+            <li className="wdc-menu-item-focused">
               <a href="#">First Item</a>
             </li>
             <li>Second Item with really really really long label</li>
@@ -31,5 +68,10 @@ storiesOf('CSS/Menu', module)
           </ul>
         </div>
       </div>
+    </div>
+  ))
+  .add('With Icons', () => (
+    <div className="story">
+      <WithIconsDemo />
     </div>
   ));
