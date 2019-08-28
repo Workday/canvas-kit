7eeda44ee1b2258a2475b50b9cc53f81f4cc8e15
Manuel Carrera <manuel.carrera.91@gmail.com>
2019-08-28 15:41:10 -0600

    refactor(Popup): Update css to match React implementation

--- modules/popup/css/stories.tsx
+++ modules/popup/css/stories.tsx
@@ -2,55 +2,76 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
-import {IconButton, Button} from '@workday/canvas-kit-react-button';
-import {xIcon} from '@workday/canvas-system-icons-web';
-import {css} from 'emotion';
+import {Button} from '@workday/canvas-kit-react-button';
 import './index.scss';
 
-const containerStyle = css({
-  '> *': {
-    margin: '20px !important',
+interface PopupWrapperState {
+  open: boolean;
+}
 
-    '&:first-child': {
-      marginLeft: 0,
-    },
-  },
-});
+class PopupWrapper extends React.Component<{}, PopupWrapperState> {
+  state = {
+    open: false,
+  };
+  public render() {
+    const {open} = this.state;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <div className="wdc-popup-container">
+          <Button buttonType={Button.Types.Delete} onClick={this.handleClick}>
+            Delete Item
+          </Button>
+          {open ? (
+            <div className="wdc-popup wdc-popup-animation-origin-top-center wdc-depth-2">
+              <h3 className="wdc-popup-title">Delete Item</h3>
+              <div style={{marginBottom: '24px'}} className="wdc-popup-body">
+                Are you sure you'd like to delete the item titled 'My Item'?
+              </div>
+              <Button
+                style={{marginRight: '16px'}}
+                onClick={this.handleSubmit}
+                buttonType={Button.Types.Delete}
+              >
+                Delete
+              </Button>
+              <Button onClick={this.handleSubmit} buttonType={Button.Types.Secondary}>
+                Cancel
+              </Button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+
+  private handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  private handleSubmit = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  private handleClick = (e: React.SyntheticEvent<HTMLButtonElement>) => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+}
 
 storiesOf('CSS/Popup', module)
   .addDecorator(withReadme(README))
   .add('All', () => (
     <div className="story">
-      <div className={containerStyle + ' wdc-type'}>
-        <div className="wdc-popup wdc-popup-top-center wdc-depth-2">
-          <div className="wdc-popup-close">
-            <IconButton
-              buttonType={IconButton.Types.Plain}
-              onClick={() => console.warn('close clicked')}
-              icon={xIcon}
-              title="Close"
-              aria-label="Close"
-            />
-          </div>
-          <h3 className="wdc-popup-title">Total Salary & Allowances</h3>
-          <div>
-            Start Date: 01/01/2000
-            <p>Total Salary & Allowances: $78,798.88</p>
-          </div>
-          <Button
-            style={{marginRight: '16px'}}
-            onClick={() => console.warn('Submit Clicked')}
-            buttonType={Button.Types.Delete}
-          >
-            Delete
-          </Button>
-          <Button
-            onClick={() => console.warn('Cancel Clicked')}
-            buttonType={Button.Types.Secondary}
-          >
-            Cancel
-          </Button>
-        </div>
-      </div>
+      <PopupWrapper />
     </div>
   ));
