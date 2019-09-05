7d9a391045e54f0ada7505e92afc8ff76637519a
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2019-09-05 10:34:27 -0600

    refactor(page-header): Refactor CSS to match React implementation (#151)
    ### Visual
    - Page Header now matches React Page Header. 
    
    ### Code
    - Update padding based on a breakpoint.
    - Page header title now has our base font family
    - We set the cap width to `1440px`

--- modules/page-header/css/stories.tsx
+++ modules/page-header/css/stories.tsx
@@ -8,26 +8,35 @@ import './index.scss';
 
 storiesOf('CSS/Page Header', module)
   .addDecorator(withReadme(README))
-  .add('All', () => (
+  .add('Default', () => (
     <div className="story">
       <header className="wdc-page-header">
         <div className="wdc-page-header-container">
-          <h2 className="wdc-page-header-title">Page Title</h2>
-          <div className="wdc-icon-list wdc-icon-list-white">
-            <div
-              className="wdc-btn wdc-btn-size-m wdc-btn-icon-circle wdc-btn-icon-inverse"
-              role="button"
-              tabIndex={0}
-            >
+          <h2 className="wdc-page-header-title">Product Context</h2>
+          <div className="wdc-icon-list">
+            <button className="wdc-btn-icon-inverse">
               <SystemIcon icon={exportIcon} />
-            </div>
-            <div
-              className="wdc-btn wdc-btn-size-m wdc-btn-icon-circle wdc-btn-icon-inverse"
-              role="button"
-              tabIndex={0}
-            >
+            </button>
+            <button className="wdc-btn-icon-inverse">
               <SystemIcon icon={fullscreenIcon} />
-            </div>
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  ))
+  .add('Cap Width', () => (
+    <div className="story">
+      <header className="wdc-page-header">
+        <div className="wdc-page-header-container wdc-page-header-cap-width">
+          <h2 className="wdc-page-header-title">Product Context</h2>
+          <div className="wdc-icon-list">
+            <button className="wdc-btn-icon-inverse">
+              <SystemIcon icon={exportIcon} />
+            </button>
+            <button className="wdc-btn-icon-inverse">
+              <SystemIcon icon={fullscreenIcon} />
+            </button>
           </div>
         </div>
       </header>
