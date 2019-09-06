641ce89200a9d5a66b6bdbedd8154b04fac56498
James Fan <james@somewhatstrange.com>
2019-09-06 14:25:16 -0700

    refactor(icon): Update CSS styles to match React implementation (#159)
    * refactor(Icon): Update CSS styles to match React implementation
    
    * refactor(Icon): Add transparent support to Accent Icon
    
    * refactor(Icon): Add circle support to System Icon
    
    * refactor(Icon): Remove old story
    
    * refactor(Icon): Organize styles, add missing import to stories, and fix package.json
    
    * refactor(Icon): Refactor javascript helpers and fix bug with custom circle background color
    
    * refactor(Icon): Update README
    
    * refactor(Icon): Add hover support to system icon
    
    * refactor(Icon): Clean up code
    
    * refactor(Icon): Update README and stories
    
    * refactor(Icon): Add color shorthand support to system icon
    
    * refactor(Icon): Clean up code
    
    * refactor(icon): Remove unnecessary data-circle attribute
    
    * docs(icon): Add stories for examples of hover attributes and props
    
    * docs(icon): Improve readability of README

--- modules/icon/css/lib/canvas-kit-css-icon.js
+++ modules/icon/css/lib/canvas-kit-css-icon.js
@@ -1,7 +1,8 @@
 import SVGInjector from 'svg-injector';
 import toSlug from 'to-slug-case';
 import canvasColors from '@workday/canvas-colors-web';
-import {getHue, getColor} from './utils';
+import {pickForegroundColor} from '@workday/canvas-kit-react-common';
+import {appendStyle, getHue, getColor} from './utils';
 
 const cdnUrl = 'https://design.workdaycdn.com/beta/assets/web-icons';
 
@@ -10,11 +11,24 @@ const colorableKeys = {
   accent: 'data-accent-color',
   background: 'data-background-color',
   color: 'data-color',
+  hover: 'data-hover-color',
+  hoverFill: 'data-hover-fill-color',
+  hoverAccent: 'data-hover-accent-color',
+  hoverBackground: 'data-hover-background-color',
 };
 
-function getColorableFn(className, defaultColor = null) {
-  // Assume valid color value if not in palette
-  return attrVal => ({[className]: getColor(attrVal) || attrVal || defaultColor});
+function getColorableFn(classNames, defaultColor = null) {
+  return attrVal =>
+    classNames.reduce((colors, className) => {
+      if (attrVal) {
+        // Assume valid color value if not in palette
+        colors[className] = getColor(attrVal) || attrVal;
+      } else if (defaultColor !== null) {
+        colors[className] = defaultColor;
+      }
+
+      return colors;
+    }, {});
 }
 
 const categories = {
@@ -23,9 +37,13 @@ const categories = {
     version: '0.11.16', // TODO: Update to use 'latest' when available
     prefix: 'wd-icon',
     colorables: {
-      [colorableKeys.fill]: getColorableFn('.wd-icon-fill', canvasColors.primary.iconStandard),
-      [colorableKeys.accent]: getColorableFn('.wd-icon-accent', canvasColors.primary.iconStandard),
-      [colorableKeys.background]: getColorableFn('.wd-icon-background', 'transparent'),
+      [colorableKeys.color]: getColorableFn(
+        ['.wd-icon-fill', '.wd-icon-accent'],
+        canvasColors.primary.iconStandard
+      ),
+      [colorableKeys.fill]: getColorableFn(['.wd-icon-fill']),
+      [colorableKeys.accent]: getColorableFn(['.wd-icon-accent']),
+      [colorableKeys.background]: getColorableFn(['.wd-icon-background'], 'transparent'),
     },
   },
   accent: {
@@ -33,7 +51,7 @@ const categories = {
     version: '0.11.13', // TODO: Update to use 'latest' when available
     prefix: 'wd-accent',
     colorables: {
-      [colorableKeys.color]: getColorableFn('.color-500', canvasColors.blueberry500),
+      [colorableKeys.color]: getColorableFn(['.color-500'], canvasColors.blueberry500),
     },
   },
   applet: {
@@ -82,6 +100,18 @@ function colorIconClass(icon, className, fillColor) {
   });
 }
 
+function colorIconHoverClasses(iconSelector, classNames, fillColor) {
+  if (fillColor) {
+    classNames.forEach(className => {
+      appendStyle(`
+        ${iconSelector}:hover ${className} {
+          fill: ${getColor(fillColor) || fillColor};
+        }
+      `);
+    });
+  }
+}
+
 function colorIcons(selector) {
   const icons = document.querySelectorAll(selector);
 
@@ -102,6 +132,103 @@ function colorIcons(selector) {
   });
 }
 
+function sizeIcons(selector) {
+  const icons = document.querySelectorAll(selector);
+
+  icons.forEach(i => {
+    const size = i.getAttribute('data-size');
+    if (size) {
+      i.style.height = `${size}px`;
+      i.style.width = `${size}px`;
+    }
+  });
+}
+
+function styleAccentIcons(selector) {
+  // Style transparent accent icons
+  const selectorFragment = '[data-category="accent"][data-transparent]';
+  const transparentSelector = `${selector}${selectorFragment}`;
+  const transparentIcons = document.querySelectorAll(transparentSelector);
+
+  transparentIcons.forEach(i => {
+    i.querySelector('.french-vanilla-100').style.fill = 'transparent';
+  });
+}
+
+function styleSystemIcons(selector) {
+  styleSystemIconCircles(selector);
+  styleSystemIconHovers(selector);
+}
+
+function styleSystemIconCircles(selector) {
+  const selectorFragment = '[data-category="system"][data-circle-background]';
+  const circleSelector = `${selector}${selectorFragment}`;
+  const iconCircles = document.querySelectorAll(circleSelector);
+
+  iconCircles.forEach(i => {
+    const circle = document.createElement('div');
+    circle.setAttribute('class', 'wdc-icon-circle-container');
+
+    const size = i.getAttribute('data-size') || 40;
+    circle.style.height = `${size}px`;
+    circle.style.width = `${size}px`;
+    i.style.height = `${size * 0.625}px`;
+    i.style.width = `${size * 0.625}px`;
+
+    let circleBgColor = i.getAttribute('data-circle-background');
+    circleBgColor = circleBgColor === 'true' ? 'soap300' : circleBgColor;
+    circleBgColor = getColor(circleBgColor) || circleBgColor;
+    circle.style.backgroundColor = circleBgColor;
+
+    const iconColor = pickForegroundColor(circleBgColor, 'rgba(0,0,0,0.65)');
+    i.setAttribute(colorableKeys.fill, iconColor);
+    i.setAttribute(colorableKeys.accent, iconColor);
+
+    circle.innerHTML = i.outerHTML;
+    i.parentNode.replaceChild(circle, i);
+  });
+}
+
+function styleSystemIconHovers(selector) {
+  const selectorFragment = '[data-category="system"]:not([data-circle-background])';
+  const hoverableSelector = `${selector}${selectorFragment}`;
+
+  // Style default hovers
+  colorIconHoverClasses(hoverableSelector, ['.wd-icon-background'], 'transparent');
+  colorIconHoverClasses(
+    hoverableSelector,
+    ['.wd-icon-accent', '.wd-icon-fill'],
+    canvasColors.primary.iconHover
+  );
+
+  // Style individual hovers
+  const hoverableSystemIcons = document.querySelectorAll(hoverableSelector);
+
+  hoverableSystemIcons.forEach((i, index) => {
+    const iconClassName = `hoverable-system-icon-${index}`;
+    const iconSelector = `.${iconClassName}${selectorFragment}`;
+
+    i.classList.add(iconClassName);
+
+    colorIconHoverClasses(
+      iconSelector,
+      ['.wd-icon-accent', '.wd-icon-fill'],
+      i.getAttribute(colorableKeys.hover)
+    );
+    colorIconHoverClasses(iconSelector, ['.wd-icon-fill'], i.getAttribute(colorableKeys.hoverFill));
+    colorIconHoverClasses(
+      iconSelector,
+      ['.wd-icon-accent'],
+      i.getAttribute(colorableKeys.hoverAccent)
+    );
+    colorIconHoverClasses(
+      iconSelector,
+      ['.wd-icon-background'],
+      i.getAttribute(colorableKeys.hoverBackground)
+    );
+  });
+}
+
 function injectIcons(iconsPath = null, selector = '.wdc-icon') {
   const icons = document.querySelectorAll(selector);
 
@@ -113,6 +240,13 @@ function injectIcons(iconsPath = null, selector = '.wdc-icon') {
   });
 
   SVGInjector(icons, {}, () => {
+    sizeIcons(selector);
+
+    styleAccentIcons(selector);
+    styleSystemIcons(selector);
+
+    // colorIcons must be called at the end of this block (since previous calls
+    // in this block may have adjusted coloring)
     colorIcons(selector);
   });
 }
