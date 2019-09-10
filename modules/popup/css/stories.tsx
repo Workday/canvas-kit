7fd0d6b9b488df4cbd3054aa1be2b928c42b1de8
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2019-09-10 09:29:15 -0600

    refactor(popup): Refactor CSS to match React implementation (#155)
    * fix(Popup): Match CSS to react Popup
    * refactor(popup): Removed unused variables
    * fix(popup): Styles now extend card styles like react does
    * refactor(popup): Add popup animation styles
    * refactor(popup): Add detail about close button in readme
    * fix(popup): Add animation to readme
    * refactor(popup): Generate keyframes automatically
    * fix(popup): Fix popup padding and update readme with set width
    * refactor(popup): Tweak docs and story

--- modules/popup/css/stories.tsx
+++ modules/popup/css/stories.tsx
@@ -2,47 +2,95 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import README from './README.md';
-import {css} from 'emotion';
+import {Button} from '@workday/canvas-kit-react-button';
+import {xIcon} from '@workday/canvas-system-icons-web';
+import {SystemIcon} from '@workday/canvas-kit-react';
 import './index.scss';
 
-const containerStyle = css({
-  '> *': {
-    margin: '20px !important',
+interface PopupWrapperState {
+  open: boolean;
+}
 
-    '&:first-child': {
-      marginLeft: 0,
-    },
-  },
-});
+class PopupWrapper extends React.Component<{}, PopupWrapperState> {
+  state = {
+    open: false,
+  };
+  public render() {
+    const {open} = this.state;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          position: 'relative',
+        }}
+      >
+        <Button buttonType={Button.Types.Delete} onClick={this.onOpenPopupClick}>
+          Delete Item
+        </Button>
+        {open ? (
+          <div
+            className="wdc-popup wdc-popup-padding-s wdc-popup-animation-origin-top-center"
+            style={{
+              position: 'absolute',
+              width: '400px',
+              top: '40px',
+            }}
+          >
+            <div className="wdc-popup-close">
+              <button onClick={this.onCloseClick} className="wdc-btn-icon-plain">
+                <SystemIcon icon={xIcon} />
+              </button>
+            </div>
+            <h3 className="wdc-popup-title">Delete Item</h3>
+            <div style={{marginBottom: '24px'}} className="wdc-popup-body">
+              Are you sure you'd like to delete the item titled 'My Item'?
+            </div>
+            <Button
+              style={{marginRight: '16px'}}
+              onClick={this.onDeleteClick}
+              buttonType={Button.Types.Delete}
+            >
+              Delete
+            </Button>
+            <Button onClick={this.onCancelClick} buttonType={Button.Types.Secondary}>
+              Cancel
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+
+  private onCloseClick = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  private onDeleteClick = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  private onCancelClick = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  private onOpenPopupClick = (e: React.SyntheticEvent<HTMLButtonElement>) => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+}
 
 storiesOf('CSS/Popup', module)
   .addDecorator(withReadme(README))
   .add('All', () => (
     <div className="story">
-      <div className={containerStyle + ' wdc-type'}>
-        <div className="wdc-popup wdc-popup-right">
-          <div className="wdc-popup-title">Total Salary & Allowances</div>
-          <p>Start Date: 01/01/2000</p>
-          <p>Total Salary & Allowances: $78,798.88</p>
-        </div>
-
-        <div className="wdc-popup wdc-popup-left">
-          <div className="wdc-popup-title">Total Salary & Allowances</div>
-          <p>Start Date: 01/01/2000</p>
-          <p>Total Salary & Allowances: $78,798.88</p>
-        </div>
-
-        <div className="wdc-popup wdc-popup-top">
-          <div className="wdc-popup-title">Total Salary & Allowances</div>
-          <p>Start Date: 01/01/2000</p>
-          <p>Total Salary & Allowances: $78,798.88</p>
-        </div>
-
-        <div className="wdc-popup wdc-popup-bottom">
-          <div className="wdc-popup-title">Total Salary & Allowances</div>
-          <p>Start Date: 01/01/2000</p>
-          <p>Total Salary & Allowances: $78,798.88</p>
-        </div>
-      </div>
+      <PopupWrapper />
     </div>
   ));
