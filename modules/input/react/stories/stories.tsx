76d1f2dbccba222b16441a8be09656a2a19abd95
Mack Berliner <mack.berliner@ADULTARMGOAL.workdayinternal.com>
2019-09-13 10:15:20 -0700

    feat(input): Added stories to input module

--- modules/input/react/stories/stories.tsx
+++ modules/input/react/stories/stories.tsx
@@ -3,13 +3,22 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 
-import MyComponent from '../index';
+import Input from '../index';
 import README from '../README.md';
+import {controlComponent} from '../../../../utils/storybook';
 
-storiesOf('Canvas Kit/Input', module)
+storiesOf('Input', module)
   .addDecorator(withReadme(README))
-  .add('All', () => (
+  .add('Default', () => <div className="story">{controlComponent(<Input />)}</div>)
+  .add('With placeholder', () => (
+    <div className="story">{controlComponent(<Input placeholder="Placeholder" />)}</div>
+  ))
+  .add('Disabled', () => <div className="story">{controlComponent(<Input disabled={true} />)}</div>)
+  .add('Disabled with placeholder', () => (
     <div className="story">
-      <MyComponent />
+      {controlComponent(<Input placeholder="Placeholder" disabled={true} />)}
     </div>
+  ))
+  .add('Grow', () => (
+    <div className="story">{controlComponent(<Input placeholder="Placeholder" grow={true} />)}</div>
   ));
