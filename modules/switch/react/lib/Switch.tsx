89998ed64745f74ad2c08a273f84ca0be471ed4a
Stephane Rangaya <stephane@rangaya.com>
2019-09-17 17:32:34 -0400

    feat(switch): Add support for alerts and errors

--- modules/switch/react/lib/Switch.tsx
+++ modules/switch/react/lib/Switch.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'react-emotion';
-import {focusRing} from '@workday/canvas-kit-react-common';
-import {colors, depth} from '@workday/canvas-kit-react-core';
+import {ErrorType, focusRing, mouseFocusBehavior} from '@workday/canvas-kit-react-common';
+import {colors, inputColors, depth} from '@workday/canvas-kit-react-core';
 
 export interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {
   checked: boolean;
@@ -10,6 +10,7 @@ export interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement>
   onChange?: (e: React.SyntheticEvent) => void;
   value?: string;
   inputRef?: React.Ref<HTMLInputElement>;
+  error?: ErrorType;
 }
 
 const circleSize = 12;
@@ -32,12 +33,60 @@ const SwitchInput = styled('input')<SwitchProps>(
     borderRadius: 999,
     '&:focus, &:active': {
       outline: 'none',
-      ...focusRing(2, 2),
+      '& ~ div:first-of-type': {
+        ...focusRing(2, 2, false),
+      },
     },
+    ...mouseFocusBehavior({
+      '&:focus, &:active': {
+        '& ~ div:first-of-type': {
+          ...focusRing(0, 0),
+          animation: 'none',
+        },
+      },
+    }),
   },
-  ({disabled}) => ({
-    cursor: disabled ? 'not-allowed' : 'pointer',
-  })
+  ({error, disabled}) => {
+    let errorRingColor;
+    let errorRingBorderColor = 'transparent';
+
+    if (error === ErrorType.Error) {
+      errorRingColor = inputColors.error.border;
+    } else if (error === ErrorType.Alert) {
+      errorRingColor = inputColors.warning.border;
+      errorRingBorderColor = colors.cantaloupe600;
+    } else {
+      return;
+    }
+
+    const styles = {
+      '&': {
+        cursor: disabled ? 'not-allowed' : 'pointer',
+      },
+      '& ~ div:first-of-type': {
+        boxShadow: `
+          0 0 0 2px ${colors.frenchVanilla100},
+          0 0 0 4px ${errorRingColor},
+          0 0 0 5px ${errorRingBorderColor}`,
+      },
+      '&:focus ~ div:first-of-type': {
+        ...focusRing(2, 2, false),
+      },
+    };
+    return {
+      ...styles,
+      // Error rings take precedence over focus
+      ...mouseFocusBehavior({
+        ...styles,
+        '&:focus ~ div:first-of-type, &:active ~ div:first-of-type': {
+          boxShadow: `
+            0 0 0 2px ${colors.frenchVanilla100},
+            0 0 0 4px ${errorRingColor},
+            0 0 0 5px ${errorRingBorderColor}`,
+        },
+      }),
+    };
+  }
 );
 
 const SwitchBackground = styled('div')<Pick<SwitchProps, 'checked' | 'disabled'>>(
