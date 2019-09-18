c77f8c6c2ddb479f6c903b43609ceeae7c2fae9a
cyril.sellam <cyril.sellam@workday.com>
2019-09-18 19:10:56 +0100

    fix(avatar): AvatarButton added

---
+++ modules/avatar/react/lib/AvatarButton.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import styled from 'react-emotion';
+import isPropValid from '@emotion/is-prop-valid';
+import {colors} from '@workday/canvas-kit-react-core';
+import {focusRing, hideMouseFocus} from '@workday/canvas-kit-react-common';
+import {SystemIconCircle, SystemIconCircleSize} from '@workday/canvas-kit-react-icon';
+import {userIcon} from '@workday/canvas-system-icons-web';
+
+export enum AvatarVariant {
+  Light,
+  Dark,
+}
+
+export interface AvatarProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * An AvatarVariant enum indicating which variant to use for the default state (Light vs. Dark)
+   */
+  variant: AvatarVariant;
+  /**
+   * An SystemIconCircleSize enum or number value indicating the size of the avatar
+   */
+  size: SystemIconCircleSize | number;
+  /**
+   * Text describing what the avatar is showing
+   */
+  altText: string;
+  /**
+   * The url of the users avatar photo
+   */
+  url?: string;
+  /**
+   * An event handler function that gets called when the avatar is clicked
+   */
+  onClick?: (e: React.SyntheticEvent) => void;
+  /**
+   * Ref of button that the styled component renders.
+   */
+  buttonRef?: React.Ref<HTMLButtonElement>;
+}
+
+const Container = styled('button', {
+  shouldForwardProp: prop => isPropValid(prop) && prop !== 'size',
+})<Omit<AvatarProps, 'altText'>>(
+  {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 0,
+    border: 0,
+    borderRadius: '100%',
+    boxSizing: 'border-box',
+    overflow: 'hidden',
+    '& img': {
+      width: '100%',
+      height: '100%',
+    },
+  },
+  ({variant, size, onClick}) => ({
+    background: colors.soap200,
+    height: size,
+    width: size,
+    cursor: onClick ? 'pointer' : 'default',
+    '&:not([disabled])': {
+      '&:focus': {
+        outline: 'none',
+        ...(variant === AvatarVariant.Dark ? focusRing(2, 2) : focusRing(2)),
+      },
+    },
+    ...hideMouseFocus,
+  })
+);
+
+export default class AvatarButton extends React.Component<AvatarProps> {
+  static Variant = AvatarVariant;
+  static Size = SystemIconCircleSize;
+
+  static defaultProps = {
+    variant: AvatarVariant.Light,
+    size: SystemIconCircleSize.m,
+    altText: 'Avatar',
+  };
+
+  render() {
+    const {buttonRef, variant, altText, size, url, onClick, ...elemProps} = this.props;
+
+    const background = variant === AvatarVariant.Dark ? colors.blueberry400 : colors.soap300;
+    return (
+      <Container
+        variant={variant}
+        size={size}
+        onClick={onClick}
+        disabled={onClick ? false : true}
+        innerRef={buttonRef}
+        aria-label={altText}
+        {...elemProps}
+      >
+        {url ? (
+          <img src={url} alt={altText} />
+        ) : (
+          <SystemIconCircle icon={userIcon} background={background} size={size} />
+        )}
+      </Container>
+    );
+  }
+}
---
+++ modules/avatar/react/stories/stories_avatarButton.tsx
@@ -0,0 +1,60 @@
+/// <reference path="../../../../typings.d.ts" />
+import * as React from 'react';
+import {storiesOf} from '@storybook/react';
+import withReadme from 'storybook-readme/with-readme';
+
+import AvatarButton from '../index';
+import README from '../README.md';
+
+const IMAGE_URL = 'https://s3-us-west-2.amazonaws.com/design-assets-internal/avatars/lmcneil.png';
+
+storiesOf('AvatarButton', module)
+  .addDecorator(withReadme(README))
+  .add('Light', () => (
+    <div className="story">
+      <h3>Extra-Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xxl} />
+      <h3>Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xl} />
+      <h3>Large</h3>
+      <AvatarButton size={AvatarButton.Size.l} />
+      <h3>Medium</h3>
+      <AvatarButton size={AvatarButton.Size.m} />
+      <h3>Small</h3>
+      <AvatarButton size={AvatarButton.Size.s} />
+      <h3>Extra Small</h3>
+      <AvatarButton size={AvatarButton.Size.xs} />
+    </div>
+  ))
+  .add('Dark', () => (
+    <div className="story">
+      <h3>Extra-Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xxl} variant={AvatarButton.Variant.Dark} />
+      <h3>Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xl} variant={AvatarButton.Variant.Dark} />
+      <h3>Large</h3>
+      <AvatarButton size={AvatarButton.Size.l} variant={AvatarButton.Variant.Dark} />
+      <h3>Medium</h3>
+      <AvatarButton size={AvatarButton.Size.m} variant={AvatarButton.Variant.Dark} />
+      <h3>Small</h3>
+      <AvatarButton size={AvatarButton.Size.s} variant={AvatarButton.Variant.Dark} />
+      <h3>Extra Small</h3>
+      <AvatarButton size={AvatarButton.Size.xs} variant={AvatarButton.Variant.Dark} />
+    </div>
+  ))
+  .add('Image', () => (
+    <div className="story">
+      <h3>Extra-Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xxl} url={IMAGE_URL} />
+      <h3>Extra Large</h3>
+      <AvatarButton size={AvatarButton.Size.xl} url={IMAGE_URL} />
+      <h3>Large</h3>
+      <AvatarButton size={AvatarButton.Size.l} url={IMAGE_URL} />
+      <h3>Medium</h3>
+      <AvatarButton size={AvatarButton.Size.m} url={IMAGE_URL} />
+      <h3>Small</h3>
+      <AvatarButton size={AvatarButton.Size.s} url={IMAGE_URL} />
+      <h3>Extra Small</h3>
+      <AvatarButton size={AvatarButton.Size.xs} url={IMAGE_URL} />
+    </div>
+  ));
