c7a3f932237de00641bb3bc3f2a1010b3ca413f3
Nicholas Boll <nicholas.boll@workday.com>
2019-09-20 14:12:23 -0600

    chore: Update cypress-plugin-tab

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,13 +1,7 @@
 import 'cypress-pipe';
 import 'cypress-plugin-tab';
 
-const raf = () =>
-  new Cypress.Promise(resolve => {
-    (cy as any).state('window').requestAnimationFrame(resolve);
-  });
-
+// TODO: Remove when https://github.com/Bkucera/cypress-plugin-tab/issues/5 is fixed
 Cypress.Commands.overwrite('tab', (originalFn, subject) => {
-  return raf().then(() =>
-    originalFn(subject).then(s => s || (cy as any).state('window').document.activeElement)
-  );
+  return originalFn(subject).then(s => s || (cy as any).state('window').document.activeElement);
 });
