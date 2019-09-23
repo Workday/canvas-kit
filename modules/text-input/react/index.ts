edddd0ad8ce941ff032ccc1f261c863ddcfbc5c3
Mack Berliner <mack.berliner@ADULTARMGOAL.workdayinternal.com>
2019-09-23 15:00:02 -0700

    feat(form-field): Decouple of input icon to form field
    BREAKING CHANGE: Form field now handles input icons

--- modules/text-input/react/index.ts
+++ modules/text-input/react/index.ts
@@ -1,7 +1,5 @@
-import InputIconContainer from './lib/InputIconContainer';
 import TextInput from './lib/TextInput';
 
 export default TextInput;
-export {InputIconContainer, TextInput};
-export * from './lib/InputIconContainer';
+export {TextInput};
 export * from './lib/TextInput';
