18969f4e618f33054fed056006d4bc98c2252e0d
Alex Nicholls <anicholls3@gmail.com>
2019-09-23 12:53:36 -0600

    refactor: Change tabs to spaces in package json template

--- utils/create-component/templates/css/packageJson.js
+++ utils/create-component/templates/css/packageJson.js
@@ -1,10 +1,10 @@
 module.exports = (name, description) => `
 {
-	"name": "@workday/canvas-kit-css-${name}",
-	"version": "0.0.0",
-	"description": "${description}",
-	"author": "Workday, Inc. (https://www.workday.com)",
-	"license": "Apache-2.0",
+  "name": "@workday/canvas-kit-css-${name}",
+  "version": "0.0.0",
+  "description": "${description}",
+  "author": "Workday, Inc. (https://www.workday.com)",
+  "license": "Apache-2.0",
   "files": [
     "dist",
     "lib",
@@ -12,23 +12,23 @@ module.exports = (name, description) => `
   ],
   "style": "dist/canvas-kit-css-${name}.min.css",
   "main": "dist/canvas-kit-css-${name}.min.css",
-	"repository": {
-		"type": "git",
-		"url": "https://github.com/Workday/canvas-kit/tree/master/modules/${name}/css"
-	},
-	"scripts": {
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/Workday/canvas-kit/tree/master/modules/${name}/css"
+  },
+  "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1",
     "build": "node ../../../utils/css-build.js index.scss"
   },
-	"keywords": [
-		"canvas",
-		"canvas-kit",
+  "keywords": [
+    "canvas",
+    "canvas-kit",
     "css",
     "scss",
     "sass",
-		"components",
-		"workday",
-		"${name}"
-	]
+    "components",
+    "workday",
+    "${name}"
+  ]
 }
 `;
