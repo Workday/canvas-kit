b990bf00d3965012981425cef5c54f14c6075deb
Stephane Rangaya <stephane@rangaya.com>
2019-09-30 16:48:34 -0400

    feat(text-area): Update test

--- modules/text-area/react/spec/TextArea.spec.tsx
+++ modules/text-area/react/spec/TextArea.spec.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import { mount } from 'enzyme';
 import ReactDOMServer from 'react-dom/server';
 import TextArea from '../lib/TextArea';
-import {axe} from 'jest-axe';
+import { axe } from 'jest-axe';
 import FormField from '@workday/canvas-kit-react-form-field';
 
 describe('TextArea', () => {
+  const cb = jest.fn();
+  afterEach(() => {
+    cb.mockReset();
+  });
   test('TextArea should spread extra props', () => {
-    const component = mount(<TextArea data-propspread="test" />);
+    const component = mount(<TextArea data-propspread="test" onChange={cb} />);
     const container = component.at(0).getDOMNode();
     expect(container.getAttribute('data-propspread')).toBe('test');
     component.unmount();
@@ -15,6 +19,10 @@ describe('TextArea', () => {
 });
 
 describe('Text Area Accessibility', () => {
+  const cb = jest.fn();
+  afterEach(() => {
+    cb.mockReset();
+  });
   test('Text Area in a FormField should pass axe DOM accessibility guidelines', async () => {
     const html = ReactDOMServer.renderToString(
       <FormField label="My Field" inputId="my-input-field">
@@ -28,7 +36,7 @@ describe('Text Area Accessibility', () => {
     const html = ReactDOMServer.renderToString(
       <>
         <label id="123">Label</label>
-        <TextArea placeholder="Placeholder" value={'Hello'} aria-labelledby="123" />;
+        <TextArea placeholder="Placeholder" value={'Hello'} aria-labelledBy="123" onChange={cb} />;
       </>
     );
     expect(await axe(html)).toHaveNoViolations();
