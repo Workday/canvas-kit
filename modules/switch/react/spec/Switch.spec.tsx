268e702273b804c49489625a84d9df8df51a620a
Stephane Rangaya <stephane@rangaya.com>
2019-09-30 16:27:27 -0400

    feat(switch): Update test

--- modules/switch/react/spec/Switch.spec.tsx
+++ modules/switch/react/spec/Switch.spec.tsx
@@ -3,8 +3,13 @@ import {mount} from 'enzyme';
 import Switch from '../lib/Switch';
 
 describe('Switch', () => {
+  const cb = jest.fn();
+  afterEach(() => {
+    cb.mockReset();
+  });
+
   test('Switch should spread extra props', () => {
-    const component = mount(<Switch data-propspread="test" />);
+    const component = mount(<Switch data-propspread="test" onChange={cb} />);
     const input = component
       .find('input')
       // TODO: Standardize on prop spread location (see #150)
