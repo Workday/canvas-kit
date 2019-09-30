3380a96423c06b01710645a0c934741af915b116
Stephane Rangaya <stephane@rangaya.com>
2019-09-30 12:19:35 -0400

    fix(radio): Fix UUID behavior

--- modules/radio/react/lib/Radio.tsx
+++ modules/radio/react/lib/Radio.tsx
@@ -181,15 +181,16 @@ export default class Radio extends React.Component<RadioProps> {
   public static defaultProps = {
     checked: false,
     label: '',
-    id: uuid(),
   };
+
+  private id = uuid();
 
   public render() {
     // TODO: Standardize on prop spread location (see #150)
     const {
       checked,
       disabled,
-      id,
+      id = this.id,
       inputRef,
       label,
       name,
