f889ab95f78f35754194a958e371b79248ef8f13
Nicholas Boll <nicholas.boll@workday.com>
2019-10-07 14:58:24 -0600

    fix: Delete button text change

--- cypress/integration/Button.spec.ts
+++ cypress/integration/Button.spec.ts
@@ -25,7 +25,7 @@ describe('Button', () => {
     it('should render the correct text', () => {
       cy.get('button')
         .first()
-        .should('contain', 'Delete Button');
+        .should('contain', 'Delete');
     });
   });
 });
