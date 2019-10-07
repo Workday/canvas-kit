1981fc802b30e0b03ef70ae8a64e37f7e30d933a
Stephane Rangaya <stephanerangaya@users.noreply.github.com>
2019-10-07 15:02:55 -0400

    fix: Form field spacing (#234)
    * refactor(form-field): Normalize spacing
    
    * refactor(form-field): Normalize spacing
    
    * refactor(form-field): Update spacing
    
    * refactor(form-field): Remove temporary stories
    
    * Revert "refactor(form-field): Remove temporary stories"
    
    This reverts commit fd743a43a36d1eb97d6fa792dfc52a99964b929d.
    
    * refactor(form-field): Vertically center Label when on the left on CSS
    
    * refactor(form-field): Use variable instead of value
    
    * fix(form-field): Fix bug with long text
    
    * fix(checkbox): Fix checkbox alignment in react if long label
    
    * fix(radio): Fix alignment in react if long label
    
    * test: Update radio and checkbox snaps
    
    * chore(common): Revert whitespace changes
    
    * refactor(form-field): Fix spacing
    
    BREAKING CHANGE: Visual, spacing around form-field in both React and CSS
    
    * refactor(form-field): Update tests
    
    BREAKING CHANGE: visual
    
    * refactor(form-field): Remove testing stories
    
    * docs(radio): Update story to match CSS
    
    * refactor(radio): Update spacing to match checkbox and switch
    
    * test(radio): Update tests
    
    * refactor(radio): Use token variable
    
    * chore(form-field): Remove superfluous divs in stories

--- modules/form-field/react/stories/stories_Radio.tsx
+++ modules/form-field/react/stories/stories_Radio.tsx
@@ -14,161 +14,129 @@ const hintId = 'error-desc-id';
 storiesOf('Form Field/Radio/Top Label/Radio Group', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField label="Label" useFieldset={true}>
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField label="Label" useFieldset={true}>
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ))
   .add('Alert', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField
-          label="Label"
-          useFieldset={true}
-          error={FormField.ErrorType.Alert}
-          hintText={hintText}
-          hintId={hintId}
-        >
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField
+      label="Label"
+      useFieldset={true}
+      error={FormField.ErrorType.Alert}
+      hintText={hintText}
+      hintId={hintId}
+    >
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ))
   .add('Error with Grow', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField
-          label="Label"
-          useFieldset={true}
-          error={FormField.ErrorType.Error}
-          grow={true}
-          hintText={hintText}
-          hintId={hintId}
-        >
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField
+      label="Label"
+      useFieldset={true}
+      error={FormField.ErrorType.Error}
+      grow={true}
+      hintText={hintText}
+      hintId={hintId}
+    >
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ));
 
 storiesOf('Form Field/Radio/Top Label/Radio', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField label="Label">
-          <ControlledComponentWrapper
-            controlledProp={ControlledComponentWrapper.ControlledProp.Checked}
-          >
-            <Radio id="1" value="email" label="E-mail" />
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField label="Label">
+      <ControlledComponentWrapper
+        controlledProp={ControlledComponentWrapper.ControlledProp.Checked}
+      >
+        <Radio id="1" value="email" label="E-mail" />
+      </ControlledComponentWrapper>
+    </FormField>
   ));
 
 storiesOf('Form Field/Radio/Left Label/Radio Group', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField label="Label" useFieldset={true} labelPosition={FormField.LabelPosition.Left}>
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField label="Label" useFieldset={true} labelPosition={FormField.LabelPosition.Left}>
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ))
   .add('Alert', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField
-          label="Label"
-          useFieldset={true}
-          error={FormField.ErrorType.Alert}
-          hintText={hintText}
-          hintId={hintId}
-          labelPosition={FormField.LabelPosition.Left}
-        >
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField
+      label="Label"
+      useFieldset={true}
+      error={FormField.ErrorType.Alert}
+      hintText={hintText}
+      hintId={hintId}
+      labelPosition={FormField.LabelPosition.Left}
+    >
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ))
   .add('Error with Grow', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField
-          label="Label"
-          useFieldset={true}
-          error={FormField.ErrorType.Error}
-          grow={true}
-          hintText={hintText}
-          hintId={hintId}
-          labelPosition={FormField.LabelPosition.Left}
-        >
-          <ControlledComponentWrapper>
-            <RadioGroup name="contact">
-              <Radio id="1" value="email" label="E-mail" />
-              <Radio id="2" value="phone" label="Phone" />
-              <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
-              <Radio id="4" value="mail" label="Mail" />
-            </RadioGroup>
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField
+      label="Label"
+      useFieldset={true}
+      error={FormField.ErrorType.Error}
+      grow={true}
+      hintText={hintText}
+      hintId={hintId}
+      labelPosition={FormField.LabelPosition.Left}
+    >
+      <ControlledComponentWrapper>
+        <RadioGroup name="contact">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+          <Radio id="3" value="fax" label="Fax (disabled)" disabled={true} />
+          <Radio id="4" value="mail" label="Mail" />
+        </RadioGroup>
+      </ControlledComponentWrapper>
+    </FormField>
   ));
 
 storiesOf('Form Field/Radio/Left Label/Radio', module)
   .addDecorator(withReadme(README))
   .add('Default', () => (
-    <div className="story">
-      <div style={{textAlign: 'left', marginBottom: '24px'}}>
-        <FormField label="Label" labelPosition={FormField.LabelPosition.Left}>
-          <ControlledComponentWrapper
-            controlledProp={ControlledComponentWrapper.ControlledProp.Checked}
-          >
-            <Radio id="1" value="email" label="E-mail" />
-          </ControlledComponentWrapper>
-        </FormField>
-      </div>
-    </div>
+    <FormField label="Label" labelPosition={FormField.LabelPosition.Left}>
+      <ControlledComponentWrapper
+        controlledProp={ControlledComponentWrapper.ControlledProp.Checked}
+      >
+        <Radio id="1" value="email" label="E-mail" />
+      </ControlledComponentWrapper>
+    </FormField>
   ));
