2294564e7739d50ef273aa40922fde5d9e89a0c3
Alex Nicholls <anicholls3@gmail.com>
2019-10-09 10:00:46 -0700

    refactor: Update tests and jest config to work with new jest-emotion

--- modules/icon/react/spec/Icon.spec.tsx
+++ modules/icon/react/spec/Icon.spec.tsx
@@ -3,10 +3,9 @@ import {shallow, render} from 'enzyme';
 import Icon from '../lib/Icon';
 import {CanvasIconTypes} from '@workday/design-assets-types';
 import {shieldIcon} from '@workday/canvas-accent-icons-web';
-import {createMatchers} from 'jest-emotion';
-import * as emotion from '@emotion/core';
+import {matchers} from 'jest-emotion';
 
-expect.extend(createMatchers(emotion)); // TODO: This syntax changes in jest-emotion 10
+expect.extend(matchers);
 
 describe('Icon component', () => {
   test('Sets size correctly', () => {
