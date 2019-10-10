1b959cf2282f166ab44eb7edcdfc56ae482bcc0a
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-10-10 13:53:41 -0700

    test(icon): Rewrite tests using test-library

--- modules/icon/react/spec/Icon.spec.tsx
+++ modules/icon/react/spec/Icon.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {mount, render} from 'enzyme';
+import {render} from '@testing-library/react';
 import Icon from '../lib/Icon';
 import {CanvasIconTypes} from '@workday/design-assets-types';
 import {shieldIcon} from '@workday/canvas-accent-icons-web';
@@ -8,18 +8,8 @@ import {matchers} from 'jest-emotion';
 expect.extend(matchers);
 
 describe('Icon component', () => {
-  test('Sets size correctly', () => {
-    const size = 14;
-    const component = shallow(
-      <Icon src={shieldIcon} size={size} type={CanvasIconTypes.Accent} styles={{}} />
-    );
-    expect(component).toHaveStyleRule('height', size + 'px');
-    expect(component).toHaveStyleRule('width', size + 'px');
-    component.unmount();
-  });
-
   test('SVG is set in innerHTML', () => {
-    const component = render(<Icon src={shieldIcon} type={CanvasIconTypes.Accent} styles={{}} />);
-    expect(component.find('svg')).toHaveLength(1);
+    const {container} = render(<Icon src={shieldIcon} type={CanvasIconTypes.Accent} styles={{}} />);
+    expect(container.querySelector('svg')).toBeTruthy();
   });
 });
