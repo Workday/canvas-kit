b414373477b63b8d504210355e13fc78bfed461c
Chad Sahlhoff <chad@sahlhoff.com>
2019-10-15 11:23:33 -0700

    chore(react): Adds example of using csf and addParameter

--- modules/card/react/stories/stories.tsx
+++ modules/card/react/stories/stories.tsx
@@ -7,17 +7,36 @@ import {spacing} from '../../../core/react/index';
 import Card from '../index';
 import README from '../README.md';
 
-storiesOf('Card', module)
-  .addDecorator(withReadme(README))
-  .add('Default', () => (
-    <div className="story">
-      <Card heading="Title">Card contents</Card>
-    </div>
-  ))
-  .add('With No Padding', () => (
-    <div className="story">
-      <Card heading="Title" padding={spacing.zero}>
-        No padding
-      </Card>
-    </div>
-  ));
+export default {
+  title: 'Card',
+  component: Card,
+};
+
+export const defaultStory = () => (
+  <div className="story">
+    <Card heading="Title">Card contents</Card>
+  </div>
+);
+
+export const withNoPadding = () => (
+  <div className="story">
+    <Card heading="Title" padding={spacing.zero}>
+      No padding
+    </Card>
+  </div>
+);
+
+// storiesOf('Card', module)
+//   .addDecorator(withReadme(README))
+//   .add('Default', () => (
+//     <div className="story">
+//       <Card heading="Title">Card contents</Card>
+//     </div>
+//   ))
+//   .add('With No Padding', () => (
+//     <div className="story">
+//       <Card heading="Title" padding={spacing.zero}>
+//         No padding
+//       </Card>
+//     </div>
+//   ));
