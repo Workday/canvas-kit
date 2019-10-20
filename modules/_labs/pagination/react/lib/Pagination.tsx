f951054ad24be449a6088f1a093e7c25bc3042ea
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-10-19 22:49:38 -0700

    feat(pagination): Add label to component

--- modules/_labs/pagination/react/lib/Pagination.tsx
+++ modules/_labs/pagination/react/lib/Pagination.tsx
@@ -5,23 +5,37 @@ import {CSSObject} from 'create-emotion';
 import {IconButton} from '@workday/canvas-kit-react-button';
 import {chevronLeftSmallIcon, chevronRightSmallIcon} from '@workday/canvas-system-icons-web';
 import canvas, {borderRadius} from '@workday/canvas-kit-react-core';
+import {type} from '@workday/canvas-kit-labs-react-core';
 
+export interface PaginationLabelProps {
+  from: number;
+  to: number;
+  items: number;
+  itemLabel: string;
+}
 export interface PaginationProps extends React.HTMLAttributes<HTMLElement> {
   items: number;
   pageSize: number;
   currentPage: number;
   onPageChange: (page: number, e: React.MouseEvent<HTMLButtonElement>) => void;
+  label: (parts: PaginationLabelProps) => string;
 }
 
-const centeredStyle: CSSObject = {
+const Label = styled('div')({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-};
+  ...type.small,
+  color: canvas.typeColors.hint,
+});
 
-const Container = styled('nav')(centeredStyle, {
+const Container = styled('nav')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginBottom: canvas.spacing.xs,
   '& > * ': {
-    margin: canvas.spacing.xxxs,
+    margin: `${0} ${canvas.spacing.xxxs}`,
   },
   '&:first-child': {
     marginLeft: 0,
@@ -31,7 +45,10 @@ const Container = styled('nav')(centeredStyle, {
   },
 });
 
-const ActivePage = styled('div')(centeredStyle, {
+const ActivePage = styled('div')({
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   fontSize: 13,
   fontWeight: 700,
   height: 32,
@@ -77,32 +94,50 @@ const Pages: React.FC<{
 };
 
 const Pagination: React.FC<PaginationProps> = props => {
-  const {items, pageSize, currentPage = 1, onPageChange, ...elemProps} = props;
+  const {items, pageSize, currentPage = 1, onPageChange, label, ...elemProps} = props;
 
   const numPages = Math.ceil(items / pageSize);
 
+  const labelParts = {
+    from: (currentPage - 1) * pageSize + 1,
+    to: currentPage * pageSize >= items ? items : currentPage * pageSize,
+    items: items,
+    itemLabel: 'items',
+  };
+
+  const defaultLabelFormatter: (p: PaginationLabelProps) => string = ({
+    from,
+    to,
+    items,
+    itemLabel,
+  }) =>
+    `${from.toLocaleString()}\u2013${to.toLocaleString()} of ${items.toLocaleString()} ${itemLabel}`;
+
   return (
-    <Container aria-label={`Pagination Navigation`} {...elemProps}>
-      <IconButton
-        disabled={currentPage - 1 <= 0}
-        aria-disabled={currentPage - 1 <= 0}
-        aria-label={'Goto previous page'}
-        variant={IconButton.Variant.Square}
-        size={IconButton.Size.Small}
-        icon={chevronLeftSmallIcon}
-        onClick={e => onPageChange(currentPage - 1, e)}
-      />
-      <Pages numPages={numPages} currentPage={currentPage} clickHandler={onPageChange} />
-      <IconButton
-        disabled={currentPage + 1 > numPages}
-        aria-disabled={currentPage + 1 > numPages}
-        aria-label={'Goto next page'}
-        variant={IconButton.Variant.Square}
-        size={IconButton.Size.Small}
-        icon={chevronRightSmallIcon}
-        onClick={e => onPageChange(currentPage + 1, e)}
-      />
-    </Container>
+    <React.Fragment>
+      <Container aria-label={`Pagination Navigation`} {...elemProps}>
+        <IconButton
+          disabled={currentPage - 1 <= 0}
+          aria-disabled={currentPage - 1 <= 0}
+          aria-label={'Goto previous page'}
+          variant={IconButton.Variant.Square}
+          size={IconButton.Size.Small}
+          icon={chevronLeftSmallIcon}
+          onClick={e => onPageChange(currentPage - 1, e)}
+        />
+        <Pages numPages={numPages} currentPage={currentPage} clickHandler={onPageChange} />
+        <IconButton
+          disabled={currentPage + 1 > numPages}
+          aria-disabled={currentPage + 1 > numPages}
+          aria-label={'Goto next page'}
+          variant={IconButton.Variant.Square}
+          size={IconButton.Size.Small}
+          icon={chevronRightSmallIcon}
+          onClick={e => onPageChange(currentPage + 1, e)}
+        />
+      </Container>
+      <Label>{defaultLabelFormatter(labelParts)}</Label>
+    </React.Fragment>
   );
 };
 
