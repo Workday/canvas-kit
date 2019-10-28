804aaf788b26f504bce9ddf6f637e4ebb1671483
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2019-10-28 16:21:07 -0700

    chore(pagination): Update emotion dependencies

--- modules/_labs/pagination/react/lib/Pagination.tsx
+++ modules/_labs/pagination/react/lib/Pagination.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled from 'react-emotion';
+import styled from '@emotion/styled';
 
 import {IconButton} from '@workday/canvas-kit-react-button';
 import {chevronLeftSmallIcon, chevronRightSmallIcon} from '@workday/canvas-system-icons-web';
