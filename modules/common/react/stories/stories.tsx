ffd0d9da0cfdf3c0277ec709098f29c46e37df0b
Chad Sahlhoff <chad@sahlhoff.com>
2019-11-15 15:49:37 -0800

    docs: Moves Popper to Popups section

--- modules/common/react/stories/stories.tsx
+++ modules/common/react/stories/stories.tsx
@@ -49,7 +49,7 @@ class PopperPopup extends React.Component<{}, PopperPopupState> {
   };
 }
 
-storiesOf('Tokens|Common', module)
+storiesOf('Components|Popups/Popper', module)
   .addParameters({component: Popper})
   .addDecorator(withReadme(README))
   .add('Popper', () => (
