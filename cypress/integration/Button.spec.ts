717beb225b437f48769dfb965a68f4b79ef8a536
Nicholas Boll <nicholas.boll@workday.com>
2019-11-25 08:42:59 -0700

    test: Updated Cypress story helper for categorization

--- cypress/integration/Button.spec.ts
+++ cypress/integration/Button.spec.ts
@@ -7,7 +7,7 @@ describe('Button', () => {
 
   context('given primary buttons are rendered', () => {
     beforeEach(() => {
-      h.stories.load('components-buttons-button-react', 'primary');
+      h.stories.load('Components|Buttons/Button/React', 'Primary');
     });
 
     it('should not have any axe errors', () => {
@@ -23,7 +23,7 @@ describe('Button', () => {
 
   context('given delete buttons are rendered', () => {
     beforeEach(() => {
-      h.stories.load('components-buttons-button-react', 'delete');
+      h.stories.load('Components|Buttons/Button/React', 'Delete');
     });
 
     it('should not have any axe errors', () => {
