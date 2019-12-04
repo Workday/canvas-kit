8539fbd267257ddd15d7b487fb7dcdcdb3bbb866
Manuel Carrera <manuel.carrera.91@gmail.com>
2019-12-04 15:27:25 -0700

    fix(button): Fix story after storybook update

--- modules/button/css/stories.tsx
+++ modules/button/css/stories.tsx
@@ -1,39 +1,34 @@
 /** @jsx jsx */
-import {css, jsx} from "@emotion/core"
-import {storiesOf} from "@storybook/react"
-import withReadme from "storybook-readme/with-readme"
-import {SystemIcon} from "@workday/canvas-kit-react-icon"
-import {activityStreamIcon} from "@workday/canvas-system-icons-web"
-import README from "./README.md"
-import "./index.scss"
+import {css, jsx} from '@emotion/core';
+import {storiesOf} from '@storybook/react';
+import withReadme from 'storybook-readme/with-readme';
+import {SystemIcon} from '@workday/canvas-kit-react-icon';
+import {activityStreamIcon} from '@workday/canvas-system-icons-web';
+import README from './README.md';
+import './index.scss';
 
 const blueBackground = css({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundColor: "#0875e1",
-  margin: "0 10px",
-  padding: "12px",
-  maxWidth: "max-content",
-  borderRadius: "4px",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#0875e1',
+  margin: '0 10px',
+  padding: '12px',
+  maxWidth: 'max-content',
+  borderRadius: '4px',
   button: {
-    margin: "12px"
-  }
-})
+    margin: '12px',
+  },
+});
 
-storiesOf("Components|Buttons/Button/CSS", module)
+storiesOf('Components|Buttons/Button/CSS', module)
   .addDecorator(withReadme(README))
-  .add("Primary", () => (
+  .add('Primary', () => (
     <div className="story">
       <section>
         <h3>Large Primary</h3>
-        <button className="wdc-btn wdc-btn-primary wdc-btn-size-l">
-          Primary
-        </button>
-        <button
-          disabled={true}
-          className="wdc-btn wdc-btn-primary wdc-btn-size-l"
-        >
+        <button className="wdc-btn wdc-btn-primary wdc-btn-size-l">Primary</button>
+        <button disabled={true} className="wdc-btn wdc-btn-primary wdc-btn-size-l">
           Primary Disabled
         </button>
         <button className="wdc-btn wdc-btn-primary wdc-btn-size-l wdc-btn-grow">
@@ -51,19 +46,14 @@ storiesOf("Components|Buttons/Button/CSS", module)
 
       <section>
         <h3>Small Primary</h3>
-        <button className="wdc-btn wdc-btn-primary wdc-btn-size-s">
-          Primary
-        </button>
-        <button
-          disabled={true}
-          className="wdc-btn wdc-btn-primary wdc-btn-size-s"
-        >
+        <button className="wdc-btn wdc-btn-primary wdc-btn-size-s">Primary</button>
+        <button disabled={true} className="wdc-btn wdc-btn-primary wdc-btn-size-s">
           Primary Disabled
         </button>
       </section>
     </div>
   ))
-  .add("Secondary", () => (
+  .add('Secondary', () => (
     <div>
       <section>
         <h3>Large Secondary</h3>
@@ -90,17 +80,12 @@ storiesOf("Components|Buttons/Button/CSS", module)
       </section>
     </div>
   ))
-  .add("Delete", () => (
+  .add('Delete', () => (
     <div>
       <section>
         <h3>Large Delete</h3>
-        <button className="wdc-btn wdc-btn-delete wdc-btn-size-l">
-          Delete
-        </button>
-        <button
-          disabled={true}
-          className="wdc-btn wdc-btn-delete wdc-btn-size-l"
-        >
+        <button className="wdc-btn wdc-btn-delete wdc-btn-size-l">Delete</button>
+        <button disabled={true} className="wdc-btn wdc-btn-delete wdc-btn-size-l">
           Delete Disabled
         </button>
       </section>
@@ -115,22 +100,17 @@ storiesOf("Components|Buttons/Button/CSS", module)
 
       <section>
         <h3>Small Delete</h3>
-        <button className="wdc-btn wdc-btn-delete wdc-btn-size-s">
-          Delete
-        </button>
-        <button
-          disabled={true}
-          className="wdc-btn wdc-btn-delete wdc-btn-size-s"
-        >
+        <button className="wdc-btn wdc-btn-delete wdc-btn-size-s">Delete</button>
+        <button disabled={true} className="wdc-btn wdc-btn-delete wdc-btn-size-s">
           Delete Disabled
         </button>
       </section>
     </div>
-  ))
+  ));
 
-storiesOf("Components|Buttons/Button/CSS/Dropdown", module)
+storiesOf('Components|Buttons/Button/CSS/Dropdown', module)
   .addDecorator(withReadme(README))
-  .add("Default", () => (
+  .add('Default', () => (
     <div className="story">
       <section>
         <h3>Primary Large</h3>
@@ -146,11 +126,11 @@ storiesOf("Components|Buttons/Button/CSS/Dropdown", module)
         </button>
       </section>
     </div>
-  ))
+  ));
 
-storiesOf("CSS/Button/Text", module)
+storiesOf('Components|Buttons/Button/CSS/Text', module)
   .addDecorator(withReadme(README))
-  .add("Default", () => (
+  .add('Default', () => (
     <div className="story">
       <h3>Large</h3>
       <a href="#" className="wdc-btn-text">
@@ -169,7 +149,7 @@ storiesOf("CSS/Button/Text", module)
       </button>
     </div>
   ))
-  .add("Inverse", () => (
+  .add('Inverse', () => (
     <div className="story">
       <h3>Large Inverse</h3>
       <div css={blueBackground}>
@@ -181,13 +161,8 @@ storiesOf("CSS/Button/Text", module)
 
       <h3>Small Inverse</h3>
       <div css={blueBackground}>
-        <button className="wdc-btn-text wdc-btn-text-inverse wdc-btn-text-size-s">
-          Text
-        </button>
-        <button
-          disabled={true}
-          className="wdc-btn-text wdc-btn-text-inverse wdc-btn-text-size-s"
-        >
+        <button className="wdc-btn-text wdc-btn-text-inverse wdc-btn-text-size-s">Text</button>
+        <button disabled={true} className="wdc-btn-text wdc-btn-text-inverse wdc-btn-text-size-s">
           Text
         </button>
       </div>
@@ -198,37 +173,25 @@ storiesOf("CSS/Button/Text", module)
         </button>
       </div>
     </div>
-  ))
+  ));
 
-storiesOf("CSS/Button/Deprecated", module)
+storiesOf('CSS/Button/Deprecated', module)
   .addDecorator(withReadme(README))
-  .add("Primary", () => (
+  .add('Primary', () => (
     <div className="story">
       <h3>Large Primary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-primary">
-        Primary Button
-      </button>
+      <button className="wdc-btn-deprecated wdc-btn-primary">Primary Button</button>
       <button disabled={true} className="wdc-btn-deprecated wdc-btn-primary">
         Primary Button
       </button>
       <h3>Medium Primary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m">
-        Primary Button
-      </button>
-      <button
-        disabled={true}
-        className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m"
-      >
+      <button className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m">Primary Button</button>
+      <button disabled={true} className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m">
         Primary Button
       </button>
       <h3>Small Primary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s">
-        Primary Button
-      </button>
-      <button
-        disabled={true}
-        className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s"
-      >
+      <button className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s">Primary Button</button>
+      <button disabled={true} className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s">
         Primary Button
       </button>
       <h3>Growing Primary</h3>
@@ -239,23 +202,15 @@ storiesOf("CSS/Button/Deprecated", module)
       <a href="#" className="wdc-btn-deprecated wdc-btn-primary" role="button">
         Primary
       </a>
-      <a
-        href="#"
-        className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m"
-        role="button"
-      >
+      <a href="#" className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-m" role="button">
         Primary
       </a>
-      <a
-        href="#"
-        className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s"
-        role="button"
-      >
+      <a href="#" className="wdc-btn-deprecated wdc-btn-primary wdc-btn-size-s" role="button">
         Primary
       </a>
     </div>
   ))
-  .add("Secondary", () => (
+  .add('Secondary', () => (
     <div className="story">
       <h3>Large Secondary</h3>
       <button className="wdc-btn-deprecated">Secondary Button</button>
@@ -263,23 +218,17 @@ storiesOf("CSS/Button/Deprecated", module)
         Secondary Button
       </button>
       <h3>Medium Secondary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-size-m">
-        Secondary Button
-      </button>
+      <button className="wdc-btn-deprecated wdc-btn-size-m">Secondary Button</button>
       <button disabled={true} className="wdc-btn-deprecated wdc-btn-size-m">
         Secondary Button
       </button>
       <h3>Small Secondary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-size-s">
-        Secondary Button
-      </button>
+      <button className="wdc-btn-deprecated wdc-btn-size-s">Secondary Button</button>
       <button disabled={true} className="wdc-btn-deprecated wdc-btn-size-s">
         Secondary Button
       </button>
       <h3>Growing Secondary</h3>
-      <button className="wdc-btn-deprecated wdc-btn-grow">
-        Growing Secondary Button
-      </button>
+      <button className="wdc-btn-deprecated wdc-btn-grow">Growing Secondary Button</button>
       <h3>Links</h3>
       <a href="#" className="wdc-btn-deprecated" role="button">
         Secondary
@@ -294,33 +243,21 @@ storiesOf("CSS/Button/Deprecated", module)
       </a>
     </div>
   ))
-  .add("Delete", () => (
+  .add('Delete', () => (
     <div className="story">
       <h3>Large Delete</h3>
-      <button className="wdc-btn-deprecated wdc-btn-delete">
-        Delete Button
-      </button>
+      <button className="wdc-btn-deprecated wdc-btn-delete">Delete Button</button>
       <button disabled={true} className="wdc-btn-deprecated wdc-btn-delete">
         Delete Button
       </button>
       <h3>Medium Delete</h3>
-      <button className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m">
-        Delete Button
-      </button>
-      <button
-        disabled={true}
-        className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m"
-      >
+      <button className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m">Delete Button</button>
+      <button disabled={true} className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m">
         Delete Button
       </button>
       <h3>Small Delete</h3>
-      <button className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s">
-        Delete Button
-      </button>
-      <button
-        disabled={true}
-        className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s"
-      >
+      <button className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s">Delete Button</button>
+      <button disabled={true} className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s">
         Delete Button
       </button>
       <h3>Growing Delete</h3>
@@ -332,33 +269,23 @@ storiesOf("CSS/Button/Deprecated", module)
         Delete
       </a>
 
-      <a
-        href="#"
-        className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m"
-        role="button"
-      >
+      <a href="#" className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-m" role="button">
         Delete
       </a>
 
-      <a
-        href="#"
-        className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s"
-        role="button"
-      >
+      <a href="#" className="wdc-btn-deprecated wdc-btn-delete wdc-btn-size-s" role="button">
         Delete
       </a>
     </div>
   ))
-  .add("Dropdown", () => (
+  .add('Dropdown', () => (
     <div className="story">
       <section>
-        <button className="wdc-btn-deprecated wdc-btn-dropdown">
-          Dropdown
-        </button>
+        <button className="wdc-btn-deprecated wdc-btn-dropdown">Dropdown</button>
       </section>
     </div>
   ))
-  .add("Split", () => (
+  .add('Split', () => (
     <div className="story">
       <section>
         <div className="wdc-btn-split">
@@ -369,35 +296,26 @@ storiesOf("CSS/Button/Deprecated", module)
         </div>
 
         <div className="wdc-btn-split">
-          <button className="wdc-btn-deprecated wdc-btn-split-text">
-            Split Button
-          </button>
+          <button className="wdc-btn-deprecated wdc-btn-split-text">Split Button</button>
           <button className="wdc-btn-deprecated wdc-btn-split-icon" />
         </div>
       </section>
     </div>
-  ))
+  ));
 
-storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
+storiesOf('Components|Buttons/Button/CSS/Icon Button', module)
   .addDecorator(withReadme(README))
-  .add("Square", () => (
+  .add('Square', () => (
     <div className="story">
       <h3>Medium Square</h3>
       <button className="wdc-btn-icon-square" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
-      <button
-        disabled={true}
-        className="wdc-btn-icon-square"
-        aria-label="Activity Stream"
-      >
+      <button disabled={true} className="wdc-btn-icon-square" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <h3>Small Square</h3>
-      <button
-        className="wdc-btn-size-s wdc-btn-icon-square"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-size-s wdc-btn-icon-square" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <button
@@ -409,27 +327,17 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </button>
     </div>
   ))
-  .add("Square Filled", () => (
+  .add('Square Filled', () => (
     <div className="story">
       <h3>Medium Filled Square</h3>
-      <button
-        className="wdc-btn-icon-square-filled"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-icon-square-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
-      <button
-        disabled={true}
-        className="wdc-btn-icon-square-filled"
-        aria-label="Activity Stream"
-      >
+      <button disabled={true} className="wdc-btn-icon-square-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <h3>Small Filled Square</h3>
-      <button
-        className="wdc-btn-size-s wdc-btn-icon-square-filled"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-size-s wdc-btn-icon-square-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <button
@@ -441,27 +349,20 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </button>
     </div>
   ))
-  .add("Plain", () => (
+  .add('Plain', () => (
     <div className="story">
       <h3>Medium Plain</h3>
 
       <button className="wdc-btn-icon-plain" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
-      <button
-        disabled={true}
-        className="wdc-btn-icon-plain"
-        aria-label="Activity Stream"
-      >
+      <button disabled={true} className="wdc-btn-icon-plain" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
 
       <h3>Small Plain</h3>
 
-      <button
-        className="wdc-btn-size-s wdc-btn-icon-plain"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-size-s wdc-btn-icon-plain" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <button
@@ -473,24 +374,17 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </button>
     </div>
   ))
-  .add("Circle", () => (
+  .add('Circle', () => (
     <div className="story">
       <h3>Medium Circle</h3>
       <button className="wdc-btn-icon-circle" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
-      <button
-        disabled={true}
-        className="wdc-btn-icon-circle"
-        aria-label="Activity Stream"
-      >
+      <button disabled={true} className="wdc-btn-icon-circle" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <h3>Small Circle</h3>
-      <button
-        className="wdc-btn-size-s wdc-btn-icon-circle"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-size-s wdc-btn-icon-circle" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <button
@@ -502,27 +396,17 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </button>
     </div>
   ))
-  .add("Circle Filled", () => (
+  .add('Circle Filled', () => (
     <div className="story">
       <h3>Medium Filled Circle</h3>
-      <button
-        className="wdc-btn-icon-circle-filled"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-icon-circle-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
-      <button
-        disabled={true}
-        className="wdc-btn-icon-circle-filled"
-        aria-label="Activity Stream"
-      >
+      <button disabled={true} className="wdc-btn-icon-circle-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <h3>Small Filled Circle</h3>
-      <button
-        className="wdc-btn-size-s wdc-btn-icon-circle-filled"
-        aria-label="Activity Stream"
-      >
+      <button className="wdc-btn-size-s wdc-btn-icon-circle-filled" aria-label="Activity Stream">
         <SystemIcon icon={activityStreamIcon} />
       </button>
       <button
@@ -534,27 +418,20 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </button>
     </div>
   ))
-  .add("Inverse", () => (
+  .add('Inverse', () => (
     <div className="story">
       <h3>Medium Inverse</h3>
       <div css={blueBackground}>
         <button className="wdc-btn-icon-inverse" aria-label="Activity Stream">
           <SystemIcon icon={activityStreamIcon} />
         </button>
-        <button
-          disabled={true}
-          className="wdc-btn-icon-inverse"
-          aria-label="Activity Stream"
-        >
+        <button disabled={true} className="wdc-btn-icon-inverse" aria-label="Activity Stream">
           <SystemIcon icon={activityStreamIcon} />
         </button>
       </div>
       <h3>Small Inverse</h3>
       <div css={blueBackground}>
-        <button
-          className="wdc-btn-size-s wdc-btn-icon-inverse"
-          aria-label="Activity Stream"
-        >
+        <button className="wdc-btn-size-s wdc-btn-icon-inverse" aria-label="Activity Stream">
           <SystemIcon icon={activityStreamIcon} />
         </button>
         <button
@@ -567,14 +444,11 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
       </div>
     </div>
   ))
-  .add("Inverse Filled", () => (
+  .add('Inverse Filled', () => (
     <div className="story">
       <h3>Medium Inverse</h3>
       <div css={blueBackground}>
-        <button
-          className="wdc-btn-icon-inverse-filled"
-          aria-label="Activity Stream"
-        >
+        <button className="wdc-btn-icon-inverse-filled" aria-label="Activity Stream">
           <SystemIcon icon={activityStreamIcon} />
         </button>
         <button
@@ -602,4 +476,4 @@ storiesOf("Components|Buttons/Button/CSS/Icon Button", module)
         </button>
       </div>
     </div>
-  ))
+  ));
