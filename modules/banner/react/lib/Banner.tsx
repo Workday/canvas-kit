7169dc1dd1957513a1e0bee32481683863aa8a83
Stephane Rangaya <stephane@rangaya.com>
2019-12-12 13:06:20 -0500

    refactor(labs): Code clean up

--- modules/banner/react/lib/Banner.tsx
+++ modules/banner/react/lib/Banner.tsx
@@ -67,7 +67,7 @@ const BannerWrapper = styled('button')<BannerProps>(
   })
 );
 
-const BannerIcon = styled(SystemIcon)<BannerProps>({
+const BannerIcon = styled(SystemIcon)({
   marginRight: spacing.xs,
 });
 
