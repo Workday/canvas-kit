10285a073bf984485f10805ae3289b5ae20e9c9c
Manuel Carrera <manuel.carrera.91@gmail.com>
2019-12-19 11:00:46 -0700

    test: Add test for &:state

--- modules/common/react/spec/changeToStaticStates.spec.ts
+++ modules/common/react/spec/changeToStaticStates.spec.ts
@@ -28,6 +28,24 @@ describe('changeToStaticStates', () => {
     };
     expect(convertPseudoStatesToClasses(true, input)).toEqual(expected);
   });
+  it('should convert "&:hover" to "&.hover"', () => {
+    const input = {'&:hover': {display: 'none'}};
+    const expected = {
+      '&.hover': {
+        display: 'none',
+      },
+    };
+    expect(convertPseudoStatesToClasses(true, input)).toEqual(expected);
+  });
+  it('should convert "&:focus" to "&.focus"', () => {
+    const input = {'&:focus': {display: 'none'}};
+    const expected = {
+      '&.focus': {
+        display: 'none',
+      },
+    };
+    expect(convertPseudoStatesToClasses(true, input)).toEqual(expected);
+  });
   it('should convert "div:focus" to "div.focus"', () => {
     const input = {'div:focus': {display: 'none'}};
     const expected = {
