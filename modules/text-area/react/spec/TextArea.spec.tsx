7ae7e9dbe5a3e1f664ef51969033ba1a08de78b6
Stephane Rangaya <stephane@rangaya.com>
2020-01-03 12:36:57 -0500

    test(text-area): Switch tests to RTL

--- modules/text-area/react/spec/TextArea.spec.tsx
+++ modules/text-area/react/spec/TextArea.spec.tsx
@@ -1,44 +1,81 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
-import ReactDOMServer from 'react-dom/server';
+import {render, fireEvent} from '@testing-library/react';
 import TextArea from '../lib/TextArea';
+import ReactDOMServer from 'react-dom/server';
 import {axe} from 'jest-axe';
 import FormField from '@workday/canvas-kit-react-form-field';
 
-describe('TextArea', () => {
+const id = 'Test Text Area';
+const label = 'Test Text Area';
+const placeholder = 'Test Text Area';
+const value = 'Test Text Area';
+
+describe('Text Area', () => {
   const cb = jest.fn();
   afterEach(() => {
     cb.mockReset();
   });
-  test('TextArea should spread extra props', () => {
-    const component = mount(<TextArea data-propspread="test" onChange={cb} />);
-    const container = component.at(0).getDOMNode();
-    expect(container.getAttribute('data-propspread')).toBe('test');
-    component.unmount();
-  });
-});
 
-describe('Text Area Accessibility', () => {
-  const cb = jest.fn();
-  afterEach(() => {
-    cb.mockReset();
-  });
-  test('Text Area in a FormField should pass axe DOM accessibility guidelines', async () => {
-    const html = ReactDOMServer.renderToString(
-      <FormField label="My Field" inputId="my-input-field">
-        <TextArea placeholder="Placeholder" value={'Hello'} onChange={jest.fn()} />;
-      </FormField>
-    );
-    expect(await axe(html)).toHaveNoViolations();
+  describe('when rendered', () => {
+    describe('with an placeholder', () => {
+      test('should render a text area with placeholder', () => {
+        const {getByPlaceholderText} = render(<TextArea onChange={cb} placeholder={placeholder} />);
+        expect(getByPlaceholderText(placeholder)).toHaveAttribute('placeholder', placeholder);
+      });
+    });
+
+    describe('with a id', () => {
+      test('should render a text area with a id', () => {
+        const {getByPlaceholderText} = render(
+          <TextArea id={id} onChange={cb} value={value} placeholder={placeholder} />
+        );
+        expect(getByPlaceholderText(placeholder)).toHaveAttribute('id', id);
+      });
+    });
+
+    describe('with a value', () => {
+      test('should render a text area with a value', () => {
+        const {getByDisplayValue} = render(<TextArea onChange={cb} value={value} />);
+        expect(getByDisplayValue(value)).toBeDefined();
+      });
+    });
+
+    describe('with disabled attribute', () => {
+      test('should render a disabled text area', () => {
+        const {getByDisplayValue} = render(
+          <TextArea onChange={cb} disabled={true} value={value} />
+        );
+        expect(getByDisplayValue(value)).toBeDisabled();
+      });
+    });
+
+    describe('wrapped in a FormField', () => {
+      test('should pass axe DOM accessibility guidelines', async () => {
+        const html = ReactDOMServer.renderToString(
+          <FormField label="My Field" inputId="my-text-area-field">
+            <TextArea id="my-text-area-field" onChange={cb} />
+          </FormField>
+        );
+        expect(await axe(html)).toHaveNoViolations();
+      });
+    });
+
+    describe('with extra, arbitrary props', () => {
+      test('should spread extra props', () => {
+        const attr = 'test';
+        const {getByPlaceholderText} = render(
+          <TextArea onChange={cb} data-propspread={attr} placeholder={placeholder} />
+        );
+        expect(getByPlaceholderText(placeholder)).toHaveAttribute('data-propspread', attr);
+      });
+    });
   });
 
-  test('Text Area with `aria-labelledby` should pass axe DOM accessibility guidelines', async () => {
-    const html = ReactDOMServer.renderToString(
-      <>
-        <label id="123">Label</label>
-        <TextArea placeholder="Placeholder" value={'Hello'} aria-labelledby="123" onChange={cb} />;
-      </>
-    );
-    expect(await axe(html)).toHaveNoViolations();
+  describe('when provided an area ref', () => {
+    test('area ref should be defined', () => {
+      const ref: React.RefObject<HTMLAreaElement> = React.createRef();
+      render(<TextArea inputRef={ref} />);
+      expect(ref.current).toBeDefined();
+    });
   });
 });
