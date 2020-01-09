5d25a7fb0843a477b96f12319e1f08bc60300d73
chad sahlhoff <chad.sahlhoff@workday.com>
2020-01-09 14:35:36 -0800

    test(ColorPreview): Updates tests to RTL

--- modules/color-picker/react/spec/ColorPreview.spec.tsx
+++ modules/color-picker/react/spec/ColorPreview.spec.tsx
@@ -1,18 +1,103 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import {render} from '@testing-library/react';
 import ReactDOMServer from 'react-dom/server';
 import {axe} from 'jest-axe';
 import FormField from '@workday/canvas-kit-react-form-field';
 import ColorPreview from '../lib/ColorPreview';
 
+const id = 'color-preview';
+const value = 'eee';
+
 describe('ColorPreview', () => {
-  test('Card should spread extra props', () => {
-    const component = mount(<ColorPreview value={'#ffffff'} data-propspread="test" />);
-    const input = component
-      .find('input') // TODO: Standardize on prop spread location (see #150)
-      .getDOMNode();
-    expect(input.getAttribute('data-propspread')).toBe('test');
-    component.unmount();
+  describe('when rendered', () => {
+    describe('with a value', () => {
+      test('should render ColorPreview with value', () => {
+        const {getByTestId} = render(<ColorPreview value={value} data-testid={id} />);
+        expect(getByTestId(id).value).toBe(value);
+      });
+    });
+
+    describe('with a value', () => {
+      test('should render ColorPreview as readonly', () => {
+        const {getByTestId} = render(<ColorPreview value={value} data-testid={id} />);
+        expect(getByTestId(id)).toHaveAttribute('readonly');
+      });
+    });
+
+    describe('with a value', () => {
+      test('should render ColorPreview with the value as a background', () => {
+        const {container} = render(<ColorPreview value={value} data-testid={id} />);
+        expect(container.querySelector('div div div[class^="css"]')).toHaveStyle(
+          'background-color: #eee;'
+        );
+      });
+    });
+
+    describe('with disabled attribute', () => {
+      test('should render a disabled ColorPreview', () => {
+        const {getByTestId} = render(
+          <ColorPreview value={value} disabled={true} data-testid={id} />
+        );
+        expect(getByTestId(id)).toBeDisabled();
+      });
+    });
+
+    describe('with an id', () => {
+      test('should render a ColorPreview with an id', () => {
+        const {getByTestId} = render(<ColorPreview id="my-id" value={value} data-testid={id} />);
+        expect(getByTestId(id)).toHaveAttribute('id', 'my-id');
+      });
+    });
+
+    describe('without a value', () => {
+      test('should render a ColorPreview with an id', () => {
+        const {getByTestId} = render(<ColorPreview id="my-id" data-testid={id} />);
+        expect(getByTestId(id)).toHaveAttribute('id', 'my-id');
+      });
+    });
+
+    describe('wrapped in a FormField', () => {
+      test('should pass axe DOM accessibility guidelines', async () => {
+        const html = ReactDOMServer.renderToString(
+          <FormField label="My Field" inputId="my-colorpreview">
+            <ColorPreview value={value} id="my-colorpreview" />
+          </FormField>
+        );
+        expect(await axe(html)).toHaveNoViolations();
+      });
+    });
+
+    describe('when provided an input ref', () => {
+      test('input ref should be defined', () => {
+        const ref: React.RefObject<HTMLInputElement> = React.createRef();
+        render(<ColorPreview value={value} inputRef={ref} />);
+        expect(ref.current).toBeDefined();
+      });
+    });
+
+    describe('when provided a value with a hashtag', () => {
+      test('the value is stripped', () => {
+        const {getByTestId} = render(<ColorPreview value={'#eee'} data-testid={id} />);
+        expect(getByTestId(id).value).toBe(value);
+      });
+    });
+
+    describe('when provided more than 6 characters as the value', () => {
+      test('the value is stripped', () => {
+        const {getByTestId} = render(<ColorPreview value={'123456789'} data-testid={id} />);
+        expect(getByTestId(id).value).toBe('123456');
+      });
+    });
+
+    describe('with extra, arbitrary props', () => {
+      test('should spread extra props', () => {
+        const attr = 'test';
+        const {getByTestId} = render(
+          <ColorPreview value={value} data-propspread={attr} data-testid={id} />
+        );
+        expect(getByTestId(id)).toHaveAttribute('data-propspread', attr);
+      });
+    });
   });
 });
 
@@ -20,7 +105,7 @@ describe('ColorPreview Accessibility', () => {
   test('ColorPreview should pass axe DOM accessibility guidelines', async () => {
     const html = ReactDOMServer.renderToString(
       <FormField label="Label" inputId="input-plain">
-        <ColorPreview value={'#ffffff'} />
+        <ColorPreview value={value} />
       </FormField>
     );
     expect(await axe(html)).toHaveNoViolations();
