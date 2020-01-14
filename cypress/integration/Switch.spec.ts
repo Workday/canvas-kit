0e1a9dc302b761665b1bf0857641ccaf6cee0ad8
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2020-01-13 21:52:08 -0800

    test(switch): Simplify and align Cypress tests
    Using #372 as a canonical example

--- cypress/integration/Switch.spec.ts
+++ cypress/integration/Switch.spec.ts
@@ -4,53 +4,41 @@ const getSwitch = () => {
   return cy.get(`[type="checkbox"]`);
 };
 
-describe('Non-disabled Switch', () => {
+describe('Switch', () => {
   before(() => {
     h.stories.visit();
   });
   ['Default', 'Alert', 'Error'].forEach(story => {
-    context(`Given the '${story}' story is rendered`, () => {
+    context(`given the '${story}' story is rendered`, () => {
       beforeEach(() => {
         h.stories.load('Components|Inputs/Switch/React/Top Label', story);
       });
 
-      it('should not have any axe errors', () => {
+      it('should pass accessibility checks', () => {
         cy.checkA11y();
       });
 
-      context('when the tab key is pressed', () => {
-        beforeEach(() => {
-          cy.tab();
-        });
-        it('should be the focused item on the page', () => {
-          getSwitch().should('have.focus');
-        });
-      });
-
-      context('when the switch is clicked', () => {
+      context('when clicked', () => {
         beforeEach(() => {
           getSwitch().click();
         });
+
         it('should be checked', () => {
           getSwitch().should('be.checked');
         });
       });
     });
   });
-});
 
-describe('Disabled Switch', () => {
-  before(() => {
-    h.stories.visit();
-  });
-  context(`Given the 'Disabled' story is rendered`, () => {
+  context(`given the 'Disabled' story is rendered`, () => {
     beforeEach(() => {
       h.stories.load('Components|Inputs/Switch/React/Top Label', 'Disabled');
     });
 
-    it('should not have any axe errors', () => {
+    it('should pass accessibility checks', () => {
       cy.checkA11y();
     });
+
     it('should be disabled', () => {
       getSwitch().should('be.disabled');
     });
