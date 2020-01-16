101d442f4bc4f8b1d68ffde7feac9b6d23f90cfe
James Fan <james@somewhatstrange.com>
2020-01-15 17:29:30 -0800

    test(select): Update tests to use react-testing-library

--- modules/select/react/spec/Select.spec.tsx
+++ modules/select/react/spec/Select.spec.tsx
@@ -1,38 +1,57 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import {render, fireEvent} from '@testing-library/react';
 import SelectOption from '../lib/SelectOption';
 import Select from '../lib/Select';
 
 describe('Select', () => {
   const cb = jest.fn();
+
+  const role = 'listbox';
+
   afterEach(() => {
     cb.mockReset();
   });
 
-  test('renders two select options as expected', () => {
-    const component = mount(
-      <Select name="contact">
-        <SelectOption value="email" label="E-mail" />
-        <SelectOption value="phone" label="Phone" />
-      </Select>
-    );
-
-    const options = component.find('option');
-
-    expect(options.length).toEqual(2);
+  describe('when rendered with an id', () => {
+    it('should render a select with id', () => {
+      const id = 'mySelect';
+      const {getByRole} = render(<Select id={id} onChange={cb}></Select>);
+      expect(getByRole(role)).toHaveAttribute('id', id);
+    });
   });
 
-  test('Select should spread extra props', () => {
-    const component = mount(
-      <Select name="contact" data-propspread="test">
-        <SelectOption value="email" label="E-mail" />
-        <SelectOption value="phone" label="Phone" />
-      </Select>
-    );
-    const select = component
-      .find('select') // TODO: Standardize on prop spread location (see #150)
-      .getDOMNode();
-    expect(select.getAttribute('data-propspread')).toBe('test');
-    component.unmount();
+  describe('when rendered with disabled attribute', () => {
+    it('should render a disabled select', () => {
+      const {getByRole} = render(<Select disabled={true} onChange={cb}></Select>);
+      expect(getByRole(role)).toHaveProperty('disabled', true);
+    });
+  });
+
+  describe('when rendered with extra, arbitrary props', () => {
+    it('should spread extra props onto the select', () => {
+      const attr = 'test';
+      const {getByRole} = render(<Select data-propspread={attr} onChange={cb}></Select>);
+      expect(getByRole(role)).toHaveAttribute('data-propspread', attr);
+    });
+  });
+
+  describe('when rendered with two select options as children', () => {
+    it('should render two options', () => {
+      const {getAllByRole} = render(
+        <Select onChange={cb}>
+          <SelectOption value="testValue1" label="testValue1" />
+          <SelectOption value="testValue2" label="testValue2" />
+        </Select>
+      );
+      expect(getAllByRole('option')).toHaveLength(2);
+    });
+  });
+
+  describe('when changed', () => {
+    it('should call a callback function', () => {
+      const {getByRole} = render(<Select onChange={cb}></Select>);
+      fireEvent.change(getByRole(role));
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
   });
 });
