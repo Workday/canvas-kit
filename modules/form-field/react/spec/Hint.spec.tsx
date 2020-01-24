f52c82fe62bd3f4a423a8deef5242d7d36c09422
Alex Nicholls <anicholls3@gmail.com>
2020-01-24 14:31:00 -0800

    test(form-field): Simplify hint tests

--- modules/form-field/react/spec/Hint.spec.tsx
+++ modules/form-field/react/spec/Hint.spec.tsx
@@ -6,17 +6,17 @@ describe('Hint', () => {
   describe('when rendered with an ErrorType', () => {
     it('should render an error label when error type is Error', async () => {
       const {getByText} = render(<Hint error={Hint.ErrorType.Error} />);
-      expect(getByText('Error:').innerHTML).toEqual('Error: ');
+      expect(getByText('Error:')).toBeDefined();
     });
     it('should render an alert label when error type is Alert', async () => {
       const {getByText} = render(<Hint error={Hint.ErrorType.Alert} />);
-      expect(getByText('Alert:').innerHTML).toEqual('Alert: ');
+      expect(getByText('Alert:')).toBeDefined();
     });
   });
   describe('when rendered with a custom Error string', () => {
     it('should render a custom label for type Error', async () => {
       const {getByText} = render(<Hint errorLabel={'Hay un error'} error={Hint.ErrorType.Error} />);
-      expect(getByText('Hay un error:').innerHTML).toEqual('Hay un error: ');
+      expect(getByText('Hay un error:')).toBeDefined();
     });
   });
 });
