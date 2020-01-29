95f1380bb4b012640b1f8143ac156c901ff6c827
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-01-29 10:42:34 -0700

    test(radio): Remove cypress testing library

--- cypress/integration/RadioGroup.spec.ts
+++ cypress/integration/RadioGroup.spec.ts
@@ -16,13 +16,12 @@ describe('Radio Group', () => {
 
       context('when clicking one radio and then selecting another radio', () => {
         beforeEach(() => {
-          cy.queryAllByLabelText('E-mail').click();
-          cy.queryAllByLabelText('Mail').click();
+          cy.getByLabelText('E-mail').click();
+          cy.getByLabelText('Mail').click();
         });
 
         it('should one have one radio selected', () => {
-          console.log(cy.queryAllByLabelText('Mail1safd'));
-          cy.queryAllByLabelText('Mail').should('be.checked');
+          cy.getByLabelText('Mail').should('be.checked');
         });
       });
     });
