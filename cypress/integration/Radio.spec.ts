92d1a5794d5f95e19421633f2e04e8c528933ac5
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-01-31 17:25:06 -0700

    test(radio): Update test to have right callback

--- cypress/integration/Radio.spec.ts
+++ cypress/integration/Radio.spec.ts
@@ -8,6 +8,30 @@ describe('Radio', () => {
   before(() => {
     h.stories.visit();
   });
+
+  context(`given the Default story is rendered`, () => {
+    beforeEach(() => {
+      h.stories.load('Components|Inputs/Radio/React/Left Label/Radio', story);
+    });
+
+    it('should pass accessibility checks', () => {
+      cy.checkA11y();
+    });
+
+    context('when clicked', () => {
+      beforeEach(() => {
+        cy.getByLabelText('E-mail').click();
+      });
+
+      it.only('should be checked', () => {
+        getRadio().should('be.checked');
+      });
+    });
+  });
+
+  before(() => {
+    h.stories.visit();
+  });
   context(`given the 'Disabled' story is rendered`, () => {
     beforeEach(() => {
       h.stories.load('Components|Inputs/Radio/React/Left Label/Radio', 'Disabled');
--- modules/radio/react/spec/RadioGroup.spec.tsx
+++ modules/radio/react/spec/RadioGroup.spec.tsx
@@ -55,4 +55,17 @@ describe('Radio Group', () => {
       expect(getByTestId('radiogroup')).toHaveAttribute('data-propspread', attr);
     });
   });
+  describe('when clicked', () => {
+    it('should call a callback function', async () => {
+      const mockFunction = jest.fn();
+      const {getByTestId} = render(
+        <RadioGroup data-testid="radiogroup" name="contact" value={1}>
+          <Radio onChange={mockFunction()} id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
+      fireEvent.click(await getByTestId('radiogroup'));
+      expect(mockFunction).toHaveBeenCalledTimes(1);
+    });
+  });
 });
