3e91eacc0ef00cd319e3b218fa8b3a7d774bdfca
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2020-02-06 14:27:43 -0700

    test(radio): Change tests to react-testing-library and improve coverage (#381)
    * test(radio): Add tests and convert them to RTL
    * test: Add cypress test for Radio
    * chore(radio): Add states story
    * test(radio): Add test for radio group
    * test(radio): Add radio group cypress test
    * fix(radio): Remove extra prop for story

--- modules/radio/react/spec/RadioGroup.spec.tsx
+++ modules/radio/react/spec/RadioGroup.spec.tsx
@@ -1,151 +1,72 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
-import Radio from '../lib/Radio';
-import RadioGroup from '../lib/RadioGroup';
+import {render, fireEvent} from '@testing-library/react';
+import {Radio, RadioGroup} from '../index';
 
-describe('Radio', () => {
+describe('Radio Group', () => {
   const cb = jest.fn();
   afterEach(() => {
     cb.mockReset();
   });
 
-  test('renders two radios as expected', () => {
-    const component = mount(
-      <RadioGroup name="contact">
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" />
-      </RadioGroup>
-    );
-
-    const inputs = component.find('input');
-
-    expect(inputs.length).toEqual(2);
-    expect(inputs.get(0).props.name).toEqual('contact');
-    expect(inputs.get(1).props.name).toEqual('contact');
+  describe('when rendered rendered with a name', () => {
+    test('should render a radio group with a name', () => {
+      const {getByTestId} = render(
+        <RadioGroup name="contact" data-testid="radiogroup">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
+      expect(getByTestId('radiogroup')).toHaveAttribute('name');
+    });
   });
+  describe('when rendered with a value', () => {
+    test('should render a selected radio that matches that value of a string', async () => {
+      const {getByLabelText} = render(
+        <RadioGroup name="contact" value="phone">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
 
-  test('renders two radios with name set at radio level as expected', () => {
-    const component = mount(
-      <RadioGroup>
-        <Radio id="1" name="contact" value="email" label="E-mail" />
-        <Radio id="2" name="contact" value="phone" label="Phone" />
-      </RadioGroup>
-    );
+      const phoneRadio = await getByLabelText('Phone');
+      expect(phoneRadio).toHaveProperty('checked', true);
+    });
+    test('should render a selected radio that matches that value of a index', async () => {
+      const {getByLabelText} = render(
+        <RadioGroup name="contact" value={1}>
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
 
-    const inputs = component.find('input');
-
-    expect(inputs.length).toEqual(2);
-    expect(inputs.get(0).props.name).toEqual('contact');
-    expect(inputs.get(1).props.name).toEqual('contact');
+      const phoneRadio = await getByLabelText('Phone');
+      expect(phoneRadio).toHaveProperty('checked', true);
+    });
   });
-
-  test('renders two radios preselected with index as expected', () => {
-    const component = mount(
-      <RadioGroup name="contact" value={1}>
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" />
-      </RadioGroup>
-    );
-
-    const inputs = component.find('input');
-
-    expect(inputs.length).toEqual(2);
-    expect(inputs.get(0).props.name).toEqual('contact');
-    expect(inputs.get(1).props.name).toEqual('contact');
+  describe('when rendered with extra, arbitrary props', () => {
+    it('should spread extra props onto the radio group', () => {
+      const attr = 'test';
+      const {getByTestId} = render(
+        <RadioGroup data-propspread={attr} name="contact" value={1} data-testid="radiogroup">
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
+      expect(getByTestId('radiogroup')).toHaveAttribute('data-propspread', attr);
+    });
   });
+  describe('when clicked', () => {
+    it('should call a  callback function', () => {
+      const {getByLabelText} = render(
+        <RadioGroup name="contact" onChange={cb}>
+          <Radio id="1" value="email" label="E-mail" />
+          <Radio id="2" value="phone" label="Phone" />
+        </RadioGroup>
+      );
 
-  test('renders two radios preselected with value as expected', () => {
-    const component = mount(
-      <RadioGroup name="contact" value="phone">
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" />
-      </RadioGroup>
-    );
+      fireEvent.click(getByLabelText('Phone'));
 
-    const inputs = component.find('input');
-
-    expect(inputs.length).toEqual(2);
-    expect(inputs.get(0).props.name).toEqual('contact');
-    expect(inputs.get(1).props.name).toEqual('contact');
-  });
-
-  test('can switch to a different radio', () => {
-    const component = mount(
-      <RadioGroup name="contact" onChange={cb}>
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" />
-      </RadioGroup>
-    );
-
-    component
-      .find('input')
-      .at(1)
-      .simulate('change');
-
-    expect(cb.mock.calls.length).toBe(1);
-    expect(cb.mock.calls[0][0]).toBe('phone');
-  });
-
-  test('can switch to a different radio without value', () => {
-    const component = mount(
-      <RadioGroup name="contact" onChange={cb}>
-        <Radio id="1" label="E-mail" />
-        <Radio id="2" label="Phone" />
-      </RadioGroup>
-    );
-
-    component
-      .find('input')
-      .at(1)
-      .simulate('change');
-
-    expect(cb.mock.calls.length).toBe(1);
-    expect(cb.mock.calls[0][0]).toBe(1);
-  });
-
-  test('renders two radios and one disabled as expected', () => {
-    const component = mount(
-      <RadioGroup name="contact" onChange={cb}>
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" disabled={true} />
-        <Radio id="3" value="fax" label="Fax" />
-        <span /> {/* ensure random elements don't break anything */}
-      </RadioGroup>
-    );
-
-    const inputs = component.find('input');
-
-    expect(inputs.length).toEqual(3);
-    expect(inputs.get(1).props.disabled).toBe(true);
-  });
-
-  test('simulate onchange on a radio', () => {
-    const component = mount(
-      <RadioGroup name="contact">
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" disabled={true} />
-        <Radio id="3" value="fax" label="Fax" onChange={cb} />
-        <span /> {/* ensure random elements don't break anything */}
-      </RadioGroup>
-    );
-
-    component
-      .find('input')
-      .at(2)
-      .simulate('change');
-
-    expect(cb.mock.calls.length).toBe(1);
-  });
-
-  test('RadioGroup should spread extra props', () => {
-    const component = mount(
-      <RadioGroup data-propspread="test">
-        <Radio id="1" value="email" label="E-mail" />
-        <Radio id="2" value="phone" label="Phone" />
-      </RadioGroup>
-    );
-    const container = component.at(0).getDOMNode();
-    expect(container.getAttribute('data-propspread')).toBe('test');
-    component.unmount();
+      expect(cb).toHaveBeenCalledWith('phone');
+    });
   });
 });
