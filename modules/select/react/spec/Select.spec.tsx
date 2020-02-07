48879d931c04d4aaced63ddeea23a56221ae0845
James Fan <james@somewhatstrange.com>
2020-02-06 16:47:15 -0800

    test(select): Change tests to react-testing-library and improve coverage (#412)
    * test(select): Update tests to use react-testing-library
    
    * test(select): Add more unit tests
    
    * test(select): Add cypress tests
    
    * test(select): Add static states
    
    * test(select): Add required Select children
    
    * test(select): Update component state table usage
    
    * chore(select): Fix storybook build errors
    
    Co-authored-by: Alex Nicholls <anicholls3@gmail.com>

--- modules/select/react/spec/Select.spec.tsx
+++ modules/select/react/spec/Select.spec.tsx
@@ -1,38 +1,90 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import {render, fireEvent} from '@testing-library/react';
 import SelectOption from '../lib/SelectOption';
 import Select from '../lib/Select';
 
 describe('Select', () => {
   const cb = jest.fn();
+
+  const role = 'listbox';
+
   afterEach(() => {
     cb.mockReset();
   });
 
-  test('renders two select options as expected', () => {
-    const component = mount(
-      <Select name="contact">
-        <SelectOption value="email" label="E-mail" />
-        <SelectOption value="phone" label="Phone" />
-      </Select>
-    );
-
-    const options = component.find('option');
-
-    expect(options.length).toEqual(2);
+  describe('when rendered with an id', () => {
+    it('should render a select with id', () => {
+      const id = 'mySelect';
+      const {getByRole} = render(
+        <Select id={id} onChange={cb}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value="phone" label="Phone" />
+        </Select>
+      );
+      expect(getByRole(role)).toHaveAttribute('id', id);
+    });
   });
 
-  test('Select should spread extra props', () => {
-    const component = mount(
-      <Select name="contact" data-propspread="test">
-        <SelectOption value="email" label="E-mail" />
-        <SelectOption value="phone" label="Phone" />
-      </Select>
-    );
-    const select = component
-      .find('select') // TODO: Standardize on prop spread location (see #150)
-      .getDOMNode();
-    expect(select.getAttribute('data-propspread')).toBe('test');
-    component.unmount();
+  describe('when rendered with disabled attribute', () => {
+    it('should render a disabled select', () => {
+      const {getByRole} = render(
+        <Select disabled={true} onChange={cb}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value="phone" label="Phone" />
+        </Select>
+      );
+      expect(getByRole(role)).toHaveProperty('disabled', true);
+    });
+  });
+
+  describe('when rendered with a value', () => {
+    it('it should render a select whose selected option matches value', () => {
+      const selectedValue = 'phone';
+      const {getByDisplayValue} = render(
+        <Select onChange={cb} value={selectedValue}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value={selectedValue} label={selectedValue} />
+        </Select>
+      );
+      expect(getByDisplayValue(selectedValue)).toBeDefined();
+    });
+  });
+
+  describe('when rendered with extra, arbitrary props', () => {
+    it('should spread extra props onto the select', () => {
+      const attr = 'test';
+      const {getByRole} = render(
+        <Select data-propspread={attr} onChange={cb}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value="phone" label="Phone" />
+        </Select>
+      );
+      expect(getByRole(role)).toHaveAttribute('data-propspread', attr);
+    });
+  });
+
+  describe('when rendered with two select options as children', () => {
+    it('should render two options', () => {
+      const {getAllByRole} = render(
+        <Select onChange={cb}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value="phone" label="Phone" />
+        </Select>
+      );
+      expect(getAllByRole('option')).toHaveLength(2);
+    });
+  });
+
+  describe('when changed', () => {
+    it('should call a callback function', () => {
+      const {getByRole} = render(
+        <Select onChange={cb}>
+          <SelectOption value="email" label="E-mail" />
+          <SelectOption value="phone" label="Phone" />
+        </Select>
+      );
+      fireEvent.change(getByRole(role));
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
   });
 });
