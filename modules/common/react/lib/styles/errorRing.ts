e752d16472f7773e6e790f24bad6ca338708dd4e
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-02-07 14:42:30 -0700

    fix: Add contrast check for error ring theme colors

--- modules/common/react/lib/styles/errorRing.ts
+++ modules/common/react/lib/styles/errorRing.ts
@@ -2,18 +2,31 @@ import {CanvasTheme} from '@workday/canvas-kit-labs-react-core';
 import {ErrorType} from '../types';
 import {CSSObject} from '@emotion/core';
 import {colors, inputColors} from '@workday/canvas-kit-react-core';
+import chroma from 'chroma-js';
 
 export default function errorRing(error?: ErrorType, theme?: CanvasTheme): CSSObject {
   let errorBorderColor;
   let errorBoxShadow;
 
+  const isAccessibleThemeColor = (themeColor: CanvasTheme): boolean => {
+    return chroma.contrast(themeColor, colors.frenchVanilla100) < 3;
+  };
+
   if (error === ErrorType.Error) {
-    errorBorderColor = theme ? theme.palette.error.main : inputColors.error.border;
+    errorBorderColor = theme
+      ? isAccessibleThemeColor(theme.palette.error.main)
+        ? theme.palette.error.main
+        : theme.palette.error.darkest
+      : inputColors.error.border;
     errorBoxShadow = `inset 0 0 0 1px ${
       theme ? theme.palette.error.main : inputColors.error.border
     }`;
   } else if (error === ErrorType.Alert) {
-    errorBorderColor = theme ? theme.palette.alert.darkest : colors.cantaloupe600;
+    errorBorderColor = theme
+      ? isAccessibleThemeColor(theme.palette.alert.main)
+        ? theme.palette.alert.main
+        : theme.palette.alert.darkest
+      : colors.cantaloupe600;
     errorBoxShadow = `inset 0 0 0 2px ${
       theme ? theme.palette.alert.main : inputColors.warning.border
     }`;
