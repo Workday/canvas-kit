f1d6c48dffc61a05da185c9b2acbc91524d50f23
Lynn Chyi <14299381+lychyi@users.noreply.github.com>
2020-02-18 23:31:08 -0800

    fix(button): Add identifier class to IconButton

--- modules/button/react/spec/IconButton.spec.tsx
+++ modules/button/react/spec/IconButton.spec.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {mount} from 'enzyme';
-import IconButton from '../lib/IconButton';
+import {render} from '@testing-library/react';
+import IconButton, {iconButtonIdentifier} from '../lib/IconButton';
 import {SystemIcon} from '@workday/canvas-kit-react-icon';
 import {activityStreamIcon} from '@workday/canvas-system-icons-web';
 import ReactDOMServer from 'react-dom/server';
@@ -20,6 +21,32 @@ describe('Icon Button', () => {
     );
     expect(component.find('button').props().id).toBe('myBtn');
     component.unmount();
+  });
+
+  it('should have an identifier class', () => {
+    const {getByTestId} = render(
+      <IconButton data-testid={iconButtonIdentifier} aria-label="Activity Stream">
+        <SystemIcon icon={activityStreamIcon} />
+      </IconButton>
+    );
+
+    expect(getByTestId(iconButtonIdentifier).className).toContain(iconButtonIdentifier);
+  });
+  it('should compose custom classNames with the identifier class', () => {
+    const testClassName = 'test classname';
+    const {getByTestId} = render(
+      <IconButton
+        data-testid={iconButtonIdentifier}
+        className={testClassName}
+        aria-label="Activity Stream"
+      >
+        <SystemIcon icon={activityStreamIcon} />
+      </IconButton>
+    );
+
+    expect(getByTestId(iconButtonIdentifier).className).toContain(
+      `${iconButtonIdentifier} ${testClassName}`
+    );
   });
 
   test('should call a callback function', () => {
