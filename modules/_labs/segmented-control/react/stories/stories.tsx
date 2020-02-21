ddfd70de9ccece73f3c097d8061be5cec47ee542
Alex Nicholls <anicholls3@gmail.com>
2020-02-21 11:59:39 -0800

    feat(labs): Use state hook instead of wrapper for SegmentedControl story

--- modules/_labs/segmented-control/react/stories/stories.tsx
+++ modules/_labs/segmented-control/react/stories/stories.tsx
@@ -12,59 +12,31 @@ import {
 } from '@workday/canvas-system-icons-web';
 
 import {IconButton} from '@workday/canvas-kit-labs-react-button';
-import {SegmentedControl, SegmentedControlProps} from '../index';
+import {SegmentedControl} from '../index';
 
 import README from '../README.md';
-
-// Wrapper to add state mgmt to SegmentedControls
-export class SegmentedControlWrapper extends React.Component<
-  {},
-  {
-    selectedValue: string | number;
-  }
-> {
-  state = {
-    selectedValue: '',
-  };
-
-  public render() {
-    const child = this.props.children as React.ReactElement<SegmentedControlProps>;
-    return React.cloneElement(child, {
-      value: this.state.selectedValue === '' ? child.props.value : this.state.selectedValue,
-      onChange: this.handleToggle,
-    });
-  }
-
-  public handleToggle = (selectedValue: string | number) => {
-    this.setState({selectedValue});
-  };
-}
 
 storiesOf('Labs|Segmented Control/React', module)
   .addParameters({component: SegmentedControl})
   .addDecorator(withReadme(README))
-  .add('Two Buttons', () => (
-    <SegmentedControlWrapper>
-      <SegmentedControl>
+  .add('Default', () => {
+    const [value, setValue] = React.useState();
+    const handleToggle = (selectedValue: string | number) => {
+      setValue(selectedValue);
+      action('Segmented Control selection change')(selectedValue);
+    };
+
+    return (
+      <SegmentedControl value={value} onChange={handleToggle}>
         <IconButton
           icon={listViewIcon}
+          value="list-view"
           aria-label="List View"
-          onClick={e => {
-            action('Existing IconButton onClick callback')(e);
-          }}
+          onClick={e => action('Existing IconButton onClick callback')(e)}
         />
-        <IconButton aria-label="Table View" icon={worksheetsIcon} />
-      </SegmentedControl>
-    </SegmentedControlWrapper>
-  ))
-
-  .add('Four Buttons', () => (
-    <SegmentedControlWrapper>
-      <SegmentedControl>
-        <IconButton icon={listViewIcon} value="list-view" aria-label="List View" />
         <IconButton icon={worksheetsIcon} value="table-view" aria-label="Table View" />
         <IconButton icon={deviceTabletIcon} value="device-view" aria-label="Device View" />
         <IconButton icon={percentageIcon} value="percent-view" aria-label="Percent View" />
       </SegmentedControl>
-    </SegmentedControlWrapper>
-  ));
+    );
+  });
