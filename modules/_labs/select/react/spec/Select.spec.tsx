69b080734408f1de415aac2b45e231c0144482d4
James Fan <james@somewhatstrange.com>
2020-03-10 20:55:01 -0700

    test(select): Add unit tests

---
+++ modules/_labs/select/react/spec/Select.spec.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import {fireEvent, render} from '@testing-library/react';
+import Select from '../lib/Select';
+import SelectOption from '../lib/SelectOption';
+
+describe('Select', () => {
+  const cb = jest.fn();
+
+  const selectButtonRole = 'button';
+  const optionRole = 'option';
+
+  const getFirstMatchingOption = (options: HTMLElement[], value: string) => {
+    return options.filter((option: HTMLElement) => option.getAttribute('data-value') === value)[0];
+  };
+
+  afterEach(() => {
+    cb.mockReset();
+  });
+
+  describe('when rendered with the id prop', () => {
+    it('should render a button with that id', () => {
+      const id = 'selectButton';
+      const {getByRole} = render(
+        <Select id={id} onChange={cb}>
+          <SelectOption value="option1" label="Option 1" />
+          <SelectOption value="option2" label="Option 2" />
+        </Select>
+      );
+      expect(getByRole(selectButtonRole)).toHaveAttribute('id', id);
+    });
+  });
+
+  describe('when rendered with the disabled prop', () => {
+    it('should render a disabled button', () => {
+      const {getByRole} = render(
+        <Select disabled={true} onChange={cb}>
+          <SelectOption value="option1" label="Option 1" />
+          <SelectOption value="option2" label="Option 2" />
+        </Select>
+      );
+      expect(getByRole(selectButtonRole)).toHaveProperty('disabled', true);
+    });
+  });
+
+  // TODO: when rendered with two options as children
+  // it should render two options
+
+  describe('when rendered with a value that matches the value of one (or more) of its options', () => {
+    const value = 'option2';
+    const testContent = () => {
+      return (
+        <Select onChange={cb} value={value}>
+          <SelectOption value="option1" label="Option 1" />
+          <SelectOption value={value} label="Option 2" />
+        </Select>
+      );
+    };
+
+    it('should render a button whose text is equal to the text of the first matching option', () => {
+      const {getAllByRole, getByRole} = render(testContent());
+      const button = getByRole(selectButtonRole);
+      const firstMatchingOption = getFirstMatchingOption(getAllByRole(optionRole), value);
+      expect(button.textContent).toEqual(firstMatchingOption.textContent);
+    });
+
+    it('should render the first matching option with aria-selected="true"', () => {
+      const {getAllByRole} = render(testContent());
+      const firstMatchingOption = getFirstMatchingOption(getAllByRole(optionRole), value);
+      expect(firstMatchingOption).toHaveAttribute('aria-selected', 'true');
+    });
+  });
+
+  describe('when rendered with a value that does NOT match the value of any of its options', () => {
+    const value = 'option3';
+    const testContent = () => {
+      return (
+        <Select onChange={cb} value={value}>
+          <SelectOption value="option1" label="Option 1" />
+          <SelectOption value="option2" label="Option 2" />
+        </Select>
+      );
+    };
+
+    it('should render a button whose text is equal to the text of the first option', () => {
+      const {getAllByRole, getByRole} = render(testContent());
+      const button = getByRole(selectButtonRole);
+      const firstOption = getAllByRole(optionRole)[0];
+      expect(button.textContent).toEqual(firstOption.textContent);
+    });
+
+    it('should render the first option with aria-selected="true"', () => {
+      const {getAllByRole} = render(testContent());
+      const firstOption = getAllByRole(optionRole)[0];
+      expect(firstOption).toHaveAttribute('aria-selected', 'true');
+    });
+  });
+
+  describe('when the mouse button is pressed on an option which has a different value than the current value of the select', () => {
+    it('should call the onChange callback', () => {
+      const {getAllByRole} = render(
+        <Select onChange={cb}>
+          <SelectOption value="option1" label="Option 1" />
+          <SelectOption value="option2" label="Option 2" />
+        </Select>
+      );
+      fireEvent.mouseDown(getAllByRole(optionRole)[1]);
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  // TODO: test change event firing with correct value
+});
