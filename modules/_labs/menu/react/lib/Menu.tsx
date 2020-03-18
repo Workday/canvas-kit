709651ab6f0e7d189c9be444ad4964ae94b354b8
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-03-18 15:19:49 -0600

    fix(modal): Revert api change to menu

--- modules/_labs/menu/react/lib/Menu.tsx
+++ modules/_labs/menu/react/lib/Menu.tsx
@@ -40,11 +40,6 @@ export interface MenuProps extends GrowthBehavior, React.HTMLAttributes<HTMLULis
    * The HTML `id` of the element that labels the Menu. Often used with menu buttons.
    */
   labeledBy?: string;
-  /**
-   * The role of the Menu. Use this to override the role of the item (e.g. you can use this element as an option in a Combobox).
-   * @default menu
-   */
-  role: string;
 }
 
 export interface MenuState {
@@ -68,7 +63,6 @@ const List = styled('ul')({
 export default class Menu extends React.Component<MenuProps, MenuState> {
   static defaultProps = {
     isOpen: true,
-    role: 'menu',
   };
 
   private id = uuid();
@@ -118,7 +112,6 @@ export default class Menu extends React.Component<MenuProps, MenuState> {
       width,
       onSelect,
       onClose,
-      role,
       initialSelectedItem,
       ...elemProps
     } = this.props;
@@ -132,7 +125,7 @@ export default class Menu extends React.Component<MenuProps, MenuState> {
         width={cardWidth}
       >
         <List
-          role={role}
+          role="role"
           tabIndex={0}
           id={id}
           aria-labelledby={labeledBy}
