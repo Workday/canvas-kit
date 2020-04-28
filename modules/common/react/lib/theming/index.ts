ec78e1a56e5909f070096266b670e3a1a46e5569
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-04-28 09:51:05 -0600

    fix: Update export for theming

--- modules/common/react/lib/theming/index.ts
+++ modules/common/react/lib/theming/index.ts
@@ -3,5 +3,6 @@ import CanvasProvider from './CanvasProvider';
 export * from './breakpoints';
 export * from './createCanvasTheme';
 export * from './types';
+export * from './styled';
 export * from './theme';
 export {CanvasProvider};
