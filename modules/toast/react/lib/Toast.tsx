7e3e1068761609e5ee8e872f98d5874cf4563efe
Alex Nicholls <anicholls3@gmail.com>
2020-05-27 12:06:40 -0700

    fix(toast): Re-align action text with message

--- modules/toast/react/lib/Toast.tsx
+++ modules/toast/react/lib/Toast.tsx
@@ -64,7 +64,6 @@ const ActionButton = styled('button')({
   display: 'block',
   backgroundColor: 'transparent', // To prevent Safari from rendering grey 'buttonface' as bgcolor
   border: 'none',
-  padding: 0,
   marginTop: spacing.xxxs,
 });
 
