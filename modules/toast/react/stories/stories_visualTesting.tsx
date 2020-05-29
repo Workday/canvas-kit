48db1b13db2227d6ba4205719f1e1546bf5c68e0
Alex Nicholls <anicholls3@gmail.com>
2020-05-29 11:57:57 -0700

    test(toast): Refactor visual testing stories to use CSF

--- modules/toast/react/stories/stories_visualTesting.tsx
+++ modules/toast/react/stories/stories_visualTesting.tsx
@@ -1,7 +1,6 @@
 /// <reference path="../../../../typings.d.ts" />
 /** @jsx jsx */
 import {jsx} from '@emotion/core';
-import {storiesOf} from '@storybook/react';
 import {colors} from '@workday/canvas-kit-react-core';
 import {StaticStates} from '@workday/canvas-kit-labs-react-core';
 import {action} from '@storybook/addon-actions';
@@ -9,37 +8,40 @@ import {ComponentStatesTable, enableSnapshots} from '../../../../utils/storybook
 import {exclamationCircleIcon} from '@workday/canvas-system-icons-web';
 import {Toast} from '../index';
 
-storiesOf('Components|Popups/Toast/React/Visual Testing', module)
-  .addParameters({
+export default {
+  title: 'Components|Popups/Toast/React/Visual Testing',
+  parameters: {
     component: Toast,
     ...enableSnapshots(),
-  })
-  .add('States', () => (
-    <StaticStates>
-      <ComponentStatesTable
-        rowProps={[
-          {label: 'Default', props: {}},
-          {label: 'Custom Icon', props: {icon: exclamationCircleIcon}},
-          {
-            label: 'Custom Icon Color',
-            props: {icon: exclamationCircleIcon, iconColor: colors.cinnamon500},
+  },
+};
+
+export const States = () => (
+  <StaticStates>
+    <ComponentStatesTable
+      rowProps={[
+        {label: 'Default', props: {}},
+        {label: 'Custom Icon', props: {icon: exclamationCircleIcon}},
+        {
+          label: 'Custom Icon Color',
+          props: {icon: exclamationCircleIcon, iconColor: colors.cinnamon500},
+        },
+        {label: 'On Close', props: {onClose: action('close button clicked')}},
+        {
+          label: 'With Action Link',
+          props: {
+            onActionClick: action('action button clicked'),
+            actionText: 'View More Details',
           },
-          {label: 'On Close', props: {onClose: action('close button clicked')}},
-          {
-            label: 'With Action Link',
-            props: {
-              onActionClick: action('action button clicked'),
-              actionText: 'View More Details',
-            },
-          },
-        ]}
-        columnProps={[{label: 'Default', props: {}}]}
-      >
-        {props => (
-          <Toast aria-label="Play" {...props}>
-            Your workbook was successfully processed.
-          </Toast>
-        )}
-      </ComponentStatesTable>
-    </StaticStates>
-  ));
+        },
+      ]}
+      columnProps={[{label: 'Default', props: {}}]}
+    >
+      {props => (
+        <Toast aria-label="Play" {...props}>
+          Your workbook was successfully processed.
+        </Toast>
+      )}
+    </ComponentStatesTable>
+  </StaticStates>
+);
