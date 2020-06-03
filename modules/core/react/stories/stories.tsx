8603e38d96ee5a844bdd3e1aa65012d77be1be57
Alex Nicholls <anicholls3@gmail.com>
2020-06-03 10:30:46 -0700

    test(core): Add story for colors

--- modules/core/react/stories/stories.tsx
+++ modules/core/react/stories/stories.tsx
@@ -2,11 +2,12 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core';
 import * as React from 'react';
-import Styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import withReadme from 'storybook-readme/with-readme';
 import {withSnapshotsEnabled} from '../../../../utils/storybook';
+import {pickForegroundColor} from '@workday/canvas-kit-react-common';
 
-import {type, depth, H1, H2, H3, H4, H5} from '..';
+import {colors, type, depth, spacing, borderRadius, H1, H2, H3, H4, H5} from '..';
 import README from '../README.md';
 
 export default withSnapshotsEnabled({
@@ -21,7 +22,7 @@ const inverseStyle = {
   borderRadius: '4px',
 };
 
-const Card = Styled('div')({
+const Card = styled('div')({
   width: 200,
   height: 200,
   margin: 20,
@@ -89,4 +90,82 @@ export const Type = () => (
       <span css={type.variant.mono}>Mono Text</span>
     </div>
   </React.Fragment>
+);
+
+const Palettes = styled('div')({
+  display: 'flex',
+  margin: -20,
+  flexWrap: 'wrap',
+});
+
+const Palette = styled('ul')({
+  ...depth[3],
+  listStyle: 'none',
+  margin: 20,
+  padding: 0,
+  overflow: 'hidden',
+  width: 250,
+  alignSelf: 'flex-start',
+  borderRadius: borderRadius.l,
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const Swatch = styled('li')<{bg: string; primary?: boolean}>(
+  {
+    ...type.body,
+    fontWeight: 700,
+    padding: `0 ${spacing.m}`,
+    height: spacing.xl,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  ({bg}) => ({
+    background: bg,
+    color: pickForegroundColor(bg),
+  }),
+  ({primary, bg}) =>
+    primary && {
+      ...type.h3,
+      color: pickForegroundColor(bg),
+      height: spacing.xxxl,
+      paddingTop: spacing.s,
+      paddingBottom: spacing.s,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'flex-start',
+      textTransform: 'capitalize',
+    }
+);
+
+const colorNames = [
+  ...new Set( // Remove duplicates using Set
+    Object.keys(colors)
+      .map(color => color.replace(/\d+$/, '')) // Remove shade numbers
+      .filter(key => key !== 'primary' && key !== 'gradients')
+  ),
+];
+
+export const Colors = () => (
+  <Palettes>
+    {colorNames.map(colorName => (
+      <Palette key={colorName}>
+        <Swatch bg={colors[`${colorName}500`]} primary={true}>
+          <div>{colorName}</div>
+          <div>500</div>
+        </Swatch>
+        {[1, 2, 3, 4, 5, 6].map(level => {
+          const color = `${colorName}${level}00`;
+          return (
+            <Swatch bg={colors[color]} key={color}>
+              <span>{level}00</span>
+              <span css={type.variant.mono}>{colors[color]}</span>
+            </Swatch>
+          );
+        })}
+      </Palette>
+    ))}
+  </Palettes>
 );
