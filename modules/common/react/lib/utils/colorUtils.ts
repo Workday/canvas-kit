107234580c19982266f8746001c1a0f60c1374e2
Donovan Gini <donovan.gini@workday.com>
2020-06-04 11:04:01 -0700

    fix(common): Auto-generate contrast color for partial theme
    fix #697

--- modules/common/react/lib/utils/colorUtils.ts
+++ modules/common/react/lib/utils/colorUtils.ts
@@ -1,4 +1,5 @@
 import {colors} from '@workday/canvas-kit-react-core';
+import chroma from 'chroma-js';
 
 // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 export const expandHex = (hex: string) => {
@@ -8,35 +9,17 @@ export const expandHex = (hex: string) => {
   });
 };
 
-const hexToRgb = (hex: string) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(expandHex(hex));
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-};
-
 /**
  *
- * Chooses luminance color depending on the rgb value of the background color.
- * Eventually should be replaced by by Chroma 2.0
+ * Chooses foreground color with highest contrast against background
+ * (https://www.w3.org/TR/WCAG20-TECHS/G18.html)
  */
 export const pickForegroundColor = (
   background: string,
   darkColor: string = colors.blackPepper600,
   lightColor: string = colors.frenchVanilla100
 ) => {
-  const rgbColor = hexToRgb(background);
-  if (rgbColor) {
-    const r: number = rgbColor.r;
-    const g: number = rgbColor.g;
-    const b: number = rgbColor.b;
-    /** Based on : https://www.w3.org/TR/WCAG20-TECHS/G18.html */
-    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
-  } else {
-    return;
-  }
+  return chroma.contrast(background, darkColor) >= chroma.contrast(background, lightColor)
+    ? darkColor
+    : lightColor;
 };
--- modules/common/react/spec/colorUtils.spec.tsx
+++ modules/common/react/spec/colorUtils.spec.tsx
@@ -3,13 +3,25 @@ import {pickForegroundColor} from '../index';
 
 describe('Color Utils methods', () => {
   describe('pickForegroundColor', () => {
-    it('should use black if luminicance is light', () => {
+    it('should default to black if luminicance is light', () => {
       expect(pickForegroundColor('#ffffff')).toEqual(colors.blackPepper600);
       expect(pickForegroundColor('#eee')).toEqual(colors.blackPepper600);
+      expect(pickForegroundColor('orange')).toEqual(colors.blackPepper600);
     });
-    it('should use white if luminicance is dark', () => {
-      expect(pickForegroundColor('#e6e')).toEqual(colors.frenchVanilla100);
+    it('should default to white if luminicance is dark', () => {
+      expect(pickForegroundColor('#555')).toEqual(colors.frenchVanilla100);
       expect(pickForegroundColor('#000000')).toEqual(colors.frenchVanilla100);
+      expect(pickForegroundColor('black')).toEqual(colors.frenchVanilla100);
+    });
+    it('should use lightColor if luminicance is dark', () => {
+      expect(pickForegroundColor('black', colors.blueberry600, colors.blueberry100)).toEqual(
+        colors.blueberry100
+      );
+    });
+    it('should use darkColor if luminicance is dark', () => {
+      expect(pickForegroundColor('white', colors.blueberry600, colors.blueberry100)).toEqual(
+        colors.blueberry600
+      );
     });
   });
 });
