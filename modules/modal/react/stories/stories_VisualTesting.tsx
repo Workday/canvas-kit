e86d712fe01c8f57647dbc7d9863634cb12260de
Alex Nicholls <anicholls3@gmail.com>
2020-06-05 14:24:31 -0700

    test(modal): Update snapshots to test modal better

--- modules/modal/react/stories/stories_VisualTesting.tsx
+++ modules/modal/react/stories/stories_VisualTesting.tsx
@@ -1,61 +1,32 @@
 /// <reference path="../../../../typings.d.ts" />
 import * as React from 'react';
-import {StaticStates} from '@workday/canvas-kit-labs-react-core';
 import {Button} from '../../../button/react';
-import {
-  ComponentStatesTable,
-  permutateProps,
-  withSnapshotsEnabled,
-} from '../../../../utils/storybook';
+import {withSnapshotsEnabled} from '../../../../utils/storybook';
 
 import {Modal} from '../';
 
 export default withSnapshotsEnabled({
   title: 'Testing|React/Popups/Modal',
   component: Modal,
+  parameters: {
+    chromatic: {
+      delay: 350,
+    },
+  },
 });
 
 const noop = () => {}; // eslint-disable-line no-empty-function
 
-export const ModalStates = () => (
-  <StaticStates>
-    <ComponentStatesTable
-      rowProps={permutateProps({
-        heading: [
-          {value: 'Delete Item', label: ''},
-          {
-            value:
-              "Delete Item named 'Something with a really long title that will hopefully wrap to at least two lines'",
-            label: 'Long Heading',
-          },
-        ],
-        padding: [
-          {value: Modal.Padding.zero, label: 'Zero Padding'},
-          {value: Modal.Padding.s, label: 'Small padding'},
-          {value: Modal.Padding.l, label: 'Large padding'},
-        ],
-        width: [
-          {value: Modal.Width.s, label: 'Small Width'},
-          {value: Modal.Width.m, label: 'Medium Width'},
-        ],
-      })}
-      columnProps={[{label: '', props: {className: ''}}]}
-    >
-      {({heading, ...props}) => (
-        <Modal
-          {...props}
-          heading={heading}
-          open={true}
-          handleClose={noop}
-          style={{position: 'relative', width: 'auto', height: 'auto', padding: 32}}
-        >
-          <p>Are you sure you'd like to delete the item titled 'My Item'?</p>
-          <Button style={{marginRight: '16px'}} variant={Button.Variant.Delete}>
-            Delete
-          </Button>
-          <Button variant={Button.Variant.Secondary}>Cancel</Button>
-        </Modal>
-      )}
-    </ComponentStatesTable>
-  </StaticStates>
+const TestModal = ({width}) => (
+  <Modal heading="Delete Item" open={true} handleClose={noop} width={width}>
+    <p>Are you sure you'd like to delete the item titled 'My Item'?</p>
+    <Button style={{marginRight: '16px'}} variant={Button.Variant.Delete}>
+      Delete
+    </Button>
+    <Button variant={Button.Variant.Secondary}>Cancel</Button>
+  </Modal>
 );
+
+export const ModalSmallWidth = () => <TestModal width={Modal.Width.s} />;
+
+export const ModalMediumWidth = () => <TestModal width={Modal.Width.m} />;
