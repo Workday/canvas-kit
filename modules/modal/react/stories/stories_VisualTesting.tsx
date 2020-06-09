f80e0d4e72dfcfdb5ac478482a2a238ae0f6f36f
Alex Nicholls <anicholls3@gmail.com>
2020-06-09 14:55:47 -0700

    test(modal): Add modal snapshots (#693)
    * test(modal): Add modal snapshots
    
    * chore(modal): Add missing dev dependency
    
    * test(modal): Update snapshots to test modal better
    
    * chore(modal): Remove unused dev dep
    
    * fix(modal): Fix type error in modal testing story
    
    * test(modal): See if longer delay works for chrome & ff
    
    * chore: One more time
    
    * test(modal): Pause animation at the end for tests
    
    Co-authored-by: lychyi <14299381+lychyi@users.noreply.github.com>

---
+++ modules/modal/react/stories/stories_VisualTesting.tsx
@@ -0,0 +1,32 @@
+/// <reference path="../../../../typings.d.ts" />
+import * as React from 'react';
+import {Button} from '../../../button/react';
+import {withSnapshotsEnabled} from '../../../../utils/storybook';
+
+import {Modal, ModalWidth} from '../';
+
+export default withSnapshotsEnabled({
+  title: 'Testing|React/Popups/Modal',
+  component: Modal,
+  parameters: {
+    chromatic: {
+      pauseAnimationAtEnd: true,
+    },
+  },
+});
+
+const noop = () => {}; // eslint-disable-line no-empty-function
+
+const TestModal = ({width}: {width: ModalWidth}) => (
+  <Modal heading="Delete Item" open={true} handleClose={noop} width={width}>
+    <p>Are you sure you'd like to delete the item titled 'My Item'?</p>
+    <Button style={{marginRight: '16px'}} variant={Button.Variant.Delete}>
+      Delete
+    </Button>
+    <Button variant={Button.Variant.Secondary}>Cancel</Button>
+  </Modal>
+);
+
+export const ModalSmallWidth = () => <TestModal width={Modal.Width.s} />;
+
+export const ModalMediumWidth = () => <TestModal width={Modal.Width.m} />;
