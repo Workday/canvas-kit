12d0a0c3398709ccad3443b8c73d981f0a7add2c
Alex Nicholls <anicholls3@gmail.com>
2020-06-11 09:58:04 -0700

    test(segmented-control): Switch to CSF and enable snapshots

--- modules/segmented-control/react/stories/stories.tsx
+++ modules/segmented-control/react/stories/stories.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 import withReadme from 'storybook-readme/with-readme';
+import {withSnapshotsEnabled} from '../../../../utils/storybook';
 
 import {
   listViewIcon,
@@ -16,33 +17,36 @@ import {SegmentedControl} from '../index';
 
 import README from '../README.md';
 
-storiesOf('Components|Buttons/Segmented Control/React', module)
-  .addParameters({component: SegmentedControl})
-  .addDecorator(withReadme(README))
-  .add('Default', () => {
-    const [value, setValue] = React.useState<string | number>();
-    const handleToggle = (selectedValue: string | number) => {
-      setValue(selectedValue);
-      action('Segmented Control selection change')(selectedValue);
-    };
+export default withSnapshotsEnabled({
+  title: 'Components|Buttons/Segmented Control/React',
+  component: SegmentedControl,
+  decorators: [withReadme(README)],
+});
 
-    return (
-      <SegmentedControl value={value} onChange={handleToggle}>
-        <IconButton
-          icon={listViewIcon}
-          value="list-view"
-          aria-label="List View"
-          // Note: For some reason this action call causes a delay in storybook
-          onClick={e => action('Existing IconButton onClick callback')(e)}
-        />
-        <IconButton
-          icon={worksheetsIcon}
-          value="table-view"
-          aria-label="Table View"
-          disabled={true}
-        />
-        <IconButton icon={deviceTabletIcon} value="device-view" aria-label="Device View" />
-        <IconButton icon={percentageIcon} value="percent-view" aria-label="Percent View" />
-      </SegmentedControl>
-    );
-  });
+export const Default = () => {
+  const [value, setValue] = React.useState<string | number>();
+  const handleToggle = (selectedValue: string | number) => {
+    setValue(selectedValue);
+    action('Segmented Control selection change')(selectedValue);
+  };
+
+  return (
+    <SegmentedControl value={value} onChange={handleToggle}>
+      <IconButton
+        icon={listViewIcon}
+        value="list-view"
+        aria-label="List View"
+        // Note: For some reason this action call causes a delay in storybook
+        onClick={e => action('Existing IconButton onClick callback')(e)}
+      />
+      <IconButton
+        icon={worksheetsIcon}
+        value="table-view"
+        aria-label="Table View"
+        disabled={true}
+      />
+      <IconButton icon={deviceTabletIcon} value="device-view" aria-label="Device View" />
+      <IconButton icon={percentageIcon} value="percent-view" aria-label="Percent View" />
+    </SegmentedControl>
+  );
+};
