17cba7799bc00395543537c6518875469ee6442f
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-06-15 14:54:07 -0600

    fix: Update stories for a better exmaple

--- modules/skeleton/react/stories/stories.tsx
+++ modules/skeleton/react/stories/stories.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import {number} from '@storybook/addon-knobs';
+import {Avatar} from '@workday/canvas-kit-react-avatar';
 
 import Skeleton, {SkeletonShape, SkeletonText, SkeletonHeader} from '../index';
 
@@ -25,45 +26,78 @@ storiesOf('Components|Indicators/Skeleton/React', module)
   .add('Complete', () => {
     return (
       <div className="story">
-        <Skeleton>
-          <FlexContainer>
-            <SkeletonShape width={50} height={50} borderRadius={99} />
-            <Container>
-              <SkeletonHeader />
-            </Container>
-          </FlexContainer>
-          <div>
-            <SkeletonText lineCount={3} />
+        <h1>Custom Elements Loader</h1>
+        <div style={{width: 200}}>
+          <div style={{display: 'inline-flex', alignItems: 'center'}}>
+            <Avatar size={Avatar.Size.l}></Avatar>
+            <h2 style={{marginLeft: 15}}>Header</h2>
           </div>
-        </Skeleton>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt
+          </div>
+        </div>
+        <div style={{width: 200}}>
+          <Skeleton>
+            <FlexContainer>
+              <SkeletonShape width={50} height={50} borderRadius={99} />
+              <Container>
+                <SkeletonHeader />
+              </Container>
+            </FlexContainer>
+            <div>
+              <SkeletonText lineCount={3} />
+            </div>
+          </Skeleton>
+        </div>
       </div>
     );
   })
   .add('Header', () => {
     return (
       <div className="story">
-        <Skeleton>
-          <SkeletonHeader />
-        </Skeleton>
+        <h1>Loading Header</h1>
+        <FlexContainer>
+          <div style={{marginRight: 20}}>
+            <h2 style={{marginTop: 0, marginBottom: 0, flex: 'none'}}>Header Text</h2>
+          </div>
+          <div style={{width: 200}}>
+            <Skeleton>
+              <SkeletonHeader />
+            </Skeleton>
+          </div>
+        </FlexContainer>
       </div>
     );
   })
   .add('Shape', () => {
     return (
       <div className="story">
-        <Skeleton>
-          <SkeletonShape
-            width={number('width', 100)}
-            height={number('height', 100)}
-            borderRadius={number('borderRadius', 99)}
-          />
-        </Skeleton>
+        <h1>Loading Shape</h1>
+        <FlexContainer>
+          <div style={{marginRight: 20}}>
+            <Avatar size={Avatar.Size.xxl}></Avatar>
+          </div>
+          <Skeleton>
+            <SkeletonShape
+              width={number('width', 120)}
+              height={number('height', 120)}
+              borderRadius={number('borderRadius', 99)}
+            />
+          </Skeleton>
+        </FlexContainer>
       </div>
     );
   })
   .add('Text', () => {
     return (
       <div className="story">
+        <h1>Loading Text</h1>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
         <Skeleton>
           <SkeletonText lineCount={number('lineCount', 2)} />
         </Skeleton>
