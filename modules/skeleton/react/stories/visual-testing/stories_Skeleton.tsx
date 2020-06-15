da78858fe8a46a2fac2e2f272f93496e3f0a8c15
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-06-15 10:09:15 -0600

    fix: Update test to be under one story

--- modules/skeleton/react/stories/visual-testing/stories_Skeleton.tsx
+++ modules/skeleton/react/stories/visual-testing/stories_Skeleton.tsx
@@ -9,57 +9,56 @@ export default withSnapshotsEnabled({
   component: Skeleton,
 });
 
-export const SkeletonTextStates = () => (
-  <StaticStates>
-    <ComponentStatesTable
-      rowProps={[
-        {label: 'Default', props: {}},
-        {label: 'With Custom Line Count', props: {lineCount: 4}},
-      ]}
-      columnProps={[{label: 'Default', props: {}}]}
-    >
-      {props => (
-        <Skeleton>
-          <SkeletonText {...props} />
-        </Skeleton>
-      )}
-    </ComponentStatesTable>
-  </StaticStates>
-);
-
-export const SkeletonShapeStates = () => (
-  <StaticStates>
-    <ComponentStatesTable
-      rowProps={[
-        {label: 'As a Circle', props: {width: 100, height: 100, borderRadius: 99}},
-        {label: 'As a custom shape', props: {width: 200, height: 100, borderRadius: 5}},
-      ]}
-      columnProps={[{label: 'Default', props: {}}]}
-    >
-      {props => (
-        <Skeleton>
-          <SkeletonShape {...props} />
-        </Skeleton>
-      )}
-    </ComponentStatesTable>
-  </StaticStates>
-);
-
-export const SkeletonFullStates = () => (
-  <StaticStates>
-    <ComponentStatesTable
-      rowProps={[{label: 'Full Custom Skeleton', props: {}}]}
-      columnProps={[{label: 'Default', props: {}}]}
-    >
-      {props => (
-        <Skeleton>
-          <SkeletonShape width={50} height={50} borderRadius={99} />
-          <SkeletonHeader />
-          <div>
-            <SkeletonText lineCount={4} />
-          </div>
-        </Skeleton>
-      )}
-    </ComponentStatesTable>
-  </StaticStates>
+export const SkeletonStates = () => (
+  <div>
+    <h3>With Custom Line Count</h3>
+    <StaticStates>
+      <ComponentStatesTable
+        rowProps={[
+          {label: 'Default', props: {}},
+          {label: 'With Custom Line Count', props: {lineCount: 4}},
+        ]}
+        columnProps={[{label: 'Default', props: {}}]}
+      >
+        {props => (
+          <Skeleton>
+            <SkeletonText {...props} />
+          </Skeleton>
+        )}
+      </ComponentStatesTable>
+    </StaticStates>
+    <h3>With Custom Shape</h3>
+    <StaticStates>
+      <ComponentStatesTable
+        rowProps={[
+          {label: 'As a Circle', props: {width: 100, height: 100, borderRadius: 99}},
+          {label: 'As a custom shape', props: {width: 200, height: 100, borderRadius: 5}},
+        ]}
+        columnProps={[{label: 'Default', props: {}}]}
+      >
+        {props => (
+          <Skeleton>
+            <SkeletonShape {...props} />
+          </Skeleton>
+        )}
+      </ComponentStatesTable>
+    </StaticStates>
+    <h3>With Full Skeleton</h3>
+    <StaticStates>
+      <ComponentStatesTable
+        rowProps={[{label: 'Full Custom Skeleton', props: {}}]}
+        columnProps={[{label: 'Default', props: {}}]}
+      >
+        {props => (
+          <Skeleton>
+            <SkeletonShape width={50} height={50} borderRadius={99} />
+            <SkeletonHeader />
+            <div>
+              <SkeletonText lineCount={4} />
+            </div>
+          </Skeleton>
+        )}
+      </ComponentStatesTable>
+    </StaticStates>
+  </div>
 );
