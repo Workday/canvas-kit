a00fcb6ab6e475918f6077c4941c49b413a616e3
Alex Nicholls <anicholls3@gmail.com>
2020-07-28 11:42:45 -0700

    refactor(core): Switch to using base 16 rems

--- modules/_labs/core/react/lib/typeTest.ts
+++ modules/_labs/core/react/lib/typeTest.ts
@@ -16,6 +16,14 @@ export interface CanvasTypeHierarchy {
   [key: string]: CSSProperties;
 }
 
+const broswerDefaultFontSize = 16;
+/**
+ * Converts px values into base 16 rems
+ */
+const rem = (pxValue: number) => {
+  return `${pxValue / broswerDefaultFontSize}rem`;
+};
+
 /**
  * The following rem values are based on an `html` element font size of 62.5%.
  * This takes the browser default (16px) and converts it into base 10 at the default size.
@@ -25,77 +33,77 @@ export interface CanvasTypeHierarchy {
 const hierarchy: CanvasTypeHierarchy = {
   brand: {
     fontFamily,
-    fontSize: '4.8rem',
-    lineHeight: '5.6rem',
+    fontSize: rem(48),
+    lineHeight: rem(56),
     fontWeight: 700,
     color: typeColors.heading,
   },
   h1: {
     fontFamily,
-    fontSize: '4.0rem',
-    lineHeight: '4.8rem',
+    fontSize: rem(40),
+    lineHeight: rem(48),
     fontWeight: 700,
     color: typeColors.heading,
   },
   h2: {
     fontFamily,
-    fontSize: '3.2rem',
-    lineHeight: '4.0rem',
+    fontSize: rem(32),
+    lineHeight: rem(40),
     fontWeight: 700,
     color: typeColors.heading,
   },
   h3: {
     fontFamily,
-    fontSize: '2.4rem',
-    lineHeight: '3.2rem',
+    fontSize: rem(24),
+    lineHeight: rem(32),
     fontWeight: 700,
     color: typeColors.heading,
   },
   h4: {
     fontFamily,
-    fontSize: '2.0rem',
-    lineHeight: '2.8rem',
-    letterSpacing: '0.016rem',
+    fontSize: rem(20),
+    lineHeight: rem(28),
+    letterSpacing: rem(0.16),
     fontWeight: 700,
     color: typeColors.heading,
   },
   h5: {
     fontFamily,
-    fontSize: '1.8rem',
-    lineHeight: '2.8rem',
-    letterSpacing: '0.016rem',
+    fontSize: rem(18),
+    lineHeight: rem(28),
+    letterSpacing: rem(0.16),
     fontWeight: 700,
     color: typeColors.heading,
   },
   body: {
     fontFamily,
-    fontSize: '1.6rem',
-    lineHeight: '2.4rem',
-    letterSpacing: '0.016rem',
+    fontSize: rem(16),
+    lineHeight: rem(24),
+    letterSpacing: rem(0.16),
     fontWeight: 400,
     color: typeColors.body,
   },
   body2: {
     fontFamily,
-    fontSize: '1.4rem',
-    lineHeight: '2.0rem',
-    letterSpacing: '0.024rem',
+    fontSize: rem(14),
+    lineHeight: rem(20),
+    letterSpacing: rem(0.24),
     fontWeight: 400,
     color: typeColors.body,
   },
   small: {
     fontFamily,
-    fontSize: '1.2rem',
-    lineHeight: '1.6rem',
-    letterSpacing: '0.032rem',
+    fontSize: rem(12),
+    lineHeight: rem(16),
+    letterSpacing: rem(0.32),
     fontWeight: 400,
     color: typeColors.body,
   },
   small2: {
     fontFamily,
-    fontSize: '1rem',
-    lineHeight: '1.6rem',
-    letterSpacing: '0.04rem',
+    fontSize: rem(10),
+    lineHeight: rem(16),
+    letterSpacing: rem(0.4),
     fontWeight: 400,
     color: typeColors.body,
   },
