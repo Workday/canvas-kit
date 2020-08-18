b9b16edecd37324bd890191974a39e563e1ad941
Jay Schultz <jay.schultz@workday.com>
2020-08-18 14:42:06 -0600

    fix(color-picker): Add visual test

---
+++ modules/_labs/color-picker/react/stories/stories_VisualTesting.tsx
@@ -0,0 +1,44 @@
+/// <reference path="../../../../../typings.d.ts" />
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
+import {colors} from '@workday/canvas-kit-react-core';
+import {StaticStates} from '@workday/canvas-kit-labs-react-core';
+import {action} from '@storybook/addon-actions';
+import {ComponentStatesTable, withSnapshotsEnabled} from '../../../../../utils/storybook';
+import ColorPicker from '../lib/ColorPicker';
+
+export default withSnapshotsEnabled({
+  title: 'Testing|React/Labs/Color Picker',
+  component: ColorPicker,
+});
+
+export const ColorPickerStates = () => (
+  <StaticStates>
+    <ComponentStatesTable
+      rowProps={[
+        {label: 'Default', props: {}},
+        {label: 'with Hex Input', props: {showCustomHexInput: true}},
+        {
+          label: 'With Reset',
+          props: {
+            resetColor: colors.blueberry400,
+            resetLabel: 'Reset',
+            onColorReset: action('Reset Clicked'),
+          },
+        },
+        {
+          label: 'With Reset and Hex Input',
+          props: {
+            showCustomHexInput: true,
+            resetColor: colors.blueberry400,
+            resetLabel: 'Reset',
+            onColorReset: action('Reset Clicked'),
+          },
+        },
+      ]}
+      columnProps={[{label: 'Default', props: {}}]}
+    >
+      {props => <ColorPicker {...props} onColorChange={action('Color Changed')} />}
+    </ComponentStatesTable>
+  </StaticStates>
+);
