898d5e38a363a032dff1ecd816053e0395e17495
Manuel Carrera <manuel.carrera.91@gmail.com>
2020-09-02 09:06:57 -0600

    fix: Remove unused imports

--- modules/_labs/color-picker/react/stories/stories_ColorPicker.tsx
+++ modules/_labs/color-picker/react/stories/stories_ColorPicker.tsx
@@ -4,7 +4,6 @@ import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import {ColorInput} from '@workday/canvas-kit-react-color-picker';
 import {colors} from '@workday/canvas-kit-react-core';
-import {TransformOrigin, getTranslateFromOrigin} from '@workday/canvas-kit-react-common';
 import {Popper, Popup, PopupPadding} from '@workday/canvas-kit-react-popup';
 import {IconButton} from '@workday/canvas-kit-react-button';
 import {bgColorIcon} from '@workday/canvas-system-icons-web';
