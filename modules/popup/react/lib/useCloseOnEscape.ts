923c9d3bcbea1afcfd4cf9d26a01e1876c60f9af
Alan Smith <a.bax.smith@gmail.com>
2020-10-05 05:20:09 -0600

    feat(popup): Add previousFocusRef to useCloseOnEscape hook

--- modules/popup/react/lib/useCloseOnEscape.ts
+++ modules/popup/react/lib/useCloseOnEscape.ts
@@ -6,12 +6,20 @@ import {PopupStack} from '@workday/canvas-kit-popup-stack';
  * Registers global detection of the Escape key. It will only call the `onClose` callback if the
  * provided `stackRef` element is the topmost in the stack. The `stackRef` should be the same as the one passed
  * to `usePopupStack` or the `Popper` component since `Popper` uses `usePopupStack` internally.
+ * If provided, the `previousFocusRef` allows you to return focus to an element before `onClose` is called
  * @param stackRef
  * @param onClose
+ * @param previousFocusRef
  */
+
+const focusPreviousElement = (previousFocusRef?: React.RefObject<HTMLElement>) => {
+  previousFocusRef?.current?.focus();
+};
+
 export const useCloseOnEscape = <E extends HTMLElement>(
   stackRef: React.RefObject<E>,
-  onClose: () => void
+  onClose: () => void,
+  previousFocusRef?: React.RefObject<HTMLElement>
 ) => {
   const onKeyDown = React.useCallback(
     (event: KeyboardEvent) => {
@@ -19,10 +27,12 @@ export const useCloseOnEscape = <E extends HTMLElement>(
         (event.key === 'Esc' || event.key === 'Escape') &&
         PopupStack.isTopmost(stackRef.current!)
       ) {
+        // return focus to previously focused element, if provided
+        focusPreviousElement(previousFocusRef);
         onClose();
       }
     },
-    [onClose, stackRef]
+    [onClose, previousFocusRef, stackRef]
   );
 
   // `useLayoutEffect` for automation
