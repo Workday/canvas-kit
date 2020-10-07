c96d1ffa18bc2b6ed11ccce6f058d503ed0cd016
Nicholas Boll <nicholas.boll@gmail.com>
2020-10-07 16:55:05 -0600

    Update .storybook/webpack-loader-redirect-mdx-to-github.js
    Co-authored-by: Alex Nicholls <anicholls3@gmail.com>

--- .storybook/webpack-loader-redirect-mdx-to-github.js
+++ .storybook/webpack-loader-redirect-mdx-to-github.js
@@ -4,7 +4,6 @@
 // [Component Status](https://github.com/Workday/canvas-kit/blob/master/COMPONENT_STATUS.md)
 
 module.exports = function webpackLoaderRedirectMDXToGithub(source) {
-  console.log(this.resourcePath);
   return source.replace(
     /(\[[^\]]+\])\(\./g,
     '$1(https://github.com/Workday/canvas-kit/blob/master'
