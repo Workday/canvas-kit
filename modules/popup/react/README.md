93e6dc955adf78b6685065cf5d6cb652a1b91ab8
Alan B Smith <alanbsmith@users.noreply.github.com>
2020-10-08 18:19:06 -0600

    [WIP] feat(popup): Add previousFocusRef to useCloseOnEscape hook (#873)
    * feat(popup): Add previousFocusRef to useCloseOnEscape hook
    
    * fix: Update name and docs

--- modules/popup/react/README.md
+++ modules/popup/react/README.md
@@ -376,7 +376,7 @@ This should be used on stacked UI elements that are meant to persist, like Windo
 ## useCloseOnEscape
 
 ```ts
-useCloseOnEscape(stackRef: React.RefObject<HTMLElement>, onClose: () => void): void
+useCloseOnEscape(stackRef: React.RefObject<HTMLElement>, onClose: () => void, returnFocusRef?: React.RefObject<HTMLElement>): void
 ```
 
 Registers global detection of the Escape key. It will only call the `onClose` callback if the
@@ -386,6 +386,9 @@ internally.
 
 This should be used stacked UI elements that are dismissible like Tooltips, Modals, non-modal
 dialogs, dropdown menus, etc.
+
+`returnFocusRef` is an optional prop that returns focus to a specific element on escape. This is
+useful for instances such as returning focus to a dropdown button when closing a menu.
 
 ## useCloseOnOutsideClick
 
--- modules/popup/react/lib/useCloseOnEscape.ts
+++ modules/popup/react/lib/useCloseOnEscape.ts
@@ -6,12 +6,20 @@ import {PopupStack} from '@workday/canvas-kit-popup-stack';
  * Registers global detection of the Escape key. It will only call the `onClose` callback if the
  * provided `stackRef` element is the topmost in the stack. The `stackRef` should be the same as the one passed
  * to `usePopupStack` or the `Popper` component since `Popper` uses `usePopupStack` internally.
+ * If provided, the `returnFocusRef` allows you to return focus to an element before `onClose` is called
  * @param stackRef
  * @param onClose
+ * @param returnFocusRef
  */
+
+const focusElement = (returnFocusRef?: React.RefObject<HTMLElement>) => {
+  returnFocusRef?.current?.focus();
+};
+
 export const useCloseOnEscape = <E extends HTMLElement>(
   stackRef: React.RefObject<E>,
-  onClose: () => void
+  onClose: () => void,
+  returnFocusRef?: React.RefObject<HTMLElement>
 ) => {
   const onKeyDown = React.useCallback(
     (event: KeyboardEvent) => {
@@ -19,10 +27,12 @@ export const useCloseOnEscape = <E extends HTMLElement>(
         (event.key === 'Esc' || event.key === 'Escape') &&
         PopupStack.isTopmost(stackRef.current!)
       ) {
+        // return focus to another element, if provided
+        focusElement(returnFocusRef);
         onClose();
       }
     },
-    [onClose, stackRef]
+    [onClose, returnFocusRef, stackRef]
   );
 
   // `useLayoutEffect` for automation
