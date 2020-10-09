8dcd919c61d419ad723b3438cf0fada89308ec5f
Alan B Smith <alanbsmith@users.noreply.github.com>
2020-10-09 10:35:04 -0600

    fix(drawer): Add support for word wrapping in IE11 (#879)

--- modules/_labs/drawer/react/lib/Drawer.tsx
+++ modules/_labs/drawer/react/lib/Drawer.tsx
@@ -80,6 +80,7 @@ const ChildrenContainer = styled('div')<Pick<DrawerProps, 'padding'>>(
     height: '100%',
     overflowY: 'auto',
     wordBreak: 'break-word',
+    wordWrap: 'break-word', // Needed for IE11
     position: 'relative',
   },
   ({padding}) => ({
