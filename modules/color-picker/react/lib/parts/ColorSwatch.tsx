6240052b23efe9ba8b5ab0f0e4c9555edd8978a3
Nicholas Boll <nicholas.boll@gmail.com>
2020-10-12 16:35:23 -0600

    fix(color-picker): Detect different formats of #ffffff
    Fixes #881

--- modules/color-picker/react/lib/parts/ColorSwatch.tsx
+++ modules/color-picker/react/lib/parts/ColorSwatch.tsx
@@ -11,6 +11,19 @@ export interface ColorSwatchProps extends React.HTMLAttributes<HTMLDivElement> {
   showCheck?: boolean;
 }
 
+// cache a div instance to decrease garbage collection
+const div = document.createElement('div');
+/**
+ * Use the browser's built-in mechanism for comparing colors. Works all the way back to IE11. The
+ * browser will take any valid color and convert to something like `rgb(0, 0, 0)` which allows us to
+ * then compare the strings even if the colors are in different formats
+ */
+function compareColors(color1: string, color2: string): boolean {
+  div.style.color = color1;
+  div.style.backgroundColor = color2;
+  return div.style.color === div.style.backgroundColor;
+}
+
 const Container = styled('div')<ColorSwatchProps>(
   {
     width: 20,
@@ -27,7 +40,7 @@ const Container = styled('div')<ColorSwatchProps>(
   ({color, showCheck}) => ({
     backgroundColor: color,
     boxShadow:
-      showCheck || color === colors.frenchVanilla100
+      showCheck || compareColors(color, colors.frenchVanilla100)
         ? 'inset 0px 0px 0px 1px rgba(0, 0, 0, 0.25)'
         : undefined,
   })
