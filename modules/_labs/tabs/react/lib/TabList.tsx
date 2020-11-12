ffdb88113511989272e0008d8f1de4c78dcbedee
Nicholas Boll <nicholas.boll@gmail.com>
2020-11-12 16:14:19 -0700

    Fix silly TS check error

--- modules/_labs/tabs/react/lib/TabList.tsx
+++ modules/_labs/tabs/react/lib/TabList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {colors, spacing, borderRadius, commonColors} from '@workday/canvas-kit-react-core';
+import {spacing, commonColors} from '@workday/canvas-kit-react-core';
 import styled from '@emotion/styled';
 import {useTab} from './Tabs';
 import {TabProps} from './Tab';
@@ -25,38 +25,10 @@ const TabsListInnerContainer = styled('div')({
   margin: `0 ${spacing.m}`,
 });
 
-const TabIndicator = styled('div')({
-  position: 'absolute',
-  height: spacing.xxxs,
-  borderRadius: `${borderRadius.m} ${borderRadius.m} 0px 0px`,
-  backgroundColor: colors.blueberry400,
-  transition: 'width 200ms ease, transform 200ms ease-out',
-  marginTop: '-2px',
-  bottom: 0,
-});
-
-const useIndicator = (containerRef?: React.RefObject<HTMLElement>) => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const setDimensions = (left: number, width: number) => {
-    const containerLeft = containerRef?.current?.getBoundingClientRect().left || 0;
-    ref.current!.style.transform = `translateX(${left - containerLeft}px)`;
-    ref.current!.style.width = `${width}px`;
-  };
-
-  return [ref, setDimensions] as const;
-};
-
 const TabList = ({children, ...elemProps}: TabListProps) => {
   const tabsListRef = React.useRef<HTMLDivElement>(null);
   // const [tabIndicatorRef, setDimensions] = useIndicator(tabsListRef);
-  const {selectedTabRect, setIntentTab, setActiveTab, intentTab, programmaticFocusRef} = useTab();
-
-  // React.useLayoutEffect(() => {
-  //   const selectedTabLeft = selectedTabRect?.left || 0;
-  //   const selectedTabWidth = selectedTabRect?.width || 0;
-
-  //   setDimensions(selectedTabLeft, selectedTabWidth);
-  // }, [tabsListRef, selectedTabRect, setDimensions]);
+  const {setIntentTab, setActiveTab, intentTab, programmaticFocusRef} = useTab();
 
   const onKeyDown = (event: React.KeyboardEvent) => {
     switch (event.key) {
@@ -98,7 +70,6 @@ const TabList = ({children, ...elemProps}: TabListProps) => {
       >
         {children}
       </TabsListInnerContainer>
-      {/* <TabIndicator ref={tabIndicatorRef} /> */}
     </TabsListContainer>
   );
 };
