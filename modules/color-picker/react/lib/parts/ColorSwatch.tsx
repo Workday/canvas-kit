15adb90a96e3fffd137eeabbf365c3369693e255
lychyi <14299381+lychyi@users.noreply.github.com>
2020-11-16 10:15:58 -0800

    fix(color-picker): Use chroma for comparison

--- modules/color-picker/react/lib/parts/ColorSwatch.tsx
+++ modules/color-picker/react/lib/parts/ColorSwatch.tsx
@@ -1,5 +1,6 @@
 import {borderRadius, colors} from '@workday/canvas-kit-react-core';
 import {pickForegroundColor} from '@workday/canvas-kit-react-common';
+import chroma from 'chroma-js';
 import * as React from 'react';
 import styled from '@emotion/styled';
 
@@ -11,17 +12,8 @@ export interface ColorSwatchProps extends React.HTMLAttributes<HTMLDivElement> {
   showCheck?: boolean;
 }
 
-// cache a div instance to decrease garbage collection
-const div = document.createElement('div');
-/**
- * Use the browser's built-in mechanism for comparing colors. Works all the way back to IE11. The
- * browser will take any valid color and convert to something like `rgb(0, 0, 0)` which allows us to
- * then compare the strings even if the colors are in different formats
- */
 function compareColors(color1: string, color2: string): boolean {
-  div.style.color = color1;
-  div.style.backgroundColor = color2;
-  return div.style.color === div.style.backgroundColor;
+  return chroma(color1) === chroma(color2);
 }
 
 const Container = styled('div')<ColorSwatchProps>(
