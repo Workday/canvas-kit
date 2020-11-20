ce80843bb9d3a774890e1eab72736d7f98946f45
Alex Nicholls <anicholls3@gmail.com>
2020-11-20 14:34:05 -0800

    chore: Remove unnecessary comments

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,3 @@
-// TODO: A semi-shoddy hack to enable syntax highlighting for tsx in storybook-readme.
-// Probably should open a PR to put tsx in storybook-readme instead?
-// import 'prismjs/components/prism-tsx';
-
 import {addons} from '@storybook/addons';
 import theme from './theme';
 
