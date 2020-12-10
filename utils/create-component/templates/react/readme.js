b3ee342932d40c56c698e048467f4b522f639446
James Fan <james@somewhatstrange.com>
2020-12-10 08:20:27 -0800

    docs: Standardize README badges for Labs components (#928)

--- utils/create-component/templates/react/readme.js
+++ utils/create-component/templates/react/readme.js
@@ -10,7 +10,7 @@ ${
   unstable
     ? `
 <a href="https://github.com/Workday/canvas-kit/tree/master/modules/_labs/README.md">
-  <img src="https://img.shields.io/badge/LABS-alpha-orange" alt="LABS: Alpha" />
+  <img src="https://img.shields.io/badge/LABS-beta-orange" alt="LABS: Beta" />
 </a>  This component is work in progress and currently in pre-release.
 `
     : ''
