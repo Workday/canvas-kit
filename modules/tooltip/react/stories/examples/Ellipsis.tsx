30f12ae93bf911c72a2abe65eac67fce62b4a6ad
Nicholas Boll <nicholas.boll@gmail.com>
2021-02-26 17:41:33 -0700

    docs(tooltip): Add normal element to the overflow example

--- modules/tooltip/react/stories/examples/Ellipsis.tsx
+++ modules/tooltip/react/stories/examples/Ellipsis.tsx
@@ -2,17 +2,32 @@ import React from 'react';
 
 import {Button} from '@workday/canvas-kit-react-button';
 import {OverflowTooltip} from '@workday/canvas-kit-react-tooltip';
+import {spacing} from '@workday/canvas-kit-react-core';
 
 export const Ellipsis = () => {
   return (
     <React.Fragment>
       <OverflowTooltip>
         <Button>Short Content</Button>
-      </OverflowTooltip>
+      </OverflowTooltip>{' '}
       <OverflowTooltip>
         <Button style={{maxWidth: 200}}>
           Super Mega Ultra Long Content With Max Width On The Button
         </Button>
+      </OverflowTooltip>
+      <OverflowTooltip>
+        <div
+          tabIndex={0}
+          style={{
+            marginTop: spacing.xs,
+            maxWidth: 200,
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          Super Mega Ultra Long Content With Max Width
+        </div>
       </OverflowTooltip>
     </React.Fragment>
   );
