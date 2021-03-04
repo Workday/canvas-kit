7dbe36d2035b10df450e1bad2ec987d3cbdfc9bc
Nicholas Boll <nicholas.boll@gmail.com>
2021-03-04 14:20:27 -0700

    Removed unused dependency

--- modules/tooltip/react/stories/examples/LineClamp.tsx
+++ modules/tooltip/react/stories/examples/LineClamp.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {OverflowTooltip} from '@workday/canvas-kit-react-tooltip';
-import {spacing} from '@workday/canvas-kit-react-core';
 
 export const LineClamp = () => {
   return (
