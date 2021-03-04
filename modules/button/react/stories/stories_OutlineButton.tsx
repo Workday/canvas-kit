2c20dffd8f0eacb7e8c8178416c2ec2f7790d5f8
manuel carrera <manuel.carrera@workday.com>
2021-03-04 11:38:44 -0700

    fix: Remove unwanted file

--- modules/button/react/stories/stories_OutlineButton.tsx
+++ modules/button/react/stories/stories_OutlineButton.tsx
@@ -80,8 +80,8 @@ storiesOf('Components/Buttons/Button/React/Outline', module)
       </OutlineButton>
 
       <h3>Small Primary</h3>
-      <OutlineButton size="small" variant={OutlineButton.Variant.Primary} icon={playCircleIcon}>
-        Outline Primary Test
+      <OutlineButton size="small" variant={OutlineButton.Variant.Primary}>
+        Outline Primary
       </OutlineButton>
       <OutlineButton size="small" disabled={true} variant={OutlineButton.Variant.Primary}>
         Outline Primary
