4b50eb274db7605f5e9a8a76fde77ad95e56e547
Alex Nicholls <anicholls3@gmail.com>
2021-03-08 15:58:05 -0800

    refactor: Refactor codemode for cleaner logic

--- modules/canvas-kit-codemod/lib/v5/spec/codemod-v5.spec.ts
+++ modules/canvas-kit-codemod/lib/v5/spec/codemod-v5.spec.ts
@@ -3,106 +3,100 @@
 import {defineInlineTest} from 'jscodeshift/dist/testUtils';
 import transform from '..';
 
-describe('reverse-identifiers', () => {
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import canvas from '@workday/canvas-kit-react';
-  //     `,
-  //   `
-  // import canvas from "@workday/canvas-kit-react/core";
-  //     `,
-  //   'Changes default canvas bundle import to core import'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import * as foo from '@workday/canvas-kit-react-button';
-  //     `,
-  //   `
-  // import * as foo from "@workday/canvas-kit-react/button";
-  //     `,
-  //   'Updates namespaced import'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import * as foo from '@workday/canvas-kit-react';
-  //     `,
-  //   `
-  // import * as foo from "@workday/canvas-kit-react/core";
-  //     `,
-  //   'Changes namespaced bundle import to namespaced core import'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  //   import Button from '@workday/canvas-kit-react-button';
-  //       `,
-  //   `
-  //   import Button from "@workday/canvas-kit-react/button";
-  //       `,
-  //   'Updates default button import'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import canvas, {Button} from '@workday/canvas-kit-react';
-  //     `,
-  //   `
-  // import canvas from "@workday/canvas-kit-react/core";
-  // import { Button } from "@workday/canvas-kit-react/button";
-  //     `,
-  //   'Remaps multiple imports from bundle'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import * as Button from '@workday/canvas-kit-react-button';
-  //     `,
-  //   `
-  // import * as Button from "@workday/canvas-kit-react/button";
-  //     `,
-  //   'Updates namespaced button import'
-  // );
-
-  // defineInlineTest(
-  //   transform,
-  //   {},
-  //   `
-  // import {ActionBar, Banner, BannerVariant} from '@workday/canvas-kit-react';
-  //     `,
-  //   `
-  // import { ActionBar } from "@workday/canvas-kit-react/action-bar";
-  // import { Banner, BannerVariant } from "@workday/canvas-kit-react/banner";
-  //     `,
-  //   'Splits bundle module import specifiers into separate imports'
-  // );
+describe('Canvas Kit v5 Codemod', () => {
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import canvas from '@workday/canvas-kit-react';
+      `,
+    `
+  import canvas from "@workday/canvas-kit-react/core";
+      `,
+    'Changes default canvas bundle import to core import'
+  );
 
   defineInlineTest(
     transform,
     {},
     `
-  import {
-    colors,
-    depth as depthValues,
-  } from '@workday/canvas-kit-react-core';`,
+  import * as foo from '@workday/canvas-kit-react-button';
+      `,
     `
-  import {
-    colors,
-    depth as depthValues,
-  } from "@workday/canvas-kit-react/core";
+  import * as foo from "@workday/canvas-kit-react/button";
+      `,
+    'Updates namespaced import'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import * as foo from '@workday/canvas-kit-react';
+      `,
+    `
+  import * as foo from "@workday/canvas-kit-react/core";
+      `,
+    'Changes namespaced bundle import to namespaced core import'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+    import Button from '@workday/canvas-kit-react-button';
+        `,
+    `
+    import Button from "@workday/canvas-kit-react/button";
+        `,
+    'Updates default button import'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import canvas, {Button} from '@workday/canvas-kit-react';
+      `,
+    `
+  import canvas from "@workday/canvas-kit-react/core";
+  import { Button } from "@workday/canvas-kit-react/button";
+      `,
+    'Remaps multiple imports from bundle'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import * as Button from '@workday/canvas-kit-react-button';
+      `,
+    `
+  import * as Button from "@workday/canvas-kit-react/button";
+      `,
+    'Updates namespaced button import'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import {ActionBar, Banner, BannerVariant} from '@workday/canvas-kit-react';
+      `,
+    `
+  import { ActionBar } from "@workday/canvas-kit-react/action-bar";
+  import { Banner, BannerVariant } from "@workday/canvas-kit-react/banner";
+      `,
+    'Splits bundle module import specifiers into separate imports'
+  );
+
+  defineInlineTest(
+    transform,
+    {},
+    `
+  import { colors, depth as depthValues } from '@workday/canvas-kit-react-core';`,
+    `
+  import { colors, depth as depthValues } from "@workday/canvas-kit-react/core";
     `,
     'Updates renamed module import'
   );
