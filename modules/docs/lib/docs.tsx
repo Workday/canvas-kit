95dbe56195cb09f4374dceb5f0db4dcdf624b7ae
Nicholas Boll <nicholas.boll@gmail.com>
2021-03-24 15:03:41 -0600

    docs: Fix Github Pages story link in Specifications (#1005)
    The story link in the Specifications table doesn't work on Github Pages,  but works locally and in Chromatic.

--- modules/docs/lib/docs.tsx
+++ modules/docs/lib/docs.tsx
@@ -2,4 +2,4 @@ import React from "react";
 
 export const GithubUrl = React.createContext('https://github.com/Workday/canvas-kit/');
 export const GithubBranch = React.createContext('master')
-export const StorybookUrl = React.createContext('/');
+export const StorybookUrl = React.createContext(typeof location !== 'undefined' && location.pathname === '/canvas-kit/' ? '/canvas-kit/' : '/');
