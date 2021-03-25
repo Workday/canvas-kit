0c18f7c2048382182cf3e7bc0cd95ce6e0019738
Nicholas Boll <nicholas.boll@gmail.com>
2021-03-25 12:03:12 -0600

    fix: Fix specification story links

--- modules/docs/lib/docs.tsx
+++ modules/docs/lib/docs.tsx
@@ -2,4 +2,4 @@ import React from "react";
 
 export const GithubUrl = React.createContext('https://github.com/Workday/canvas-kit/');
 export const GithubBranch = React.createContext('master')
-export const StorybookUrl = React.createContext(typeof location !== 'undefined' && location.pathname === '/canvas-kit/' ? '/canvas-kit/' : '/');
+export const StorybookUrl = React.createContext(typeof location !== 'undefined' && location.pathname.indexOf('/canvas-kit/') === 0 ? '/canvas-kit/' : '/');
