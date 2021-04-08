b12ec765052a0f80c0ab6d15867c0cf7d4900f38
Alex Nicholls <anicholls3@gmail.com>
2021-04-08 15:52:47 -0700

    refactor: Update story file names and update visual testing props

--- modules/react/button/index.ts
+++ modules/react/button/index.ts
@@ -1,15 +1,12 @@
-export {Button, ButtonProps} from './lib/Button';
-
+export {PrimaryButton, PrimaryButtonProps} from './lib/PrimaryButton';
+export {SecondaryButton, SecondaryButtonProps} from './lib/SecondaryButton';
+export {TertiaryButton, TertiaryButtonProps} from './lib/TertiaryButton';
 export {DeleteButton, DeleteButtonProps} from './lib/DeleteButton';
 export {
   DeprecatedButton as deprecated_Button,
   DeprecatedButtonProps,
 } from './lib/deprecated_Button';
-export {DropdownButton, DropdownButtonProps} from './lib/DropdownButton';
-export {HighlightButton, HighlightButtonProps} from './lib/HighlightButton';
-export {OutlineButton, OutlineButtonProps} from './lib/OutlineButton';
 export {IconButton, IconButtonProps} from './lib/IconButton';
-export {TextButton, TextButtonProps} from './lib/TextButton';
 export {ToolbarIconButton, ToolbarIconButtonProps} from './lib/ToolbarIconButton';
 export {ToolbarDropdownButton, ToolbarDropdownButtonProps} from './lib/ToolbarDropdownButton';
 export {Hyperlink} from './lib/Hyperlink';
---
+++ modules/react/button/stories/visual-testing/stories_SecondaryButton.tsx
@@ -0,0 +1,56 @@
+/// <reference path="../../../../../typings.d.ts" />
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
+import {StaticStates} from '@workday/canvas-kit-labs-react/core';
+import {
+  ComponentStatesTable,
+  permutateProps,
+  withSnapshotsEnabled,
+  customColorTheme,
+} from '../../../../../utils/storybook';
+import {playCircleIcon} from '@workday/canvas-system-icons-web';
+import {SecondaryButton} from '@workday/canvas-kit-react/button';
+import {Container, stateTableColumnProps} from './utils';
+import {PartialEmotionCanvasTheme} from '@workday/canvas-kit-react/common';
+
+export default withSnapshotsEnabled({
+  title: 'Testing/React/Buttons/Button/Secondary Button',
+  component: SecondaryButton,
+});
+
+export const SecondaryButtonStates = (props: {theme?: PartialEmotionCanvasTheme}) => (
+  <StaticStates theme={props.theme}>
+    <ComponentStatesTable
+      rowProps={permutateProps({
+        size: [
+          {value: 'small', label: 'Small'},
+          {value: 'medium', label: 'Medium'},
+          {value: 'large', label: 'Large'},
+        ],
+        icon: [
+          {value: undefined, label: ''},
+          {value: playCircleIcon, label: 'w/ Icon'},
+        ],
+        iconPosition: [
+          {value: 'left', label: 'Left Icon'},
+          {value: 'right', label: 'right Icon'},
+        ],
+        dataLabel: [
+          {value: undefined, label: ''},
+          {value: '1:23', label: 'w/ Data Label'},
+        ],
+      })}
+      columnProps={stateTableColumnProps}
+    >
+      {props => (
+        <Container>
+          <SecondaryButton {...props}>Test</SecondaryButton>
+        </Container>
+      )}
+    </ComponentStatesTable>
+  </StaticStates>
+);
+
+export const SecondaryButtonThemedStates = () => (
+  <SecondaryButtonStates theme={{canvas: customColorTheme}} />
+);
