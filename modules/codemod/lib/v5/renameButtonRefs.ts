c4fc177a0a480bfe79d418e85fcb550b8be4b915
Nicholas Boll <nicholas.boll@gmail.com>
2021-04-09 12:58:20 -0600

    fix: Scope buttonRef codemod to only Button components (#1022)
    * fix: Scope buttonRef codemod to only Button components

--- modules/codemod/lib/v5/renameButtonRefs.ts
+++ modules/codemod/lib/v5/renameButtonRefs.ts
@@ -1,12 +1,30 @@
 import {API, FileInfo, Options, JSXIdentifier} from 'jscodeshift';
 import {getImportRenameMap} from './getImportRenameMap';
 
+const buttonNames = [
+  'Button',
+  'DeleteButton',
+  'deprecated_Button',
+  'DropdownButton',
+  'HighlightButton',
+  'Hyperlink',
+  'IconButton',
+  'OutlineButton',
+  'TextButton',
+  'ToolbarDropdownButton',
+  'ToolbarIconButton',
+];
+
 export default function transformer(file: FileInfo, api: API, options: Options) {
   const j = api.jscodeshift;
 
   const root = j(file.source);
 
-  const {containsCanvasImports} = getImportRenameMap(j, root, '@workday/canvas-kit-react/button');
+  const {containsCanvasImports, importMap} = getImportRenameMap(
+    j,
+    root,
+    '@workday/canvas-kit-react/button'
+  );
 
   if (!containsCanvasImports) {
     return file.source;
@@ -14,8 +32,13 @@ export default function transformer(file: FileInfo, api: API, options: Options) 
 
   root
     .find(j.JSXIdentifier, (value: JSXIdentifier) => value.name === 'buttonRef')
-    .replaceWith(() => {
-      return j.jsxIdentifier('ref');
+    .replaceWith(nodePath => {
+      // only replace via one of our Button imports
+      const elementName = nodePath.parent?.parent?.value?.name?.name;
+      if (buttonNames.includes(elementName) || buttonNames.includes(importMap[elementName])) {
+        return j.jsxIdentifier('ref');
+      }
+      return nodePath.value;
     });
 
   return root.toSource();
