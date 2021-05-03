b4525d6d8999231fed87f828fca3479b0d08ba23
Alan Smith <a.bax.smith@gmail.com>
2021-05-03 10:26:39 -0600

    fix: Add flex and stack style prop tests

---
+++ modules/labs-react/layout/spec/stack.spec.ts
@@ -0,0 +1,92 @@
+import {stack, StackProps} from '../lib/utils/stack';
+import {space} from '@workday/canvas-kit-react/tokens';
+const context = describe;
+
+const selector = '& > *:not(style) ~ *:not(style)';
+
+describe('Stack Style Props Function', () => {
+  it('should ignore non-stack props', () => {
+    const props = {
+      paddingX: space.s,
+      children: null,
+      flexDirection: 'column',
+      spacing: space.s,
+    } as StackProps;
+    const expected = {
+      [selector]: {
+        marginTop: space.s,
+      },
+    };
+    const stackStyles = stack(props);
+
+    expect(stackStyles).toEqual(expected);
+  });
+
+  context('given flexDirection is set to column', () => {
+    it('should set marginTop spacing', () => {
+      const props = {
+        flexDirection: 'column',
+        spacing: space.m,
+      } as StackProps;
+      const expected = {
+        [selector]: {
+          marginTop: space.m,
+        },
+      };
+      const stackStyles = stack(props);
+
+      expect(stackStyles).toEqual(expected);
+    });
+  });
+
+  context('given flexDirection is set to column-reverse', () => {
+    it('should set marginBottom spacing', () => {
+      const props = {
+        flexDirection: 'column-reverse',
+        spacing: space.xxs,
+      } as StackProps;
+      const expected = {
+        [selector]: {
+          marginBottom: space.xxs,
+        },
+      };
+      const stackStyles = stack(props);
+
+      expect(stackStyles).toEqual(expected);
+    });
+  });
+
+  context('given flexDirection is set to row', () => {
+    it('should set marginLeft spacing', () => {
+      const props = {
+        flexDirection: 'row',
+        spacing: space.m,
+      } as StackProps;
+      const expected = {
+        [selector]: {
+          marginLeft: space.m,
+        },
+      };
+      const stackStyles = stack(props);
+
+      expect(stackStyles).toEqual(expected);
+    });
+  });
+
+  context('given flexDirection is set to row-reverse', () => {
+    it('should set marginRight spacing', () => {
+      const props = {
+        flexDirection: 'row-reverse',
+        spacing: space.m,
+      } as StackProps;
+      const expected = {
+        [selector]: {
+          marginRight: space.m,
+        },
+      };
+      const stackStyles = stack(props);
+
+      expect(stackStyles).toEqual(expected);
+    });
+  });
+});
