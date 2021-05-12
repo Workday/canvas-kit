57da678f0970d303cb28260bc2b00e51d811f495
James Fan <james@somewhatstrange.com>
2021-05-11 17:38:26 -0700

    fix: Fix Combobox to use ref instead of inputRef

--- modules/labs-react/combobox/lib/Combobox.tsx
+++ modules/labs-react/combobox/lib/Combobox.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useLayoutEffect, useRef, useState, useCallback} from 'react';
 import styled from '@emotion/styled';
 import {CSSObject, jsx, keyframes} from '@emotion/core';
-import {GrowthBehavior} from '@workday/canvas-kit-react/common';
+import {GrowthBehavior, PropsWithAs, useForkRef} from '@workday/canvas-kit-react/common';
 import {depth, space, commonColors, borderRadius} from '@workday/canvas-kit-react/tokens';
 import {MenuItemProps} from '@workday/canvas-kit-labs-react/menu';
 import {Card} from '@workday/canvas-kit-react/card';
@@ -184,11 +184,12 @@ const Combobox = ({
   >([]);
   const [announcementText, setAnnouncementText] = useState('');
 
-  // The text input that is wrapped may have a ref already set, if it doesn't we will use our own default.
-  // This default shouldn't be used on it's own, ut we need to declare separately so hooks are always called in the same order.
-  const _defaultInputRef: React.RefObject<HTMLInputElement> = useRef(null);
-  const inputRef: React.RefObject<HTMLInputElement> =
-    (typeof children.props.inputRef !== 'function' && children.props.inputRef) || _defaultInputRef;
+  // Create a ref to the text input for internal use and use useForkRef to
+  // combine it with the ref already assigned to the text input (if it exists)
+  // to create a single callback ref which can be forwarded to the cloned
+  // text input created later.
+  const inputRef: React.RefObject<HTMLInputElement> = useRef(null);
+  const elementRef = useForkRef((children as PropsWithAs<TextInputProps, 'input'>).ref, inputRef);
 
   const comboboxRef: React.RefObject<HTMLDivElement> = useRef(null);
 
@@ -409,12 +410,12 @@ const Combobox = ({
         paddingRight: space.xl,
       };
     }
-    const newTextInputProps: Partial<TextInputProps> = {
+    const newTextInputProps: Partial<PropsWithAs<TextInputProps, 'input'>> = {
       type: 'text',
       id: componentId,
       grow: grow,
       value: value,
-      inputRef: inputRef,
+      ref: elementRef,
       'aria-autocomplete': 'list',
       'aria-activedescendant':
         selectedAutocompleteIndex !== null
