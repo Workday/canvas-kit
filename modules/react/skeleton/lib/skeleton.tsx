5b7eb072a1adb16de37fdd7115384204b1ee115e
Will Klein <will@willklein.co>
2021-05-25 14:08:09 -0600

    fix(skeleton): Change skeleton animation to fading in and out

--- modules/react/skeleton/lib/skeleton.tsx
+++ modules/react/skeleton/lib/skeleton.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import {keyframes} from '@emotion/core';
-import canvas from '@workday/canvas-kit-react/tokens';
 import {accessibleHide} from '@workday/canvas-kit-react/common';
 
 export interface SkeletonProps {
@@ -17,90 +16,45 @@ export interface SkeletonProps {
   'aria-label'?: string;
 }
 
-const TRANSPARENCY_POSITION = 45;
-const WHITE_SHEEN_WIDTH = 10;
-const DURATION = 5;
-
 const AccessibleHide = styled('div')(accessibleHide);
 
-const SkeletonAnimator = styled('div')<{diagonal: number; topPosition: number; width: number}>(
-  ({diagonal, topPosition, width}) => {
-    const backgroundPositionAnimation = keyframes({
-      from: {
-        left: `-${diagonal}px`,
-      },
-      to: {
-        left: `${diagonal + width}px`,
-      },
-    });
+const fade50 = keyframes({
+  '0%': {opacity: 1},
+  '50%': {opacity: 0.5},
+  '100%': {opacity: 1},
+});
 
-    return {
-      animation: `${backgroundPositionAnimation} ${DURATION}s ease-in-out infinite`,
-      background: `linear-gradient(130deg, rgba(255,255,255,0) ${TRANSPARENCY_POSITION}%, ${
-        canvas.colors.frenchVanilla100
-      }, rgba(255,255,255,0)  ${TRANSPARENCY_POSITION + WHITE_SHEEN_WIDTH}%)`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: `${diagonal}px ${diagonal}px`,
-      width: diagonal,
-      height: diagonal,
-      top: topPosition,
-      position: 'absolute',
-    };
-  }
-);
+// similar to MUI
+const fade50Animation = `${fade50} 1s ease-in-out 0.5s infinite`;
+
+const fade = keyframes({
+  from: {opacity: 0},
+  to: {opacity: 1},
+});
+// const animation = `${fade} 0.8s`;
 
 const SkeletonContainer = styled('div')<SkeletonProps>({
+  animation: fade50Animation,
   overflow: 'hidden',
   width: '100%',
   height: '100%',
   position: 'relative',
 });
 
-export interface SkeletonState {
-  width: number;
-  height: number;
-}
+const Skeleton: React.FunctionComponent<SkeletonProps> = ({
+  'aria-label': loadingAriaLabel = 'Loading',
+  children,
+  ...elemProps
+}) => {
+  const ref: React.RefObject<HTMLDivElement> = React.createRef();
 
-export default class Skeleton extends React.Component<SkeletonProps, SkeletonState> {
-  private ref: React.RefObject<HTMLDivElement> = React.createRef();
+  return (
+    <SkeletonContainer ref={ref} {...elemProps}>
+      <AccessibleHide>{loadingAriaLabel}</AccessibleHide>
 
-  state = {
-    width: 0,
-    height: 0,
-  };
+      <div aria-hidden={true}>{children}</div>
+    </SkeletonContainer>
+  );
+};
 
-  render(): React.ReactNode {
-    const {'aria-label': loadingAriaLabel = 'Loading', children, ...elemProps} = this.props;
-    const {width, height} = this.state;
-    const diagonal = Math.sqrt(width * width + height * height) + WHITE_SHEEN_WIDTH;
-    const topPosition = (-1 * (diagonal - height)) / 2;
-
-    return (
-      <SkeletonContainer ref={this.ref} {...elemProps}>
-        <AccessibleHide>{loadingAriaLabel}</AccessibleHide>
-        <SkeletonAnimator diagonal={diagonal} topPosition={topPosition} width={width} />
-        <div aria-hidden={true}>{children}</div>
-      </SkeletonContainer>
-    );
-  }
-
-  componentDidMount(): void {
-    if (this.ref.current) {
-      this.setState({
-        height: this.ref.current.clientHeight,
-        width: this.ref.current.clientWidth,
-      });
-    }
-  }
-
-  componentDidUpdate(): void {
-    const {width, height} = this.state;
-    const current = this.ref.current;
-    if (current && (width !== current.clientWidth || height !== current.clientHeight)) {
-      this.setState({
-        height: current.clientHeight,
-        width: current.clientWidth,
-      });
-    }
-  }
-}
+export default Skeleton;
