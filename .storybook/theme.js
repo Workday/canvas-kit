6ff3da018b33a8416a696cea05bf2201236458e0
Mihaela Miches <mihaela.miches@gmail.com>
2021-05-27 10:58:00 +0100

    style: Follow the existing formatting on imports

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,6 +1,6 @@
 import {themes, create} from '@storybook/theming';
 import {commonColors, typeColors, fontFamily} from '../modules/core/react';
-import { version } from '../lerna.json';
+import {version} from '../lerna.json';
 
 export default create({
   ...themes.light, // Overrides a user's preferred color scheme (e.g. Dark Mode), will need to flesh this out later when CK is compatible with a dark mode
