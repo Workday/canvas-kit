6327ac71dbc882d9db22215b9be923485e8acc6c
Nicholas Boll <nicholas.boll@gmail.com>
2021-05-28 17:55:55 -0600

    Fix Card.Body

--- modules/react/card/lib/CardBody.tsx
+++ modules/react/card/lib/CardBody.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import {type} from '@workday/canvas-kit-react/tokens';
 import {StyledType, createComponent} from '@workday/canvas-kit-react/common';
-import {BoxProps} from '@workday/canvas-kit-labs-react/common';
+import {Box, BoxProps} from '@workday/canvas-kit-labs-react/common';
 
-const Body = styled('div')<BoxProps & StyledType>(type.body);
+const Body = styled(Box)<BoxProps & StyledType>(type.body);
 
 export const CardBody = createComponent('div')({
   displayName: 'Card.Body',
