4bd764c21b0ec46ff621f67dceb0c6cc9d2883df
Will Klein <will@willklein.co>
2021-06-04 09:38:51 -0600

    refactor(skeleton): Convert Skeleton.Text to createComponent

--- modules/react/skeleton/lib/parts/skeletonText.tsx
+++ modules/react/skeleton/lib/parts/skeletonText.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
 
+import {createComponent, styled, StyledType} from '@workday/canvas-kit-react/common';
 import canvas from '@workday/canvas-kit-react/tokens';
 
 import {TEXT_BORDER_RADIUS} from './utils';
 
-const TextContainer = styled('div')({
+const TextContainer = styled('div')<StyledType>({
   marginBottom: canvas.space.m,
 });
 
@@ -22,53 +22,45 @@ export interface SkeletonTextProps extends React.HTMLAttributes<HTMLDivElement> 
   backgroundColor?: string;
 }
 
-const Line = styled('div')<{
-  width: number | string;
-  height: number | string;
-  borderRadius: number;
-  backgroundColor: string;
-}>(({width, height, borderRadius, backgroundColor}) => {
+const Line = styled('div')<
+  {
+    width: number | string;
+    backgroundColor: string;
+  } & StyledType
+>(({backgroundColor, width}) => {
   return {
     backgroundColor,
     width,
-    height,
-    borderRadius,
+    borderRadius: TEXT_BORDER_RADIUS,
+    height: '21px',
     marginBottom: canvas.space.xs,
   };
 });
 
 const createTextLines = (lineCount: number, backgroundColor: string) => {
-  const lines = [];
+  const lines = new Array(lineCount).fill(null);
 
-  const lineProps = {
-    backgroundColor,
-    borderRadius: TEXT_BORDER_RADIUS,
-    height: '21px',
-    width: '100%',
-  };
-
-  for (let i = 0; i < lineCount; i++) {
-    lines.push(
-      <Line
-        key={i}
-        {...lineProps}
-        width={lineCount === 1 || i + 1 !== lineCount ? '100%' : '60%'}
-      />
-    );
-  }
-  return lines;
+  return lines.map((_value, index) => (
+    <Line
+      key={index}
+      backgroundColor={backgroundColor}
+      width={index < lineCount - 1 || lineCount === 1 ? '100%' : '60%'}
+    />
+  ));
 };
 
-export default class SkeletonText extends React.Component<SkeletonTextProps> {
-  render(): React.ReactNode {
-    const {lineCount = 2, backgroundColor = canvas.colors.soap200, ...elemProps} = this.props;
+const SkeletonText = createComponent('div')({
+  displayName: 'Skeleton.Text',
+  Component: (
+    {backgroundColor = canvas.colors.soap200, lineCount = 2, ...elemProps}: SkeletonTextProps,
+    ref,
+    Element
+  ) =>
+    lineCount <= 0 ? null : (
+      <TextContainer ref={ref} as={Element} {...elemProps}>
+        {createTextLines(lineCount, backgroundColor)}
+      </TextContainer>
+    ),
+});
 
-    if (lineCount <= 0) {
-      return null;
-    }
-
-    return (
-      <TextContainer {...elemProps}>{createTextLines(lineCount, backgroundColor)}</TextContainer>
-    );
-  }
-}
+export default SkeletonText;
