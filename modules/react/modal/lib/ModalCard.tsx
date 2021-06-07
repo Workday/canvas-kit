461631413b5c5f0f93f5d19dfae8902a9c554905
Nicholas Boll <nicholas.boll@gmail.com>
2021-06-07 16:11:13 -0600

    fix(modal): Remove 1px border on modals

--- modules/react/modal/lib/ModalCard.tsx
+++ modules/react/modal/lib/ModalCard.tsx
@@ -14,7 +14,7 @@ export const ModalCard = createComponent('div')({
 
     const props = useModalCard(localModel, elemProps, ref);
     return (
-      <Popup.Card as={Element} width={440} {...props}>
+      <Popup.Card as={Element} width={440} borderWidth={0} {...props}>
         {children}
       </Popup.Card>
     );
