15bee93775af10b505a046b56e0333b02ecdb9d8
James Fan <james@somewhatstrange.com>
2021-06-25 11:24:05 -0700

    docs(text-input): Migrate Text Input MDX from canvas-site

---
+++ modules/react/text-input/stories/examples/RefForwarding.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {PrimaryButton} from '@workday/canvas-kit-react/button';
+import {FormField} from '@workday/canvas-kit-react/form-field';
+import {TextInput} from '@workday/canvas-kit-react/text-input';
+
+export const RefForwarding = () => {
+  const [value, setValue] = React.useState('');
+  const ref = React.useRef(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = () => {
+    ref.current.focus();
+  };
+
+  return (
+    <>
+      <FormField label="Email">
+        <TextInput onChange={handleChange} ref={ref} value={value} />
+      </FormField>
+      <PrimaryButton onClick={handleClick}>Focus Text Input</PrimaryButton>
+    </>
+  );
+};
