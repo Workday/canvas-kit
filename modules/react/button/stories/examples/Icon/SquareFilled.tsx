a98b7a7760acff8f00528931621b77f11d066b96
Alex Nicholls <anicholls3@gmail.com>
2021-06-25 15:32:35 -0700

    docs(button): Switch button and icon button docs to mdx

---
+++ modules/react/button/stories/examples/Icon/SquareFilled.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import {IconButton} from '@workday/canvas-kit-react/button';
+import {activityStreamIcon} from '@workday/canvas-system-icons-web';
+
+export const SquareFilled = () => (
+  <IconButton icon={activityStreamIcon} aria-label="Activity Stream" variant="squareFilled" />
+);
