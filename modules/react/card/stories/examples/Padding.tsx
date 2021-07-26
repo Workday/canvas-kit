1beeac09fa2b452c9635e20851fe8a506b6b3a2e
James Fan <james@somewhatstrange.com>
2021-07-26 11:46:05 -0700

    docs(card): Merge Card MDX from canvas-site

--- modules/react/card/stories/examples/Padding.tsx
+++ modules/react/card/stories/examples/Padding.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Card} from '@workday/canvas-kit-react/card';
+import {space} from '@workday/canvas-kit-react/tokens';
 
-export const Depth = () => {
+export const Padding = () => {
   return (
-    <Card depth="inset">
+    <Card padding={space.xs}>
       <Card.Heading>Canvas Supreme</Card.Heading>
       <Card.Body>
         Our house special supreme pizza includes pepperoni, sausage, bell peppers, mushrooms,
