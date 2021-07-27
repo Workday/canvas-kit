5fa324d53e672b75a56c9e7e8ec35adb54821960
James Fan <james@somewhatstrange.com>
2021-07-26 18:40:28 -0700

    docs(card): Update examples

--- modules/react/card/stories/examples/Depth.tsx
+++ modules/react/card/stories/examples/Depth.tsx
@@ -3,7 +3,7 @@ import {Card} from '@workday/canvas-kit-react/card';
 
 export const Depth = () => {
   return (
-    <Card depth="inset">
+    <Card depth={1}>
       <Card.Heading>Canvas Supreme</Card.Heading>
       <Card.Body>
         Our house special supreme pizza includes pepperoni, sausage, bell peppers, mushrooms,
--- modules/react/card/stories/examples/Padding.tsx
+++ modules/react/card/stories/examples/Padding.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {Card} from '@workday/canvas-kit-react/card';
-import {space} from '@workday/canvas-kit-react/tokens';
 
 export const Padding = () => {
   return (
-    <Card padding={space.xs}>
+    <Card padding="xs">
       <Card.Heading>Canvas Supreme</Card.Heading>
       <Card.Body>
         Our house special supreme pizza includes pepperoni, sausage, bell peppers, mushrooms,
