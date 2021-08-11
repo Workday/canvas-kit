6fd10c8f6933e38045dd7dd52cc63306475d4047
Chad Sahlhoff <chad@sahlhoff.com>
2021-08-11 12:30:37 -0700

    Adds sixteen and seventeen to ManyItems example
    Co-authored-by: Alan B Smith <alanbsmith@users.noreply.github.com>

--- modules/preview-react/menu/stories/examples/ManyItems.tsx
+++ modules/preview-react/menu/stories/examples/ManyItems.tsx
@@ -5,7 +5,7 @@ import {Menu, MenuItem} from '@workday/canvas-kit-preview-react/menu';
 export const ManyItems = () => {
   return (
     <Menu title="Menu Titles">
-      {'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen'
+      {'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen'
         .split(' ')
         .map(item => {
           return <MenuItem>Item {item}</MenuItem>;
