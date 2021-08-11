ce4ffb919ca98d4f36ded84e1b71d817c1930341
chad <chad.sahlhoff@workday.com>
2021-08-11 15:53:07 -0700

    docs: Removes container from ContextMenu example

--- modules/preview-react/menu/stories/examples/ContextMenu.tsx
+++ modules/preview-react/menu/stories/examples/ContextMenu.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 
-import {styled, CanvasProvider, ContentDirection} from '@workday/canvas-kit-react/common';
-import {type} from '@workday/canvas-kit-react/tokens';
-import {fonts} from '@workday/canvas-kit-react-fonts';
+import {CanvasProvider, ContentDirection} from '@workday/canvas-kit-react/common';
 import {Menu, MenuItem} from '@workday/canvas-kit-preview-react/menu';
 import {
   Popup,
@@ -12,10 +10,6 @@ import {
 } from '@workday/canvas-kit-react/popup';
 
 import {ContextMenuTarget} from './ContextMenuTarget';
-
-const Container = styled('div')(...fonts, {
-  ...type.levels.body.small,
-});
 
 export const ContextMenu = () => {
   const canvasTheme = {
@@ -31,27 +25,25 @@ export const ContextMenu = () => {
 
   return (
     <CanvasProvider theme={canvasTheme}>
-      <Container>
-        <Popup model={model}>
-          <ContextMenuTarget style={{display: 'inline'}} tabIndex={0}>
-            Right click on this text (Context menu target)
-          </ContextMenuTarget>
-          <Popup.Popper>
-            <Menu onClose={model.events.hide}>
-              <MenuItem>Back</MenuItem>
-              <MenuItem>Forward</MenuItem>fd
-              <MenuItem>Reload</MenuItem>
-              <MenuItem hasDivider>Bookmark Page</MenuItem>
-              <MenuItem>Save Page As...</MenuItem>
-              <MenuItem>Select All</MenuItem>
-              <MenuItem hasDivider>Take Screenshot</MenuItem>
-              <MenuItem hasDivider>View Page Source</MenuItem>
-              <MenuItem>Inspect Accessibility Properties</MenuItem>
-              <MenuItem>Inspect</MenuItem>
-            </Menu>
-          </Popup.Popper>
-        </Popup>
-      </Container>
+      <Popup model={model}>
+        <ContextMenuTarget style={{display: 'inline'}} tabIndex={0}>
+          Right click on this text (Context menu target)
+        </ContextMenuTarget>
+        <Popup.Popper>
+          <Menu onClose={model.events.hide}>
+            <MenuItem>Back</MenuItem>
+            <MenuItem>Forward</MenuItem>fd
+            <MenuItem>Reload</MenuItem>
+            <MenuItem hasDivider>Bookmark Page</MenuItem>
+            <MenuItem>Save Page As...</MenuItem>
+            <MenuItem>Select All</MenuItem>
+            <MenuItem hasDivider>Take Screenshot</MenuItem>
+            <MenuItem hasDivider>View Page Source</MenuItem>
+            <MenuItem>Inspect Accessibility Properties</MenuItem>
+            <MenuItem>Inspect</MenuItem>
+          </Menu>
+        </Popup.Popper>
+      </Popup>
     </CanvasProvider>
   );
 };
