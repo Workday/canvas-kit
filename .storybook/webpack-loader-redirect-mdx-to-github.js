b9641211dcc9de5d56f75cd6a4810d75bb143572
Nicholas Boll <nicholas.boll@gmail.com>
2021-08-13 17:50:44 -0600

    docs: Update inter-linking to match Canvas Site

--- .storybook/webpack-loader-redirect-mdx-to-github.js
+++ .storybook/webpack-loader-redirect-mdx-to-github.js
@@ -1,15 +1,29 @@
 const path = require('path');
 
+const routes = require('./routes');
+
 const basePath = path.resolve(__dirname, '../');
 
 module.exports = function webpackLoaderRedirectMDXToGithub(source) {
   const {resourcePath} = this;
-  return source.replace(/\[([^\]]+)\]\((\.\.?[^\)]+)\)/g, function replacer(match, p1, p2) {
-    // extract the directory from the resourcePath given by Webpack
-    const {dir} = path.parse(resourcePath);
+  const routeKeys = Object.keys(routes);
 
-    const newPath = path.relative(basePath, path.resolve(dir, p2));
+  // find absolute paths that match something in routes
+  return source
+    .replace(/\[([^\]]+)\]\((\/[^\)]+)\)/g, function replacer(match, p1, p2) {
+      const [url, hash] = p2.split('#');
+      if (routeKeys.includes(url)) {
+        return `[${p1}](?path=/docs/${routes[url]}${hash ? '#' + hash : ''})`;
+      }
+      // no match, return original
+      return `[${p1}](${p2})`;
+    })
+    .replace(/\[([^\]]+)\]\((\.\.?[^\)]+)\)/g, function replacer(match, p1, p2) {
+      // extract the directory from the resourcePath given by Webpack
+      const {dir} = path.parse(resourcePath);
 
-    return `[${p1}](https://github.com/Workday/canvas-kit/blob/master/${newPath})`;
-  });
+      const newPath = path.relative(basePath, path.resolve(dir, p2));
+
+      return `[${p1}](https://github.com/Workday/canvas-kit/blob/master/${newPath})`;
+    });
 };
