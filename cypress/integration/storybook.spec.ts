66ad8a1fa122e71aaf04b2101cee3a84668e7d16
Nicholas Boll <nicholas.boll@gmail.com>
2021-08-24 15:19:53 -0600

    docs: Update inter-linking to match Canvas Site (#1220)
    Co-authored-by: James Fan <james@somewhatstrange.com>
    Co-authored-by: Alex Nicholls <anicholls3@gmail.com>

--- cypress/integration/storybook.spec.ts
+++ cypress/integration/storybook.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable compat/compat */
 function getIframeBody($iframe: JQuery): JQuery {
   return $iframe.contents().find('body') as JQuery;
 }
@@ -8,5 +9,27 @@ describe('Storybook', () => {
     cy.get('iframe#storybook-preview-iframe')
       .pipe(getIframeBody, {timeout: 20000})
       .should('contain', 'Workday Canvas Kit');
+  });
+
+  context('routes', () => {
+    it('should contain valid story ids', () => {
+      // if this test fails, it means the routes in `routes.js` contains a Storybook link that is
+      // not valid. You can find valid stories by typing the following in an `iframe.html` console:
+      // `__STORYBOOK_STORY_STORE__.extract()`. This will give all the stories registered.
+      cy.visit('/iframe.html');
+      cy.window().then((win: any) => {
+        const routes: Record<string, string> = win.__routes;
+        const stories: Record<string, unknown> = win.__STORYBOOK_STORY_STORE__.extract({
+          includeDocsOnly: true,
+        });
+
+        const storybookRoutes = Object.values(routes);
+        const storybookStories = Object.keys(stories);
+
+        storybookRoutes.forEach(route => {
+          expect(storybookStories).to.include(route);
+        });
+      });
+    });
   });
 });
