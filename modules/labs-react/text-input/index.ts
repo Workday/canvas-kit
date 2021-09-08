d8c419b496e69214421de0f462f0d91236372e7b
Matthew Rust <matthew.rust@workday.com>
2021-09-08 13:19:43 -0700

    fix(labs): Export the new behavior hooks

--- modules/labs-react/text-input/index.ts
+++ modules/labs-react/text-input/index.ts
@@ -1,2 +1,6 @@
 export * from './lib/TextInput';
 export * from './lib/hooks/useTextInputModel';
+export * from './lib/hooks/useTextInputHint';
+export * from './lib/hooks/useTextInputField';
+export * from './lib/hooks/useTextInputHint';
+export * from './lib/hooks/useTextInputLabel';
