e2fc234cd94ded850b2286e8e7802da55fe95c8a
Nicholas Boll <nicholas.boll@gmail.com>
2021-09-08 11:04:21 -0600

    fix(tooltip): Fix tooltip placement overlap on keyboard focus
    Fixes #1071

--- modules/react/tooltip/stories/stories_testing.tsx
+++ modules/react/tooltip/stories/stories_testing.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {Popper, Placement} from '@workday/canvas-kit-react/popup';
 import {TooltipContainer, Tooltip} from '@workday/canvas-kit-react/tooltip';
 import {Card} from '@workday/canvas-kit-react/card';
+import {SecondaryButton} from '@workday/canvas-kit-react/button';
+import {StaticStates} from '@workday/canvas-kit-react/common';
+
 import {withSnapshotsEnabled} from '../../../../utils/storybook';
 
 const fontDelay = 150; // best guess for the font delay to prevent incorrect Chromatic regressions
@@ -20,13 +23,10 @@ export const NonInteractive = () => {
 
 export const Placements = withSnapshotsEnabled(() => {
   const [open, setOpen] = React.useState(false);
-  const [ref, setRef] = React.useState<null | Element>(null);
-  const [containerElement, setContainerElement] = React.useState<null | Element>(null);
+  const ref = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     const id = setTimeout(() => {
-      setContainerElement(document.querySelector('[data-testid="tooltip-container"]'));
-      setRef(document.querySelector('[data-testid="tooltip-target"]'));
       setOpen(true);
     });
 
@@ -51,7 +51,6 @@ export const Placements = withSnapshotsEnabled(() => {
   ];
   return (
     <div
-      data-testid="tooltip-container"
       style={{
         display: 'inline-block',
         overflow: 'auto',
@@ -59,7 +58,7 @@ export const Placements = withSnapshotsEnabled(() => {
       }}
     >
       <Card
-        data-testid="tooltip-target"
+        ref={ref}
         style={{
           width: 300,
           height: 300,
@@ -90,5 +89,81 @@ export const Placements = withSnapshotsEnabled(() => {
         )
       )}
     </div>
+  );
+});
+
+export const PlacementsFocus = withSnapshotsEnabled(() => {
+  const [open, setOpen] = React.useState(false);
+  const ref = React.useRef<HTMLButtonElement>(null);
+
+  React.useEffect(() => {
+    document.body.setAttribute('data-whatinput', 'keyboard');
+    const id = setTimeout(() => {
+      setOpen(true);
+    });
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
+  const placements: Placement[] = [
+    'top-start',
+    'top',
+    'top-end',
+    'right-start',
+    'right',
+    'right-end',
+    'bottom-end',
+    'bottom',
+    'bottom-start',
+    'left-end',
+    'left',
+    'left-start',
+  ];
+  return (
+    <StaticStates>
+      <div
+        style={{
+          display: 'inline-block',
+          overflow: 'auto',
+          padding: 100,
+        }}
+      >
+        <SecondaryButton
+          style={{
+            width: 300,
+            height: 300,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 4,
+          }}
+          className="focus"
+          ref={ref}
+        >
+          Target
+        </SecondaryButton>
+        {placements.map(placement =>
+          !open ? null : (
+            <Popper
+              key={placement}
+              placement={placement}
+              popperOptions={{
+                modifiers: [
+                  // keep the tooltips from moving - no matter what!
+                  {name: 'flip', enabled: false},
+                  {name: 'preventOverflow', enabled: false},
+                ],
+              }}
+              open={open}
+              anchorElement={ref}
+            >
+              <TooltipContainer transformOrigin={null}>{placement}</TooltipContainer>
+            </Popper>
+          )
+        )}
+      </div>
+    </StaticStates>
   );
 });
