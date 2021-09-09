bffddfbb68b512bb06291e0068bb643f9ec0bbb2
Alan B Smith <a.bax.smith@gmail.com>
2021-09-08 23:54:51 -0600

    feat(page-header): Finish PageHeader deprecation codemod

--- modules/codemod/lib/v6/deprecatePageHeader.ts
+++ modules/codemod/lib/v6/deprecatePageHeader.ts
@@ -1,16 +1,47 @@
-import {API, FileInfo, Identifier} from 'jscodeshift';
+import {API, FileInfo, Identifier, ImportDeclaration} from 'jscodeshift';
+
+const mainPackage = '@workday/canvas-kit-react';
+const pageHeaderPackage = '@workday/canvas-kit-react/page-header';
 
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
 
   const root = j(file.source);
+  let hasPageHeaderImports = false;
 
-  // update PageHeader import statements from the main package
+  // This toggles the failsafe that prevents us from accidentally transforming something unintentionally.
+  root.find(j.ImportDeclaration, (nodePath: ImportDeclaration) => {
+    const value = nodePath.source.value;
+    // If there's an import from the page-header package, set the import boolean check to true
+    if (value === pageHeaderPackage) {
+      hasPageHeaderImports = true;
+      return;
+    }
+    // If there's an import from the main package, check to see if PageHeader or PageHeaderProps are among the named imports
+    // e.g. import {PageHeader} from '@workday/canvas-kit-react';
+    if (value === mainPackage) {
+      (nodePath.specifiers || []).forEach(specifier => {
+        if (specifier.type === 'ImportSpecifier') {
+          const specifierName = specifier.imported.name;
+          if (specifierName === 'PageHeader' || specifierName === 'PageHeaderProps') {
+            hasPageHeaderImports = true;
+          }
+        }
+      });
+    }
+  });
+
+  // Failsafe to skip transforms unless a PageHeader import is detected
+  if (!hasPageHeaderImports) {
+    return root.toSource();
+  }
+
   root
     .find(j.ImportDeclaration, {source: {value: '@workday/canvas-kit-react'}})
     .forEach(nodePath => {
       nodePath.value.specifiers?.forEach(specifier => {
         if (specifier.type === 'ImportSpecifier') {
+          //  `import {PageHeader}` becomes `import {DeprecatedPageHeader}`
           if (specifier.imported.name === 'PageHeader') {
             specifier.imported.name = 'DeprecatedPageHeader';
           }
@@ -19,51 +50,60 @@ export default function transformer(file: FileInfo, api: API) {
       });
     });
 
-  // update PageHeader named import statements from the page-header package
+  // Transform PageHeader default and named import statements from the page-header package
   root
     .find(j.ImportDeclaration, {source: {value: '@workday/canvas-kit-react/page-header'}})
     .forEach(nodePath => {
       nodePath.value.specifiers?.forEach(specifier => {
+        if (specifier.type === 'ImportDefaultSpecifier') {
+          //  `import PageHeader` becomes `import DeprecatedPageHeader`
+          if (specifier.local?.name === 'PageHeader') {
+            specifier.local.name = 'DeprecatedPageHeader';
+          }
+        }
         if (specifier.type === 'ImportSpecifier') {
+          //  `import {PageHeader}` becomes `import {DeprecatedPageHeader}`
           if (specifier.imported.name === 'PageHeader') {
             specifier.imported.name = 'DeprecatedPageHeader';
+          }
+          //  `import {PageHeaderProps}` becomes `import {DeprecatedPageHeaderProps}`
+          if (specifier.imported.name === 'PageHeaderProps') {
+            specifier.imported.name = 'DeprecatedPageHeaderProps';
           }
         }
         return specifier;
       });
     });
 
-  // update PageHeader default import statements from the page-header package
-  root
-    .find(j.ImportDeclaration, {source: {value: '@workday/canvas-kit-react/page-header'}})
-    .forEach(nodePath => {
-      nodePath.value.specifiers?.forEach(specifier => {
-        if (specifier.type === 'ImportSpecifier') {
-          if (specifier.imported.name === 'PageHeader') {
-            specifier.imported.name = 'DeprecatedPageHeader';
-          }
-        }
-        return specifier;
-      });
-    });
-
-  // update PageHeader identifiers
-  root
-    .find(j.TSTypeReference, {typeName: {type: 'Identifier', name: 'PageHeader'}})
-    .forEach(nodePath => {
-      const identifier = nodePath.value.typeName as Identifier;
-      identifier.name = 'DeprecatedPageHeader';
-      return nodePath;
-    });
-
-  // update PageHeaderProps identifiers
+  // Transform PageHeaderProp type references
+  // e.g. `type CustomProps = PageHeaderProps;` becomes `type CustomProps = DeprecatedPageHeaderProps;`
   root
     .find(j.TSTypeReference, {typeName: {type: 'Identifier', name: 'PageHeaderProps'}})
     .forEach(nodePath => {
       const identifier = nodePath.value.typeName as Identifier;
       identifier.name = 'DeprecatedPageHeaderProps';
-      return nodePath;
+      // return nodePath;
     });
+
+  // Transform PageHeaderProp type interface declaration references
+  // e.g. `interface CustomProps extends PageHeaderProps` becomes `interface CustomProps extends DeprecatedPageHeaderProps`
+  root.find(j.TSInterfaceDeclaration).forEach(nodePath => {
+    // If the interface is extending PageHeaderProps, transform the extension name to DeprecatedPageHeaderProps
+    nodePath.node.extends?.forEach(typeExtension => {
+      if (
+        typeExtension.expression.type === 'Identifier' &&
+        typeExtension.expression.name === 'PageHeaderProps'
+      ) {
+        typeExtension.expression.name = 'DeprecatedPageHeaderProps';
+      }
+    });
+  });
+
+  // Transform PageHeader JSXElements
+  // e.g. `<PageHeader>` becomes `<DeprecatedPageHeader>`
+  root.find(j.JSXIdentifier, {name: 'PageHeader'}).forEach(nodePath => {
+    nodePath.node.name = 'DeprecatedPageHeader';
+  });
 
   return root.toSource();
 }
--- modules/codemod/lib/v6/spec/deprecatePageHeader.spec.ts
+++ modules/codemod/lib/v6/spec/deprecatePageHeader.spec.ts
@@ -7,56 +7,64 @@ const expectTransform = expectTransformFactory(transformer);
 describe('Canvas Kit Deprecate PageHeader Codemod', () => {
   context('when transforming PageHeader imports', () => {
     it('should ignore non-canvas-kit imports', () => {
-      const input = `import { PageHeader } from "@workday/some-other-library"`;
-      const expected = `import { PageHeader } from "@workday/some-other-library"`;
+      const input = `import {PageHeader} from '@workday/some-other-library'`;
+      const expected = `import {PageHeader} from '@workday/some-other-library'`;
 
       expectTransform(input, expected);
     });
 
     it('should properly transform named imports from @workday/canvas-kit-react', () => {
-      const input = `import { PageHeader } from "@workday/canvas-kit-react"`;
-      const expected = `import { DeprecatedPageHeader } from "@workday/canvas-kit-react"`;
+      const input = `import {PageHeader} from '@workday/canvas-kit-react'`;
+      const expected = `import {DeprecatedPageHeader} from '@workday/canvas-kit-react'`;
 
       expectTransform(input, expected);
     });
 
     it('should properly transform named imports from @workday/canvas-kit-react/page-header', () => {
-      const input = `import { PageHeader } from "@workday/canvas-kit-react/page-header"`;
-      const expected = `import { DeprecatedPageHeader } from "@workday/canvas-kit-react/page-header"`;
+      const input = `import {PageHeader} from '@workday/canvas-kit-react/page-header'`;
+      const expected = `import {DeprecatedPageHeader} from '@workday/canvas-kit-react/page-header'`;
 
       expectTransform(input, expected);
     });
 
     it('should properly transform default imports from @workday/canvas-kit-react/page-header', () => {
-      const input = `import PageHeader from "@workday/canvas-kit-react/page-header"`;
-      const expected = `import DeprecatedPageHeader from "@workday/canvas-kit-react/page-header"`;
+      const input = `import PageHeader from '@workday/canvas-kit-react/page-header'`;
+      const expected = `import DeprecatedPageHeader from '@workday/canvas-kit-react/page-header'`;
 
       expectTransform(input, expected);
     });
 
     it('should properly transform mixed imports from @workday/canvas-kit-react/page-header', () => {
-      const input = `import PageHeader, { PageHeaderProps } from "@workday/canvas-kit-react/page-header"`;
-      const expected = `import DeprecatedPageHeader, { DeprecatedPageHeaderProps } from "@workday/canvas-kit-react/page-header"`;
+      const input = `import PageHeader, { PageHeaderProps } from '@workday/canvas-kit-react/page-header'`;
+      const expected = `import DeprecatedPageHeader, { DeprecatedPageHeaderProps } from '@workday/canvas-kit-react/page-header'`;
 
       expectTransform(input, expected);
     });
   });
 
   context('when transforming identifiers', () => {
-    it('should properly transform PageHeader identifiers', () => {
+    it('should properly transform PageHeader JSX identifiers', () => {
       const input = `
-      import { PageHeader } from "@workday/canvas-kit-react/page-header";
+      import {PageHeader} from '@workday/canvas-kit-react/page-header';
   
       const CustomPageHeader = () => {
-        return <PageHeader />;
+        return <PageHeader>Hello World</PageHeader>;
       };
+
+      const AnotherPageHeader = (props) => {
+        return <PageHeader {...props} />;
+      }
       `;
       const expected = `
-      import { DeprecatedPageHeader } from "@workday/canvas-kit-react/page-header";
+      import {DeprecatedPageHeader} from '@workday/canvas-kit-react/page-header';
   
       const CustomPageHeader = () => {
-        return <DeprecatedPageHeader />;
+        return <DeprecatedPageHeader>Hello World</DeprecatedPageHeader>;
       };
+
+      const AnotherPageHeader = (props) => {
+        return <DeprecatedPageHeader {...props} />;
+      }
       `;
 
       expectTransform(input, expected);
@@ -64,14 +72,20 @@ describe('Canvas Kit Deprecate PageHeader Codemod', () => {
 
     it('should properly transform type reference identifiers', () => {
       const input = `
-      import { PageHeaderProps } from "@workday/canvas-kit-react/page-header";
+      import { PageHeaderProps } from '@workday/canvas-kit-react/page-header';
   
       type CustomPageHeaderProps = PageHeaderProps;
+      interface AnotherPageHeaderProps extends PageHeaderProps {
+        specialProp?: boolean;
+      }
       `;
       const expected = `
-      import { DeprecatedPageHeaderProps } from "@workday/canvas-kit-react/page-header";
+      import { DeprecatedPageHeaderProps } from '@workday/canvas-kit-react/page-header';
   
       type CustomPageHeaderProps = DeprecatedPageHeaderProps;
+      interface AnotherPageHeaderProps extends DeprecatedPageHeaderProps {
+        specialProp?: boolean;
+      }
       `;
 
       expectTransform(input, expected);
