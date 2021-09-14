b7ea97fbe934b3770e2c9a25f90f927dc8b7dcb9
manuel carrera <manuel.carrera@workday.com>
2021-09-14 12:51:03 -0600

    fix: Update import to point to common

--- utils/create-component/templates/react/stories_VisualTesting.js
+++ utils/create-component/templates/react/stories_VisualTesting.js
@@ -2,7 +2,8 @@
 
 module.exports = (modulePath, storyPath, pascalCaseName, rootPath) => `import React from 'react';
 
-import {ComponentStatesTable, StaticStates} from '@workday/canvas-kit-labs-react/common';
+import {StaticStates} from '@workday/canvas-kit-react/common;
+import {ComponentStatesTable} from '@workday/canvas-kit-labs-react/common';
 import {withSnapshotsEnabled} from '../${rootPath}/utils/storybook';
 
 import {${pascalCaseName}, use${pascalCaseName}Model} from '${modulePath}';
