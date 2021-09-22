4f6e4ab9b069d60a49efccf6e8f4effe4f49dee6
Alan B Smith <a.bax.smith@gmail.com>
2021-09-22 14:32:09 -0600

    feat(page-header): Deprecate PageHeader (#1247)
    Co-authored-by: James Fan <james@somewhatstrange.com>

--- modules/react/page-header/stories/stories.tsx
+++ modules/react/page-header/stories/stories.tsx
@@ -6,25 +6,25 @@ import withReadme from 'storybook-readme/with-readme';
 import {IconButton} from '@workday/canvas-kit-react/button';
 import {exportIcon, fullscreenIcon} from '@workday/canvas-system-icons-web';
 
-import PageHeader from '../index';
+import DeprecatedPageHeader from '../index';
 import README from '../README.md';
 
 storiesOf('Components/Navigation/Page Header/React', module)
-  .addParameters({component: PageHeader})
+  .addParameters({component: DeprecatedPageHeader})
   .addDecorator(withReadme(README))
   .add('Product Page Header', () => (
     <div className="story">
-      <PageHeader title="Product Context">
+      <DeprecatedPageHeader title="Product Context">
         <IconButton icon={exportIcon} aria-label="Export" />
         <IconButton icon={fullscreenIcon} aria-label="Fullscreen" />
-      </PageHeader>
+      </DeprecatedPageHeader>
     </div>
   ))
   .add('With Cap Width', () => (
     <div className="story">
-      <PageHeader title="With Cap Width" capWidth={true}>
+      <DeprecatedPageHeader title="With Cap Width" capWidth={true}>
         <IconButton icon={exportIcon} aria-label="Export" />
         <IconButton icon={fullscreenIcon} aria-label="Fullscreen" />
-      </PageHeader>
+      </DeprecatedPageHeader>
     </div>
   ));
