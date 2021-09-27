6469378c3abe89676eebb7893076f5e2eb246b39
Alan B Smith <a.bax.smith@gmail.com>
2021-09-27 11:01:14 -0600

    feat(cookie-banner): Deprecate CookieBanner (#1265)

--- modules/codemod/lib/v6/index.ts
+++ modules/codemod/lib/v6/index.ts
@@ -1,11 +1,13 @@
 import {API, FileInfo, Options} from 'jscodeshift';
 // deprecate PageHeader
 import deprecatePageHeader from './deprecatePageHeader';
+// deprecate CookieBanner
+import deprecateCookieBanner from './deprecateCookieBanner';
 
 export default function transformer(file: FileInfo, api: API, options: Options) {
   // These will run in order. If your transform depends on others, place yours after dependent
   // transforms
-  const fixes = [deprecatePageHeader];
+  const fixes = [deprecatePageHeader, deprecateCookieBanner];
 
   return fixes.reduce((source, fix) => fix({...file, source}, api, options), file.source);
 }
