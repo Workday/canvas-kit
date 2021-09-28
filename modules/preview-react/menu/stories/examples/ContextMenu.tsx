296e6e6d65e37adcc5c76c112d9756d5c1da998c
James Fan <james@somewhatstrange.com>
2021-09-28 14:25:52 -0700

    docs(menu): Consolidate ContextMenu example

--- modules/preview-react/menu/stories/examples/ContextMenu.tsx
+++ modules/preview-react/menu/stories/examples/ContextMenu.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react';
 
-import {CanvasProvider, ContentDirection} from '@workday/canvas-kit-react/common';
+import {
+  CanvasProvider,
+  ContentDirection,
+  createComponent,
+  useForkRef,
+} from '@workday/canvas-kit-react/common';
 import {Menu, MenuItem} from '@workday/canvas-kit-preview-react/menu';
 import {
   Popup,
+  PopupModelContext,
   usePopupModel,
   useAlwaysCloseOnOutsideClick,
   useCloseOnEscape,
 } from '@workday/canvas-kit-react/popup';
 
-import {ContextMenuTarget} from './ContextMenuTarget';
+const ContextMenuTarget = createComponent('div')({
+  displayName: 'Popup.ContextMenuTarget',
+  Component: ({children, ...elemProps}, ref, Element) => {
+    const model = React.useContext(PopupModelContext);
+    const elementRef = useForkRef(ref, model.state.targetRef as any);
+
+    const onContextMenu = (event: React.MouseEvent) => {
+      if (model.state.visibility === 'visible') {
+        model.events.hide({event});
+      } else if (model.state.visibility === 'hidden') {
+        model.events.show({event});
+      }
+
+      // prevent the default context menu from showing to avoid double menus
+      event.preventDefault();
+    };
+
+    return (
+      <Element ref={elementRef} onContextMenu={onContextMenu} {...elemProps}>
+        {children}
+      </Element>
+    );
+  },
+});
 
 export const ContextMenu = () => {
   const canvasTheme = {
