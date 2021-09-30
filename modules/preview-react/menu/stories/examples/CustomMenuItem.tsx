9a61746a7d80485136e9866e2c7d3e8c1195cfa2
Alan B Smith <a.bax.smith@gmail.com>
2021-09-29 19:58:06 -0600

    fix: Apply suggestions from code review

--- modules/preview-react/menu/stories/examples/CustomMenuItem.tsx
+++ modules/preview-react/menu/stories/examples/CustomMenuItem.tsx
@@ -4,7 +4,7 @@ import {Menu, MenuItem} from '@workday/canvas-kit-preview-react/menu';
 
 export const CustomMenuItem = () => {
   return (
-    <Menu title="Menu Title">
+    <Menu>
       <MenuItem>First Item</MenuItem>
       <MenuItem>Second Item</MenuItem>
       <li role="menuitem" tabIndex={-1} style={{listStyle: 'none'}}>
