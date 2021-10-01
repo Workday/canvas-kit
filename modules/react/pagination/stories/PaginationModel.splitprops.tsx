3c0a9b62834113f4342d2e96606a76c703f5d15f
James Fan <james@somewhatstrange.com>
2021-09-30 17:40:10 -0700

    docs(pagination): Update Model section

---
+++ modules/react/pagination/stories/PaginationModel.splitprops.tsx
@@ -0,0 +1,9 @@
+import {
+  UsePaginationModelConfig,
+  PaginationState,
+  PaginationEvents,
+} from '@workday/canvas-kit-react/pagination';
+
+export const PaginationModelConfigComponent = (_: UsePaginationModelConfig) => <div />;
+export const PaginationStateComponent = (_: PaginationState) => <div />;
+export const PaginationEventsComponent = (_: PaginationEvents) => <div />;
