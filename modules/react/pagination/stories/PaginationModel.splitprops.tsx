e51e9d300e5696e2b80ee5f3e97afcc22e2ad486
James Fan <james@somewhatstrange.com>
2021-10-04 17:58:57 -0700

    docs(pagination): Update Components section

--- modules/react/pagination/stories/PaginationModel.splitprops.tsx
+++ modules/react/pagination/stories/PaginationModel.splitprops.tsx
@@ -1,8 +1,42 @@
 import {
-  UsePaginationModelConfig,
+  PaginationModel,
   PaginationState,
   PaginationEvents,
+  UsePaginationModelConfig,
 } from '@workday/canvas-kit-react/pagination';
+
+export const PaginationHoistedComponent = (_: {model: PaginationModel}) => <div />;
+
+export const PageButtonComponent = (_: {pageNumber: number}) => <div />;
+
+export const PageListComponent = (_: {
+  /**
+   * Accepts child elements or a render prop.
+   */
+  children: (model: PaginationModel) => React.ReactNode | React.ReactNode;
+}) => <div />;
+
+export const GoToLabelComponent = (_: {
+  /**
+   * Accepts child elements or a render prop.
+   */
+  children: (model: PaginationModel) => React.ReactNode | React.ReactNode;
+}) => <div />;
+
+export const AdditionalDetailsComponent = (_: {
+  /**
+   * Accepts child elements or a render prop.
+   */
+  children: (model: PaginationModel) => React.ReactNode | React.ReactNode;
+  /**
+   * @default true
+   */
+  shouldAnnounceToScreenReader?: boolean;
+  /**
+   * @default false
+   */
+  shouldHideDetails?: boolean;
+}) => <div />;
 
 export const PaginationModelConfigComponent = (_: UsePaginationModelConfig) => <div />;
 export const PaginationStateComponent = (_: PaginationState) => <div />;
