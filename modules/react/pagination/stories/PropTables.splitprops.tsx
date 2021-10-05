2b5cac8577cc40048d74540a43b380375fe80cd4
James Fan <james@somewhatstrange.com>
2021-10-05 00:53:29 -0700

    docs(pagination): Clean up docs

--- modules/react/pagination/stories/PropTables.splitprops.tsx
+++ modules/react/pagination/stories/PropTables.splitprops.tsx
@@ -5,10 +5,18 @@ import {
   UsePaginationModelConfig,
 } from '@workday/canvas-kit-react/pagination';
 
+// <ArgsTable of={Pagination} /> generates a very large props table given that
+// PaginationProps includes FlexProps. Use this dummy component instead to
+// limit the props shown.
 export const PaginationHoistedComponent = (_: {model: PaginationModel}) => <div />;
 
+// <ArgsTable of={Pagination.PageButton} /> generates a props table with
+// IconButton props. Use this dummy component instead to limit the props shown.
 export const PageButtonComponent = (_: {pageNumber: number}) => <div />;
 
+// <ArgsTable of={Pagination.PageListComponent} /> generates a very large props
+// table given that PageListProps includes FlexProps. Use this dummy component
+// instead to limit the props shown.
 export const PageListComponent = (_: {
   /**
    * Accepts child elements or a render prop.
@@ -16,13 +24,9 @@ export const PageListComponent = (_: {
   children: (model: PaginationModel) => React.ReactNode | React.ReactNode;
 }) => <div />;
 
-export const GoToLabelComponent = (_: {
-  /**
-   * Accepts child elements or a render prop.
-   */
-  children: (model: PaginationModel) => React.ReactNode | React.ReactNode;
-}) => <div />;
-
+// <ArgsTable of={Pagination.AdditionalDetails} /> generates a very large props
+// table given that AdditionalDetailsProps includes FlexProps. Use this dummy
+// component instead to limit the props shown.
 export const AdditionalDetailsComponent = (_: {
   /**
    * Accepts child elements or a render prop.
