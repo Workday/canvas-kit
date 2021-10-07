b556f2322e5be1ebdc1890fbed16620b5f0c5432
James Fan <james@somewhatstrange.com>
2021-10-06 17:16:20 -0700

    docs(radio): Fix examples to follow case guidance

--- modules/react/radio/stories/examples/Required.tsx
+++ modules/react/radio/stories/examples/Required.tsx
@@ -10,11 +10,11 @@ export const Required = () => {
   };
 
   return (
-    <FormField label="Choose your pizza crust" required={true} useFieldset={true}>
+    <FormField label="Choose Your Pizza Crust" required={true} useFieldset={true}>
       <RadioGroup name="crust" onChange={handleChange} value={value}>
-        <Radio label="Deep Dish" value="deep-dish" />
+        <Radio label="Deep dish" value="deep-dish" />
         <Radio label="Thin" value="thin" />
-        <Radio label="Gluten Free" value="gluten-free" />
+        <Radio label="Gluten free" value="gluten-free" />
         <Radio label="Cauliflower" value="cauliflower" />
       </RadioGroup>
     </FormField>
