4b6bab5355ec5e680198f67fea6c1615c0f1c9ab
James Fan <james@somewhatstrange.com>
2021-10-06 17:37:52 -0700

    fix(menu): Remove unnecessary title attributes in examples

--- modules/preview-react/menu/stories/examples/ManyItems.tsx
+++ modules/preview-react/menu/stories/examples/ManyItems.tsx
@@ -4,7 +4,7 @@ import {Menu, MenuItem} from '@workday/canvas-kit-preview-react/menu';
 
 export const ManyItems = () => {
   return (
-    <Menu title="Menu Titles">
+    <Menu>
       {'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen'
         .split(' ')
         .map(item => {
