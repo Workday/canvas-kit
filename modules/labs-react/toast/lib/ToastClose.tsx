4dbed7a8534e8d5719779667aae5391b41551dba
Cem Kaptan <cem.kaptan@workday.com>
2021-10-18 10:23:29 -0700

    feat(labs): Passing default elem props

--- modules/labs-react/toast/lib/ToastClose.tsx
+++ modules/labs-react/toast/lib/ToastClose.tsx
@@ -2,22 +2,22 @@
 import {jsx} from '@emotion/core';
 
 import {createComponent, useModelContext} from '@workday/canvas-kit-react/common';
-import {Popup} from '@workday/canvas-kit-react';
+import {IconButtonProps, Popup} from '@workday/canvas-kit-react';
 import {ToastButtonType, ToastModel} from './useToastModel';
 import {ToastModelContext} from './Toast';
 
-export interface ToastCloseProps {
+export interface ToastCloseProps extends Partial<IconButtonProps> {
   model?: ToastModel;
   onClose: () => void;
 }
 
 export const ToastClose = createComponent('div')({
   displayName: 'Toast.Close',
-  Component: ({onClose, model}: ToastCloseProps, ref, Element) => {
+  Component: ({onClose, model, ...elemProps}: ToastCloseProps, ref, Element) => {
     const {
       events: {updateButtonExistState},
     } = useModelContext(ToastModelContext, model);
     updateButtonExistState({buttonType: ToastButtonType.Close, status: true});
-    return <Popup.CloseIcon aria-label="Close" onClick={onClose} size="small" />;
+    return <Popup.CloseIcon aria-label="Close" onClick={onClose} size="small" {...elemProps} />;
   },
 });
