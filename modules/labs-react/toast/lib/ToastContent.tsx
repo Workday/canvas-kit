95b08132584f42c2f63dedcb7c6f76e1e352e63a
Cem Kaptan <cem.kaptan@workday.com>
2021-10-18 10:20:44 -0700

    feat(labs): Introducing toast model

--- modules/labs-react/toast/lib/ToastContent.tsx
+++ modules/labs-react/toast/lib/ToastContent.tsx
@@ -2,25 +2,36 @@
 import {jsx} from '@emotion/core';
 import React from 'react';
 
-import {createComponent, styled} from '@workday/canvas-kit-react/common';
-import {Popup, type} from '@workday/canvas-kit-react';
+import {createComponent, styled, useModelContext} from '@workday/canvas-kit-react/common';
+import {Popup, space, type} from '@workday/canvas-kit-react';
+import {ToastModel} from './useToastModel';
+import {ToastModelContext} from './Toast';
 
 export interface ToastContentProps {
+  model?: ToastModel;
   children: React.ReactNode;
 }
 
-const ToastContentContainer = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  ...type.levels.subtext.large,
-});
+const ToastContentContainer = styled('div')<{closeButtonExist: any}>(
+  {
+    display: 'flex',
+    alignItems: 'center',
+    ...type.levels.subtext.large,
+  },
+  ({closeButtonExist}) => ({
+    marginRight: closeButtonExist ? space.m : undefined,
+  })
+);
 
 export const ToastContent = createComponent('div')({
   displayName: 'Toast.Content',
-  Component: ({children}: ToastContentProps, ref, Element) => {
+  Component: ({children, model}: ToastContentProps, ref, Element) => {
+    const {state} = useModelContext(ToastModelContext, model);
     return (
       <Popup.Body>
-        <ToastContentContainer>{children}</ToastContentContainer>
+        <ToastContentContainer closeButtonExist={state.closeButtonExist}>
+          {children}
+        </ToastContentContainer>
       </Popup.Body>
     );
   },
