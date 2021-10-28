4f2ff9e3631f60511c16126599c9c7a730c66e57
Cem Kaptan <cem.kaptan@workday.com>
2021-10-28 09:35:28 -0700

    feat(labs): Refactor unit tests

--- modules/labs-react/toast/lib/ToastClose.tsx
+++ modules/labs-react/toast/lib/ToastClose.tsx
@@ -1,19 +1,19 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core';
 
-import {createComponent, ExtractProps} from '@workday/canvas-kit-react/common';
+import {createComponent, ExtractProps, StyledType} from '@workday/canvas-kit-react/common';
 import {Popup} from '@workday/canvas-kit-react';
 import styled from '@emotion/styled';
 
 export interface ToastCloseProps extends ExtractProps<typeof Popup.CloseIcon, never> {}
 
-const PopupCloseIcon = styled(Popup.CloseIcon)({
+const PopupCloseIcon = styled(Popup.CloseIcon)<StyledType & ToastCloseProps>({
   position: 'relative',
 });
 
 export const ToastClose = createComponent('button')({
   displayName: 'Toast.Close',
   Component: ({...elemProps}: ToastCloseProps, ref, Element) => {
-    return <PopupCloseIcon size="small" {...elemProps} />;
+    return <PopupCloseIcon as={Element} ref={ref} size="small" {...elemProps} />;
   },
 });
--- modules/labs-react/toast/spec/ToastAction.spec.tsx
+++ modules/labs-react/toast/spec/ToastAction.spec.tsx
@@ -1,45 +1,20 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import {Toast} from '../lib/Toast';
-import {checkIcon} from '@workday/canvas-system-icons-web';
-import {colors} from '@workday/canvas-kit-react/tokens';
 import React from 'react';
 
 describe('Toast.Action', () => {
   const cb = jest.fn();
 
-  it('should render an action text', () => {
-    const toastMessage = 'Your workbook was successfully processed.';
-    const actionText = 'View more details';
-    render(
-      <Toast mode="interactive">
-        <Toast.Content>
-          <Toast.Icon icon={checkIcon} iconColor={colors.greenApple400} />
-          <Toast.Message>
-            {toastMessage}
-            <Toast.Action onClick={cb}>{actionText}</Toast.Action>
-          </Toast.Message>
-        </Toast.Content>
-      </Toast>
-    );
+  it('should have a role of "button"', () => {
+    render(<Toast.Action>Action Button</Toast.Action>);
 
-    expect(screen.getByRole('dialog')).toHaveTextContent(actionText);
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
   it('should call the onActionClick callback', () => {
-    const toastMessage = 'Your workbook was successfully processed.';
     const actionText = 'View more details';
 
-    render(
-      <Toast mode="interactive">
-        <Toast.Content>
-          <Toast.Icon icon={checkIcon} iconColor={colors.greenApple400} />
-          <Toast.Message>
-            {toastMessage}
-            <Toast.Action onClick={cb}>{actionText}</Toast.Action>
-          </Toast.Message>
-        </Toast.Content>
-      </Toast>
-    );
+    render(<Toast.Action onClick={cb}>{actionText}</Toast.Action>);
 
     fireEvent.click(screen.getByRole('button', {name: actionText}));
     expect(cb).toHaveBeenCalledTimes(1);
