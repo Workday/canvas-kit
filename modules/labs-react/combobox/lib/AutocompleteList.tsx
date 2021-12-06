Workday/canvas-kit

commit 6417369e835304c298325f9a811eea12fcd81bf5

chore: Bump uuid to stable non deprecated version (#1367)
* chore: Bump uuid to stable non deprecated version

Co-authored-by: manuel carrera <manuel.carrera.91@gmail.com>

modules/labs-react/combobox/lib/AutocompleteList.tsx
@@ -4,7 +4,7 @@ import {space} from '@workday/canvas-kit-react/tokens';
 import {accessibleHide} from '@workday/canvas-kit-react/common';
 import {MenuItemProps} from '@workday/canvas-kit-preview-react/menu';
 import {ComboBoxMenuItemGroup, getOptionId, listBoxIdPart, getTextFromElement} from './Combobox';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 
 const Autocomplete = styled('ul')({
   margin: 0,
@@ -55,7 +55,7 @@ const AutocompleteList = ({
   labelId,
   showGroupText,
 }: AutocompleteListProps) => {
-  const [randomComponentId] = useState(() => uuid()); // https://codesandbox.io/s/p2ndq
+  const [randomComponentId] = useState(() => uuidv4()); // https://codesandbox.io/s/p2ndq
 
   const componentId = comboboxId || randomComponentId;
 

modules/labs-react/combobox/lib/Combobox.tsx
@@ -7,7 +7,7 @@ import {Card} from '@workday/canvas-kit-react/card';
 import {IconButton, IconButtonProps} from '@workday/canvas-kit-react/button';
 import {xSmallIcon} from '@workday/canvas-system-icons-web';
 import {TextInputProps} from '@workday/canvas-kit-react/text-input';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 import flatten from 'lodash.flatten';
 import AutocompleteList from './AutocompleteList';
 import Status from './Status';
@@ -200,8 +200,8 @@ const Combobox = ({
 
   const comboboxRef: React.RefObject<HTMLDivElement> = useRef(null);
 
-  const [randomComponentId] = React.useState(() => uuid()); // https://codesandbox.io/s/p2ndq
-  const [randomLabelId] = React.useState(() => uuid());
+  const [randomComponentId] = React.useState(() => uuidv4()); // https://codesandbox.io/s/p2ndq
+  const [randomLabelId] = React.useState(() => uuidv4());
 
   const componentId = id || randomComponentId;
   const formLabelId = labelId || randomLabelId;

modules/labs-react/header/lib/parts/SearchBar.tsx
@@ -11,7 +11,7 @@ import {MenuItemProps} from '@workday/canvas-kit-preview-react/menu';
 import {SearchThemeAttributes, searchThemes} from '../shared/themes';
 import {SearchTheme} from '../shared/types';
 import chroma from 'chroma-js';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 
 export interface SearchBarProps extends GrowthBehavior, React.FormHTMLAttributes<HTMLFormElement> {
   /**
@@ -294,7 +294,7 @@ export class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
 
   private inputRef = React.createRef<HTMLInputElement>();
   private openRef = React.createRef<HTMLButtonElement>();
-  private labelId = uuid();
+  private labelId = uuidv4();
 
   state: Readonly<SearchBarState> = {
     showForm: false,

modules/preview-react/menu/lib/Menu.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 import {MenuItemProps} from './MenuItem';
 import {Card} from '@workday/canvas-kit-react/card';
 import {commonColors, space, borderRadius} from '@workday/canvas-kit-react/tokens';
@@ -58,7 +58,7 @@ const List = styled('ul')({
 });
 
 export default class Menu extends React.Component<MenuProps, MenuState> {
-  private id = uuid();
+  private id = uuidv4();
   private animateId!: number;
 
   private menuRef: React.RefObject<HTMLUListElement>;

modules/preview-react/select/lib/Select.tsx
@@ -4,7 +4,7 @@ import {menuAnimationDuration} from './SelectMenu';
 import SelectBase, {CoreSelectBaseProps, Option, NormalizedOption} from './SelectBase';
 import {MenuVisibility} from './types';
 import {getCorrectedIndexByValue} from './utils';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 
 export interface SelectProps extends CoreSelectBaseProps {
   /**
@@ -87,13 +87,13 @@ class SelectContainer extends React.Component<SelectContainerProps, SelectContai
 
       if (typeof option === 'string') {
         disabled = false;
-        id = uuid();
+        id = uuidv4();
         value = option;
         label = option;
       } else {
         data = option.data || data;
         disabled = !!option.disabled;
-        id = option.id || uuid();
+        id = option.id || uuidv4();
         value = option.value;
         label = option.label || option.value;
       }

modules/react/common/lib/utils/useUniqueId.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 
-export const generateUniqueId = () => uuid().replace(/^[0-9]*/gi, '');
+export const generateUniqueId = () => uuidv4().replace(/^[0-9]*/gi, '');
 
 /**
  * Generate a unique ID if one is not provided. The generated ID will be stable across renders

modules/react/form-field/lib/FormField.tsx
@@ -4,7 +4,7 @@ import {GrowthBehavior, ErrorType, styled, Themeable} from '@workday/canvas-kit-
 import Hint from './Hint';
 import Label from './Label';
 import {FormFieldLabelPosition, FormFieldLabelPositionBehavior} from './types';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 
 export interface FormFieldProps
   extends Themeable,
@@ -135,7 +135,7 @@ class FormField extends React.Component<FormFieldProps> {
   static LabelPosition = FormFieldLabelPosition;
   static ErrorType = ErrorType;
 
-  private inputId: string = this.props.inputId || uuid();
+  private inputId: string = this.props.inputId || uuidv4();
 
   private renderChildren = (child: React.ReactNode): React.ReactNode => {
     if (React.isValidElement(child)) {

modules/react/tooltip/lib/useTooltip.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import uuid from 'uuid/v4';
+import {v4 as uuidv4} from 'uuid';
 import {
   useCloseOnEscape,
   useAlwaysCloseOnOutsideClick,
@@ -90,7 +90,7 @@ export function useTooltip<T extends Element = Element>({
   const mouseDownRef = React.useRef(false); // use to prevent newly focused from making tooltip flash
   const popupModel = usePopupModel();
   const [anchorElement, setAnchorElement] = React.useState<T | null>(null);
-  const [id] = React.useState(() => uuid());
+  const [id] = React.useState(() => uuidv4());
   const intentTimerHide = useIntentTimer(popupModel.events.hide, hideDelay);
   const intentTimerShow = useIntentTimer(popupModel.events.show, showDelay);
 

commit c4b4bf71aecea959ef9cdc565b27f849aa1aeb87

fix: Revert uuid upgrade to get releases working [skip release] (#1371)
Fix broken releases introduced by #1367 that unintentionally removed `@types/node` from Typescript which is causing `process.env` to be undeclared. Also updating the `yarn typecheck` scripts to catch this type of error. The PR job does not run `yarn build`, but runs `yarn typecheck`. Before the whole project was typechecked as a single project which worked because the `css` module included storyfiles using `@storybook/react` which included `webpack` which included definitions files for `process` and thus this PR was able to be merged even though a `yarn build` would fail. `yarn typecheck` is now being run the same way as `yarn build` is. This should prevent this type of issue.

[category:Revert]

modules/labs-react/combobox/lib/AutocompleteList.tsx
@@ -4,7 +4,7 @@ import {space} from '@workday/canvas-kit-react/tokens';
 import {accessibleHide} from '@workday/canvas-kit-react/common';
 import {MenuItemProps} from '@workday/canvas-kit-preview-react/menu';
 import {ComboBoxMenuItemGroup, getOptionId, listBoxIdPart, getTextFromElement} from './Combobox';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 
 const Autocomplete = styled('ul')({
   margin: 0,
@@ -55,7 +55,7 @@ const AutocompleteList = ({
   labelId,
   showGroupText,
 }: AutocompleteListProps) => {
-  const [randomComponentId] = useState(() => uuidv4()); // https://codesandbox.io/s/p2ndq
+  const [randomComponentId] = useState(() => uuid()); // https://codesandbox.io/s/p2ndq
 
   const componentId = comboboxId || randomComponentId;
 

modules/labs-react/combobox/lib/Combobox.tsx
@@ -7,7 +7,7 @@ import {Card} from '@workday/canvas-kit-react/card';
 import {IconButton, IconButtonProps} from '@workday/canvas-kit-react/button';
 import {xSmallIcon} from '@workday/canvas-system-icons-web';
 import {TextInputProps} from '@workday/canvas-kit-react/text-input';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 import flatten from 'lodash.flatten';
 import AutocompleteList from './AutocompleteList';
 import Status from './Status';
@@ -200,8 +200,8 @@ const Combobox = ({
 
   const comboboxRef: React.RefObject<HTMLDivElement> = useRef(null);
 
-  const [randomComponentId] = React.useState(() => uuidv4()); // https://codesandbox.io/s/p2ndq
-  const [randomLabelId] = React.useState(() => uuidv4());
+  const [randomComponentId] = React.useState(() => uuid()); // https://codesandbox.io/s/p2ndq
+  const [randomLabelId] = React.useState(() => uuid());
 
   const componentId = id || randomComponentId;
   const formLabelId = labelId || randomLabelId;

modules/labs-react/header/lib/parts/SearchBar.tsx
@@ -11,7 +11,7 @@ import {MenuItemProps} from '@workday/canvas-kit-preview-react/menu';
 import {SearchThemeAttributes, searchThemes} from '../shared/themes';
 import {SearchTheme} from '../shared/types';
 import chroma from 'chroma-js';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 
 export interface SearchBarProps extends GrowthBehavior, React.FormHTMLAttributes<HTMLFormElement> {
   /**
@@ -294,7 +294,7 @@ export class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
 
   private inputRef = React.createRef<HTMLInputElement>();
   private openRef = React.createRef<HTMLButtonElement>();
-  private labelId = uuidv4();
+  private labelId = uuid();
 
   state: Readonly<SearchBarState> = {
     showForm: false,

modules/preview-react/menu/lib/Menu.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 import {MenuItemProps} from './MenuItem';
 import {Card} from '@workday/canvas-kit-react/card';
 import {commonColors, space, borderRadius} from '@workday/canvas-kit-react/tokens';
@@ -58,7 +58,7 @@ const List = styled('ul')({
 });
 
 export default class Menu extends React.Component<MenuProps, MenuState> {
-  private id = uuidv4();
+  private id = uuid();
   private animateId!: number;
 
   private menuRef: React.RefObject<HTMLUListElement>;

modules/preview-react/select/lib/Select.tsx
@@ -4,7 +4,7 @@ import {menuAnimationDuration} from './SelectMenu';
 import SelectBase, {CoreSelectBaseProps, Option, NormalizedOption} from './SelectBase';
 import {MenuVisibility} from './types';
 import {getCorrectedIndexByValue} from './utils';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 
 export interface SelectProps extends CoreSelectBaseProps {
   /**
@@ -87,13 +87,13 @@ class SelectContainer extends React.Component<SelectContainerProps, SelectContai
 
       if (typeof option === 'string') {
         disabled = false;
-        id = uuidv4();
+        id = uuid();
         value = option;
         label = option;
       } else {
         data = option.data || data;
         disabled = !!option.disabled;
-        id = option.id || uuidv4();
+        id = option.id || uuid();
         value = option.value;
         label = option.label || option.value;
       }

modules/react/common/lib/utils/useUniqueId.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 
-export const generateUniqueId = () => uuidv4().replace(/^[0-9]*/gi, '');
+export const generateUniqueId = () => uuid().replace(/^[0-9]*/gi, '');
 
 /**
  * Generate a unique ID if one is not provided. The generated ID will be stable across renders

modules/react/form-field/lib/FormField.tsx
@@ -4,7 +4,7 @@ import {GrowthBehavior, ErrorType, styled, Themeable} from '@workday/canvas-kit-
 import Hint from './Hint';
 import Label from './Label';
 import {FormFieldLabelPosition, FormFieldLabelPositionBehavior} from './types';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 
 export interface FormFieldProps
   extends Themeable,
@@ -135,7 +135,7 @@ class FormField extends React.Component<FormFieldProps> {
   static LabelPosition = FormFieldLabelPosition;
   static ErrorType = ErrorType;
 
-  private inputId: string = this.props.inputId || uuidv4();
+  private inputId: string = this.props.inputId || uuid();
 
   private renderChildren = (child: React.ReactNode): React.ReactNode => {
     if (React.isValidElement(child)) {

modules/react/tooltip/lib/useTooltip.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {v4 as uuidv4} from 'uuid';
+import uuid from 'uuid/v4';
 import {
   useCloseOnEscape,
   useAlwaysCloseOnOutsideClick,
@@ -90,7 +90,7 @@ export function useTooltip<T extends Element = Element>({
   const mouseDownRef = React.useRef(false); // use to prevent newly focused from making tooltip flash
   const popupModel = usePopupModel();
   const [anchorElement, setAnchorElement] = React.useState<T | null>(null);
-  const [id] = React.useState(() => uuidv4());
+  const [id] = React.useState(() => uuid());
   const intentTimerHide = useIntentTimer(popupModel.events.hide, hideDelay);
   const intentTimerShow = useIntentTimer(popupModel.events.show, showDelay);
 