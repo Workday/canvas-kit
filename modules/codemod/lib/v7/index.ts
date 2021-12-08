4fa88c9eecf5dbad4203b00c3a16060b9a386b79
manuel carrera <manuel.carrera@workday.com>
2021-12-08 14:06:41 -0700

    fix: Comment out variable for now

--- modules/codemod/lib/v7/index.ts
+++ modules/codemod/lib/v7/index.ts
@@ -2,7 +2,6 @@ import {API, FileInfo, Options} from 'jscodeshift';
 
 export default function transformer(file: FileInfo, api: API, options: Options) {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
-  const fixes = [];
-
+  //   const fixes = [];
   //   return fixes.reduce((source, fix) => fix({...file, source}, api, options), file.source);
 }
