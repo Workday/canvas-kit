f51e74d11696f9ce663fec104f7f505196393cb6
Nicholas Boll <nicholas.boll@gmail.com>
2021-12-16 16:39:19 -0700

    fix(tabs): Fix Dynamic Tabs example

--- modules/react/tabs/stories/examples/DynamicTabs.tsx
+++ modules/react/tabs/stories/examples/DynamicTabs.tsx
@@ -4,16 +4,28 @@ import {space} from '@workday/canvas-kit-react/tokens';
 import {Tabs, useTabsModel, TabsModel} from '@workday/canvas-kit-react/tabs';
 import {SelectionModel} from '../../lib/selection';
 
+type Tab = {
+  tab: string;
+  id: string;
+};
+
 export const DynamicTabs = () => {
-  const [tabs, setTabs] = React.useState([
+  // save a ref of the currently selected tab. `Tabs.List` takes a render function an memoizes. We
+  // need to get around this
+  const activeTabRef = React.useRef('');
+
+  const [tabs, setTabs] = React.useState<Tab[]>([
     {tab: 'Tab 1', id: 'tab-1'},
     {tab: 'Tab 2', id: 'tab-2'},
     {tab: 'Tab 3', id: 'tab-3'},
+    {tab: 'Add New', id: 'add'},
   ]);
-  const addedRef = React.useRef(tabs.length);
+  const addedRef = React.useRef(tabs.length - 1);
   const model = useTabsModel({
-    shouldSelect: ({data}) => data.id !== 'last',
+    items: tabs,
+    shouldSelect: ({data}) => data.id !== 'add',
   });
+  activeTabRef.current = model.state.selectedIds[0];
 
   /**
    * Helper function that should be called when an item is programmatically removed. The following
@@ -22,11 +34,11 @@ export const DynamicTabs = () => {
    * * **Item is selected**: Selection will be moved to the next item in the list
    * @param id The id of the item that will be removed
    */
-  const removeItem = (id: string, model: SelectionModel) => {
+  const removeItem = <T extends unknown>(id: string, model: SelectionModel<T>) => {
     const index = model.state.items.findIndex(item => model.getId(item) === model.state.cursorId);
     const nextIndex = index === model.state.items.length - 1 ? index - 1 : index + 1;
     const nextId = model.getId(model.state.items[nextIndex]);
-    if (model.state.selectedIds[0] === id) {
+    if (activeTabRef.current === id) {
       // We're removing the currently selected item. Select next item
       model.events.select({id: nextId});
     }
@@ -41,48 +53,51 @@ export const DynamicTabs = () => {
     }
   };
 
-  const onKeyDown = (e: React.KeyboardEvent<HTMLElement>, id: string) => {
-    if (e.key === 'Delete') {
+  const onKeyDown = (id: string) => (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.key === 'Backspace' && id !== 'add') {
       setTabs(tabs.filter(item => item.id !== id));
       removeItem(id, model);
     }
   };
 
+  const onClick = (id: string) => (e: React.MouseEvent) => {
+    if (id === 'add') {
+      addedRef.current += 1;
+      setTabs(tabs => {
+        const newTabs = tabs.slice(0, tabs.length - 1);
+        const addTab = tabs.slice(-1);
+        return newTabs.concat(
+          {tab: `Tab ${addedRef.current}`, id: `tab-${addedRef.current}`},
+          addTab
+        );
+      });
+      model.events.goTo({id: 'add'});
+    }
+  };
+
   return (
     <Tabs model={model}>
-      <Tabs.List>
-        {tabs.map((item, index) => (
-          <Tabs.Item
-            key={item.id}
-            name={item.id}
-            index={index}
-            onKeyDown={e => onKeyDown(e, item.id)}
-          >
+      <Tabs.List overflowButton={<Tabs.OverflowButton>More</Tabs.OverflowButton>}>
+        {(item: Tab) => (
+          <Tabs.Item name={item.id} onKeyDown={onKeyDown(item.id)} onClick={onClick(item.id)}>
             {item.tab}
           </Tabs.Item>
-        ))}
-        <Tabs.Item
-          key={'last'}
-          index={tabs.length}
-          name={'last'}
-          onClick={() => {
-            addedRef.current += 1;
-            setTabs(tabs =>
-              tabs.concat({tab: `Tab ${addedRef.current}`, id: `tab-${addedRef.current}`})
-            );
-            model.events.goTo({id: 'last'});
-          }}
-        >
-          Add Tab
-        </Tabs.Item>
+        )}
       </Tabs.List>
-      <div style={{marginTop: space.m}}>
-        {tabs.map((item, index) => (
-          <Tabs.Panel key={item.id} name={item.id}>
+      <Tabs.Menu.Popper>
+        <Tabs.Menu.Card maxWidth={300} maxHeight={200}>
+          <Tabs.Menu.List>
+            {(item: Tab) => <Tabs.Menu.Item name={item.id}>{item.tab}</Tabs.Menu.Item>}
+          </Tabs.Menu.List>
+        </Tabs.Menu.Card>
+      </Tabs.Menu.Popper>
+      <Tabs.Panels>
+        {(item: Tab) => (
+          <Tabs.Panel marginTop="m" name={item.id}>
             Contents of {item.tab}
           </Tabs.Panel>
-        ))}
-      </div>
+        )}
+      </Tabs.Panels>
     </Tabs>
   );
 };
