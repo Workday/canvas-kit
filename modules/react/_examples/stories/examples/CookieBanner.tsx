8a7f3dce588d0aa3a3cabb0ccb484501bfe77596
Raisa Primerova <riyuhide@gmail.com>
2021-12-21 14:43:37 -0700

    docs: Update CookieBanner Example

--- modules/react/_examples/stories/examples/CookieBanner.tsx
+++ modules/react/_examples/stories/examples/CookieBanner.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import {createComponent, styled, StyledType} from '@workday/canvas-kit-react/common';
 import {colors, commonColors, depth, type, space} from '@workday/canvas-kit-react/tokens';
-import {Hyperlink, HyperlinkProps, PrimaryButton} from '@workday/canvas-kit-react/button';
+import {PrimaryButton, TertiaryButton} from '@workday/canvas-kit-react/button';
 
 const CookieBannerItem = createComponent('div')({
   displayName: 'CookieBanner.Item',
@@ -11,32 +11,24 @@ const CookieBannerItem = createComponent('div')({
   ),
 });
 
-const CookieBannerSettings = createComponent('a')({
-  displayName: 'CookieBanner.Settings',
-  Component: (props: CookieSettingsProps, ref, Element) => (
-    <CookieSettings ref={ref} as={Element} {...props} />
-  ),
-});
-
 const CookieBanner = createComponent('div')({
   displayName: 'CookieBanner',
   Component: (props: CookieBannerProps, ref, Element) => (
     <Banner ref={ref} as={Element} {...props} />
   ),
-  subComponents: {Item: CookieBannerItem, Settings: CookieBannerSettings},
+  subComponents: {Item: CookieBannerItem},
 });
 
 export const Basic = () => {
   const DefaultNotice = `We use cookies to ensure that we give you the best experience on our website. 
     If you continue without changing your settings, we'll assume that you are willing to receive cookies.`;
-  const onClickSettings = e => e.preventDefault();
 
   return (
     <ExampleContainer>
       <CookieBanner isClosed={false}>
         <CookieBanner.Item>{DefaultNotice}</CookieBanner.Item>
         <CookieBanner.Item isRow>
-          <CookieBanner.Settings onClick={onClickSettings}>Settings</CookieBanner.Settings>
+          <TertiaryButton>Settings</TertiaryButton>
           <PrimaryButton>Continue</PrimaryButton>
         </CookieBanner.Item>
       </CookieBanner>
@@ -51,8 +43,6 @@ interface CookieBannerProps extends StyledType {
 interface ItemProps extends StyledType {
   isRow?: boolean;
 }
-
-interface CookieSettingsProps extends HyperlinkProps, StyledType {}
 
 const ExampleContainer = styled('div')({
   minHeight: 84,
@@ -83,8 +73,7 @@ const Banner = styled('div')(
       padding: `${space.s} 0`,
     },
   },
-  ({isClosed}: Pick<CookieBannerProps, 'isClosed'>) =>
-    isClosed ? {transform: 'translateY(100%)'} : null
+  ({isClosed}: Pick<CookieBannerProps, 'isClosed'>) => isClosed && {transform: 'translateY(100%)'}
 );
 
 const BannerItem = styled('div')(
@@ -94,20 +83,17 @@ const BannerItem = styled('div')(
     '@media (max-width: 450px)': {
       '&:not(:first-of-type)': {
         marginTop: space.s,
+        '> *': {
+          flex: 1,
+        },
       },
     },
   },
-  ({isRow}: Pick<ItemProps, 'isRow'>) => (isRow ? {display: 'flex'} : null)
+  ({isRow}: Pick<ItemProps, 'isRow'>) =>
+    isRow && {
+      display: 'flex',
+      '> *': {
+        marginLeft: space.s,
+      },
+    }
 );
-
-const CookieSettings = styled(Hyperlink)({
-  marginRight: space.s,
-  border: 0,
-  fontWeight: 500,
-  whiteSpace: 'nowrap',
-  cursor: 'pointer',
-  padding: 0,
-  height: '0%',
-  alignSelf: 'center',
-  backgroundColor: 'transparent',
-});
