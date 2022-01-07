03cec819d2142110c470852523bd7a940a5f7c54
sheenasi <64246446+sheenasi@users.noreply.github.com>
2022-01-07 14:52:26 -0800

    fix(search-form): Remove default autocomplete (#1407)
    Removed browsers' default autocomplete as it is overlapping with the search form's autocomplete results.
    
    [category:Components]

--- modules/labs-react/search-form/lib/SearchForm.tsx
+++ modules/labs-react/search-form/lib/SearchForm.tsx
@@ -472,6 +472,7 @@ export class SearchForm extends React.Component<SearchFormProps, SearchFormState
                 inputColors={this.getThemeColors()}
                 height={height}
                 name="search"
+                autoComplete="off"
               />
             </SearchCombobox>
           </SearchField>
