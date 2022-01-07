3655f6a8559b62639a885b0c35d9dec4d9850545
Nicholas Boll <nicholas.boll@gmail.com>
2022-01-07 14:57:00 -0700

    Remove redundant types

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -1,14 +1,13 @@
 import Adapter from 'enzyme-adapter-react-16';
 import {configure} from 'enzyme';
 import {toHaveNoViolations} from 'jest-axe';
-import serializer, {matchers} from 'jest-emotion';
+import {matchers} from 'jest-emotion';
 import '@testing-library/jest-dom/extend-expect';
 import {verifyComponent} from './verifyComponent';
 import {jest} from '@jest/globals';
 
 import {setUniqueSeed, resetUniqueIdCount} from '@workday/canvas-kit-react/common';
 
-expect.addSnapshotSerializer(serializer);
 expect.extend(toHaveNoViolations);
 expect.extend(matchers);
 configure({adapter: new Adapter()});
