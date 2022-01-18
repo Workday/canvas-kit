bb4c2fc1f4d320095fdce42f9752bc555564bb7e
manuel carrera <manuel.carrera@workday.com>
2022-01-18 10:12:02 -0700

    fix: Add pragma back to css file

--- modules/css/tooltip/stories.tsx
+++ modules/css/tooltip/stories.tsx
@@ -1,24 +1,24 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core';
 import {storiesOf} from '@storybook/react';
 import {SystemIcon} from '@workday/canvas-kit-react/icon';
 import {xIcon} from '@workday/canvas-system-icons-web';
 import './index.scss';
 import './../button/index.scss';
-import {styled} from '@workday/canvas-kit-react/common';
-import {colors, type} from '@workday/canvas-kit-react/tokens';
 
-const StyledHeader = styled('h3')({
-  ...type.levels.body.large,
-  color: colors.licorice500,
-  fontWeight: type.properties.fontWeights.bold,
-  marginBottom: '18px;',
+const containerStyle = css({
+  h3: {
+    marginBottom: '18px;',
+  },
 });
 
 storiesOf('Components/Popups/Tooltip/CSS', module)
   .addParameters({ReadmePath: 'css/tooltip'})
   .add('Default', () => (
     <div className="story">
-      <div>
-        <StyledHeader>Hover over the icon.</StyledHeader>
+      <div css={containerStyle}>
+        <h3>Hover over the icon.</h3>
         <div className="wdc-tooltip-container">
           <button className="wdc-btn-icon-circle" aria-describedby="tooltip">
             <SystemIcon icon={xIcon} />
