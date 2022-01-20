dc43874f49c299f91a0a364ebf1c090857c07734
manuel carrera <manuel.carrera@workday.com>
2022-01-20 12:25:36 -0700

    fix: Remove test or classnames

--- modules/react/icon/spec/Svg.spec.tsx
+++ modules/react/icon/spec/Svg.spec.tsx
@@ -3,6 +3,7 @@ import {render} from '@testing-library/react';
 import Svg from '../lib/Svg';
 import {CanvasIconTypes} from '@workday/design-assets-types';
 import {shieldIcon} from '@workday/canvas-accent-icons-web';
+import {mergeProps} from '../../common';
 
 describe('Icon component', () => {
   test('Mismatched icon types catches error and returns null', () => {
@@ -17,23 +18,5 @@ describe('Icon component', () => {
 
     // container is not a semantic element
     expect(container.firstChild).toContainHTML('<svg');
-  });
-
-  test('Custom className overrides base iconStyle class', () => {
-    const customClassName = 'custom-class-name';
-
-    const {container} = render(
-      <Svg src={shieldIcon} className={customClassName} type={CanvasIconTypes.Accent} styles={{}} />
-    );
-
-    expect(container.firstChild).toHaveClass(customClassName);
-
-    // The user defined class (customClassName), should always be last
-    expect(
-      container
-        .querySelector('span')!
-        .className.split(' ')
-        .reverse()[0]
-    ).toBe(customClassName);
   });
 });
