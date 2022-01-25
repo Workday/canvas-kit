bc489b63d0778e2855a8934ea311a7a589c0b252
Matthew Rust <matthew.rust@workday.com>
2022-01-25 15:49:51 -0800

    feat(button): Use HStack at the top level

--- modules/react/button/lib/HyperlinkExternal.tsx
+++ modules/react/button/lib/HyperlinkExternal.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import {colors} from '@workday/canvas-kit-react/tokens';
-import {styled, createComponent, StyledType} from '@workday/canvas-kit-react/common';
+import {
+  styled,
+  createComponent,
+  StyledType,
+  ElementComponent,
+} from '@workday/canvas-kit-react/common';
 import {HStack} from '@workday/canvas-kit-labs-react/layout';
 import {extLinkIcon} from '@workday/canvas-system-icons-web';
 import {SystemIcon, systemIconStyles} from '@workday/canvas-kit-react/icon';
@@ -10,7 +15,7 @@ export interface HyperlinkExternalProps extends HyperlinkProps {
   iconLabel?: string;
 }
 
-const Anchor = styled(Hyperlink)<HyperlinkExternalProps & StyledType>({
+const Anchor = (styled(Hyperlink)<HyperlinkExternalProps & StyledType>({
   '&:not(:hover)': {
     ...systemIconStyles({fill: colors.blueberry400, fillHover: colors.blueberry400}),
   },
@@ -20,20 +25,26 @@ const Anchor = styled(Hyperlink)<HyperlinkExternalProps & StyledType>({
   '&:active': {
     ...systemIconStyles({fill: colors.blueberry600, fillHover: colors.blueberry600}),
   },
-});
+}) as any) as ElementComponent<'a', HyperlinkExternalProps>;
 
 export const HyperlinkExternal = createComponent('a')({
   displayName: 'HyperlinkExternalProps',
   Component: (
     {children, iconLabel = 'Opens link in new window', ...elemProps}: HyperlinkExternalProps,
-    ref,
-    Element
+    ref
   ) => (
-    <Anchor as={Element} ref={ref} target="_blank" rel="noreferrer" {...elemProps}>
-      <HStack spacing="xxxs" display="inline-flex" alignItems="center">
-        <span>{children}</span>
-        <SystemIcon icon={extLinkIcon} role="img" aria-label={iconLabel} size="1em" />
-      </HStack>
-    </Anchor>
+    <HStack
+      as={Anchor}
+      ref={ref}
+      target="_blank"
+      rel="noreferrer"
+      spacing="xxxs"
+      display="inline-flex"
+      alignItems="center"
+      {...elemProps}
+    >
+      <span>{children}</span>
+      <SystemIcon icon={extLinkIcon} role="img" aria-label={iconLabel} size="1em" />
+    </HStack>
   ),
 });
