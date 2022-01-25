dc88d783360bacf63d0a34a71110495913078214
Matthew Rust <matthew.rust@workday.com>
2022-01-25 15:29:41 -0800

    feat(button): Add href storybook example

--- modules/react/button/stories/button/examples/HyperlinkExternal.tsx
+++ modules/react/button/stories/button/examples/HyperlinkExternal.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 
 import {HyperlinkExternal} from '@workday/canvas-kit-react/button';
 
-export const LinkExternal = () => <HyperlinkExternal>External Hyperlink</HyperlinkExternal>;
+export const LinkExternal = () => (
+  <HyperlinkExternal href="https://workday.com">External Hyperlink</HyperlinkExternal>
+);
