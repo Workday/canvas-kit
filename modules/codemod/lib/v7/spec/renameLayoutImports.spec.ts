98b8d29633897e42b20f90bf52f0de661300d671
manuel carrera <manuel.carrera@workday.com>
2022-02-11 12:21:56 -0700

    fix: Add codemod to work through imports

--- modules/codemod/lib/v7/spec/renameLayoutImports.spec.ts
+++ modules/codemod/lib/v7/spec/renameLayoutImports.spec.ts
@@ -5,17 +5,76 @@ import {stripIndent} from 'common-tags';
 const expectTransform = expectTransformFactory(transform);
 
 describe('Rename Box, Flex and Stack', () => {
-  it.only('should do properly transform labs import for Flex to react layout for Flex', () => {
-    const input = "import { Flex } from '@workday/canvas-kit-labs-react/layout'";
+  it('should  properly transform labs import for Flex to react layout', () => {
+    const input = stripIndent`
+      import { Flex } from "@workday/canvas-kit-labs-react/layout";
+    `;
 
-    const expected = `import { Flex } from "@workday/canvas-kit-react/layout"`;
+    const expected = stripIndent`
+      import { Flex } from "@workday/canvas-kit-react/layout";
+    `;
 
     expectTransform(input, expected);
   });
-  it.only('should do properly transform multiple imports from labs layout to react layout', () => {
-    const input = "import { Flex, Stack } from '@workday/canvas-kit-labs-react/layout'";
 
-    const expected = `import { Flex, Stack } from "@workday/canvas-kit-react/layout"`;
+  it('should properly transform multiple imports from labs layout to react layout', () => {
+    const input = stripIndent`
+      import { Flex, Stack, HStack } from "@workday/canvas-kit-labs-react/layout";
+    `;
+
+    const expected = stripIndent`
+      import { Flex, Stack, HStack } from "@workday/canvas-kit-react/layout";
+    `;
+
+    expectTransform(input, expected); //
+  });
+
+  it('filter out non Box imports and rename package', () => {
+    const input = stripIndent`
+      import { Box, ComponentStatesTable, permutateProps, useThemeRTL, useThemedRing, ColorStyleProps } from "@workday/canvas-kit-labs-react/common";
+    `;
+    const expected = stripIndent`
+      import { Box, ColorStyleProps } from "@workday/canvas-kit-react/layout";
+      import { ComponentStatesTable, permutateProps, useThemeRTL, useThemedRing } from "@workday/canvas-kit-labs-react/common";
+    `;
+
+    expectTransform(input, expected);
+  });
+
+  it('rename import for box from common to layout', () => {
+    const input = stripIndent`
+      import { Box } from "@workday/canvas-kit-labs-react/common";
+    `;
+
+    const expected = stripIndent`
+    import { Box } from "@workday/canvas-kit-react/layout";
+    `;
+
+    expectTransform(input, expected);
+  });
+
+  it('should add new import', () => {
+    const input = stripIndent`
+      import { Box, useThemeRTL } from "@workday/canvas-kit-labs-react/common";
+    `;
+
+    const expected = stripIndent`
+      import { Box } from "@workday/canvas-kit-react/layout";
+      import { useThemeRTL } from "@workday/canvas-kit-labs-react/common";
+    `;
+
+    expectTransform(input, expected);
+  });
+
+  it('rename import for box from common to layout', () => {
+    const input = stripIndent`
+      import { Box } from "@workday/canvas-kit-labs-react/common";
+      import { Layout } from "@workday/canvas-kit-react/layout";
+    `;
+
+    const expected = stripIndent`
+      import { Layout, Box } from "@workday/canvas-kit-react/layout";
+    `;
 
     expectTransform(input, expected);
   });
