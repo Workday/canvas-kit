d29e2bd310bdd0b679316410b57d46158e3c3345
Bill Geoghegan <billgeoghegan92@gmail.com>
2022-03-30 22:18:57 +0100

    fix(avatar): Resetting imageLoaded when new url is provided (#1521)
    Resolves: resets the imageLoaded to false when a new image url is provided within the Avatar component. 
    
    Motivation: This would allow us to swap back to the default image when a url value is no longer passed.
    
    [category:Components]

--- modules/react/avatar/lib/Avatar.tsx
+++ modules/react/avatar/lib/Avatar.tsx
@@ -139,6 +139,10 @@ const Avatar: AvatarOverload = React.forwardRef(
       }
     };
 
+    React.useLayoutEffect(() => {
+      setImageLoaded(false);
+    }, [url]);
+
     const background = variant === AvatarVariant.Dark ? colors.blueberry400 : colors.soap300;
 
     return (
