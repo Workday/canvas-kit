78f19b1d6e361ee4b2b1f3ccf6d618b4d1292a68
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2022-05-16 12:31:25 -0600

    fix(action-bar): Fix ActionBar tests

--- modules/react/action-bar/spec/ActionBar.spec.tsx
+++ modules/react/action-bar/spec/ActionBar.spec.tsx
@@ -1,6 +1,6 @@
 import {ActionBar} from '../lib/ActionBar';
 import {useActionBarModel} from '../lib/useActionBarModel';
 
-describe('Tab', () => {
-  verifyComponent(ActionBar.Item, {modelFn: useActionBarModel});
+describe('ActionBar', () => {
+  verifyComponent(ActionBar, {modelFn: useActionBarModel});
 });
