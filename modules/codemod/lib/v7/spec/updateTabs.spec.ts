9e6def82f79ee72a23c5013cac747b05105923ff
Nicholas Boll <nicholas.boll@gmail.com>
2022-05-16 10:40:19 -0600

    Update modules/codemod/lib/v7/spec/updateTabs.spec.ts
    Co-authored-by: James Fan <james@somewhatstrange.com>

--- modules/codemod/lib/v7/spec/updateTabs.spec.ts
+++ modules/codemod/lib/v7/spec/updateTabs.spec.ts
@@ -5,7 +5,7 @@ import {stripIndent} from 'common-tags';
 const expectTransform = expectTransformFactory(transform);
 
 describe('updateModelSignatures', () => {
-  it('should replace the "name" attribute in Tabs.Items and Tabs.Panel with "data-id"', () => {
+  it('should replace the "name" attribute in Tabs.Items with "data-id"', () => {
     const input = stripIndent`
       import {Tabs} from '@workday/canvas-kit-react/tabs'
 
