53117d099166fe3574d91dedb961afba600a4bf9
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2022-05-16 13:46:51 -0600

    Update ActionBar.spec.tsx

--- modules/react/action-bar/spec/ActionBar.spec.tsx
+++ modules/react/action-bar/spec/ActionBar.spec.tsx
@@ -2,5 +2,5 @@ import {ActionBar} from '../lib/ActionBar';
 import {useActionBarModel} from '../lib/useActionBarModel';
 
 describe('ActionBar', () => {
-  verifyComponent(ActionBar, {modelFn: useActionBarModel});
+  verifyComponent(ActionBar.List, {modelFn: useActionBarModel});
 });
