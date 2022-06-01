609bc597d83c3708bed4bc524e48bf4fb9b9071e
Emily Roller <emroller16@gmail.com>
2022-06-01 15:40:05 -0600

    chore: Update storybook examples for modal

--- modules/react/modal/stories/examples/Basic.tsx
+++ modules/react/modal/stories/examples/Basic.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 
 import {Modal} from '@workday/canvas-kit-react/modal';
-import {DeleteButton} from '@workday/canvas-kit-react/button';
+import {PrimaryButton} from '@workday/canvas-kit-react/button';
 import {HStack, Box} from '@workday/canvas-kit-react/layout';
 
 export const Basic = () => {
-  const handleDelete = () => {
-    console.log('Deleted item');
+  const handleAllow = () => {
+    console.log('Allow notifications');
   };
 
   return (
     <Modal>
-      <Modal.Target as={DeleteButton}>Delete Item</Modal.Target>
+      <Modal.Target as={PrimaryButton}>Edit Notification Settings</Modal.Target>
       <Modal.Overlay>
         <Modal.Card>
           <Modal.CloseIcon aria-label="Close" />
-          <Modal.Heading>Delete Item</Modal.Heading>
+          <Modal.Heading>Allow Notifications?</Modal.Heading>
           <Modal.Body>
             <Box as="p" marginTop={0} marginBottom="m">
-              Are you sure you want to delete the item?
+              Notifications may include alerts, sounds, and icon badges.
             </Box>
             <HStack spacing="s">
-              <Modal.CloseButton as={DeleteButton} onClick={handleDelete}>
-                Delete
+              <Modal.CloseButton as={PrimaryButton} onClick={handleAllow}>
+                Allow
               </Modal.CloseButton>
-              <Modal.CloseButton>Cancel</Modal.CloseButton>
+              <Modal.CloseButton>Don't Allow</Modal.CloseButton>
             </HStack>
           </Modal.Body>
         </Modal.Card>
--- modules/react/modal/stories/examples/BodyOverflow.tsx
+++ modules/react/modal/stories/examples/BodyOverflow.tsx
@@ -1,51 +1,51 @@
 import React from 'react';
 
 import {Modal} from '@workday/canvas-kit-react/modal';
-import {DeleteButton} from '@workday/canvas-kit-react/button';
+import {PrimaryButton} from '@workday/canvas-kit-react/button';
 import {HStack, VStack, Stack, Box} from '@workday/canvas-kit-react/layout';
 import {space} from '@workday/canvas-kit-react/tokens';
 
 export const BodyOverflow = () => {
-  const handleDelete = () => {
-    console.log('Deleted item');
+  const handleAllow = () => {
+    console.log('Allow notifications');
   };
 
   return (
     <Modal>
-      <Modal.Target as={DeleteButton}>Delete Item</Modal.Target>
+      <Modal.Target as={PrimaryButton}>Edit Notification Settings</Modal.Target>
       <Modal.Overlay>
         <Modal.Card>
           <Modal.CloseIcon aria-label="Close" />
-          <Modal.Heading>Delete Item</Modal.Heading>
+          <Modal.Heading>Allow Notifications?</Modal.Heading>
           <Modal.Body>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
-            <p>Are you sure you want to delete the item?</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
+            <p>Notifications may include alerts, sounds, and icon badges.</p>
           </Modal.Body>
           <HStack spacing="s" paddingTop="s">
-            <Modal.CloseButton as={DeleteButton} onClick={handleDelete}>
-              Delete
+            <Modal.CloseButton as={PrimaryButton} onClick={handleAllow}>
+              Allow
             </Modal.CloseButton>
             <Modal.CloseButton>Cancel</Modal.CloseButton>
           </HStack>
