06aa3670d685a79e524785c19997dcb4ced5b7ec
Nicholas Boll <nicholas.boll@gmail.com>
2022-07-29 17:25:50 -0600

    fix: Update `useReturnFocus` to change focus if target is visible
    Fixes #1436
    
    `useReturnFocus` will now check if the target is renderd onscreen and if the user clicked on a focusable element. It will not return focus to the target in these cases.

--- modules/react/popup/lib/hooks/useReturnFocus.tsx
+++ modules/react/popup/lib/hooks/useReturnFocus.tsx
@@ -4,6 +4,19 @@ import {changeFocus, createElemPropsHook} from '@workday/canvas-kit-react/common
 
 import {usePopupModel} from './usePopupModel';
 
+function getScrollParent(element: HTMLElement): HTMLElement {
+  if (element === document.body || !element.parentElement) {
+    return element;
+  }
+  const styles = getComputedStyle(element);
+
+  if (styles.overflowY === 'scroll' || styles.overflowY === 'auto') {
+    return element;
+  }
+
+  return getScrollParent(element.parentElement);
+}
+
 /**
  * Returns focus to the target element when the popup is hidden. This works well with
  * `useInitialFocus`. This should be used with `useFocusRedirect` or `useFocusTrap` for a complete
@@ -14,13 +27,25 @@ import {usePopupModel} from './usePopupModel';
 export const useReturnFocus = createElemPropsHook(usePopupModel)(model => {
   const visible = model.state.visibility !== 'hidden';
 
-  React.useLayoutEffect(() => {
+  React.useEffect(() => {
     if (!visible) {
       return;
     }
     // capture the element here. The refs will be null by the time the cleanup function is called
-    const element = (model.state.returnFocusRef || model.state.targetRef).current;
+    const element = (model.state.returnFocusRef || model.state.targetRef).current as HTMLElement;
     return () => {
+      const scrollParent = getScrollParent(element);
+      const scrollParentRect = scrollParent.getBoundingClientRect();
+      const elementRect = element.getBoundingClientRect();
+
+      // Don't change focus if user focused on a different element like a `input` or the target element isn't on the screen
+      if (
+        document.activeElement !== document.body ||
+        elementRect.top > scrollParentRect.bottom ||
+        elementRect.bottom < scrollParentRect.top
+      ) {
+        return;
+      }
       changeFocus(element);
     };
   }, [model.state.returnFocusRef, model.state.targetRef, visible]);
