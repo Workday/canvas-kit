dbb15ef6bb0b519ad770bae179d7385ea8ae85f1
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2022-08-01 17:10:09 -0600

    Update modules/preview-react/side-panel/stories/examples/RightOrigin.tsx
    Co-authored-by: Alan B Smith <a.bax.smith@gmail.com>

--- modules/preview-react/side-panel/stories/examples/RightOrigin.tsx
+++ modules/preview-react/side-panel/stories/examples/RightOrigin.tsx
@@ -26,9 +26,9 @@ const RightPanel = () => {
       <SidePanel.ToggleButton {...controlProps} />
       {panelState === 'expanded' && (
         <Flex alignItems="center" justifyContent="flex-end" paddingY="s" paddingX="s">
-          <Heading as="h3" size="large" color="licorice500" fontWeight="bold" {...labelProps}>
+          <Text as="h3" tokenLevel="body.large" color="licorice500" fontWeight="bold" {...labelProps}>
             Tasks Panel
-          </Heading>
+          </Text>
         </Flex>
       )}
     </StyledSidePanel>
