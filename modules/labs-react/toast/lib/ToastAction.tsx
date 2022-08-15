0c6d6731099d25dc8c4d1769798c958ed53baac7
manuel.carrera <manuel.carrera@workday.com>
2022-08-15 15:05:40 -0600

    fix: Update import for emotion

--- modules/labs-react/toast/lib/ToastAction.tsx
+++ modules/labs-react/toast/lib/ToastAction.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core';
+import {jsx} from '@emotion/react';
 import React from 'react';
 
 import {createComponent} from '@workday/canvas-kit-react/common';
