05744a3ca1007881fe1db1da550110404f613f1c
manuel.carrera <manuel.carrera@workday.com>
2022-08-15 09:46:30 -0600

    fix: Add back in spacing

--- modules/react/modal/lib/ModalFooter.tsx
+++ modules/react/modal/lib/ModalFooter.tsx
@@ -4,7 +4,6 @@ import {createSubcomponent, ExtractProps} from '@workday/canvas-kit-react/common
 
 import {HStack} from '@workday/canvas-kit-react/layout';
 import {useModalModel} from './hooks';
-import {space} from '@workday/canvas-kit-react/tokens';
 
 export interface ModalFooterProps<T = unknown>
   extends Partial<ExtractProps<typeof HStack, never>> {}
@@ -13,5 +12,5 @@ export const ModalFooter = createSubcomponent('div')({
   displayName: 'Modal.Footer',
   modelHook: useModalModel,
 })<ModalFooterProps>(({...elemProps}) => {
-  return <HStack paddingX="l" paddingBottom="l" paddingTop="s" {...elemProps} />;
+  return <HStack paddingX="l" paddingBottom="l" paddingTop="s" spacing="s" {...elemProps} />;
 });
