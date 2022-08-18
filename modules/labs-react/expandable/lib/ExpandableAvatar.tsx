e4bdf29a4e9c076348f886d372d0159f618e785b
manuel.carrera <manuel.carrera@workday.com>
2022-08-18 13:44:03 -0600

    fix: Remove lat text from Avatar and make it a div

--- modules/labs-react/expandable/lib/ExpandableAvatar.tsx
+++ modules/labs-react/expandable/lib/ExpandableAvatar.tsx
@@ -19,9 +19,12 @@ const StyledAvatar = styled(Avatar)<StyledType>({
   flexShrink: 0,
 });
 
+// When the component is created, it needs to be a button element to match AvatarProps.
+// Once Avatar becomes a `createComponent` we can default the element type to a `div`
+// and the types should be properly extracted
 export const ExpandableAvatar = createComponent('button')({
   displayName: 'Expandable.Avatar',
-  Component: ({...elemProps}: ExpandableAvatarProps, ref, Element) => {
-    return <StyledAvatar altText={undefined} as={Element} ref={ref} size={32} {...elemProps} />;
+  Component: ({altText, ...elemProps}: ExpandableAvatarProps, ref, Element) => {
+    return <StyledAvatar altText={undefined} as={'div'} ref={ref} size={32} {...elemProps} />;
   },
 });
