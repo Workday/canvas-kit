02d633a12ae4f46c38b0bbac10960b962ce09408
manuel.carrera <manuel.carrera@workday.com>
2022-08-22 14:07:11 -0600

    fix: Clean up working tree

--- modules/labs-react/toast/stories/stories_VisualTesting.tsx
+++ modules/labs-react/toast/stories/stories_VisualTesting.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import {checkIcon} from '@workday/canvas-system-icons-web';
+import {colors} from '@workday/canvas-kit-react/tokens';
+
 import {StaticStates} from '@workday/canvas-kit-react/common';
 import {ComponentStatesTable} from '@workday/canvas-kit-labs-react/common';
 import {Toast} from '@workday/canvas-kit-labs-react/toast';
@@ -7,18 +10,131 @@ import {Toast} from '@workday/canvas-kit-labs-react/toast';
 import {withSnapshotsEnabled} from '../../../../utils/storybook';
 
 export default withSnapshotsEnabled({
-  title: 'Testing/React/Labs/Toast',
+  title: 'Labs/Toast/React',
   component: Toast,
 });
 
-export const ToastStates = () => {
+export const ToastStates = withSnapshotsEnabled(() => {
   return (
     <StaticStates>
-      <ComponentStatesTable rowProps={[{label: 'Default', props: {}}]} columnProps={[]}>
-        {props => {
-          return <Toast>Content</Toast>;
-        }}
+      <ComponentStatesTable
+        rowProps={[
+          {
+            label: 'Default',
+            props: {
+              mode: 'noninteractive',
+            },
+          },
+          {
+            label: 'With Icon',
+            props: {
+              mode: 'noninteractive',
+              icon: checkIcon,
+              iconColor: colors.greenApple400,
+            },
+          },
+          {
+            label: 'On Close',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+            },
+          },
+          {
+            label: 'With Action',
+            props: {
+              mode: 'interactive',
+              hasAction: true,
+            },
+          },
+          {
+            label: 'With small padding',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              padding: 's',
+            },
+          },
+          {
+            label: 'With no depth',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 'none',
+            },
+          },
+          {
+            label: 'With depth value set to 1',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 1,
+            },
+          },
+          {
+            label: 'With depth value set to 2',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 2,
+            },
+          },
+          {
+            label: 'With depth value set to 3',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 3,
+            },
+          },
+          {
+            label: 'With depth value set to 4',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 4,
+            },
+          },
+          {
+            label: 'With depth value set to 5',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 5,
+            },
+          },
+          {
+            label: 'With depth value set to 6',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              depth: 6,
+            },
+          },
+          {
+            label: 'With custom width',
+            props: {
+              mode: 'noninteractive',
+              hasCloseIcon: true,
+              width: 300,
+            },
+          },
+        ]}
+        columnProps={[{label: 'Default', props: {}}]}
+      >
+        {({mode, hasCloseIcon, hasAction, icon, iconColor, ...props}) => (
+          <Toast mode={mode} {...props}>
+            <Toast.Body>
+              {icon ? <Toast.Icon icon={icon} color={iconColor} /> : null}
+              <Toast.Message>
+                Your workbook was successfully processed.
+                {hasAction ? <Toast.Action>Custom Action</Toast.Action> : null}
+              </Toast.Message>
+            </Toast.Body>
+            {hasCloseIcon ? <Toast.Close aria-label="Close" /> : null}
+          </Toast>
+        )}
       </ComponentStatesTable>
     </StaticStates>
   );
-};
+});
