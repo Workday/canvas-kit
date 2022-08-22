75e0a87031884503d4955b6384a4c888fcca2049
manuel.carrera <manuel.carrera@workday.com>
2022-08-22 11:36:37 -0600

    fix: Update avatar on expandable component

--- modules/labs-react/expandable/lib/ExpandableAvatar.tsx
+++ modules/labs-react/expandable/lib/ExpandableAvatar.tsx
@@ -1,7 +1,3 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import {jsx} from '@emotion/react';
-
 import React from 'react';
 
 import {createComponent, ExtractProps, styled, StyledType} from '@workday/canvas-kit-react/common';
@@ -19,9 +15,12 @@ const StyledAvatar = styled(Avatar)<StyledType>({
   flexShrink: 0,
 });
 
+// When the component is created, it needs to be a button element to match AvatarProps.
+// Once Avatar becomes a `createComponent` we can default the element type to a `div`
+// and the types should be properly extracted
 export const ExpandableAvatar = createComponent('button')({
   displayName: 'Expandable.Avatar',
-  Component: ({...elemProps}: ExpandableAvatarProps, ref, Element) => {
-    return <StyledAvatar altText={undefined} as={Element} ref={ref} size={32} {...elemProps} />;
+  Component: ({altText, ...elemProps}: ExpandableAvatarProps, ref) => {
+    return <StyledAvatar altText={undefined} as={'div'} ref={ref} size={32} {...elemProps} />;
   },
 });
