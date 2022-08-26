fd865b72d78791dd3ec6dd34fb10f25c215bcb80
James Fan <james@somewhatstrange.com>
2022-08-26 16:30:01 -0700

    docs(toast): Remove unnecessary import

--- modules/labs-react/toast/stories/examples/ToastDialog.tsx
+++ modules/labs-react/toast/stories/examples/ToastDialog.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {Toast} from '@workday/canvas-kit-labs-react/toast';
 import {checkIcon} from '@workday/canvas-system-icons-web';
 import {colors} from '@workday/canvas-kit-react/tokens';
-import {Box} from '@workday/canvas-kit-react';
 
 export const ToastDialog = () => {
   return (
