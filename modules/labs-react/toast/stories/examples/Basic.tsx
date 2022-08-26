d72864166d7c9e3d7e9cb7228e2200094915bc69
James Fan <james@somewhatstrange.com>
2022-08-26 15:49:54 -0700

    docs(toast): Update MDX docs

--- modules/labs-react/toast/stories/examples/Basic.tsx
+++ modules/labs-react/toast/stories/examples/Basic.tsx
@@ -4,7 +4,7 @@ import {Toast} from '@workday/canvas-kit-labs-react/toast';
 import {checkIcon} from '@workday/canvas-system-icons-web';
 import {colors} from '@workday/canvas-kit-react/tokens';
 
-export const ToastStatus = () => (
+export const Basic = () => (
   <Toast>
     <Toast.Icon icon={checkIcon} color={colors.greenApple400} />
     <Toast.Body>
