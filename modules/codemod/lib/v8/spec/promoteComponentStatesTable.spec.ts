67850f94a2bda4746ba8632d3d6d5f292dd14599
manuel.carrera <manuel.carrera@workday.com>
2022-09-26 14:46:09 -0600

    fix: Add test and docs

--- modules/codemod/lib/v8/spec/promoteComponentStatesTable.spec.ts
+++ modules/codemod/lib/v8/spec/promoteComponentStatesTable.spec.ts
@@ -7,11 +7,35 @@ const expectTransform = expectTransformFactory(transform);
 describe('renameIconRefs', () => {
   it('should replace default export with a named export for react package', () => {
     const input = stripIndent`
-      import { ComponentStatesTable, permutateProps, useTheme } from '@workday/canvas-kit-labs-react/common';
+      import { ComponentStatesTable } from '@workday/canvas-kit-labs-react/common';
     `;
 
     const expected = stripIndent`
-      import { ComponentStatesTable, permutateProps } from "@workday/canvas-kit-react/common";
+      import { ComponentStatesTable } from "@workday/canvas-kit-react/common";
+    `;
+
+    expectTransform(input, expected);
+  });
+  it('should rename imports from main labs package', () => {
+    const input = stripIndent`
+      import { ComponentStatesTable } from "@workday/canvas-kit-labs-react";
+    `;
+
+    const expected = stripIndent`
+      import { ComponentStatesTable } from "@workday/canvas-kit-react/common";
+    `;
+
+    expectTransform(input, expected);
+  });
+
+  it('should rename imports from main labs package and remove import', () => {
+    const input = stripIndent`
+      import { ComponentStatesTable, SearchForm } from "@workday/canvas-kit-labs-react";
+    `;
+
+    const expected = stripIndent`
+      import { ComponentStatesTable } from "@workday/canvas-kit-react/common";
+      import { SearchForm } from "@workday/canvas-kit-labs-react";
     `;
 
     expectTransform(input, expected);
