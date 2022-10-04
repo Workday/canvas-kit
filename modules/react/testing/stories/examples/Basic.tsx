41123a1c29047c8e24cd5b149d194e2f0f9a7713
manuel.carrera <manuel.carrera@workday.com>
2022-10-04 11:17:07 -0600

    fix: Add docs on testing

--- modules/react/testing/stories/examples/Basic.tsx
+++ modules/react/testing/stories/examples/Basic.tsx
@@ -1,13 +1,37 @@
-// import React from 'react';
+import React from 'react';
+import {
+  ComponentStatesTable,
+  StaticStates,
+  permutateProps,
+} from '@workday/canvas-kit-react/testing';
+import {PartialEmotionCanvasTheme} from '@workday/canvas-kit-react/common';
 
-// import {Testing} from '@workday/canvas-kit-react/testing';
-// import {SecondaryButton} from '@workday/canvas-kit-react/button';
+import {DeleteButton} from '@workday/canvas-kit-react/button';
 
-// export const Basic = () => {
-//   return (
-//     <Testing>
-//       <Testing.Target as={SecondaryButton}>Toggle</Testing.Target>
-//       <Testing.Content>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Testing.Content>
-//     </Testing>
-//   );
-// };
+export const stateTableColumnProps = [
+  {label: 'Default ', props: {className: '', disabled: false}},
+  {label: 'Default Disabled', props: {className: '', disabled: true}},
+  {label: 'Hover ', props: {className: 'hover', disabled: false}},
+  {label: 'Hover Disabled', props: {className: 'hover', disabled: true}},
+  {label: 'Focus ', props: {className: 'focus', disabled: false}},
+  {label: 'Focus Hover ', props: {className: 'focus hover', disabled: false}},
+  {label: 'Active ', props: {className: 'active', disabled: false}},
+  {label: 'Active Hover ', props: {className: 'active hover', disabled: false}},
+];
+
+export const Basic = (props: {theme?: PartialEmotionCanvasTheme}) => (
+  <StaticStates theme={props.theme}>
+    <ComponentStatesTable
+      rowProps={permutateProps({
+        size: [
+          {value: 'small', label: 'Small'},
+          {value: 'medium', label: 'Medium'},
+          {value: 'large', label: 'Large'},
+        ],
+      })}
+      columnProps={stateTableColumnProps}
+    >
+      {props => <DeleteButton {...props}>Test</DeleteButton>}
+    </ComponentStatesTable>
+  </StaticStates>
+);
