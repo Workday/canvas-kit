e820f3adf6fd563cebc522cc02488965bc76b668
James Fan <james@somewhatstrange.com>
2022-10-13 11:46:22 -0700

    docs: Separate Text MDX into separate MDXes (#1843)
    Breaks the Text MDX into separate MDXes for each Text component. This will allow us to consume the docs properly on the Canvas Site.
    
    [category:Documentation]

--- modules/react/text/stories/examples/Text/Basic.tsx
+++ modules/react/text/stories/examples/Text/Basic.tsx
@@ -8,16 +8,13 @@ export const Basic = () => (
   <Box>
     <Text as="h4">Text as h4</Text>
     <Text as="p" fontSize={14} fontWeight="regular" fontFamily="monospace">
-      Text with props passed
+      Text styled using style props
     </Text>
     <Text as="p" {...type.levels.subtext.large}>
-      Text with type token passed
+      Text styled using type token level
     </Text>
-    <Box {...type.levels.subtext.large} color={typeColors.hint} marginBottom="s">
-      <Text>Text with inherenced styles</Text>
+    <Box {...type.levels.subtext.large} color={typeColors.hint}>
+      <Text>Text with inherited styles</Text>
     </Box>
-    <Text as="p" typeLevel="body.small">
-      Small Body level text
-    </Text>
   </Box>
 );
---
+++ modules/react/text/stories/examples/Title/Basic.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Title} from '@workday/canvas-kit-react/text';
+
+export const Basic = () => (
+  <>
+    <Title as="h1" size="large">
+      Large Title Text
+    </Title>
+    <Title as="h2" size="medium">
+      Medium Title Text
+    </Title>
+    <Title as="h3" size="small">
+      Small Title Text
+    </Title>
+  </>
+);
