32be4da6fd54d71eede8c5eb94d668f0f80e71c7
manuel.carrera <manuel.carrera@workday.com>
2022-10-18 15:33:27 -0600

    fix: Update upgrade guide

--- modules/preview-react/status-indicator/stories/examples/Emphasis.tsx
+++ modules/preview-react/status-indicator/stories/examples/Emphasis.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {StatusIndicator} from '@workday/canvas-kit-preview-react/status-indicator';
 import {uploadCloudIcon} from '@workday/canvas-system-icons-web';
-import {HStack, VStack} from '@workday/canvas-kit-react/layout';
+import {HStack} from '@workday/canvas-kit-react/layout';
 
 export const Emphasis = () => {
   return (
