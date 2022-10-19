787888e1da784d6f0a90735d3f0a568ee91a0d13
Nicholas Boll <nicholas.boll@gmail.com>
2022-10-19 13:30:52 -0600

    Add documentation and tests

--- modules/react/popup/lib/hooks/useCloseOnTargetHidden.ts
+++ modules/react/popup/lib/hooks/useCloseOnTargetHidden.ts
@@ -21,6 +21,15 @@ function getScrollParent(element: HTMLElement): HTMLElement {
   return getScrollParent(element.parentElement);
 }
 
+/**
+ * Sets up an
+ * [IntersectionObserver](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
+ * for the target element. When the target is detected as being less than 50% visible, the popup
+ * will close. Most likely, this will happen if the user scrolls an overflowed content area of the
+ * page and the target is no longer visible.
+ *
+ * This should be used with popup elements that are transitory like Tooltips and dropdown menus.
+ */
 export const useCloseOnTargetHidden = createElemPropsHook(usePopupModel)(model => {
   const visible = model.state.visibility !== 'hidden';
 
