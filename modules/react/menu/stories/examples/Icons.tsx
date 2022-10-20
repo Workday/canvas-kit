8178ab33c72393c3ac895131c129881fe6c639ce
James Fan <james@somewhatstrange.com>
2022-10-20 16:48:54 -0700

    docs(menu): Move menu in Icons example behind a button

--- modules/react/menu/stories/examples/Icons.tsx
+++ modules/react/menu/stories/examples/Icons.tsx
@@ -8,34 +8,41 @@ import {
 import {Menu} from '@workday/canvas-kit-react/menu';
 
 export const Icons = () => {
+  const [selected, setSelected] = React.useState('');
   return (
-    <Menu>
-      <Menu.Card>
-        <Menu.List>
-          <Menu.Item>
-            <Menu.Item.Icon icon={uploadCloudIcon} />
-            <Menu.Item.Text>First Item</Menu.Item.Text>
-          </Menu.Item>
-          <Menu.Item>
-            <Menu.Item.Icon icon={setupIcon} />
-            <Menu.Item.Text>Second Item (with a really really really long label)</Menu.Item.Text>
-          </Menu.Item>
-          <Menu.Item aria-disabled>
-            <Menu.Item.Icon icon={uploadCloudIcon} />
-            <Menu.Item.Text>Third Item</Menu.Item.Text>
-            <Menu.Item.Icon icon={taskContactIcon} />
-          </Menu.Item>
-          <Menu.Item>
-            <Menu.Item.Icon icon={userIcon} />
-            <Menu.Item.Text></Menu.Item.Text>
-          </Menu.Item>
-          <Menu.Divider />
-          <Menu.Item>
-            <Menu.Item.Icon icon={taskContactIcon} />
-            <Menu.Item.Text>Fifth Item (with divider)</Menu.Item.Text>
-          </Menu.Item>
-        </Menu.List>
-      </Menu.Card>
+    <Menu onSelect={data => setSelected(data.id)}>
+      <Menu.Target>Open Menu</Menu.Target>
+      <Menu.Popper>
+        <Menu.Card>
+          <Menu.List>
+            <Menu.Item>
+              <Menu.Item.Icon icon={uploadCloudIcon} />
+              <Menu.Item.Text>First Item</Menu.Item.Text>
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Item.Icon icon={setupIcon} />
+              <Menu.Item.Text>Second Item (with a really really really long label)</Menu.Item.Text>
+            </Menu.Item>
+            <Menu.Item aria-disabled>
+              <Menu.Item.Icon icon={uploadCloudIcon} />
+              <Menu.Item.Text>Third Item</Menu.Item.Text>
+              <Menu.Item.Icon icon={taskContactIcon} />
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Item.Icon icon={userIcon} />
+              <Menu.Item.Text></Menu.Item.Text>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item>
+              <Menu.Item.Icon icon={taskContactIcon} />
+              <Menu.Item.Text>Fifth Item (with divider)</Menu.Item.Text>
+            </Menu.Item>
+          </Menu.List>
+        </Menu.Card>
+      </Menu.Popper>
+      <p>
+        Selected: <span data-testid="output">{selected}</span>
+      </p>
     </Menu>
   );
 };
