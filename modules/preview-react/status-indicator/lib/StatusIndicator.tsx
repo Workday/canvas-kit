547b4e52818eb0f6123e32e8c7b1647640f1f2a7
manuel.carrera <manuel.carrera@workday.com>
2022-10-21 14:31:58 -0600

    fix: Remove circular dep

--- modules/preview-react/status-indicator/lib/StatusIndicator.tsx
+++ modules/preview-react/status-indicator/lib/StatusIndicator.tsx
@@ -1,74 +1,10 @@
 import React from 'react';
 
 import {ExtractProps, createContainer, createElemPropsHook} from '@workday/canvas-kit-react/common';
-import {colors} from '@workday/canvas-kit-react/tokens';
 import {HStack} from '@workday/canvas-kit-react/layout';
-import {StatusIndicatorIcon} from './StatusIndicatorIcon';
+import {StatusIndicatorIcon, statusIndicatorColors} from './StatusIndicatorIcon';
 import {StatusIndicatorLabel} from './StatusIndicatorLabel';
 import {useStatusIndicatorModel} from './hooks';
-
-export const colorsMap = {
-  gray: {
-    high: {
-      text: colors.frenchVanilla100,
-      background: colors.licorice300,
-    },
-    low: {
-      text: colors.licorice400,
-      background: colors.soap300,
-    },
-  },
-  orange: {
-    high: {
-      text: colors.licorice500,
-      background: colors.cantaloupe400,
-    },
-    low: {
-      text: colors.toastedMarshmallow600,
-      background: colors.cantaloupe100,
-    },
-  },
-  blue: {
-    high: {
-      text: colors.frenchVanilla100,
-      background: colors.blueberry400,
-    },
-    low: {
-      text: colors.blueberry500,
-      background: colors.blueberry100,
-    },
-  },
-  green: {
-    high: {
-      text: colors.frenchVanilla100,
-      background: colors.greenApple600,
-    },
-    low: {
-      text: colors.greenApple600,
-      background: colors.greenApple100,
-    },
-  },
-  red: {
-    high: {
-      text: colors.frenchVanilla100,
-      background: colors.cinnamon500,
-    },
-    low: {
-      text: colors.cinnamon600,
-      background: colors.cinnamon100,
-    },
-  },
-  transparent: {
-    high: {
-      text: colors.frenchVanilla100,
-      background: colors.blackPepper600,
-    },
-    low: {
-      text: colors.frenchVanilla100,
-      background: colors.blackPepper600,
-    },
-  },
-};
 
 export interface StatusIndicatorProps extends Partial<ExtractProps<typeof HStack, never>> {
   /**
@@ -78,7 +14,7 @@ export interface StatusIndicatorProps extends Partial<ExtractProps<typeof HStack
 }
 
 export const useStatusIndicator = createElemPropsHook(useStatusIndicatorModel)(({state}) => {
-  const colors = colorsMap[state.variant][state.emphasis];
+  const colors = statusIndicatorColors[state.variant][state.emphasis];
   return {
     opacity: state.variant === 'transparent' ? '0.85' : undefined,
     color: colors.text,
