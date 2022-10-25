ccdf294918307c1b9652e7d6aef5aab4d6596f5c
James Fan <james@somewhatstrange.com>
2022-10-25 16:46:34 -0700

    docs: Update Basic Segmented Control example

--- modules/preview-react/segmented-control/stories/examples/Basic.tsx
+++ modules/preview-react/segmented-control/stories/examples/Basic.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import {SegmentedControl} from '@workday/canvas-kit-preview-react/segmented-control';
+import {BodyText} from '@workday/canvas-kit-react/text';
 import {
   gridIcon,
   listViewIcon,
@@ -11,25 +13,30 @@ export const Basic = () => {
   const [viewType, setViewType] = React.useState('table');
 
   return (
-    <SegmentedControl initialValue={viewType} onSelect={data => setViewType(data.id)}>
-      <SegmentedControl.List aria-label="View type">
-        <SegmentedControl.Item data-id="table" icon={gridIcon} tooltipProps={{title: 'Table'}} />
-        <SegmentedControl.Item
-          data-id="list-view"
-          icon={listViewIcon}
-          tooltipProps={{title: 'List'}}
-        />
-        <SegmentedControl.Item
-          data-id="list-detail"
-          icon={listDetailIcon}
-          tooltipProps={{title: 'Detail'}}
-        />
-        <SegmentedControl.Item
-          data-id="diagrams"
-          icon={pieChartIcon}
-          tooltipProps={{title: 'Diagram'}}
-        />
-      </SegmentedControl.List>
-    </SegmentedControl>
+    <>
+      <SegmentedControl initialValue={viewType} onSelect={data => setViewType(data.id)}>
+        <SegmentedControl.List aria-label="View type">
+          <SegmentedControl.Item data-id="table" icon={gridIcon} tooltipProps={{title: 'Table'}} />
+          <SegmentedControl.Item
+            data-id="list-view"
+            icon={listViewIcon}
+            tooltipProps={{title: 'List'}}
+          />
+          <SegmentedControl.Item
+            data-id="list-detail"
+            icon={listDetailIcon}
+            tooltipProps={{title: 'Detail'}}
+          />
+          <SegmentedControl.Item
+            data-id="diagrams"
+            icon={pieChartIcon}
+            tooltipProps={{title: 'Diagram'}}
+          />
+        </SegmentedControl.List>
+      </SegmentedControl>
+      <BodyText size="small" marginTop="s">
+        Selected: {viewType}
+      </BodyText>
+    </>
   );
 };
