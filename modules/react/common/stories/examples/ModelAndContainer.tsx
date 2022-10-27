38b3f289aac3427353bf5d6db3ec7d2e323e366e
manuel.carrera <manuel.carrera@workday.com>
2022-10-27 13:13:59 -0600

    docs: Add docs on compose model hook

---
+++ modules/react/common/stories/examples/ModelAndContainer.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {createContainer, createModelHook, useUniqueId} from '@workday/canvas-kit-react/common';
+
+export type Visibility = 'hidden' | 'visible';
+
+const useDisclosureModel = createModelHook({
+  // This becomes the default values on the model
+  defaultConfig: {
+    /** ID reference of the list. Children ids can be derived from this id */
+    id: '',
+    /**
+     * The initial visibility of the disclosed content
+     * @default 'hidden'
+     */
+    initialVisibility: 'hidden' as Visibility,
+  },
+})(config => {
+  const id = useUniqueId(config.id);
+  const [visibility, setVisibility] = React.useState(config.initialVisibility || 'hidden');
+  //  Set the default internal state for your model.
+  const state = {
+    /** ID reference of the list. Children ids can be derived from this id */
+    id,
+    /**
+     * Visibility state of the disclosed content. Models are allowed to extend the states to fit
+     * their needs, so if you need to consistently determine "not hidden", use `visibility !==
+     * 'hidden'` rather than `visibility === 'visible'`
+     */
+    visibility,
+  };
+  // Sets events that be used across sub components
+  const events = {
+    /**
+     * Start showing the disclosed content. If a DOM event triggered this event, the event data will
+     * be passed along. This data can be used by guards and callbacks.
+     */
+    show(event?: Event | React.SyntheticEvent) {
+      setVisibility('visible');
+    },
+    /**
+     * Start hiding this disclosed content. If a DOM event triggered this event, the event data will
+     * be passed along. This data can be used by guards and callbacks.
+     */
+    hide(event?: Event | React.SyntheticEvent) {
+      setVisibility('hidden');
+    },
+  };
+
+  return {state, events};
+});
+
+// interface DisclosureProps {}
+export const DisclosureExample = createContainer('div')({
+  modelHook: useDisclosureModel,
+})(({}, _, model) => {
+  return (
+    <div>
+      <button onClick={() => model.events.show()}>Open</button>
+      {model.state.visibility === 'visible' ? <div>Show content</div> : null}
+    </div>
+  );
+});
