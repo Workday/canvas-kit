60df55269567b8126423d31f9483ad505bb83aa3
Will Klein <will@willklein.co>
2022-10-31 17:20:28 -0600

    chore(loading-dots): Add code mod support

---
+++ modules/codemod/lib/v8/spec/renameLoadingAnimation.spec.ts
@@ -0,0 +1,119 @@
+import {expectTransformFactory} from './expectTransformFactory';
+import transformer from '../renameLoadingAnimation';
+import {stripIndent} from 'common-tags';
+
+const expectTransform = expectTransformFactory(transformer);
+
+describe('renameLoadingAnimation', () => {
+  describe('when updating imports', () => {
+    it('should rename LoadingAnimation to LoadingDots', () => {
+      const input = `import {LoadingAnimation} from "@workday/canvas-kit-react/loading-animation"`;
+      const expected = `import {LoadingDots} from "@workday/canvas-kit-react/loading-dots"`;
+
+      expectTransform(input, expected);
+    });
+
+    it('should update the import when importing from the main package', () => {
+      const input = `import {LoadingAnimation} from "@workday/canvas-kit-react"`;
+      const expected = `import {LoadingDots} from "@workday/canvas-kit-react"`;
+
+      expectTransform(input, expected);
+    });
+
+    it('should ignore non-canvas-kit imports', () => {
+      const input = `import {LoadingAnimation} from "@workday/some-other-library"`;
+      const expected = `import {LoadingAnimation} from "@workday/some-other-library"`;
+
+      expectTransform(input, expected);
+    });
+
+    it('should update renamed exports', () => {
+      const input = stripIndent`
+        import {LoadingAnimation as CanvasLoadingAnimation} from "@workday/canvas-kit-react/loading-animation";
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots as CanvasLoadingAnimation} from "@workday/canvas-kit-react/loading-dots";
+      `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should update slash import with LoadingDots', () => {
+      const input = stripIndent`
+        import {LoadingDots} from "@workday/canvas-kit-react/loading-animation";
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots} from "@workday/canvas-kit-react/loading-dots";
+      `;
+
+      expectTransform(input, expected);
+    });
+  });
+
+  describe('when updating usages', () => {
+    it('should update JSX usage', () => {
+      const input = stripIndent`
+        import {LoadingAnimation} from "@workday/canvas-kit-react/loading-animation";
+
+        <LoadingAnimation />
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots} from "@workday/canvas-kit-react/loading-dots";
+
+        <LoadingDots />
+      `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should update JSX with attributes unaffected', () => {
+      const input = stripIndent`
+        import {LoadingAnimation} from "@workday/canvas-kit-react/loading-animation";
+
+        <LoadingAnimation className='loading-centered' data-some-prop="untouched" />
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots} from "@workday/canvas-kit-react/loading-dots";
+
+        <LoadingDots className='loading-centered' data-some-prop="untouched" />
+      `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should update styled components', () => {
+      const input = stripIndent`
+        import {LoadingAnimation} from "@workday/canvas-kit-react/loading-animation";
+
+        const StyledLoadingAnimation = styled(LoadingAnimation)({ width: space.xxs, height: space.xxs });
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots} from "@workday/canvas-kit-react/loading-dots";
+
+        const StyledLoadingAnimation = styled(LoadingDots)({ width: space.xxs, height: space.xxs });
+      `;
+
+      expectTransform(input, expected);
+    });
+    it('should not update non-Canvas styled components', () => {
+      const input = stripIndent`
+        import {LoadingAnimation as CanvasLoadingAnimation} from "@workday/canvas-kit-react/loading-animation";
+
+        const StyledLoadingAnimation = styled(LoadingAnimation)({ width: space.xxs, height: space.xxs });
+      `;
+
+      const expected = stripIndent`
+        import {LoadingDots as CanvasLoadingAnimation} from "@workday/canvas-kit-react/loading-dots";
+
+        const StyledLoadingAnimation = styled(LoadingAnimation)({ width: space.xxs, height: space.xxs });
+      `;
+
+      expectTransform(input, expected);
+    });
+  });
+});
