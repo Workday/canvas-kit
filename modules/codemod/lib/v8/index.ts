7e187818aacf4b33f8e462d7e66a0684e96776ba
Will Klein <will@willklein.co>
2022-11-01 13:18:11 -0600

    chore(loading-dots): Add to the v8 codemod index

--- modules/codemod/lib/v8/index.ts
+++ modules/codemod/lib/v8/index.ts
@@ -8,6 +8,7 @@ import promoteComponentsToTesting from './promoteComponentsToTesting';
 import promoteBreadcrumbs from './promoteBreadcrumbs';
 import restructureBreadcrumbs from './restructureBreadcrumbs';
 import refactorActionBarOverflowButton from './refactorActionBarOverflowButton';
+import renameLoadingAnimation from './renameLoadingAnimation';
 
 const transform: Transform = (file, api, options) => {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
@@ -20,6 +21,7 @@ const transform: Transform = (file, api, options) => {
     softDeprecatePreviewMenu,
     promoteComponentsToTesting,
     refactorActionBarOverflowButton,
+    renameLoadingAnimation,
   ];
 
   return fixes.reduce((source, fix) => fix({...file, source}, api, options) as string, file.source);
