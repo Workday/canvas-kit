b45c3a5119a38bf2716542ccc9a826bc6f5c620c
manuel.carrera <manuel.carrera@workday.com>
2022-11-04 13:59:36 -0600

    fix: Add function description

--- modules/react/common/lib/theming/getCanvasTheme.ts
+++ modules/react/common/lib/theming/getCanvasTheme.ts
@@ -1,6 +1,11 @@
 import {getObjectProxy} from './getObjectProxy';
 import {defaultCanvasTheme} from './theme';
 
+/**
+ *
+ * @param theme
+ * @returns a theme object if it is defined, otherwise it will fall back on using defaultCanvasTheme
+ */
 export function getCanvasTheme(theme: any = {}) {
   return getObjectProxy(theme.canvas || {}, defaultCanvasTheme);
 }
