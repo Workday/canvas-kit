f4e89994b4e220e18b7f984da15512bffbeb95c6
Thu Nguyen <thuminhnguyen@berkeley.edu>
2023-01-20 09:53:19 -0800

    fix: Radio button navigation does not stay within its form

--- modules/react/radio/stories/examples/Disabled.tsx
+++ modules/react/radio/stories/examples/Disabled.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FormField} from '@workday/canvas-kit-react/form-field';
 import {Radio, RadioGroup} from '@workday/canvas-kit-react/radio';
+import {useUniqueId} from '@workday/canvas-kit-react/common';
 
 export const Disabled = () => {
   const [value, setValue] = React.useState<string | number>('deep-dish');
@@ -11,7 +12,7 @@ export const Disabled = () => {
 
   return (
     <FormField label="Choose Your Pizza Crust" useFieldset={true}>
-      <RadioGroup name="crust" onChange={handleChange} value={value}>
+      <RadioGroup name={useUniqueId()} onChange={handleChange} value={value}>
         <Radio label="Deep dish" value="deep-dish" />
         <Radio label="Thin" value="thin" />
         <Radio disabled={true} label="Gluten free (sold out)" value="gluten-free" />
