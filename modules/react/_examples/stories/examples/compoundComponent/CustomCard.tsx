8a341474f31f0971c8357d682b7f2118c2ddf6f9
Matthew Rust <matthew.rust@workday.com>
2023-01-24 13:57:02 -0800

    docs: Add example for a simple compound component

---
+++ modules/react/_examples/stories/examples/compoundComponent/CustomCard.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  createContainer,
+  createElemPropsHook,
+  createModelHook,
+  createSubcomponent,
+  ExtractProps,
+} from '@workday/canvas-kit-react/common';
+import {Card} from '@workday/canvas-kit-react/card';
+import {PrimaryButton} from '@workday/canvas-kit-react/button';
+import {Box} from '@workday/canvas-kit-react/layout';
+import {colors} from '@workday/canvas-kit-react/tokens';
+
+export type Theme = 'dark' | 'light';
+interface CustomCardProps extends ExtractProps<typeof Card> {
+  theme?: Theme;
+}
+
+/**
+ * Compound Component Model Hook
+ */
+const useCustomCardModel = createModelHook({
+  defaultConfig: {theme: 'dark' as Theme},
+})(config => {
+  const lightThemeColor = colors.frenchVanilla100;
+  const darkThemeColor = colors.blackPepper300;
+  const inverseColor = color => (color === darkThemeColor ? lightThemeColor : darkThemeColor);
+
+  const [theme, setTheme] = React.useState(config.theme);
+  const [mainColor, setMainColor] = React.useState(
+    config.theme === 'dark' ? darkThemeColor : lightThemeColor
+  );
+  const [contrastColor, setContrastColor] = React.useState(
+    config.theme === 'dark' ? lightThemeColor : darkThemeColor
+  );
+  const state = {theme, mainColor, contrastColor};
+
+  const events = {
+    toggleTheme: () => {
+      setTheme(currentTheme => (currentTheme === 'dark' ? 'light' : 'dark'));
+      setMainColor(inverseColor);
+      setContrastColor(inverseColor);
+    },
+  };
+
+  return {state, events};
+});
+
+/**
+ * Subcomponent Hook
+ */
+const useCustomCardButton = createElemPropsHook(useCustomCardModel)(({state, events}) => ({
+  colors: {
+    default: {
+      background: state.contrastColor,
+      label: state.mainColor,
+    },
+    hover: {},
+    active: {},
+    focus: {},
+    disabled: {},
+  },
+  onClick: () => events.toggleTheme(),
+}));
+
+/**
+ * Subcomponent Component
+ */
+const CustomCardButton = createSubcomponent(PrimaryButton)({
+  displayName: 'CustomCard.Button',
+  modelHook: useCustomCardModel,
+  elemPropsHook: useCustomCardButton,
+})(({children, ...elemProps}) => {
+  return <PrimaryButton {...elemProps}>{children}</PrimaryButton>;
+});
+
+/**
+ * Main Component Hook
+ */
+const useCustomCard = createElemPropsHook(useCustomCardModel)(({state}) => ({
+  backgroundColor: state.mainColor,
+}));
+
+/**
+ * Main Component
+ */
+const CustomCard = createContainer(Card)({
+  displayName: 'CustomCard',
+  subComponents: {
+    Heading: Card.Heading,
+    Body: Card.Body,
+    Button: CustomCardButton,
+  },
+  modelHook: useCustomCardModel,
+  elemPropsHook: useCustomCard,
+})<CustomCardProps>(({theme, children, ...elemProps}) => {
+  return (
+    <Card depth={2} {...elemProps}>
+      {children}
+    </Card>
+  );
+});
+
+/**
+ * Usage
+ */
+export const Basic = () => {
+  const [icon, setIcon] = React.useState('🌝');
+  const model = useCustomCardModel({
+    onToggleTheme: (_, {theme: previousTheme}) => setIcon(previousTheme === 'dark' ? '🌚' : '🌝'),
+  });
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
+  React.useEffect(() => buttonRef.current?.focus(), []);
+
+  return (
+    <CustomCard model={model}>
+      <CustomCard.Heading>
+        <Box color={model.state.contrastColor}>Change your theme {icon}</Box>
+      </CustomCard.Heading>
+      <CustomCard.Body>
+        <CustomCard.Button ref={buttonRef}>Toggle</CustomCard.Button>
+      </CustomCard.Body>
+    </CustomCard>
+  );
+};
