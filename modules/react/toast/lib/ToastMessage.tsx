0fdff001b1455d01f89207b2d7322cdeabb66e47
manuel.carrera <manuel.carrera@workday.com>
2023-03-03 09:06:09 -0700

    fix: Update  message code

--- modules/react/toast/lib/ToastMessage.tsx
+++ modules/react/toast/lib/ToastMessage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import {createSubcomponent} from '@workday/canvas-kit-react/common';
+import {createSubcomponent, ExtractProps} from '@workday/canvas-kit-react/common';
 import {useToastModel} from './hooks/useToastModel';
 import {Subtext} from '@workday/canvas-kit-react/text';
 
-export interface ToastMessageProps extends FlexProps {}
+export interface ToastMessageProps extends Omit<ExtractProps<typeof Subtext>, 'size'> {}
 
 export const ToastMessage = createSubcomponent('p')({
   modelHook: useToastModel,
