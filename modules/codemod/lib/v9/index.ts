b89ecc91d8ed5002115718632dad0020a8dc925d
manuel.carrera <manuel.carrera@workday.com>
2023-03-06 15:44:14 -0700

    fix: Add codemod to transform

--- modules/codemod/lib/v9/index.ts
+++ modules/codemod/lib/v9/index.ts
@@ -2,6 +2,7 @@ import {Transform} from 'jscodeshift';
 
 import softDeprecateStack from './deprecateStack';
 import promoteToast from './promoteToast';
+import compoundToast from './compoundToast';
 
 const transform: Transform = (file, api, options) => {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
@@ -9,6 +10,7 @@ const transform: Transform = (file, api, options) => {
     // add codemods here
     softDeprecateStack,
     promoteToast,
+    compoundToast,
   ];
   return fixes.reduce((source, fix) => fix({...file, source}, api, options) as string, file.source);
 };
