67eb8a0e360019fcb56050bce65e1a4d6630c964
manuel.carrera <manuel.carrera@workday.com>
2023-03-21 12:13:02 -0600

    fix: Revert change to story

--- modules/react/action-bar/stories/examples/Basic.tsx
+++ modules/react/action-bar/stories/examples/Basic.tsx
@@ -4,7 +4,7 @@ import {PrimaryButton} from '@workday/canvas-kit-react/button';
 
 export const Basic = () => (
   <ActionBar>
-    <ActionBar.List depth={'none'} border="none" position="relative" aria-label="actions">
+    <ActionBar.List position="relative" aria-label="actions">
       <ActionBar.Item as={PrimaryButton} onClick={() => console.log('first action')}>
         First Action
       </ActionBar.Item>
