df91cb18e7b23ac0ef53b4884560b9d708f86c9e
manuel.carrera <manuel.carrera@workday.com>
2023-03-27 16:21:06 -0600

    fix: Revert exmaple

--- modules/react/toast/stories/examples/Basic.tsx
+++ modules/react/toast/stories/examples/Basic.tsx
@@ -2,32 +2,14 @@ import React from 'react';
 
 import {Toast} from '@workday/canvas-kit-react/toast';
 import {checkIcon} from '@workday/canvas-system-icons-web';
-import {Popper} from '@workday/canvas-kit-react/popup';
-import {SecondaryButton} from '@workday/canvas-kit-react/button';
 
 export const Basic = () => {
-  const [open, setOpen] = React.useState(false);
-  const containerRef = React.useRef(null);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
   return (
-    <div ref={containerRef}>
-      <SecondaryButton onClick={handleOpen}>Show Status Toast</SecondaryButton>
-      <Popper placement="top" open={open} anchorElement={containerRef}>
-        <Toast>
-          <Toast.Icon icon={checkIcon} color="greenApple400" />
-          <Toast.Body>
-            <Toast.Message>Your workbook was successfully processed.</Toast.Message>
-          </Toast.Body>
-          <Toast.CloseIcon onClick={handleClose} />
-        </Toast>
-      </Popper>
-    </div>
+    <Toast>
+      <Toast.Icon icon={checkIcon} color="greenApple400" />
+      <Toast.Body>
+        <Toast.Message>Your workbook was successfully processed.</Toast.Message>
+      </Toast.Body>
+    </Toast>
   );
 };
---
+++ modules/react/toast/stories/examples/WithActionLinkAndCloseIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {Toast} from '@workday/canvas-kit-react/toast';
+import {checkIcon} from '@workday/canvas-system-icons-web';
+import {colors} from '@workday/canvas-kit-react/tokens';
+
+export const WithActionLinkAndCloseIcon = () => {
+  const handleClose = () => console.log('close button clicked');
+
+  return (
+    <Toast mode="dialog" aria-label="notification">
+      <Toast.Icon icon={checkIcon} color="greenApple400" />
+      <Toast.Body>
+        <Toast.Message>Your workbook was successfully</Toast.Message>
+        <Toast.Link href="#hyperlink">Custom Link</Toast.Link>
+      </Toast.Body>
+      <Toast.CloseIcon aria-label="Close notification" onClick={handleClose} />
+    </Toast>
+  );
+};
