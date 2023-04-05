8d78893fd1038f11ac51e9bc2580528e573d2c29
josh.bagwell <josh.bagwell@workday.com>
2023-04-05 14:35:50 -0600

    fix: Adjusted docs to proper structure

--- modules/preview-react/table/lib/TableCaption.tsx
+++ modules/preview-react/table/lib/TableCaption.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {colors} from '@workday/canvas-kit-react/tokens';
 import {FlexProps, Flex} from '@workday/canvas-kit-react/layout';
 import {createComponent} from '@workday/canvas-kit-react/common';
 
@@ -6,7 +7,15 @@ export const TableCaption = createComponent('caption')({
   displayName: 'Table.Caption',
   Component: ({children, ...elemProps}: FlexProps, ref, Element) => {
     return (
-      <Flex as={Element} ref={ref} paddingY="xxs" paddingX="s" textAlign="left" {...elemProps}>
+      <Flex
+        as={Element}
+        ref={ref}
+        borderBottom={`1px solid ${colors.soap500}`}
+        paddingY="xxs"
+        paddingX="s"
+        textAlign="left"
+        {...elemProps}
+      >
         {children}
       </Flex>
     );
