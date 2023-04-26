70656f315f63925e275f5140ca98f86cdea65b92
josh.bagwell <josh.bagwell@workday.com>
2023-04-26 11:17:05 -0600

    fix: Renamed softDeprecateStack codemod

--- modules/codemod/lib/v9/index.ts
+++ modules/codemod/lib/v9/index.ts
@@ -1,6 +1,6 @@
 import {Transform} from 'jscodeshift';
 
-import softDeprecateStack from './deprecateStack';
+import deprecateStack from './deprecateStack';
 import promoteToast from './promoteToast';
 import compoundToast from './compoundToast';
 import promoteUseThemedRing from './promoteUseThemedRing';
@@ -9,7 +9,7 @@ const transform: Transform = (file, api, options) => {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
   const fixes = [
     // add codemods here
-    softDeprecateStack,
+    deprecateStack,
     compoundToast,
     promoteToast,
     promoteUseThemedRing,
