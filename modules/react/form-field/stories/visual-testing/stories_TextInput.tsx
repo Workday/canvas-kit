45acb977aa44c3c36e05949bea6d4872464fe790
Nicholas Boll <nicholas.boll@gmail.com>
2023-05-01 10:27:44 -0600

    Added visual test story

--- modules/react/form-field/stories/visual-testing/stories_TextInput.tsx
+++ modules/react/form-field/stories/visual-testing/stories_TextInput.tsx
@@ -6,7 +6,11 @@ import {
   StaticStates,
 } from '@workday/canvas-kit-react/testing';
 import {withSnapshotsEnabled, customColorTheme} from '../../../../../utils/storybook';
-import {TextInput} from '@workday/canvas-kit-react/text-input';
+import {InputGroup, TextInput} from '@workday/canvas-kit-react/text-input';
+import {searchIcon, xSmallIcon} from '@workday/canvas-system-icons-web';
+import {SystemIcon} from '@workday/canvas-kit-react/icon';
+import {TertiaryButton} from '@workday/canvas-kit-react/button';
+import {CanvasProvider} from '@workday/canvas-kit-react/common';
 
 export default withSnapshotsEnabled({
   title: 'Testing/Inputs/Text Input',
@@ -76,3 +80,44 @@ TextInputThemedStates.parameters = {
     theme: customColorTheme,
   },
 };
+
+export const InputGroupStates = () => (
+  <StaticStates>
+    <ComponentStatesTable
+      rowProps={[
+        {label: 'Start', props: {start: <SystemIcon icon={searchIcon} size="small" />}},
+        {
+          label: 'End',
+          props: {
+            end: (
+              <TertiaryButton role="presentation" icon={xSmallIcon} size="small" tabIndex={-1} />
+            ),
+          },
+        },
+        {
+          label: 'Both',
+          props: {
+            start: <SystemIcon icon={searchIcon} size="small" />,
+            end: (
+              <TertiaryButton role="presentation" icon={xSmallIcon} size="small" tabIndex={-1} />
+            ),
+          },
+        },
+      ]}
+      columnProps={[
+        {label: 'LTR', props: {dir: 'ltr'}},
+        {label: 'RTL', props: {dir: 'rtl'}},
+      ]}
+    >
+      {props => (
+        <CanvasProvider theme={{canvas: {direction: props.dir}}}>
+          <InputGroup width={280}>
+            {props.start && <InputGroup.Start>{props.start}</InputGroup.Start>}
+            <InputGroup.Input value="Very Long Text. Very Long Text. Very Long Text." />
+            {props.end && <InputGroup.End>{props.end}</InputGroup.End>}
+          </InputGroup>
+        </CanvasProvider>
+      )}
+    </ComponentStatesTable>
+  </StaticStates>
+);
