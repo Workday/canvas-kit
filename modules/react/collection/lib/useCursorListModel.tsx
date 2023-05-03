acec4fd8438a42768175bb926d29d87390454991
Nicholas Boll <nicholas.boll@gmail.com>
2023-05-03 09:45:07 -0600

    Rollback unintended change

--- modules/react/collection/lib/useCursorListModel.tsx
+++ modules/react/collection/lib/useCursorListModel.tsx
@@ -322,6 +322,9 @@ export const useCursorListModel = createModelHook({
   const pageSizeRef = React.useRef(config.pageSize);
   const columnCount = config.columnCount || 0;
   const list = useBaseListModel(config);
+  const initialCurrentRef = React.useRef(
+    config.initialCursorId || (config.items?.length ? list.state.items![0].id : '')
+  );
   const navigation = config.navigation;
   const [cursorIndex, setCursorIndex] = React.useState(() => {
     return list.state.items.findIndex(item => item.id === cursorId) || 0;
@@ -355,6 +358,18 @@ export const useCursorListModel = createModelHook({
 
   const events = {
     ...list.events,
+    /**
+     * Register an item to the list. Takes in an identifier, a React.Ref and an optional index. This
+     * should be called on component mount
+     */
+    registerItem(data: Parameters<typeof list.events.registerItem>[0]) {
+      // point the cursor at the first item
+      if (!initialCurrentRef.current) {
+        initialCurrentRef.current = getId(data.item);
+        setCursorId(initialCurrentRef.current);
+      }
+      list.events.registerItem(data);
+    },
     /** Directly sets the cursor to the list item by its identifier. */
     goTo(data: {id: string}) {
       const index = state.items.findIndex(item => item.id === data.id);
