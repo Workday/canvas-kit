b71252b29bc36e3fc7fffbf98b09a04a4d78a070
My Vu <my.vu@workday.com>
2023-05-15 13:45:18 -0700

    fix: A11Y fixes

--- modules/react/_examples/stories/examples/SidePanelWithNavigation.tsx
+++ modules/react/_examples/stories/examples/SidePanelWithNavigation.tsx
@@ -8,7 +8,7 @@ import {Expandable} from '@workday/canvas-kit-labs-react/expandable';
 import {SystemIcon} from '@workday/canvas-kit-react/icon';
 import {birthdayIcon, ribbonIcon} from '@workday/canvas-system-icons-web';
 
-const StyledPanel = styled(SidePanel)({
+const StyledPanel = styled(SidePanel.as('nav'))({
   backgroundColor: colors.soap200,
   zIndex: 1,
 });
@@ -35,10 +35,10 @@ const cakes = [
 ];
 
 const bestsellers = [
-  {value: 'Lemon Rhubarb Bars', linkValue: '#LemonRhubarbBars'},
-  {value: 'Pistachio Rosewater Cake', linkValue: '#PistachioRosewaterCake'},
-  {value: 'Triple Chocolate Chip Muffins', linkValue: '#TripleChocolateChipMuffins'},
-  {value: 'Brownie Cheesecake Bites', linkValue: '#BrownieCheesecakeBites'},
+  {value: 'Mango Coco Sago', linkValue: '#LemonRhubarbBars'},
+  {value: 'Matcha Creme Pie', linkValue: '#MatchaCremePie'},
+  {value: 'Key Lime Cheesecake', linkValue: '#KeyLimeCheesecake'},
+  {value: 'Tiramisu', linkValue: '#Tiramisu'},
 ];
 
 const gettingStarted = [
@@ -53,66 +53,56 @@ export const WithNavigation = () => {
 
   return (
     <Flex height={800} position="relative" backgroundColor="frenchVanilla100">
-      <StyledPanel touched={true} variant="alternate">
-        <Flex flexDirection="column" rowGap="zero" paddingTop="m">
-          <Flex.Item>
+      <StyledPanel aria-label="Main navigation" touched={true} variant="alternate">
+        <Flex as="ul" flexDirection="column" rowGap="zero" paddingInlineStart="zero">
+          <Flex.Item as="li" listStyle="none">
             <Expandable padding="zero">
               <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="s" textAlign="center">
                 <SystemIcon icon={ribbonIcon} marginX="xxs" />
                 <Expandable.Title padding="zero">
-                  <BodyText
-                    as="h2"
-                    size="small"
-                    fontWeight="bold"
-                    color="blackpepper300"
-                    marginY="zero"
-                  >
+                  <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
                     Bestsellers
                   </BodyText>
                   <Subtext size="small" color="blackpepper300" marginY="zero">
                     Award winning sweet treats
                   </Subtext>
                 </Expandable.Title>
-                <Expandable.Icon iconPosition="end" />
+                <Expandable.Icon iconPosition="end" flex="1" />
               </StyledExpandable>
-              <Expandable.Content paddingY="zero" paddingX="zero">
+              <Expandable.Content paddingY="zero" paddingX="zero" as="ul">
                 {bestsellers.map(item => {
                   return (
-                    <StyledLink
-                      size="large"
-                      display="flex"
-                      textDecoration="none"
-                      paddingLeft="xxl"
-                      paddingTop="xxs"
-                      paddingBottom="zero"
-                      href={item.linkValue}
-                    >
-                      {item.value}
-                    </StyledLink>
+                    <li>
+                      <StyledLink
+                        size="large"
+                        display="flex"
+                        textDecoration="none"
+                        paddingLeft="xxl"
+                        paddingTop="xxs"
+                        paddingBottom="zero"
+                        href={item.linkValue}
+                      >
+                        {item.value}
+                      </StyledLink>
+                    </li>
                   );
                 })}
               </Expandable.Content>
             </Expandable>
           </Flex.Item>
-          <Flex.Item>
+          <Flex.Item as="li" listStyle="none">
             <Expandable padding="zero">
               <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="s" textAlign="center">
                 <SystemIcon icon={birthdayIcon} marginX="xxs" />
                 <Expandable.Title padding="zero">
-                  <BodyText
-                    as="h2"
-                    size="small"
-                    fontWeight="bold"
-                    color="blackpepper300"
-                    marginY="zero"
-                  >
+                  <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
                     Custom Cakes
                   </BodyText>
                   <Subtext size="small" color="blackpepper300" marginY="zero">
                     To celebrate all the milestones in life!
                   </Subtext>
                 </Expandable.Title>
-                <Expandable.Icon iconPosition="end" />
+                <Expandable.Icon iconPosition="end" flex="1" />
               </StyledExpandable>
               <Expandable.Content paddingY="zero" paddingX="zero">
                 {cakes.map(item => {
