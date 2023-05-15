ff9e845e8bcc6ef5dd131ddb6587fa57c1bad3eb
My Vu <my.vu@workday.com>
2023-05-15 08:34:15 -0700

    fix: Removed extra navigation layer

--- modules/react/_examples/stories/examples/SidePanelWithNavigation.tsx
+++ modules/react/_examples/stories/examples/SidePanelWithNavigation.tsx
@@ -6,7 +6,7 @@ import {Flex, Box} from '@workday/canvas-kit-react/layout';
 import {BodyText, Subtext} from '@workday/canvas-kit-react/text';
 import {Expandable} from '@workday/canvas-kit-labs-react/expandable';
 import {SystemIcon} from '@workday/canvas-kit-react/icon';
-import {rocketIcon, notificationsIcon} from '@workday/canvas-system-icons-web';
+import {birthdayIcon, ribbonIcon} from '@workday/canvas-system-icons-web';
 
 const StyledPanel = styled(SidePanel)({
   backgroundColor: colors.soap200,
@@ -27,17 +27,18 @@ const StyledLink = styled(Subtext.as('a'))<StyledType>({
   },
 });
 
-const forDesigners = [
-  {value: 'Understanding Canvas', linkValue: '#UnderstandingCanvas'},
-  {value: 'Customizing Canvas', linkValue: '#CustomizingCanvas'},
-  {value: 'Contributing to Canvas', linkValue: '#ContributingtoCanvas'},
-  {value: 'Resources', linkValue: '#Resources'},
+const cakes = [
+  {value: 'Anniversary', linkValue: '#AnniversaryCakes'},
+  {value: 'Birthday', linkValue: '#BirthdayCakes'},
+  {value: 'Graduation', linkValue: '#GraduationCakes'},
+  {value: 'Wedding', linkValue: 'WeddingCakes'},
 ];
 
-const forDevelopers = [
-  {value: 'Using Canvas', linkValue: '#UsingCanvas'},
-  {value: 'Contributing', linkValue: '#Contributing'},
-  {value: 'Resources', linkValue: '#Resources'},
+const bestsellers = [
+  {value: 'Lemon Rhubarb Bars', linkValue: '#LemonRhubarbBars'},
+  {value: 'Pistachio Rosewater Cake', linkValue: '#PistachioRosewaterCake'},
+  {value: 'Triple Chocolate Chip Muffins', linkValue: '#TripleChocolateChipMuffins'},
+  {value: 'Brownie Cheesecake Bites', linkValue: '#BrownieCheesecakeBites'},
 ];
 
 const gettingStarted = [
@@ -56,178 +57,79 @@ export const WithNavigation = () => {
         <Flex flexDirection="column" rowGap="zero" paddingTop="m">
           <Flex.Item>
             <Expandable padding="zero">
-              <StyledExpandable paddingY="s" paddingX="m" textAlign="center">
-                <SystemIcon icon={rocketIcon} marginX="xxs" />
+              <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="s" textAlign="center">
+                <SystemIcon icon={ribbonIcon} marginX="xxs" />
                 <Expandable.Title padding="zero">
                   <BodyText
-                    as="h4"
+                    as="h2"
                     size="small"
                     fontWeight="bold"
                     color="blackpepper300"
                     marginY="zero"
                   >
-                    Getting Started
+                    Bestsellers
                   </BodyText>
                   <Subtext size="small" color="blackpepper300" marginY="zero">
-                    Guidance for beginners to pros
+                    Award winning sweet treats
                   </Subtext>
                 </Expandable.Title>
                 <Expandable.Icon iconPosition="end" />
               </StyledExpandable>
               <Expandable.Content paddingY="zero" paddingX="zero">
-                <StyledLink
-                  as="a"
-                  href="#"
-                  size="large"
-                  color={colors.blackpepper300}
-                  textDecoration="none"
-                  marginY="zero"
-                  display="flex"
-                  paddingTop="10px"
-                  paddingBottom="9px"
-                  paddingLeft="xxl"
-                >
-                  Introduction
-                </StyledLink>
-                <Expandable paddingBottom="9px" paddingX="zero" paddingTop="zero">
-                  <StyledExpandable
-                    alignItems="center"
-                    paddingLeft="xxl"
-                    paddingTop="10px"
-                    paddingBottom="zero"
-                  >
-                    <Expandable.Icon iconPosition="start" size="16px" padding="zero" />
-                    <Expandable.Title>
-                      <Subtext
-                        as="a"
-                        href="#"
-                        size="large"
-                        color={colors.blackpepper300}
-                        textDecoration="none"
-                      >
-                        For Designers
-                      </Subtext>
-                    </Expandable.Title>
-                  </StyledExpandable>
-                  <Expandable.Content padding="zero">
-                    {forDesigners.map(item => {
-                      return (
-                        <StyledLink
-                          size="large"
-                          display="flex"
-                          textDecoration="none"
-                          paddingLeft="112px"
-                          paddingTop="6px"
-                          paddingBottom="5px"
-                          href={item.linkValue}
-                        >
-                          {item.value}
-                        </StyledLink>
-                      );
-                    })}
-                  </Expandable.Content>
-                </Expandable>
-                <Expandable paddingBottom="9px" paddingX="zero" paddingTop="zero">
-                  <StyledExpandable
-                    alignItems="center"
-                    paddingLeft="xxl"
-                    paddingTop="10px"
-                    paddingBottom="zero"
-                  >
-                    <Expandable.Icon iconPosition="start" size="16px" padding="zero" />
-                    <Expandable.Title>
-                      <Subtext
-                        as="a"
-                        href="#"
-                        size="large"
-                        color={colors.blackpepper300}
-                        textDecoration="none"
-                      >
-                        For Developers
-                      </Subtext>
-                    </Expandable.Title>
-                  </StyledExpandable>
-                  <Expandable.Content padding="zero">
-                    {forDevelopers.map(item => {
-                      return (
-                        <StyledLink
-                          size="large"
-                          display="flex"
-                          textDecoration="none"
-                          paddingLeft="112px"
-                          paddingTop="6px"
-                          paddingBottom="5px"
-                          href={item.linkValue}
-                        >
-                          {item.value}
-                        </StyledLink>
-                      );
-                    })}
-                  </Expandable.Content>
-                </Expandable>
-                <StyledLink
-                  as="a"
-                  href="#"
-                  size="large"
-                  textDecoration="none"
-                  marginY="zero"
-                  display="flex"
-                  paddingTop="10px"
-                  paddingBottom="9px"
-                  paddingLeft="xxl"
-                >
-                  Extended Systems
-                </StyledLink>
+                {bestsellers.map(item => {
+                  return (
+                    <StyledLink
+                      size="large"
+                      display="flex"
+                      textDecoration="none"
+                      paddingLeft="xxl"
+                      paddingTop="xxs"
+                      paddingBottom="zero"
+                      href={item.linkValue}
+                    >
+                      {item.value}
+                    </StyledLink>
+                  );
+                })}
               </Expandable.Content>
             </Expandable>
           </Flex.Item>
           <Flex.Item>
             <Expandable padding="zero">
-              <StyledExpandable paddingY="s" paddingX="m" textAlign="center">
-                <SystemIcon icon={notificationsIcon} marginX="xxs" />
+              <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="s" textAlign="center">
+                <SystemIcon icon={birthdayIcon} marginX="xxs" />
                 <Expandable.Title padding="zero">
                   <BodyText
-                    as="h4"
+                    as="h2"
                     size="small"
                     fontWeight="bold"
                     color="blackpepper300"
                     marginY="zero"
                   >
-                    What's New
+                    Custom Cakes
                   </BodyText>
                   <Subtext size="small" color="blackpepper300" marginY="zero">
-                    Announcements and more
+                    To celebrate all the milestones in life!
                   </Subtext>
                 </Expandable.Title>
                 <Expandable.Icon iconPosition="end" />
               </StyledExpandable>
               <Expandable.Content paddingY="zero" paddingX="zero">
-                <StyledLink
-                  as="a"
-                  href="#"
-                  size="large"
-                  textDecoration="none"
-                  marginY="zero"
-                  display="flex"
-                  paddingTop="10px"
-                  paddingBottom="9px"
-                  paddingLeft="xxl"
-                >
-                  Announcements
-                </StyledLink>
-                <StyledLink
-                  as="a"
-                  href="#"
-                  size="large"
-                  textDecoration="none"
-                  marginY="zero"
-                  display="flex"
-                  paddingTop="10px"
-                  paddingBottom="9px"
-                  paddingLeft="xxl"
-                >
-                  Upgrade Guides
-                </StyledLink>
+                {cakes.map(item => {
+                  return (
+                    <StyledLink
+                      size="large"
+                      display="flex"
+                      textDecoration="none"
+                      paddingLeft="xxl"
+                      paddingTop="xxs"
+                      paddingBottom="zero"
+                      href={item.linkValue}
+                    >
+                      {item.value}
+                    </StyledLink>
+                  );
+                })}
               </Expandable.Content>
             </Expandable>
           </Flex.Item>
