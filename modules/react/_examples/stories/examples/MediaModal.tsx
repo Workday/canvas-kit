05a201e92b3108ced0c1d9ff15afbcd9474d7599
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-05-24 19:41:11 -0600

    docs: Change to compound components

--- modules/react/_examples/stories/examples/MediaModal.tsx
+++ modules/react/_examples/stories/examples/MediaModal.tsx
@@ -4,37 +4,49 @@ import {Box, BoxProps, Flex} from '@workday/canvas-kit-react/layout';
 import {Text} from '@workday/canvas-kit-react/text';
 import {PrimaryButton} from '@workday/canvas-kit-react/button';
 import {spaceNumbers} from '@workday/canvas-kit-react/tokens';
+import {createComponent} from '@workday/canvas-kit-react/common';
 
-const Image = (elemProps: React.ImgHTMLAttributes<HTMLImageElement>) => {
-  const [loaded, setLoaded] = React.useState(false);
+const MediaImage = createComponent('img')({
+  displayName: 'Media.Image',
+  Component: (elemProps: React.ImgHTMLAttributes<HTMLImageElement>, ref, Element) => {
+    const [loaded, setLoaded] = React.useState(false);
 
-  return (
-    <Box
-      as="img"
-      display="block"
-      backgroundColor="frenchVanilla200"
-      transition="opacity ease 200ms"
-      opacity={loaded ? 1 : 0}
-      width="100%"
-      height={293}
-      onLoad={() => setLoaded(true)}
-      {...elemProps}
-    />
-  );
-};
+    return (
+      <Box
+        as={Element}
+        ref={ref}
+        display="block"
+        backgroundColor="frenchVanilla200"
+        transition="opacity ease 200ms"
+        width="100%"
+        height={293}
+        style={{opacity: loaded ? 1 : 0}}
+        onLoad={() => setLoaded(true)}
+        {...elemProps}
+      />
+    );
+  },
+});
 
-const MediaContainer = ({children, ...elemProps}: BoxProps) => {
-  return (
-    <Box
-      width={`calc(100% + ${spaceNumbers.l * 2}px)`}
-      marginLeft={`-${spaceNumbers.l}px`}
-      marginY="s"
-      {...elemProps}
-    >
-      {children}
-    </Box>
-  );
-};
+const Media = createComponent('div')({
+  displayName: 'Media',
+  subComponents: {Image: MediaImage},
+  Component: ({children, ...elemProps}: BoxProps, ref, Element) => {
+    return (
+      <Box
+        as={Element}
+        ref={ref}
+        width={`calc(100% + ${spaceNumbers.l * 2}px)`}
+        marginLeft={`-${spaceNumbers.l}px`}
+        marginTop="m"
+        marginBottom="s"
+        {...elemProps}
+      >
+        {children}
+      </Box>
+    );
+  },
+});
 
 export const Basic = () => {
   return (
@@ -43,12 +55,12 @@ export const Basic = () => {
       <Modal.Overlay>
         <Modal.Card>
           <Modal.CloseIcon aria-label="Close" size="small" />
-          <MediaContainer>
-            <Image
+          <Media>
+            <Media.Image
               src="https://www.theuiaa.org/wp-content/uploads/2017/11/RTM19-banner-web.jpg"
               alt="Sunrise in the mountains"
             />
-          </MediaContainer>
+          </Media>
           <Modal.Heading>TED's Secret to Public Speaking</Modal.Heading>
           <Modal.Body>
             <Text as="p" marginTop={0} marginBottom="m">
