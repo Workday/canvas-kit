866d3cbfc6603724f46099e2f16f503d39162ca4
Clare Huang <clarehuang1126@gmail.com>
2023-06-05 10:00:41 -0700

    fix: Add custom fallback placements modifier (#2200)
    Fixes: #1100 
    This fix is to ensure the Popper has the ability to dynamically calculate the available spaces around the trigger and display the Popper at the proper position provided by `fallbackPlacements` array.
    
    With the new fix, Popper will dynamically check the available positions in the following order of **the initial placement**, **the opposite placement**, and **the rest of fallback placements** via the custom `fallbackPlacement` modifier.
    
    [category:Components]
    
    Release Note:
    This fix adds additional fallback placements to prevent popups from rendering off screen. This behavior is different than before and might result in a popup rendering in an unexpected position. `fallbackPlacements` was added as a prop to `Popper` and `Popup.Popper` to allow you to override the default fallback placements (which are top/right/bottom/left in that order). This shouldn't be considered a breaking change unless your popup position needs to be restricted.
    
    Co-authored-by: clare.huang <clare.huang@workday.com>
    Co-authored-by: @NicholasBoll <nicholas.boll@gmail.com>

---
+++ modules/react/popup/lib/fallbackPlacements.ts
@@ -0,0 +1,90 @@
+import * as PopperJS from '@popperjs/core';
+
+export type Placement = PopperJS.Placement; // Use template literals to make documentation list them out
+
+const getOppositePlacement = (popperPlacement: Placement): Placement => {
+  const [first, second] = popperPlacement.split('-');
+  let oppositePlacement: Placement;
+  switch (first) {
+    case 'top':
+      oppositePlacement = 'bottom';
+      break;
+    case 'bottom':
+      oppositePlacement = 'top';
+      break;
+    case 'left':
+      oppositePlacement = 'right';
+      break;
+    case 'right':
+      oppositePlacement = 'left';
+      break;
+    default:
+      oppositePlacement = 'auto';
+  }
+  if (second) {
+    oppositePlacement =
+      PopperJS.placements.find(placement => placement.includes(`${oppositePlacement}-${second}`)) ??
+      oppositePlacement;
+  }
+  return oppositePlacement;
+};
+
+const choseAvailablePlacement = (
+  placements: Placement[],
+  state: PopperJS.State,
+  preferredPlacement: Placement
+): Placement => {
+  if (placements.length === 0 || placements[0].split('-')[0] === 'auto') {
+    return preferredPlacement;
+  }
+
+  const {reference, popper} = state.rects;
+  const overflow = PopperJS.detectOverflow({...state, placement: placements[0]});
+  const direction = /left|right/.test(placements[0].split('-')[0]) ? 'horizontal' : 'vertical';
+  const isOverflowed =
+    (overflow.top > 0 || overflow.bottom > 0 || overflow.left > 0 || overflow.right > 0) &&
+    (direction === 'horizontal'
+      ? popper.height / 2 - overflow.top < reference.height ||
+        popper.height / 2 - overflow.bottom < reference.height
+      : popper.width - overflow.left < reference.width ||
+        popper.width - overflow.right < reference.width);
+  const key = placements[0].split('-')[0] as keyof PopperJS.SideObject;
+
+  if (!isOverflowed && overflow[key] <= 0) {
+    return placements[0];
+  } else {
+    return choseAvailablePlacement(placements.slice(1), state, preferredPlacement);
+  }
+};
+
+export type Options = {
+  fallbackPlacements: Placement[];
+};
+
+const name = 'fallbackModifier';
+
+export const fallbackPlacementsModifier: PopperJS.Modifier<typeof name, Options> = {
+  name: name,
+  enabled: true,
+  phase: 'main',
+  data: {
+    _skip: false,
+  },
+  fn({state, options}: PopperJS.ModifierArguments<Options>) {
+    const preferredPlacement = state.options.placement;
+    const {fallbackPlacements} = options;
+
+    const placements = [
+      preferredPlacement,
+      getOppositePlacement(preferredPlacement),
+      ...(fallbackPlacements || []),
+    ];
+
+    const placementOverride = choseAvailablePlacement(placements, state, preferredPlacement);
+    if (state.placement !== placementOverride) {
+      state.modifiersData[name]._skip = true;
+      state.placement = placementOverride;
+      state.reset = true;
+    }
+  },
+};
