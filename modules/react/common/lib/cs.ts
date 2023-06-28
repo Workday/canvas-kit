a56f895e5a3b8f315f8940f50f2c71c4b9cb2c7d
Nicholas Boll <nicholas.boll@gmail.com>
2023-06-28 10:51:09 -0600

    feat: Add static css function

---
+++ modules/react/common/lib/cs.ts
@@ -0,0 +1,307 @@
+import React from 'react';
+import {CommonStyleProps} from '@workday/canvas-kit-react/layout';
+// import {colors, space, type} from '@workday/canvas-kit-react/tokens';
+import {generateUniqueId} from '@workday/canvas-kit-react/common';
+// eslint-disable-next-line @emotion/no-vanilla
+// import {css} from '@emotion/css';
+// import { Box } from "./Box";
+// import { css } from "./styles";
+
+const css = (input: any) => `css-${generateUniqueId()}`;
+
+export const theme = {
+  palette: {
+    primary: {
+      lightest: 'var(--ck-palette-primary-lightest)',
+      light: 'var(--ck-palette-primary-light)',
+      main: 'var(--ck-palette-primary-main)',
+      dark: 'var(--ck-palette-primary-dark)',
+      darkest: 'var(--ck-palette-primary-darkest)',
+    },
+    alert: {
+      lightest: 'var(--ck-palette-alert-lightest)',
+      light: 'var(--ck-palette-alert-light)',
+      main: 'var(--ck-palette-alert-main)',
+      dark: 'var(--ck-palette-alert-dark)',
+      darkest: 'var(--ck-palette-alert-darkest)',
+    },
+    error: {
+      lightest: 'var(--ck-palette-error-lightest)',
+      light: 'var(--ck-palette-error-light)',
+      main: 'var(--ck-palette-error-main)',
+      dark: 'var(--ck-palette-error-dark)',
+      darkest: 'var(--ck-palette-error-darkest)',
+    },
+    success: {
+      lightest: 'var(--ck-palette-success-lightest)',
+      light: 'var(--ck-palette-success-light)',
+      main: 'var(--ck-palette-success-main)',
+      dark: 'var(--ck-palette-success-dark)',
+      darkest: 'var(--ck-palette-success-darkest)',
+    },
+    neutral: {
+      lightest: 'var(--ck-palette-neutral-lightest)',
+      light: 'var(--ck-palette-neutral-light)',
+      main: 'var(--ck-palette-neutral-main)',
+      dark: 'var(--ck-palette-neutral-dark)',
+      darkest: 'var(--ck-palette-neutral-darkest)',
+    },
+    common: {
+      focusOutline: '#0875e1',
+    },
+  },
+} as const;
+
+// get all tokens for lookup... We might need to watch for duplicates and separate by key
+const tokens = {}; /*{
+  ...colors,
+  ...space,
+  ...type.properties.fontFamilies,
+  // etc
+};*/
+
+type NestedStyles =
+  | string
+  | number
+  | undefined
+  | (CommonStyleProps & {
+      [propertyName: string]: NestedStyles;
+    });
+
+export interface StyleProps extends CommonStyleProps {
+  [propertiesName: string]: NestedStyles;
+}
+
+function replaceWithToken<T>(value: T): T {
+  return (tokens as any)[value] || value;
+}
+
+// This function current replaces token values like "blueberry400" with a color value
+// but it can be used to replace other things if we want or remove it entirely
+
+function replaceAllWithTokens<T extends unknown>(obj: T): T {
+  if (typeof obj === 'object') {
+    const converted = {};
+    for (const key in obj) {
+      if ((obj as Object).hasOwnProperty(key)) {
+        (converted as any)[key] = replaceAllWithTokens(obj[key]);
+      }
+    }
+    return converted as T;
+  }
+  return replaceWithToken(obj);
+}
+
+export type CS = string | Record<string, string>;
+
+export type CsVars<T extends string> = Record<T, string> & {
+  (input: Partial<Record<T, string>>): Record<T, string>;
+};
+
+/**
+ * Take a CSS Variable name and return a variable property
+ *
+ * ```ts
+ * const myVars = createVars('color')
+ *
+ * const myStyles = cs({
+ *   color: cssVar(myVars.color) // color: var(--{hash}-color)
+ * })
+ * ```
+ */
+export function cssVar(input: string) {
+  return `var(${input})`;
+}
+
+/**
+ * Create temporary CSS variables to use in components. The CSS variable names will
+ * be unique at runtime to avoid collisions. The return value is a function and a
+ * Map. The function can be used to pass in values from JavaScript. The function will
+ * return a map of variable keys to CSS Variable names.
+ *
+ * ```ts
+ * // creates a `color` and `background` variable
+ * const myVars = createVars('color', 'background')
+ *
+ * // 'color' is a typed property. The type is `string`
+ * console.log(myValues.color) // `'var(--{hash}-color)'`
+ *
+ * // 'color' is a typed property. The type is `string?`
+ * // The returned object can be assigned to the `style` property of an element
+ * console.log(myVars({ color: 'red' })) //  `{'--{hash}-color': 'red'}`
+ *
+ * const div = document.createElement('div')
+ * div.style = myVars({ color: 'red' }) // <div style="--{hash}-color: red;" />
+ * ```
+ */
+export function createVars<T extends string>(...args: T[]): CsVars<T> {
+  const id = generateUniqueId();
+  const result = (input: Record<T, string>) => {
+    const vars = {};
+    args.forEach(key => {
+      if (input[key]) {
+        // @ts-ignore TS complains about `key` not in object `{}`
+        vars[result[key]] = input[key];
+      }
+    });
+    return vars;
+  };
+
+  args.forEach(key => {
+    // @ts-ignore
+    result[key] = `--${id}-${key}`;
+  }, {});
+
+  return result as CsVars<T>;
+}
+
+type ModifierConfig = Record<string, Record<string, CS>>;
+
+type ModifierValues<T extends ModifierConfig> = {
+  [P in keyof T]: keyof T[P];
+};
+
+type ModifierReturn<T extends ModifierConfig> = T &
+  ((modifiers: Partial<ModifierValues<T>>) => string);
+
+export function createModifiers<T extends ModifierConfig>(input: T): ModifierReturn<T> {
+  const modifierFn = (modifiers: Partial<ModifierValues<T>>) => {
+    return Object.keys(modifiers)
+      .filter(key => input[key] && (input as any)[key][modifiers[key]])
+      .map(key => (input as any)[key][modifiers[key]])
+      .join(' ');
+  };
+
+  Object.keys(input).forEach(key => {
+    // @ts-ignore TypeScript makes it a pain to deal with index types
+    modifierFn[key] = input[key];
+  });
+
+  return modifierFn as ModifierReturn<T>;
+}
+
+function isCsPropsReturn(input: {}): input is CsToPropsReturn {
+  return input.hasOwnProperty('style') || input.hasOwnProperty('className');
+}
+
+export type CsToPropsReturn = {className?: string; style?: React.CSSProperties};
+/**
+ * A function that takes in a single input, or an array. The type of the input is either:
+ *
+ * - `string` - it represents a CSS class name
+ * - `undefined` - there is no value. This is provided for convenience for developers to not have to
+ *   filter out undefined
+ * - `{--{hash}-{varName}: {value}}` - a `Map` of CSS variable to values
+ * - `{style: ..., className: ...}` an object already returned by another `csToProps` function call
+ *   (for nesting)
+ * - An array containing any of the above. This will recurse over each entry to produce a single,
+ *   reduced `{style: ..., className: ...}` object
+ * @param input
+ * @returns
+ */
+export function csToProps(
+  input: undefined | CS | CsToPropsReturn | (undefined | CS | CsToPropsReturn)[]
+): CsToPropsReturn {
+  // input is a string, so it must only be a class name
+  if (typeof input === 'string') {
+    return {className: input};
+  }
+
+  // input isn't defined, so we'll return an empty object
+  if (!input) {
+    return {};
+  }
+
+  // A nested `csToProps`, we'll simply return with no modification
+  if (isCsPropsReturn(input)) {
+    return input;
+  }
+
+  // At this point, `input` is either an array or a `Record<T, string>`. If it isn't an array, it
+  // must be a style object for setting CSS variables. So set the `style` attribute
+  if (!Array.isArray(input)) {
+    return {style: input};
+  }
+
+  // An array. it is the only thing left. We iterate and recurse over the array to produce a single
+  // object with `style` and `className` attributes to spread on an element
+  return input.map(csToProps).reduce((result, val) => {
+    return {
+      className: [result.className, val.className].filter(v => v).join(' '),
+      style: {...result.style, ...val.style},
+    };
+  }, {} as CsToPropsReturn);
+}
+
+export interface CSProps {
+  cs?: CS | (CS | undefined)[];
+}
+
+/**
+ * The cs function is curried into 2 parts. The first function could be done at
+ * build time. The returned function combines classnames and will remain as a
+ * small runtime.
+ */
+
+export function cs(styles: StyleProps, ...args: CS[]): CS {
+  const convertedStyles = replaceAllWithTokens(styles);
+
+  const className = css(convertedStyles as any);
+  // const className = emotionCSS(convertedStyles as any);
+
+  return args.length ? `${args.map(s => s).join(' ')} ${className}` : className;
+}
+
+/**
+ * React hook to apply CSS class names and dynamic CSS variables
+ *
+ * ```tsx
+ * const MyComponent = (props: any) => {
+ *   return <div {...useCs(props)} />
+ * }
+ * ```
+ *
+ * It will return an object with `className` and `style` attributes.
+ * If a `className` is provided to the component, it will merge the classNames.
+ * If a `style` is provided to the component, it will merge the styles.
+ *
+ * ```tsx
+ * const vars = createVars('background')
+ * const styles = cs({
+ *   color: vars.color
+ * })
+ * <MyComponent
+ *   className="foobar"
+ *   style={{ padding: 10 }}
+ *   cs={styles}
+ *   csVars
+ ={vars({background: 'red'})}
+ * />
+ *
+ * // Output
+ * <div
+ *   className="foobar {hashedClassName}"
+ *   style="padding: 10px; --{hash}-background: red;"
+ * />
+ * ```
+ */
+export function useCs<
+  T extends CSProps & {
+    className?: string | undefined;
+
+    style?: React.CSSProperties | undefined;
+  }
+>({cs, style, className, ...props}: T) {
+  return {
+    ...csToProps(cs),
+    ...props,
+  } as Omit<T, 'cs' | 'csVars'>;
+}
+
+/**
+ * Takes in a CSS Variable created by `createVars` and a fallback value and returns `var({input},
+ * {fallback})`. This can be used inside `cs` functions to generated fallback values for properties.
+ */
+export function fallback(input: string, fallbackValue: string) {
+  return `var(${input}, ${fallbackValue})`;
+}
---
+++ modules/react/common/spec/cs.spec.ts
@@ -0,0 +1,143 @@
+import {setUniqueSeed, resetUniqueIdCount} from '@workday/canvas-kit-react/common';
+import {cs, cssVar, createVars, createModifiers, csToProps, CS, fallback} from '../lib/cs';
+import {expectTypeOf} from 'expect-type';
+import {Properties} from 'csstype';
+
+describe.only('cs', () => {
+  beforeEach(() => {
+    setUniqueSeed('a');
+    resetUniqueIdCount();
+  });
+
+  describe('cs', () => {
+    it('should accept an object of CSS Properties', () => {
+      type Input = Parameters<typeof cs>[0];
+      type PositionProperty = Input['position'];
+      expectTypeOf<PositionProperty>().toMatchTypeOf<Properties['position']>();
+    });
+  });
+
+  describe('cssVar', () => {
+    it('should return a css var wrapper around the input', () => {
+      const input = '--my-var';
+      const expected = cssVar(input);
+
+      expect(expected).toEqual('var(--my-var)');
+    });
+  });
+
+  describe('fallback', () => {
+    it('should return a css var wrapper that includes a fallback', () => {
+      const input = '--my-var';
+      const expected = fallback(input, 'red');
+
+      expect(expected).toEqual('var(--my-var, red)');
+    });
+  });
+
+  describe('createVars', () => {
+    it('should create an object with a key of the "color"', () => {
+      const myVars = createVars('color');
+
+      expect(myVars).toHaveProperty('color', '--a0-color');
+    });
+
+    it('should return a type object with keys matching each input', () => {
+      const myVars = createVars('color', 'fill');
+
+      expectTypeOf(myVars).toMatchTypeOf<{color: string; fill: string}>();
+    });
+
+    it('should return a function type that takes an object of each config and string value', () => {
+      const myVars = createVars('color', 'fill');
+
+      type Input = Parameters<typeof myVars>[0];
+      expectTypeOf<Input>().toMatchTypeOf<{color?: string; fill?: string}>();
+    });
+
+    it('should return a function that takes in an object with the values and return a style object', () => {
+      const myVars = createVars('color');
+
+      expect(myVars({color: 'red'})).toHaveProperty('--a0-color', 'red');
+    });
+  });
+
+  describe('createModifiers', () => {
+    const myModifiers = createModifiers({
+      size: {
+        large: cs({fontSize: '1.5rem'}),
+        small: cs({fontSize: '0.8rem'}),
+      },
+    });
+
+    it('should return an object with class names for each modifier', () => {
+      expect(myModifiers).toHaveProperty('size.large', expect.stringContaining('css'));
+      expect(myModifiers).toHaveProperty('size.small', expect.stringContaining('css'));
+    });
+
+    it('should return a function that in an object of keys with limited values', () => {
+      expect(myModifiers({size: 'large'})).toEqual(myModifiers.size.large);
+    });
+
+    it('should return a function with a type that expects optional parameters matching the config object', () => {
+      type Input = Parameters<typeof myModifiers>[0];
+      expectTypeOf<Input>().toMatchTypeOf<{size?: 'large' | 'small'}>();
+    });
+
+    it('should return an object with a type that matches the input object type', () => {
+      expectTypeOf(myModifiers.size).toMatchTypeOf<{large: CS; small: CS}>();
+    });
+  });
+
+  describe('csToProps', () => {
+    it('should return a className', () => {
+      const input = 'foo';
+
+      expect(csToProps(input)).toHaveProperty('className', 'foo');
+    });
+
+    it('should return the className of a modifier', () => {
+      const myModifiers = createModifiers({
+        size: {
+          large: 'foo',
+          small: 'bar',
+        },
+      });
+      const input = myModifiers({size: 'small'});
+
+      expect(csToProps(input)).toHaveProperty('className', 'bar');
+    });
+
+    it('should return the style that sets variables', () => {
+      const myVariables = createVars('color');
+      const input = myVariables({color: 'red'});
+
+      expect(csToProps(input)).toHaveProperty('style', {'--a0-color': 'red'});
+    });
+
+    it('should merge multiple classNames together', () => {
+      const input = ['foo', 'bar'];
+
+      expect(csToProps(input)).toHaveProperty('className', 'foo bar');
+    });
+
+    it('should handle undefined parameters', () => {
+      const input = ['foo', undefined];
+
+      expect(csToProps(input)).toHaveProperty('className', 'foo');
+    });
+
+    it('should handle nested csToProps for composition', () => {
+      const myVariables = createVars('color');
+      const innerVariables = createVars('fill');
+      const actual = csToProps([
+        'foo',
+        myVariables({color: 'red'}),
+        csToProps(['bar', innerVariables({fill: 'blue'})]),
+      ]);
+
+      expect(actual).toHaveProperty('className', 'foo bar');
+      expect(actual).toHaveProperty('style', {'--a0-color': 'red', '--a1-fill': 'blue'});
+    });
+  });
+});
