db418ab37cb8a7cd81299a13224e2db71e13995d
Abhijit Misra <abhijit.misra@workday.com>
2023-08-03 09:15:21 +0100

    fix: Breadcrumbs first item gets cut-off when two items in array

--- modules/react/breadcrumbs/lib/hooks/utils/index.ts
+++ modules/react/breadcrumbs/lib/hooks/utils/index.ts
@@ -7,10 +7,10 @@ export const reorganizeHiddenItems = (
   const totalSize = items.length;
   const itemSpace = totalSize - hiddenIds.length;
 
-  if (itemSpace <= 2) {
+  if (totalSize > 2 && itemSpace <= 2) {
     // Keep only last items if there is place for two or less items
     updatedHiddenIds = items.slice(0, totalSize - 1).map(getId);
-  } else if (itemSpace === 3) {
+  } else if (totalSize > 3 && itemSpace === 3) {
     // Always keep first and last item if there is place for 3 items
     updatedHiddenIds = items.slice(1, totalSize - 1).map(getId);
   } else if (itemSpace > 3 && itemSpace < totalSize) {
