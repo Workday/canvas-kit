c66d30fdfffbeacfa302b672f7a888e117c80056
manuel.carrera <manuel.carrera@workday.com>
2023-08-31 16:23:47 -0600

    fix: Update mdx file

--- modules/react/select/lib/hooks/useSelectItem.tsx
+++ modules/react/select/lib/hooks/useSelectItem.tsx
@@ -1,7 +1,7 @@
 import {createElemPropsHook} from '@workday/canvas-kit-react/common';
 import {useSelectModel} from './useSelectModel';
 
-/** Adds the correct aria role for the select option */
+/** Adds the correct aria role for the select item */
 export const useSelectItem = createElemPropsHook(useSelectModel)(({state}) => {
   return {
     role: 'option',
