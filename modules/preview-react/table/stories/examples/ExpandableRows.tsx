ecc1b9fd7f6b41b58d91faefcd694e22e091e45b
William Stanton <40372497+williamjstanton@users.noreply.github.com>
2023-11-13 07:46:33 -0800

    Adding new interactive pattern examples

---
+++ modules/preview-react/table/stories/examples/ExpandableRows.tsx
@@ -0,0 +1,178 @@
+import React from 'react';
+
+import {Table} from '@workday/canvas-kit-preview-react/table';
+import {Heading, Subtext} from '@workday/canvas-kit-react/text';
+import {colors} from '@workday/canvas-kit-react/tokens';
+import {useUniqueId} from '@workday/canvas-kit-react/common';
+import {SecondaryButton} from '@workday/canvas-kit-react/button';
+import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {chevronDownIcon, chevronRightIcon} from '@workday/canvas-system-icons-web';
+
+function ExpandableRow(props) {
+  const [rowExpanded, setRowExpanded] = React.useState(false);
+  const listItemStyles = {
+    display: 'inline',
+    border: `1px solid ${colors.blueberry300}`,
+    margin: '0 .5em',
+    padding: '.25em .5em',
+  };
+
+  const expansion = (
+    <Table.Row>
+      <Table.Cell colspan={10} style={{border: `1px solid ${colors.blueberry400}`}}>
+        <Subtext as="h4" size="medium">
+          Order status:
+        </Subtext>
+        <ul>
+          <li style={listItemStyles} aria-current="true">
+            Order Placed
+          </li>
+          <li style={listItemStyles}>Order Fulfilled</li>
+          <li style={listItemStyles}>Shipped: In Transit</li>
+          <li style={listItemStyles}>Delivered</li>
+        </ul>
+      </Table.Cell>
+    </Table.Row>
+  );
+
+  function expandToggleHandler() {
+    setRowExpanded(prev => (prev ? false : true));
+  }
+
+  return (
+    <>
+      <Table.Row gridTemplateColumns="4em 25% 25% 25% 25%">
+        <Table.Cell width={'3.5em'}>
+          <Tooltip title={`${props.rowData.brand} ${props.rowData.model}`}>
+            <SecondaryButton
+              icon={rowExpanded ? chevronDownIcon : chevronRightIcon}
+              aria-expanded={rowExpanded ? 'true' : 'false'}
+              onClick={expandToggleHandler}
+            />
+          </Tooltip>
+        </Table.Cell>
+        <Table.Header
+          scope="row"
+          left="0"
+          backgroundColor={colors.soap100}
+          borderRight={`2px solid ${colors.soap400}`}
+        >
+          {props.rowData.brand}
+        </Table.Header>
+        <Table.Cell>{props.rowData.model}</Table.Cell>
+        <Table.Cell>{props.rowData.year}</Table.Cell>
+        <Table.Cell>{props.rowData.price}</Table.Cell>
+      </Table.Row>
+      {rowExpanded && expansion}
+    </>
+  );
+}
+
+export const ExpandableRows = () => {
+  const headingID = useUniqueId();
+  const exampleData = [
+    {
+      id: useUniqueId(),
+      brand: 'Porsche',
+      model: '992 911 GT3',
+      year: '2022',
+      price: 'Starts at $160,000',
+      engine: '4.0L Flat 6',
+      transmission: 'PDK or 7-Speed Manual',
+      horsepower: '502hp',
+      torque: '346 lb-ft',
+      curbWeight: '3,164 lbs',
+    },
+    {
+      id: useUniqueId(),
+      brand: 'BMW',
+      model: 'M5 Competition',
+      year: '2018',
+      price: 'Starts at $105,000',
+      engine: 'Twin-Turbo 4.4L V8',
+      transmission: 'Automatic',
+      horsepower: '627hp',
+      torque: '553 lb-ft',
+      curbWeight: '4,345 lbs',
+    },
+    {
+      id: useUniqueId(),
+      brand: 'Audi',
+      model: 'R8',
+      year: '2022',
+      price: 'Starts at $148,000',
+      engine: '5.2L V10',
+      transmission: 'Automatic',
+      horsepower: '562hp',
+      torque: '408 lb-ft',
+      curbWeight: '3,594 lbs',
+    },
+    {
+      id: useUniqueId(),
+      brand: 'Lotus',
+      model: 'Emira',
+      year: '2023',
+      price: 'Starts at $78,000',
+      engine: 'Supercharged 3.5L V6',
+      transmission: 'Automatic or 6-Speed Manual',
+      horsepower: '400hp',
+      torque: '317 lb-ft',
+      curbWeight: '3520 lbs',
+    },
+    {
+      id: useUniqueId(),
+      brand: 'Toyota',
+      model: 'Supra',
+      year: '1998',
+      price: '$40,000 - $80,000',
+      engine: '3.0L Inline 6',
+      transmission: 'Automatic or 6-Speed Manual',
+      horsepower: '320hp',
+      torque: '315 lb-ft',
+      curbWeight: '3,599 lbs',
+    },
+    {
+      id: useUniqueId(),
+      brand: 'Nissan',
+      model: 'Skyline GT-R',
+      year: '1994',
+      price: '$45,000 - $90,000',
+      engine: '2.6L Twin-Turbo Inline 6',
+      transmission: '5-Speed Manual',
+      horsepower: '276hp',
+      torque: '260 lb-ft',
+      curbWeight: '3,153 lbs',
+    },
+  ];
+
+  return (
+    <>
+      <Heading as="h3" id={headingID} size="small" marginBottom="s">
+        Showroom Inventory
+      </Heading>
+      <Table aria-labelledby={headingID}>
+        <Table.Head>
+          <Table.Row gridTemplateColumns="4em 25% 25% 25% 25%">
+            <Table.Cell></Table.Cell>
+            <Table.Header
+              scope="col"
+              left="0"
+              backgroundColor={colors.soap100}
+              borderRight={`2px solid ${colors.soap400}`}
+            >
+              Brand
+            </Table.Header>
+            <Table.Header scope="col">Model</Table.Header>
+            <Table.Header scope="col">Year</Table.Header>
+            <Table.Header scope="col">Price</Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {exampleData.map((item, index) => (
+            <ExpandableRow key={item.id} rowData={item} />
+          ))}
+        </Table.Body>
+      </Table>
+    </>
+  );
+};
---
+++ modules/preview-react/table/stories/examples/SortableColumnHeaders.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+
+import {Table} from '@workday/canvas-kit-preview-react/table';
+import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {TertiaryButton} from '@workday/canvas-kit-react/button';
+import {Text} from '@workday/canvas-kit-react/text';
+import {sortDownIcon, sortUpIcon} from '@workday/canvas-system-icons-web';
+
+const exampleData = [
+  {country: 'Australia', capital: 'Canberra', pop: 25690000},
+  {country: 'Bahamas', capital: 'Nassau', pop: 407906},
+  {country: 'Canada', capital: 'Ottawa', pop: 38250000},
+  {country: 'Fiji', capital: 'Suva', pop: 924610},
+  {country: 'Ghana', capital: 'Accra', pop: 32830000},
+  {country: 'Hong Kong', capital: 'City of Victoria', pop: 7413000},
+  {country: 'India', capital: 'New Delhi', pop: 1408000000},
+  {country: 'Ireland', capital: 'Dublin', pop: 5033000},
+  {country: 'Jamaica', capital: 'Kingston', pop: 2828000},
+  {country: 'Kenya', capital: 'Nairobi', pop: 53010000},
+  {country: 'Micronesia', capital: 'Palikir', pop: 113131},
+  {country: 'New Zealand', capital: 'Wellington', pop: 5123000},
+  {country: 'Philippines', capital: 'Manila', pop: 113900000},
+  {country: 'Puerto Rico', capital: 'San Juan', pop: 3264000},
+  {country: 'Samoa', capital: 'Apia', pop: 218764},
+  {country: 'Singapore', capital: 'Singapore', pop: 5454000},
+  {country: 'Tanzania', capital: 'Dodoma', pop: 63590000},
+  {country: 'United Kingdom', capital: 'London', pop: 67330000},
+  {country: 'United States', capital: 'Washington, D.C.', pop: 331900000},
+  {country: 'Zimbabwe', capital: 'Harare', pop: 15990000},
+];
+
+// copy original data to sort later
+const sortedData = exampleData.slice();
+
+function headerRowReducer(state, action) {
+  console.log(action.payload);
+  switch (action.column) {
+    case 'Country':
+      action.payload.sort((a, b) => {
+        const x = a.country.toLowerCase();
+        const y = b.country.toLowerCase();
+        if (x < y) return -1;
+        if (x > y) return 1;
+        return 0;
+      });
+      state.col1 === 'ascending' && action.payload.reverse();
+      return {
+        col1: state.col1 === 'ascending' ? 'descending' : 'ascending',
+        col2: 'none',
+        col3: 'none',
+      };
+    case 'Capital':
+      action.payload.sort((a, b) => {
+        const x = a.capital.toLowerCase();
+        const y = b.capital.toLowerCase();
+        if (x < y) return -1;
+        if (x > y) return 1;
+        return 0;
+      });
+      state.col2 === 'ascending' && action.payload.reverse();
+      return {
+        col1: 'none',
+        col2: state.col2 === 'ascending' ? 'descending' : 'ascending',
+        col3: 'none',
+      };
+    case 'Population':
+      action.payload.sort((a, b) => a.pop - b.pop);
+      state.col3 === 'ascending' && action.payload.reverse();
+      return {
+        col1: 'none',
+        col2: 'none',
+        col3: state.col3 === 'ascending' ? 'descending' : 'ascending',
+      };
+    default:
+      return {
+        col1: 'none',
+        col2: 'none',
+        col3: 'none',
+      };
+  }
+}
+
+function SortableColumnHeader(props) {
+  // PROPS:
+  // sortOrder : string
+  // onSortAction : function pointer
+  const isAscending = props.sortOrder === 'ascending';
+
+  function showSortIcon() {
+    // determine which icon to show in col header
+    if (isAscending) return sortUpIcon;
+    if (props.sortOrder === 'descending') return sortDownIcon;
+    return undefined;
+  }
+
+  function clickHandler() {
+    // lift up column name to parent
+    props.onSortAction(props.children);
+  }
+
+  return (
+    <Table.Header scope="col" aria-sort={props.sortOrder}>
+      <Tooltip type="describe" title={`Sort ${isAscending ? 'descending' : 'ascending'}`}>
+        <TertiaryButton icon={showSortIcon()} iconPosition="end" onClick={clickHandler}>
+          {props.children}
+        </TertiaryButton>
+      </Tooltip>
+    </Table.Header>
+  );
+}
+
+export const SortableColumnHeaders = () => {
+  const [headerRowState, headerRowDispatch] = React.useReducer(headerRowReducer, {
+    col1: 'none',
+    col2: 'none',
+    col3: 'none',
+  });
+
+  function sortColumnHandler(columnName) {
+    // columnName : string
+
+    headerRowDispatch({
+      column: columnName,
+      payload: sortedData,
+    });
+  }
+
+  return (
+    <Table>
+      <Table.Caption>Population listed by Country</Table.Caption>
+      <Table.Head>
+        <Table.Row>
+          <SortableColumnHeader sortOrder={headerRowState.col1} onSortAction={sortColumnHandler}>
+            Country
+          </SortableColumnHeader>
+          <SortableColumnHeader sortOrder={headerRowState.col2} onSortAction={sortColumnHandler}>
+            Capital
+          </SortableColumnHeader>
+          <SortableColumnHeader sortOrder={headerRowState.col3} onSortAction={sortColumnHandler}>
+            Population (2021)
+          </SortableColumnHeader>
+        </Table.Row>
+      </Table.Head>
+      <Table.Body>
+        {sortedData.map(i => {
+          return (
+            <Table.Row key={i.country}>
+              <Table.Header scope="row">
+                <Text paddingInlineStart="0.75rem">{i.country}</Text>
+              </Table.Header>
+              <Table.Cell>
+                <Text paddingInlineStart="0.75rem">{i.capital}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text paddingInlineStart="0.75rem">{i.pop}</Text>
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  );
+};
