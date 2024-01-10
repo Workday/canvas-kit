740a4db9b26d491fe9ebd66eb93a11beb2bb3d98
manuel.carrera <manuel.carrera@workday.com>
2024-01-10 14:14:27 -0700

    fix: Update label text in formfield

--- modules/preview-react/form-field/lib/FormFieldLabel.tsx
+++ modules/preview-react/form-field/lib/FormFieldLabel.tsx
@@ -4,9 +4,9 @@ import {createSubcomponent, ExtractProps} from '@workday/canvas-kit-react/common
 import {type, space} from '@workday/canvas-kit-react/tokens';
 import {LabelText, Text} from '@workday/canvas-kit-react/text';
 import {useFormFieldLabel, useFormFieldModel} from './hooks';
-import {createModifiers, createStyles, cssVar} from '@workday/canvas-kit-styling';
+import {createModifiers, createStencil, createStyles, cssVar} from '@workday/canvas-kit-styling';
 import {mergeStyles} from '@workday/canvas-kit-react/layout';
-import {brand} from '@workday/canvas-tokens-web';
+import {base, brand, system} from '@workday/canvas-tokens-web';
 
 export interface FormFieldLabelProps extends ExtractProps<typeof LabelText, never> {
   /**
@@ -44,14 +44,51 @@ const labelModifiers = createModifiers({
   },
 });
 
-export const FormFieldLabel = createSubcomponent(LabelText)({
+const labelTextStencil = createStencil({
+  base: {
+    ...system.type.subtext.large,
+    color: base.blackPepper300,
+  },
+  modifiers: {
+    orientation: {
+      horizontal: {
+        float: 'left',
+        maxHeight: space.xl,
+      },
+      vertical: {
+        width: '100%',
+      },
+    },
+    variant: {
+      error: {color: base.cinnamon500},
+      hint: {color: base.licorice300},
+      inverse: {color: base.frenchVanilla100},
+    },
+    disabled: {
+      true: {
+        cursor: 'default',
+        color: base.licorice100,
+      },
+    },
+  },
+  compound: [
+    {
+      modifiers: {variant: 'inverse', disabled: 'true'},
+      styles: {
+        opacity: system.opacity.disabled,
+        color: base.frenchVanilla100,
+      },
+    },
+  ],
+});
+
+export const FormFieldLabel = createSubcomponent('label')({
   displayName: 'FormField.Label',
   modelHook: useFormFieldModel,
   elemPropsHook: useFormFieldLabel,
 })<FormFieldLabelProps>(({children, ...elemProps}, Element, model) => {
   return (
-    <LabelText
-      as={Element}
+    <Element
       {...mergeStyles(elemProps, [
         labelStyles,
         labelModifiers({orientation: model.state.orientation}),
@@ -63,6 +100,6 @@ export const FormFieldLabel = createSubcomponent(LabelText)({
           *
         </Text>
       )}
-    </LabelText>
+    </Element>
   );
 });
