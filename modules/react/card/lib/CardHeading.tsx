e3e9cb3a795da4da0c415ab8a5ae804f81a53736
Alan B Smith <a.bax.smith@gmail.com>
2024-01-11 13:32:55 -0700

    chore: Refactor Card Styles (#2471)
    Resolves: #2400
    
    This PR:
    
    - Updates Card to use CSS vars
    - Updates Card styles to use new styling utils
    - Adds the `cs` prop
    - Updates the docs
    
    [category:Components]
    
    Co-authored-by: @RayRedGoose <RayRedGoose@users.noreply.github.com>

--- modules/react/card/lib/CardHeading.tsx
+++ modules/react/card/lib/CardHeading.tsx
@@ -1,29 +1,34 @@
 import * as React from 'react';
+
 import {createComponent} from '@workday/canvas-kit-react/common';
-import {BoxProps} from '@workday/canvas-kit-react/layout';
+import {BoxProps, mergeStyles} from '@workday/canvas-kit-react/layout';
 import {BodyText} from '@workday/canvas-kit-react/text';
+import {createStyles} from '@workday/canvas-kit-styling';
+import {system} from '@workday/canvas-tokens-web';
 
 export interface CardHeadingProps extends BoxProps {
   /**
    * The id of the Card heading. Tie this to an `aria-labelledby` for accessibility.
    */
   id?: string;
-
   children?: React.ReactNode;
 }
+
+const cardHeadingBaseStyles = createStyles({
+  fontWeight: system.fontWeight.bold,
+  marginBottom: system.space.x6,
+  marginTop: 0,
+});
 
 export const CardHeading = createComponent('h3')({
   displayName: 'Card.Heading',
   Component: ({children, ...elemProps}: CardHeadingProps, ref, Element) => {
     return (
       <BodyText
-        ref={ref}
         size="large"
-        fontWeight="bold"
-        marginBottom="m"
-        marginTop={0}
+        ref={ref}
         as={Element}
-        {...elemProps}
+        {...mergeStyles(elemProps, cardHeadingBaseStyles)}
       >
         {children}
       </BodyText>
