dcffd571c7c244a180f860021d5053ce4af0062f
manuel.carrera <manuel.carrera@workday.com>
2024-01-16 09:34:27 -0700

    fix: Prevent dialog from closing when a combobox item is clicked

--- modules/react/popup/stories/stories_testing.tsx
+++ modules/react/popup/stories/stories_testing.tsx
@@ -5,6 +5,7 @@ import {TextInput} from '@workday/canvas-kit-react/text-input';
 import {FormField} from '@workday/canvas-kit-react/form-field';
 import {Tooltip} from '@workday/canvas-kit-react/tooltip';
 import {DeleteButton, SecondaryButton, TertiaryButton} from '@workday/canvas-kit-react/button';
+import {Combobox} from '@workday/canvas-kit-react/combobox';
 import {useMount} from '@workday/canvas-kit-react/common';
 import {Flex} from '@workday/canvas-kit-react/layout';
 import {
@@ -328,6 +329,41 @@ export const TooltipReturnFocus = () => {
           <Popup.CloseIcon aria-label="Close" />
           <Popup.Heading>Popup</Popup.Heading>
           <Popup.Body>Contents</Popup.Body>
+        </Popup.Card>
+      </Popup.Popper>
+    </Popup>
+  );
+};
+
+export const ComboboxWithinPopup = () => {
+  const model = usePopupModel();
+
+  useCloseOnOutsideClick(model);
+  useCloseOnEscape(model);
+  useInitialFocus(model);
+  useReturnFocus(model);
+
+  return (
+    <Popup model={model}>
+      <Popup.Target as={SecondaryButton}>Open Popup</Popup.Target>
+      <Popup.Popper>
+        <Popup.Card>
+          <Popup.CloseIcon aria-label="Close" />
+          <Popup.Heading>Popup With Combobox</Popup.Heading>
+          <Popup.Body>
+            <FormField label="Choose Your Food">
+              <Combobox items={['Pizza', 'Cheeseburger', 'Fries', 'Hot Dog']}>
+                <Combobox.Input as={TextInput} />
+                <Combobox.Menu.Popper>
+                  <Combobox.Menu.Card>
+                    <Combobox.Menu.List>
+                      {item => <Combobox.Menu.Item>{item}</Combobox.Menu.Item>}
+                    </Combobox.Menu.List>
+                  </Combobox.Menu.Card>
+                </Combobox.Menu.Popper>
+              </Combobox>
+            </FormField>
+          </Popup.Body>
         </Popup.Card>
       </Popup.Popper>
     </Popup>
