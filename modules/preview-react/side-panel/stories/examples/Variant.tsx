3ff95fd56fbe1dd245245fb1af057af7da292240
William Stanton <40372497+williamjstanton@users.noreply.github.com>
2024-01-24 08:05:32 -0800

    Update Variant.tsx

--- modules/preview-react/side-panel/stories/examples/Variant.tsx
+++ modules/preview-react/side-panel/stories/examples/Variant.tsx
@@ -7,7 +7,7 @@ import {
 } from '@workday/canvas-kit-preview-react/side-panel';
 import {Flex} from '@workday/canvas-kit-react/layout';
 import {Text} from '@workday/canvas-kit-react/text';
-import {CanvasProvider} from '@workday/canvas-kit-react/common';
+import {AccessibleHide, CanvasProvider} from '@workday/canvas-kit-react/common';
 // local helper hook for setting content direction;
 import {useDirection} from './useDirection';
 
@@ -18,23 +18,25 @@ export const AlternatePanel = () => {
     expanded ? 'expanded' : 'collapsed'
   );
 
+  const expandedContent = (
+    <Flex alignItems="center" paddingY="s" paddingX="s">
+      <Text as="h3" typeLevel="body.large" color="licorice500" fontWeight="bold" {...labelProps}>
+        Alternate Panel
+      </Text>
+    </Flex>
+  );
+
   return (
     <CanvasProvider theme={{canvas: {direction}}}>
       <Flex height={320} backgroundColor="soap100">
         <SidePanel {...panelProps} onStateTransition={setPanelState} variant="alternate">
           <SidePanel.ToggleButton {...controlProps} />
-          {panelState === 'expanded' && (
-            <Flex alignItems="center" paddingY="s" paddingX="s">
-              <Text
-                as="h3"
-                typeLevel="body.large"
-                color="licorice500"
-                fontWeight="bold"
-                {...labelProps}
-              >
-                Alternate Panel
-              </Text>
-            </Flex>
+          {panelState === 'expanded' ? (
+            expandedContent
+          ) : (
+            <AccessibleHide {...labelProps} aria-hidden="true">
+              Alternate Panel
+            </AccessibleHide>
           )}
         </SidePanel>
         <Flex
