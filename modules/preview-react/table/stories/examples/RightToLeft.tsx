61542e72063ab73975d64c4863792481f09e67c9
Matthew Rust <matthew.rust@workday.com>
2024-02-07 09:32:14 -0800

    fix(preview): Add rtl table storybook example

---
+++ modules/preview-react/table/stories/examples/RightToLeft.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {Table} from '@workday/canvas-kit-preview-react/table';
+import {CanvasProvider, ContentDirection} from '@workday/canvas-kit-react/common';
+
+export const RightToLeft = () => {
+  return (
+    <CanvasProvider theme={{canvas: {direction: ContentDirection.RTL}}}>
+      <Table>
+        <Table.Caption>משקאות קפה וגדלים</Table.Caption>
+        <Table.Head>
+          <Table.Row>
+            <Table.Header scope="col" backgroundColor="soap100">
+              מַשׁקֶה
+            </Table.Header>
+            <Table.Header scope="col" backgroundColor="soap100">
+              גודל
+            </Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>אספרסו</Table.Cell>
+            <Table.Cell>1 גר</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>מקיאטו</Table.Cell>
+            <Table.Cell>2 גרם אספרסו</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>גזירה</Table.Cell>
+            <Table.Cell>2 גרם אספרסו, 1 גרם חלב מוקצף</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </CanvasProvider>
+  );
+};
