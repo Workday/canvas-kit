529d6ffe39f89c67f696485d52f7e25acdcf7ea4
Nicholas Boll <nicholas.boll@gmail.com>
2024-02-09 17:04:24 -0700

    ci: Fix forward-merge version conflicts

---
+++ utils/spec/fix-package-json-versions.spec.ts
@@ -0,0 +1,36 @@
+import fixPackageJsonVersions from '../fix-package-json-versions';
+import {stripIndent} from 'common-tags';
+
+describe('updatePackageJson', () => {
+  it('should replace a matched dependency with the provided version', () => {
+    const input = stripIndent`
+    {
+      "@workday/canvas-kit-styling": "^10.3.3",
+    }`;
+
+    const expected = stripIndent`
+    {
+      "@workday/canvas-kit-styling": "^10.3.9",
+    }`;
+
+    expect(
+      fixPackageJsonVersions(input, [{name: '@workday/canvas-kit-styling', version: '10.3.9'}])
+    ).toEqual(expected);
+  });
+
+  it('should do anything to non-matched dependencies', () => {
+    const input = stripIndent`
+    {
+      "react": "^18.1.0",
+    }`;
+
+    const expected = stripIndent`
+    {
+      "react": "^18.1.0",
+    }`;
+
+    expect(
+      fixPackageJsonVersions(input, [{name: '@workday/canvas-kit-styling', version: '10.3.9'}])
+    ).toEqual(expected);
+  });
+});
