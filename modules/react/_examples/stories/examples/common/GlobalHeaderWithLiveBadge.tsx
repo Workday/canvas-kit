3a657c35fd5a45e8c08a20c13484ecb902978f0c
William Stanton <william.stanton@workday.com>
2024-02-18 20:40:38 -0500

    docs: Adding global header example with live badge

---
+++ modules/react/_examples/stories/examples/common/GlobalHeaderWithLiveBadge.tsx
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import {
+  styled,
+  createComponent,
+  dubLogoBlue,
+  AccessibleHide,
+  AriaLiveRegion,
+  useUniqueId,
+} from '@workday/canvas-kit-react/common';
+import {
+  notificationsIcon,
+  inboxIcon,
+  justifyIcon,
+  assistantIcon,
+  userIcon,
+} from '@workday/canvas-system-icons-web';
+import {colors, depth, space, type} from '@workday/canvas-kit-react/tokens';
+import {SecondaryButton, TertiaryButton} from '@workday/canvas-kit-react/button';
+import {Flex, FlexProps} from '@workday/canvas-kit-react/layout';
+import {SearchForm} from '@workday/canvas-kit-labs-react/search-form';
+import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {CountBadge} from '@workday/canvas-kit-react/badge';
+
+interface HeaderItemProps extends FlexProps {}
+
+export const GlobalHeaderWithLiveBadge = () => {
+  const badgeID = useUniqueId();
+  const myTasksID = useUniqueId();
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(prev => prev + 1);
+  };
+
+  return (
+    <>
+      <GlobalHeader>
+        <GlobalHeader.Item>
+          <Tooltip title="Menu">
+            <TertiaryButton icon={justifyIcon} />
+          </Tooltip>
+          <Tooltip title="Workday Home">
+            <TertiaryButton>
+              <WorkdayLogo dangerouslySetInnerHTML={{__html: dubLogoBlue}} />
+            </TertiaryButton>
+          </Tooltip>
+        </GlobalHeader.Item>
+        <GlobalHeader.Item margin="auto" width="100%" maxWidth={`calc(${space.xxxl} * 6)`}>
+          <SearchForm onSubmit={() => 1} />
+        </GlobalHeader.Item>
+        <GlobalHeader.Item>
+          <Tooltip title="Workday Assistant">
+            <TertiaryButton icon={assistantIcon} />
+          </Tooltip>
+          <Tooltip title="Notifications">
+            <TertiaryButton icon={notificationsIcon} />
+          </Tooltip>
+          <Tooltip title="My Tasks">
+            <TertiaryButton icon={inboxIcon} id={myTasksID} aria-describedby={badgeID}>
+              <AriaLiveRegion id={badgeID} aria-labelledby={myTasksID}>
+                <CountBadge count={counter} />
+                <AccessibleHide>New</AccessibleHide>
+              </AriaLiveRegion>
+            </TertiaryButton>
+          </Tooltip>
+          <Tooltip title="Profile">
+            <TertiaryButton icon={userIcon} />
+          </Tooltip>
+        </GlobalHeader.Item>
+      </GlobalHeader>
+      <Flex padding={space.s} as="main">
+        <SecondaryButton onClick={handleClick}>Add an item to My Tasks</SecondaryButton>
+      </Flex>
+    </>
+  );
+};
+
+const GlobalHeaderItem = createComponent('div')({
+  displayName: 'GlobalHeader.Item',
+  Component: ({gap = 's', ...props}: HeaderItemProps, ref) => (
+    <Flex gap={gap} alignItems="center" marginX={space.xs} ref={ref} {...props} />
+  ),
+});
+
+const GlobalHeader = createComponent('header')({
+  displayName: 'GlobalHeader',
+  Component: (props, ref, Element) => <HeaderWrapper ref={ref} as={Element} {...props} />,
+  subComponents: {Item: GlobalHeaderItem},
+});
+
+const HeaderWrapper = styled('header')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  boxSizing: 'border-box',
+  ...type.levels.subtext.large,
+  WebkitFontSmoothing: 'antialiased',
+  MozOsxFontSmoothing: 'grayscale',
+  backgroundColor: colors.frenchVanilla100,
+  ...depth[1],
+  padding: space.xxs,
+});
+
+const WorkdayLogo = styled('span')({lineHeight: 0});
