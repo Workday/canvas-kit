0c40ef8179b19922c98d2e6c667cd144c45095d4
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2024-03-13 17:01:37 -0600

    chore!: Refactor icon components (#2615)
    Resolves: #2528  
    
    Icon components, `Svg`, `SystemIcon`, `AccentIcon`, `AppletIcon`, `Graphic` now uses Canvas Tokens 
    and new styling utilities. Components now supports the `cs` prop, but otherwise the API has not changed. It should behave similar as it did in previous versions.
    
    [category:Components]
    
    Release Note:
    Icon components, `Svg`, `SystemIcon`, `AccentIcon`, `AppletIcon`, `Graphic` now uses Canvas Tokens 
    and new styling utilities. Components now supports the `cs` prop instead `styles`, but otherwise the API has not changed. It should behave similar as it did in previous versions.
    
    ### BREAKING CHANGES
    Impacts only internal Canvas Kit code. `Icon` component has been removed and no longer used.
    
    Co-authored-by: @alanbsmith <a.bax.smith@gmail.com>
    Co-authored-by: @NicholasBoll <nicholas.boll@gmail.com>
    Co-authored-by: @mannycarrera4 <mannycarrera4@users.noreply.github.com>
    Co-authored-by: manuel.carrera <manuel.carrera@workday.com>

--- modules/react/button/lib/TertiaryButton.tsx
+++ modules/react/button/lib/TertiaryButton.tsx
@@ -7,6 +7,7 @@ import {mergeStyles} from '@workday/canvas-kit-react/layout';
 import {system, brand, base} from '@workday/canvas-tokens-web';
 import {borderRadius, space} from '@workday/canvas-kit-react/tokens';
 import {Button, ButtonProps} from './Button';
+import {systemIconStencil} from '../../icon';
 
 /**
  * Extends all the style properties from Box to our buttons as well as props from ButtonProps.
@@ -26,37 +27,18 @@ const tertiaryStyles = createStyles({
   minWidth: 'auto',
   textDecoration: 'underline',
   border: 0,
-  // Default Styles
   [buttonVars.default.background]: 'transparent',
   [buttonVars.default.border]: 'transparent',
   [buttonVars.default.borderRadius]: system.shape.x1,
   [buttonVars.default.label]: brand.primary.base,
-  [buttonVars.default.icon]: base.blackPepper400,
-  // Hover Styles
-  [buttonVars.hover.background]: base.soap300,
-  [buttonVars.hover.border]: 'transparent',
-  [buttonVars.hover.label]: brand.primary.dark,
-  [buttonVars.hover.icon]: base.blackPepper400,
-  // Focus Styles
-  [buttonVars.focus.background]: 'transparent',
-  [buttonVars.focus.border]: 'transparent',
-  [buttonVars.focus.label]: brand.primary.base,
-  [buttonVars.focus.icon]: base.blackPepper400,
-  [buttonVars.focus.boxShadowInner]: brand.common.focusOutline,
-  [buttonVars.focus.boxShadowOuter]: brand.common.focusOutline,
-  // Active Styles
-  [buttonVars.active.background]: base.soap400,
-  [buttonVars.active.border]: 'transparent',
-  [buttonVars.active.label]: brand.primary.darkest,
-  [buttonVars.active.icon]: base.blackPepper500,
-  // Disabled Styles
-  [buttonVars.disabled.background]: 'transparent',
-  [buttonVars.disabled.border]: 'transparent',
-  [buttonVars.disabled.label]: brand.primary.base,
-  [buttonVars.disabled.icon]: base.blackPepper400,
-  [buttonVars.disabled.opacity]: '0.4',
-
+  [systemIconStencil.vars.color]: base.blackPepper400,
   '&:focus-visible, &.focus': {
+    [buttonVars.focus.background]: 'transparent',
+    [buttonVars.focus.border]: 'transparent',
+    [buttonVars.focus.label]: brand.primary.base,
+    [buttonVars.focus.boxShadowInner]: brand.common.focusOutline,
+    [buttonVars.focus.boxShadowOuter]: brand.common.focusOutline,
+    [systemIconStencil.vars.color]: base.blackPepper400,
     ...focusRing({
       width: 2,
       separation: 0,
@@ -64,47 +46,55 @@ const tertiaryStyles = createStyles({
       outerColor: brand.common.focusOutline,
     }),
   },
+  '&:hover, &.hover': {
+    [buttonVars.hover.background]: base.soap300,
+    [buttonVars.hover.border]: 'transparent',
+    [buttonVars.hover.label]: brand.primary.dark,
+    [systemIconStencil.vars.color]: base.blackPepper400,
+  },
+  '&:active, &.active': {
+    [buttonVars.active.background]: base.soap400,
+    [buttonVars.active.border]: 'transparent',
+    [buttonVars.active.label]: brand.primary.darkest,
+    [systemIconStencil.vars.color]: base.blackPepper500,
+  },
+  '&:disabled, &.disabled': {
+    [buttonVars.disabled.background]: 'transparent',
+    [buttonVars.disabled.border]: 'transparent',
+    [buttonVars.disabled.label]: brand.primary.base,
+    [buttonVars.disabled.opacity]: '0.4',
+  },
 });
 
 export const tertiaryButtonModifiers = createModifiers({
   isThemeable: {
     true: createStyles({
-      [buttonVars.default.icon]: brand.primary.base,
-      [buttonVars.hover.icon]: brand.primary.dark,
-      [buttonVars.focus.icon]: brand.primary.base,
-      [buttonVars.active.icon]: brand.primary.darkest,
-      [buttonVars.disabled.icon]: brand.primary.base,
+      [systemIconStencil.vars.color]: brand.primary.base,
+      '&:focus-visible, &.focus': {
+        [systemIconStencil.vars.color]: brand.primary.base,
+      },
+      '&:hover, &.hover': {
+        [systemIconStencil.vars.color]: brand.primary.dark,
+      },
+      '&:active, &.active': {
+        [systemIconStencil.vars.color]: brand.primary.darkest,
+      },
+      '&:disabled, &.disabled': {
+        [systemIconStencil.vars.color]: brand.primary.base,
+      },
     }),
   },
   variant: {
     inverse: createStyles({
-      // Default Styles
       [buttonVars.default.background]: 'transparent',
       [buttonVars.default.border]: 'transparent',
       [buttonVars.default.label]: base.frenchVanilla100,
-      [buttonVars.default.icon]: base.frenchVanilla100,
-      // Hover Styles
-      [buttonVars.hover.background]: base.frenchVanilla100,
-      [buttonVars.hover.border]: 'transparent',
-      [buttonVars.hover.label]: base.blackPepper400,
-      [buttonVars.hover.icon]: base.blackPepper400,
-      // Focus Styles
-      [buttonVars.focus.background]: base.frenchVanilla100,
-      [buttonVars.focus.border]: 'transparent',
-      [buttonVars.focus.label]: base.blackPepper400,
-      [buttonVars.focus.icon]: base.blackPepper400,
-      // Active Styles
-      [buttonVars.active.background]: base.soap200,
-      [buttonVars.active.border]: 'transparent',
-      [buttonVars.active.label]: base.blackPepper400,
-      [buttonVars.active.icon]: base.blackPepper400,
-      // Disabled Styles
-      [buttonVars.disabled.background]: 'transparent',
-      [buttonVars.disabled.border]: base.frenchVanilla100,
-      [buttonVars.disabled.label]: base.frenchVanilla100,
-      [buttonVars.disabled.icon]: base.frenchVanilla100,
-
+      [systemIconStencil.vars.color]: base.frenchVanilla100,
       '&:focus-visible, &.focus': {
+        [buttonVars.focus.background]: base.frenchVanilla100,
+        [buttonVars.focus.border]: 'transparent',
+        [buttonVars.focus.label]: base.blackPepper400,
+        [systemIconStencil.vars.color]: base.blackPepper400,
         ...focusRing({
           inset: 'inner',
           width: 2,
@@ -112,6 +102,25 @@ export const tertiaryButtonModifiers = createModifiers({
           innerColor: base.blackPepper400,
           outerColor: base.frenchVanilla100,
         }),
+      },
+      '&:hover, &.hover': {
+        [buttonVars.hover.background]: base.frenchVanilla100,
+        [buttonVars.hover.border]: 'transparent',
+        [buttonVars.hover.label]: base.blackPepper400,
+        [systemIconStencil.vars.color]: base.blackPepper400,
+      },
+      '&:active, &.active': {
+        [buttonVars.active.background]: base.soap200,
+        [buttonVars.active.border]: 'transparent',
+        [buttonVars.active.label]: base.blackPepper400,
+        [systemIconStencil.vars.color]: base.blackPepper400,
+      },
+      '&:disabled, &.disabled': {
+        // Disabled Styles
+        [buttonVars.disabled.background]: 'transparent',
+        [buttonVars.disabled.border]: base.frenchVanilla100,
+        [buttonVars.disabled.label]: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
       },
     }),
   },
