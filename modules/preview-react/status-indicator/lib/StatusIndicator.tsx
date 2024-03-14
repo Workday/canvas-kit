0d42f1a1d409ebfa46deec80b675291625123adf
harshanarisetty <harshan010@gmail.com>
2024-03-14 11:39:56 -0400

    fix: Update status-indicator-Icon color using systemIconStencil

--- modules/preview-react/status-indicator/lib/StatusIndicator.tsx
+++ modules/preview-react/status-indicator/lib/StatusIndicator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {ExtractProps, createContainer} from '@workday/canvas-kit-react/common';
 import {Flex, mergeStyles} from '@workday/canvas-kit-react/layout';
+import {systemIconStencil} from '@workday/canvas-kit-react/icon';
 import {StatusIndicatorIcon} from './StatusIndicatorIcon';
 import {StatusIndicatorLabel} from './StatusIndicatorLabel';
 import {useStatusIndicatorModel} from './hooks';
@@ -16,10 +17,7 @@ export interface StatusIndicatorProps extends ExtractProps<typeof Flex, never> {
 }
 
 const statusIndicatorStencil = createStencil({
-  vars: {
-    color: '',
-  },
-  base: ({color}) => ({
+  base: {
     display: 'inline-flex',
     gap: system.space.x1,
     maxWidth: px2rem(200),
@@ -27,83 +25,82 @@ const statusIndicatorStencil = createStencil({
     borderRadius: system.shape.half,
     height: px2rem(20),
     padding: `${system.space.zero} ${system.space.x1}}`,
-    color: color,
-    '& .wd-icon-fill': {
-      fill: color,
-    },
-    ':hover .wd-icon-fill': {
-      fill: color,
-    },
-    '& span': {
-      color: color,
-    },
-  }),
+  },
   modifiers: {
     gray: {
-      high: ({color}) => ({
-        [color]: base.frenchVanilla100,
+      high: {
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.licorice300,
-      }),
-      low: ({color}) => ({
-        [color]: base.licorice400,
+      },
+      low: {
+        color: base.licorice400,
+        [systemIconStencil.vars.color]: base.licorice400,
         background: base.soap300,
-      }),
+      },
     },
     orange: {
-      high: ({color}) => ({
-        [color]: base.licorice500,
+      high: {
+        color: base.licorice500,
+        [systemIconStencil.vars.color]: base.licorice500,
         background: base.cantaloupe400,
-      }),
-      low: ({color}) => ({
-        [color]: base.toastedMarshmallow600,
+      },
+      low: {
+        color: base.toastedMarshmallow600,
+        [systemIconStencil.vars.color]: base.toastedMarshmallow600,
         background: base.cantaloupe100,
-      }),
+      },
     },
     blue: {
-      high: ({color}) => ({
-        [color]: base.frenchVanilla100,
+      high: {
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.blueberry400,
-      }),
-      low: ({color}) => ({
-        [color]: base.blueberry500,
+      },
+      low: {
+        color: base.blueberry500,
+        [systemIconStencil.vars.color]: base.blueberry500,
         background: base.blueberry100,
-      }),
+      },
     },
     green: {
-      high: ({color}) => ({
-        [color]: base.frenchVanilla100,
+      high: {
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.greenApple600,
-      }),
-      low: ({color}) => ({
-        [color]: base.greenApple600,
+      },
+      low: {
+        color: base.greenApple600,
+        [systemIconStencil.vars.color]: base.greenApple600,
         background: base.greenApple100,
-      }),
+      },
     },
     red: {
-      high: ({color}) => ({
-        [color]: base.frenchVanilla100,
+      high: {
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.cinnamon500,
-      }),
-      low: ({color}) => ({
-        [color]: base.cinnamon600,
+      },
+      low: {
+        color: base.cinnamon600,
+        [systemIconStencil.vars.color]: base.cinnamon600,
         background: base.cinnamon100,
-      }),
+      },
     },
     transparent: {
-      high: ({color}) => ({
+      high: {
         opacity: 0.85,
-        [color]: base.frenchVanilla100,
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.blackPepper600,
-      }),
-      low: ({color}) => ({
+      },
+      low: {
         opacity: 0.85,
-        [color]: base.frenchVanilla100,
+        color: base.frenchVanilla100,
+        [systemIconStencil.vars.color]: base.frenchVanilla100,
         background: base.blackPepper600,
-      }),
+      },
     },
-  },
-  defaultModifiers: {
-    gray: 'low',
   },
 });
 
