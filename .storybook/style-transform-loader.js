e65876f9aef9b84d0a1de1d0263670ae87ec849b
Nicholas Boll <nicholas.boll@gmail.com>
2024-04-12 11:43:30 -0600

    ci: Fix SymbolDoc

--- .storybook/style-transform-loader.js
+++ .storybook/style-transform-loader.js
@@ -5,6 +5,13 @@ const {transform} = require('@workday/canvas-kit-styling-transform/testing');
 /** @typedef {import('webpack').loader.Loader} Loader */
 /** @typedef {import('webpack').loader.LoaderContext} LoaderContext */
 
+// Tracks files that have been processed. If a file is already processed, it
+// means the file has been updated and we need to update the Typescript program
+// so the changes are reflected in our doc output. If we don't update the TS
+// program, the docs will be processed with the outdated cache of the file
+// contents.
+const filesProcessedMap = new Map();
+
 /**
  * @this {LoaderContext}
  * @param {Parameters<Loader>[0]} source
@@ -14,6 +21,11 @@ function styleTransformLoader(source) {
     /** @type {any} */
     Doc,
   } = getOptions(this);
+
+  if (filesProcessedMap.has(this.resourcePath)) {
+    Doc.update();
+  }
+  filesProcessedMap.set(this.resourcePath, true);
 
   return transform(Doc.program, this.resourcePath);
 }
