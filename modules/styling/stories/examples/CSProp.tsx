518fb62596e9a2cdfeb4dd62dc167bfc29675795
manuel.carrera <manuel.carrera@workday.com>
2024-04-24 08:28:45 -0600

    fix: Clean up cs prop example

--- modules/styling/stories/examples/CSProp.tsx
+++ modules/styling/stories/examples/CSProp.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import {createStyles, createVars, cssVar} from '@workday/canvas-kit-styling';
+import {createStencil, cssVar} from '@workday/canvas-kit-styling';
 import {Card} from '@workday/canvas-kit-react/card';
 
-const myVars = createVars('backgroundColor', 'color');
-
-const myStyles = createStyles({
-  backgroundColor: myVars.backgroundColor,
-  maxWidth: '30rem',
+const myStencil = createStencil({
+  vars: {
+    color: '#E0AAFF',
+  },
+  base: {
+    maxWidth: '30rem',
+    backgroundColor: '#1E2329',
+  },
 });
 
 export const CSProp = () => {
   return (
-    <Card cs={[myStyles, myVars({backgroundColor: '#1E2329', color: '#E0AAFF'})]}>
-      <Card.Heading cs={{color: cssVar(myVars.color)}}>The Future of Styling</Card.Heading>
+    <Card cs={myStencil()}>
+      <Card.Heading cs={{color: cssVar(myStencil.vars.color)}}>The Future of Styling</Card.Heading>
       <Card.Body cs={{color: '#fff'}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin egestas blandit consectetur.
         Nam in congue mauris. Ut non metus a arcu rutrum accumsan. Duis luctus, diam vitae iaculis
