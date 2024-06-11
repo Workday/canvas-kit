60128be5a045e4fb245a9f8a9f3ee0ddab8fe257
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2024-06-11 13:24:44 -0600

    fix: Delete unused folder [skip release] (#2771)
    Remove unused folder
    
    [category:Infrastructure]
    
    Co-authored-by: manuel.carrera <manuel.carrera@workday.com>

--- dev-site/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/manuel.carrera/code/canvas-kit/dev-site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/manuel.carrera/code/canvas-kit/dev-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/manuel.carrera/code/canvas-kit/dev-site/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/manuel.carrera/code/canvas-kit/dev-site/src/css/custom.css"),
-];
