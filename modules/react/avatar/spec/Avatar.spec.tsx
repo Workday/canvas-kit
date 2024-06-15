d4c65bf6641908007bea4e8cd735102d34569f95
krissy.conant <krissy.conant@workday.com>
2024-06-15 13:12:18 -0400

    Starting refactor of Avatar component using storybook - WIP as breaking build

--- modules/react/avatar/spec/Avatar.spec.tsx
+++ modules/react/avatar/spec/Avatar.spec.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react';
-import {Avatar} from '../lib/Avatar';
 import {render, fireEvent} from '@testing-library/react';
+import {Avatar} from '../lib/Avatar';
+import {px2rem} from '@workday/canvas-kit-styling';
 
 describe('Avatar', () => {
   it('should render a button element', () => {
     const screen = render(<Avatar data-testid="test" />);
-
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
   it('should forward extra attributes to the container', () => {
-    const screen = render(<Avatar id="myAvatar" />);
-
-    expect(screen.getByRole('button')).toHaveAttribute('id', 'myAvatar');
+    const {getByRole} = render(<Avatar id="myAvatar" />);
+    expect(getByRole('button')).toHaveAttribute('id', 'myAvatar');
   });
 
   it('should set the aria-label of the button with the altText prop', () => {
     const screen = render(<Avatar altText="My alt text" />);
-
     expect(screen.getByRole('button')).toHaveAttribute('aria-label', 'My alt text');
   });
 
   it('should set the url of the image when passed the url prop', () => {
     const screen = render(<Avatar url="https://example.com/image.png" altText="My alt text" />);
-
     expect(screen.getByRole('img')).toHaveAttribute('src', 'https://example.com/image.png');
     expect(screen.getByRole('img')).toHaveAttribute('alt', 'My alt text');
   });
@@ -31,21 +28,49 @@ describe('Avatar', () => {
   it('should forward ref to the button element', () => {
     const ref = React.createRef<HTMLButtonElement>();
     const screen = render(<Avatar ref={ref} />);
-
     expect(ref.current).toEqual(screen.getByRole('button'));
   });
 
   it('should call onClick callback when clicked', () => {
     const fn = jest.fn();
     const screen = render(<Avatar onClick={fn} />);
-
     fireEvent.click(screen.getByRole('button'));
     expect(fn).toBeCalled();
   });
 
   it('should render a div when as prop is specified', () => {
     const screen = render(<Avatar as="div" data-testid="test" />);
-
     expect(screen.getByTestId('test').tagName.toLowerCase()).toEqual('div');
+  });
+
+  it('should apply the variant class when variant prop is specified', () => {
+    const {container} = render(<Avatar variant="dark" />);
+    expect(container.firstChild).toHaveClass('dark');
+  });
+
+  it('should apply the size class when size prop is specified', () => {
+    const {container} = render(<Avatar size={40} />);
+    expect(container.firstChild).toHaveStyle(`width: ${px2rem(40)}`);
+  });
+
+  it('should set the background color when background prop is specified', () => {
+    const {container} = render(<Avatar background="red" />);
+    expect(container.firstChild).toHaveStyle('background: red');
+  });
+
+  it('should set the object fit of the image when objectFit prop is specified', () => {
+    const screen = render(<Avatar url="https://example.com/image.png" objectFit="cover" />);
+    expect(screen.getByRole('img')).toHaveStyle('object-fit: cover');
+  });
+
+  it('should hide the icon when the image is loaded', () => {
+    const screen = render(<Avatar url="https://example.com/image.png" />);
+    fireEvent.load(screen.getByRole('img'));
+    expect(screen.queryByRole('img')).toHaveStyle('opacity: 1');
+  });
+
+  it('should show the icon when the image is not loaded', () => {
+    const {container} = render(<Avatar url="https://example.com/image.png" />);
+    expect(container.querySelector('svg')).toBeInTheDocument();
   });
 });
