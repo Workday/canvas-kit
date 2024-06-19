a07bc64fc1d77ca7a91cfa1a390aa55c5449c523
William Stanton <william.stanton@workday.com>
2024-06-19 09:34:35 -0400

    docs: Updating the describe tooltip example

--- modules/react/tooltip/stories/examples/DescribeType.tsx
+++ modules/react/tooltip/stories/examples/DescribeType.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import {DeleteButton} from '@workday/canvas-kit-react/button';
+import {Heading, Text} from '@workday/canvas-kit-react/text';
 import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {trashIcon} from '@workday/canvas-system-icons-web';
+
+const files = ['Cover Letter.docx', 'Resume.docx', 'Portfolio.pptx', 'Portrait.png'];
 
 export const DescribeType = () => {
   return (
-    <Tooltip type="describe" title="The service will restart after this action">
-      <DeleteButton>Delete</DeleteButton>
-    </Tooltip>
+    <>
+      <Heading size="medium">Uploaded Files:</Heading>
+      <Tooltip type="describe" title="The service will restart after this action">
+        <DeleteButton icon={trashIcon}>Delete</DeleteButton>
+      </Tooltip>
+    </>
   );
 };
