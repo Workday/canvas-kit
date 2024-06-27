03f38eae950d720271b17cf02e15bbf7be2ce8ff
William Stanton <william.stanton@workday.com>
2024-06-27 15:28:36 -0400

    docs: Finishing uploaded files example

--- modules/react/tooltip/stories/examples/DescribeType.tsx
+++ modules/react/tooltip/stories/examples/DescribeType.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
 import {DeleteButton} from '@workday/canvas-kit-react/button';
+import {Flex} from '@workday/canvas-kit-react/layout';
 import {Heading, Text} from '@workday/canvas-kit-react/text';
 import {Tooltip} from '@workday/canvas-kit-react/tooltip';
-import {trashIcon} from '@workday/canvas-system-icons-web';
+import {alignTopIcon, trashIcon} from '@workday/canvas-system-icons-web';
 
 const files = ['Cover Letter.docx', 'Resume.docx', 'Portfolio.pptx', 'Portrait.png'];
+const listStyles = {
+  alignItems: 'center',
+  width: '35rem',
+};
+const deleteBtnStyle = {
+  marginLeft: 'auto',
+};
 
 export const DescribeType = () => {
   return (
     <>
       <Heading size="medium">Uploaded Files:</Heading>
-      <Tooltip type="describe" title="The service will restart after this action">
-        <DeleteButton icon={trashIcon}>Delete</DeleteButton>
-      </Tooltip>
+      <Flex as="ul" gap="1rem" flexDirection="column">
+        {files.map(i => (
+          <Flex as="li" style={listStyles}>
+            <Text>{i}</Text>
+            <Tooltip type="describe" title={i}>
+              <DeleteButton icon={trashIcon} style={deleteBtnStyle}>
+                Delete
+              </DeleteButton>
+            </Tooltip>
+          </Flex>
+        ))}
+      </Flex>
     </>
   );
 };
