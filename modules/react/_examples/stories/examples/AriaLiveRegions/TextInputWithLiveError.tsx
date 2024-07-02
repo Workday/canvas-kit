ca2b7a3a90969f55ddb7bace9ef3c5be62086502
William Stanton <william.stanton@workday.com>
2024-07-01 22:30:38 -0400

    docs: Fixing text input with live error example
    Replacing deprecated preview TextInput with preview FormField.

--- modules/react/_examples/stories/examples/AriaLiveRegions/TextInputWithLiveError.tsx
+++ modules/react/_examples/stories/examples/AriaLiveRegions/TextInputWithLiveError.tsx
@@ -1,24 +1,25 @@
-import React, {useState, useRef} from 'react';
-import {TextInput} from '@workday/canvas-kit-preview-react/text-input';
+import React from 'react';
 import {AriaLiveRegion, changeFocus} from '@workday/canvas-kit-react/common';
+import {FormField} from '@workday/canvas-kit-preview-react/form-field';
 import {PrimaryButton} from '@workday/canvas-kit-react/button';
+import {TextInput} from '@workday/canvas-kit-react/text-input';
 
 export const TextInputWithLiveError = () => {
   const errMsg = 'Error: First name is required.';
-  const [hasError, setHasError] = useState(false);
-  const inputRef = useRef();
-  const handleBlur = e => setHasError(e.target.value.trim().length === 0);
+  const [hasError, setHasError] = React.useState('no');
+  const inputRef = React.useRef();
+  const handleBlur = e => setHasError(e.target.value.trim().length === 0 ? 'error' : 'no');
   const handleSubmit = () => hasError && changeFocus(inputRef.current);
 
   return (
     <>
-      <TextInput orientation="vertical" hasError={hasError} isRequired={true}>
-        <TextInput.Label>First Name:</TextInput.Label>
-        <TextInput.Field onBlur={handleBlur} ref={inputRef} />
-        <TextInput.Hint height={'16px'}>
-          <AriaLiveRegion>{hasError && errMsg}</AriaLiveRegion>
-        </TextInput.Hint>
-      </TextInput>
+      <FormField error={hasError} isRequired={true}>
+        <FormField.Label>First Name:</FormField.Label>
+        <FormField.Input as={TextInput} onBlur={handleBlur} ref={inputRef} />
+        <FormField.Hint height="m">
+          <AriaLiveRegion>{hasError === 'error' && errMsg}</AriaLiveRegion>
+        </FormField.Hint>
+      </FormField>
       <PrimaryButton onClick={handleSubmit}>Continue</PrimaryButton>
     </>
   );
