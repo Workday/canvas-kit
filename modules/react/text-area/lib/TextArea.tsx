4aed13848952c8942475c3d47febdaae8863426f
manuel.carrera <manuel.carrera@workday.com>
2024-07-11 15:48:36 -0600

    fix: Update focus rings and error and alert

--- modules/react/text-area/lib/TextArea.tsx
+++ modules/react/text-area/lib/TextArea.tsx
@@ -8,9 +8,10 @@ import {
   styled,
   Themeable,
 } from '@workday/canvas-kit-react/common';
-import {createStencil, cssVar, px2rem} from '@workday/canvas-kit-styling';
+import {createStencil, cssVar, px2rem, calc} from '@workday/canvas-kit-styling';
 import {borderRadius, inputColors, space, type} from '@workday/canvas-kit-react/tokens';
 import {system, brand} from '@workday/canvas-tokens-web';
+import {mergeStyles} from '@workday/canvas-kit-react/layout';
 export type ValueOf<T> = T[keyof T];
 
 export interface TextAreaProps extends Themeable, GrowthBehavior {
@@ -54,49 +55,49 @@ export const TextAreaResizeDirection = {
   Vertical: 'vertical' as 'vertical',
 };
 
-const StyledTextArea = styled('textarea')<TextAreaProps & StyledType>(
-  ({theme, error}) => ({
-    ...type.levels.subtext.large,
-    border: `1px solid ${inputColors.border}`,
-    display: 'block',
-    backgroundColor: inputColors.background,
-    borderRadius: borderRadius.m,
-    boxSizing: 'border-box',
-    minHeight: space.xxl,
-    minWidth: `calc((${space.xxxl} * 3) + ${space.xl})`,
-    transition: '0.2s box-shadow, 0.2s border-color',
-    padding: space.xxs, // Compensate for border
-    margin: 0, // Fix Safari
-    '&::webkit-resizer': {
-      display: 'none',
-    },
-    '&::placeholder': {
-      color: inputColors.placeholder,
-    },
-    '&:hover': {
-      borderColor: inputColors.hoverBorder,
-    },
-    '&:focus:not([disabled])': {
-      borderColor: theme.canvas.palette.common.focusOutline,
-      boxShadow: `inset 0 0 0 1px ${theme.canvas.palette.common.focusOutline}`,
-      outline: 'none',
-    },
-    '&:disabled': {
-      backgroundColor: inputColors.disabled.background,
-      borderColor: inputColors.disabled.border,
-      color: inputColors.disabled.text,
-      '&::placeholder': {
-        color: inputColors.disabled.text,
-      },
-    },
-    ...errorRing(error, theme),
-  }),
+// const StyledTextArea = styled('textarea')<TextAreaProps & StyledType>(
+//   ({theme, error}) => ({
+//     ...type.levels.subtext.large,
+//     border: `1px solid ${inputColors.border}`,
+//     display: 'block',
+//     backgroundColor: inputColors.background,
+//     borderRadius: borderRadius.m,
+//     boxSizing: 'border-box',
+//     minHeight: space.xxl,
+//     minWidth: `calc((${space.xxxl} * 3) + ${space.xl})`,
+//     transition: '0.2s box-shadow, 0.2s border-color',
+//     padding: space.xxs, // Compensate for border
+//     margin: 0, // Fix Safari
+//     '&::webkit-resizer': {
+//       display: 'none',
+//     },
+//     '&::placeholder': {
+//       color: inputColors.placeholder,
+//     },
+//     '&:hover': {
+//       borderColor: inputColors.hoverBorder,
+//     },
+//     '&:focus:not([disabled])': {
+//       borderColor: theme.canvas.palette.common.focusOutline,
+//       boxShadow: `inset 0 0 0 1px ${theme.canvas.palette.common.focusOutline}`,
+//       outline: 'none',
+//     },
+//     '&:disabled': {
+//       backgroundColor: inputColors.disabled.background,
+//       borderColor: inputColors.disabled.border,
+//       color: inputColors.disabled.text,
+//       '&::placeholder': {
+//         color: inputColors.disabled.text,
+//       },
+//     },
+//     ...errorRing(error, theme),
+//   }),
 
-  ({resize, grow}) => ({
-    width: grow ? '100%' : undefined,
-    resize: grow ? TextAreaResizeDirection.Vertical : resize,
-  })
-);
+//   ({resize, grow}) => ({
+//     width: grow ? '100%' : undefined,
+//     resize: grow ? TextAreaResizeDirection.Vertical : resize,
+//   })
+// );
 
 export const textAreaStencil = createStencil({
   base: {
@@ -107,7 +108,7 @@ export const textAreaStencil = createStencil({
     borderRadius: system.shape.x1,
     boxSizing: 'border-box',
     minHeight: system.space.x16,
-    minWidth: `calc((${cssVar(system.space.x20)} * ${px2rem(3)}) + ${cssVar(system.space.x10)})`,
+    minWidth: calc.add(calc.multiply(system.space.x20, 3), system.space.x10),
     transition: '0.2s box-shadow, 0.2s border-color',
     padding: system.space.x2, // Compensate for border
     margin: px2rem(0), // Fix Safari
@@ -121,16 +122,16 @@ export const textAreaStencil = createStencil({
       borderColor: system.color.border.input.strong,
     },
     '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
-      borderColor: brand.primary.base,
-      boxShadow: `inset 0 0 0 1px ${cssVar(brand.primary.base)}`,
+      borderColor: brand.common.focusOutline,
+      boxShadow: `inset 0 0 0 1px ${cssVar(brand.common.focusOutline)}`,
       outline: 'none',
     },
     '&:disabled, .disabled': {
-      backgroundColor: inputColors.disabled.background,
+      backgroundColor: system.color.bg.alt.softer,
       borderColor: system.color.border.input.strong,
-      color: inputColors.disabled.text,
+      color: system.color.text.disabled,
       '&::placeholder': {
-        color: inputColors.disabled.text,
+        color: system.color.text.disabled,
       },
     },
   },
@@ -139,6 +140,54 @@ export const textAreaStencil = createStencil({
       true: {
         width: '100%',
         resize: 'vertical',
+      },
+    },
+    error: {
+      error: {
+        borderColor: brand.error.base,
+        boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.error.base}`,
+        '&:hover, &.hover': {
+          borderColor: brand.error.base,
+        },
+        '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
+          borderColor: brand.error.base,
+          boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.error.base},
+        0 0 0 2px ${system.color.border.inverse},
+        0 0 0 4px ${brand.common.focusOutline}`,
+        },
+
+        // borderColor: brand.error.base,
+      },
+      alert: {
+        borderColor: brand.alert.darkest,
+        boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.alert.darkest}, inset 0 0 0 ${px2rem(3)} ${
+          brand.alert.base
+        }`,
+        '&:hover, &.hover': {
+          borderColor: brand.alert.darkest,
+        },
+        '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
+          borderColor: brand.alert.darkest,
+          boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.alert.darkest}, inset 0 0 0 ${px2rem(3)} ${
+            brand.alert.base
+          },
+        0 0 0 2px ${system.color.border.inverse},
+        0 0 0 4px ${brand.common.focusOutline}`,
+        },
+      },
+    },
+    resize: {
+      both: {
+        resize: 'both',
+      },
+      horizontal: {
+        resize: 'horizontal',
+      },
+      vertical: {
+        resize: 'vertical',
+      },
+      none: {
+        resize: 'none',
       },
     },
   },
@@ -150,7 +199,12 @@ export const TextArea = createComponent('textarea')({
     {resize = TextAreaResizeDirection.Both, grow, ...elemProps}: TextAreaProps,
     ref,
     Element
-  ) => <StyledTextArea ref={ref} as={Element} grow={grow} resize={resize} {...elemProps} />,
+  ) => (
+    <Element
+      ref={ref}
+      {...mergeStyles(elemProps, textAreaStencil({error: elemProps.error, grow, resize}))}
+    />
+  ),
   subComponents: {
     ErrorType,
     ResizeDirection: TextAreaResizeDirection,
