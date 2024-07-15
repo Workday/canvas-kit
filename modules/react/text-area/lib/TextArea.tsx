8bc0cda88cd1512e85feccfb8583208d6bfe044a
manuel.carrera <manuel.carrera@workday.com>
2024-07-15 15:45:40 -0600

    fix: Update text input and extend stencil for text area

--- modules/react/text-area/lib/TextArea.tsx
+++ modules/react/text-area/lib/TextArea.tsx
@@ -7,6 +7,7 @@ import {
 } from '@workday/canvas-kit-react/common';
 import {createStencil, cssVar, px2rem, calc} from '@workday/canvas-kit-styling';
 import {system, brand} from '@workday/canvas-tokens-web';
+import {textInputStencil} from '@workday/canvas-kit-react/text-input';
 import {mergeStyles} from '@workday/canvas-kit-react/layout';
 export type ValueOf<T> = T[keyof T];
 
@@ -52,77 +53,15 @@ export const TextAreaResizeDirection = {
 };
 
 export const textAreaStencil = createStencil({
+  extends: textInputStencil,
   base: {
-    ...system.type.subtext.large,
-    display: 'block',
-    border: `${px2rem(1)} solid ${cssVar(system.color.border.input.default)}`,
-    backgroundColor: system.color.bg.default,
-    borderRadius: system.shape.x1,
-    boxSizing: 'border-box',
     minHeight: system.space.x16,
     minWidth: calc.add(calc.multiply(system.space.x20, 3), system.space.x10),
-    transition: '0.2s box-shadow, 0.2s border-color',
-    padding: system.space.x2, // Compensate for border
-    margin: px2rem(0), // Fix Safari
-    color: system.color.text.default,
     '&::webkit-resizer': {
       display: 'none',
     },
-    '&::placeholder': {
-      color: system.color.text.hint,
-    },
-    '&:hover': {
-      borderColor: system.color.border.input.strong,
-    },
-    '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
-      borderColor: brand.common.focusOutline,
-      boxShadow: `inset 0 0 0 1px ${cssVar(brand.common.focusOutline)}`,
-      outline: 'none',
-    },
-    '&:disabled, .disabled': {
-      backgroundColor: system.color.bg.alt.softer,
-      borderColor: system.color.border.input.strong,
-      color: system.color.text.disabled,
-      '&::placeholder': {
-        color: system.color.text.disabled,
-      },
-    },
   },
   modifiers: {
-    grow: {
-      true: {
-        width: '100%',
-        resize: 'vertical',
-      },
-    },
-    error: {
-      error: {
-        borderColor: brand.error.base,
-        boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.error.base}`,
-        '&:hover, &.hover': {
-          borderColor: brand.error.base,
-        },
-        '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
-          borderColor: brand.error.base,
-          boxShadow: `inset 0 0 0 ${px2rem(1)} ${brand.error.base},
-        0 0 0 2px ${system.color.border.inverse},
-        0 0 0 4px ${brand.common.focusOutline}`,
-        },
-      },
-      alert: {
-        borderColor: brand.alert.darkest,
-        boxShadow: `inset 0 0 0 ${px2rem(2)} ${brand.alert.base}`,
-        '&:hover, &.hover': {
-          borderColor: brand.alert.darkest,
-        },
-        '&:focus-visible:not([disabled]), &.focus:not([disabled])': {
-          borderColor: brand.alert.darkest,
-          boxShadow: `inset 0 0 0 ${px2rem(2)} ${brand.alert.base},
-        0 0 0 2px ${system.color.border.inverse},
-        0 0 0 4px ${brand.common.focusOutline}`,
-        },
-      },
-    },
     resize: {
       both: {
         resize: 'both',
