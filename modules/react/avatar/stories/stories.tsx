91f27cea7d847952da4bf50abb3510f26f1c9f74
Nicholas Boll <nicholas.boll@gmail.com>
2024-07-18 16:34:52 -0600

    fix(avatar): Lazy load avatar images (#2834)
    Add `loading="lazy"` to the Avatar image element.
    
    Fixes: #2832
    
    [category:Components]

--- modules/react/avatar/stories/stories.tsx
+++ modules/react/avatar/stories/stories.tsx
@@ -69,6 +69,16 @@ storiesOf('Components/Indicators/Avatar', module)
       <h3>Using a Square Image</h3>
       <Avatar as="div" size={200} url="https://placekitten.com/g/450/450" />
     </div>
+  ))
+  .add('Lazy Loading', () => (
+    <div className="story">
+      {Array.from({length: 100}, (v, index) => (
+        <>
+          <Avatar as="div" size={Avatar.Size.xs} url={testAvatar + '?v=' + index} />
+          <br />
+        </>
+      ))}
+    </div>
   ));
 
 storiesOf('Components/Indicators/Avatar/Avatar Button', module)
