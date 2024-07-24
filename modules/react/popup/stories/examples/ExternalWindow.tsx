Workday/canvas-kit

commit fa07ba9d2d4e3e1d54d14d20f5ec45cdeec51762

fix: Popups render in the target's window (#2829)
Popups now render in the same window as their target element.

Fixes #2831

[category:Components]

Co-authored-by: @mannycarrera4 <mannycarrera4@users.noreply.github.com>

modules/react/popup/stories/Popup.stories.mdx
@@ -10,6 +10,7 @@ import {FocusRedirect} from './examples/FocusRedirect';
 import {FocusTrap} from './examples/FocusTrap';
 import {RTL} from './examples/RTL';
 import {CustomTarget} from './examples/CustomTarget';
+import {ExternalWindow} from './examples/ExternalWindow';
 import {FullScreen} from './examples/FullScreen';
 
 <Meta title="Components/Popups/Popup" component={Popup} />
@@ -160,6 +161,16 @@ the popup's stack context when entering/exiting fullscreen.
 
 <ExampleCodeBlock code={FullScreen} />
 
+### Opening an external window
+
+A popup can open an external window. This isn't supported directly. The `Popup.Popper` subcomponent
+is replaced with a custom subcomponent that connects to the Popup model and controls the lifecycle
+of the extenal window. Be sure to connect the `unload` event of both the parent `window` and the
+external child `window` to the lifecycle of the Popup model to prevent memory leaks or zombie
+windows.
+
+<ExampleCodeBlock code={ExternalWindow} />
+
 ### RTL
 
 The Popup component automatically handles right-to-left rendering.
@@ -171,8 +182,8 @@ The Popup component automatically handles right-to-left rendering.
 ## Component API
 
 <>
-<SymbolDoc name="Popper" fileName="/react/" />
-<SymbolDoc name="Popup" fileName="/react/" />
+  <SymbolDoc name="Popper" fileName="/react/" />
+  <SymbolDoc name="Popup" fileName="/react/" />
 </>
 
 ## Hooks

modules/react/popup/stories/examples/ExternalWindow.tsx
@@ -1,0 +1,177 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {system} from '@workday/canvas-tokens-web';
+import {createStyles} from '@workday/canvas-kit-styling';
+import {infoIcon} from '@workday/canvas-system-icons-web';
+
+import {
+  CanvasProvider,
+  ContentDirection,
+  createSubcomponent,
+  PartialEmotionCanvasTheme,
+  useMount,
+  useTheme,
+} from '@workday/canvas-kit-react/common';
+import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {Popup, usePopupModel} from '@workday/canvas-kit-react/popup';
+import {SecondaryButton} from '@workday/canvas-kit-react/button';
+import {Flex} from '../../../layout';
+
+const mainContentStyles = createStyles({
+  padding: system.space.x4,
+});
+
+export interface ExternalWindowPortalProps {
+  /**
+   * Child components of WindowPortal
+   */
+  children: React.ReactNode;
+  /**
+   * Callback to close the popup
+   */
+  onWindowClose?: () => void;
+  /**
+   * Width of the popup window
+   */
+  width?: number;
+  /**
+   * Height of the popup window
+   */
+  height?: number;
+  /**
+   * The name of the popup window. If another popup opens with the same name, that instance will
+   * be reused. Use caution with setting this value
+   */
+  target?: string;
+}
+
+async function copyAssets(sourceDoc: Document, targetDoc: Document) {
+  for (const font of (sourceDoc as any).fonts.values()) {
+    (targetDoc as any).fonts.add(font);
+
+    font.load();
+  }
+
+  await (targetDoc as any).fonts.ready;
+
+  // The current ES lib version doesn't include iterable interfaces, so we cast as an iterable
+  for (const styleSheet of sourceDoc.styleSheets as StyleSheetList & Iterable<CSSStyleSheet>) {
+    if (styleSheet.cssRules) {
+      // text based styles
+      const styleEl = targetDoc.createElement('style');
+      for (const cssRule of styleSheet.cssRules as CSSRuleList & Iterable<CSSRule>) {
+        styleEl.appendChild(targetDoc.createTextNode(cssRule.cssText));
+      }
+      targetDoc.head.appendChild(styleEl);
+    } else if (styleSheet.href) {
+      // link based styles
+      const linkEl = targetDoc.createElement('link');
+
+      linkEl.rel = 'stylesheet';
+      linkEl.href = styleSheet.href;
+      targetDoc.head.appendChild(linkEl);
+    }
+  }
+}
+
+const ExternalWindowPortal = ({
+  children,
+  width = 300,
+  height = 500,
+  target = '',
+  onWindowClose,
+}: ExternalWindowPortalProps) => {
+  const [portalElement, setPortalElement] = React.useState<HTMLDivElement | null>(null);
+
+  useMount(() => {
+    const newWindow = window.open(
+      '', // url
+      target,
+      `width=${width},height=${height},left=100,top=100,popup=true`
+    );
+
+    if (newWindow) {
+      // copy fonts and styles
+      copyAssets(document, newWindow.document);
+
+      const element = newWindow.document.createElement('div');
+      newWindow.document.body.appendChild(element);
+      setPortalElement(element);
+    } else {
+      onWindowClose();
+    }
+
+    const closeWindow = event => {
+      onWindowClose();
+    };
+
+
+    window.addEventListener('unload', closeWindow);
+    newWindow?.addEventListener('unload', closeWindow);
+
+    return () => {
+      window.removeEventListener('unload', closeWindow);
+      newWindow?.removeEventListener('unload', closeWindow);
+      newWindow?.close();
+    };
+  });
+
+  if (!portalElement) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(<CanvasProvider>{children}</CanvasProvider>, portalElement);
+};
+
+const PopupExternalWindow = createSubcomponent()({
+  displayName: 'Popup.ExternalWindow',
+  modelHook: usePopupModel,
+})<ExternalWindowPortalProps>(({children, ...elemProps}, Element, model) => {
+  if (model.state.visibility === 'visible') {
+    return (
+      <ExternalWindowPortal onWindowClose={model.events.hide} {...elemProps}>
+        {children}
+      </ExternalWindowPortal>
+    );
+  }
+
+  return null;
+});
+
+export const ExternalWindow = () => {
+  // useTheme is filling in the Canvas theme object if any keys are missing
+  const canvasTheme: PartialEmotionCanvasTheme = useTheme({
+    canvas: {
+      // Switch to `ContentDirection.RTL` to change direction
+      direction: ContentDirection.LTR,
+    },
+  });
+
+  const model = usePopupModel();
+
+  return (
+    <CanvasProvider theme={canvasTheme}>
+      <>
+        <main className={mainContentStyles}>
+          <p>Popup that opens a new Operating System Window</p>
+          <Popup model={model}>
+            <Tooltip title="Open External Window Tooltip">
+              <Popup.Target>Open External Window</Popup.Target>
+            </Tooltip>
+            <PopupExternalWindow>
+              <p>External Window Contents! Mouse over the info icon to get a tooltip</p>
+              <Flex gap="s">
+                <Tooltip title="More information">
+                  <SecondaryButton icon={infoIcon} />
+                </Tooltip>
+                <Popup.CloseButton>Close Window</Popup.CloseButton>
+              </Flex>
+            </PopupExternalWindow>
+          </Popup>
+          <p>Popup visibility: {model.state.visibility}</p>
+        </main>
+      </>
+    </CanvasProvider>
+  );
+};

modules/popup-stack/lib/PopupStack.ts
@@ -93,7 +93,10 @@ function getChildPopups(item: PopupStackItem, items: PopupStackItem[]): PopupSta
 
 interface Stack {
   items: PopupStackItem[];
-  container?: () => HTMLElement;
+  /**
+   * Returns the container of a stack given an optional element.
+   */
+  container?: (element?: HTMLElement) => HTMLElement;
   zIndex: {
     min: number;
     max: number;
@@ -167,12 +170,27 @@ const setToWindow = (path: string, value: any) => {
   }
 };
 
+/**
+ * TODO: Remove this after v12 and use `stack.container(el)` directly. This is temporary to make
+ * sure Popups can open in new windows while supporting older versions of Canvas Kit AND full screen
+ * mode.
+ */
+const getContainer = (stack: Stack, element?: HTMLElement): HTMLElement => {
+  let stackContainer = stack.container?.();
+  if (stackContainer === document.body) {
+    // Here's the transitory code
+    stackContainer = element?.ownerDocument.body;
+  }
+
+  return stackContainer || document.body;
+};
+
 // We need to make sure only one stack is ever in use on the page - ever. If a stack is already
 // defined on the page, we need to use that one. Never, ever, ever change this variable name on
 // window
 const stack: Stack = getFromWindow('workday.__popupStack') || {
   description: 'Global popup stack from @workday/canvas-kit/popup-stack',
-  container: () => document.body,
+  container: el => el?.ownerDocument.body || document.body,
   items: [],
   zIndex: {min: 30, max: 50, getValue: getValue},
   _adapter: {},
@@ -235,7 +253,7 @@ export const PopupStack = {
       return;
     }
     stack.items.push(item);
-    (stack.container?.() || document.body).appendChild(item.element);
+    getContainer(stack, item.owner).appendChild(item.element);
 
     setZIndexOfElements(PopupStack.getElements());
   },
@@ -255,8 +273,9 @@ export const PopupStack = {
         stack._adapter.remove(element);
         return;
       }
+      const item = stack.items.find(item => item.element === element);
       stack.items = stack.items.filter(item => item.element !== element);
-      (stack.container?.() || document.body).removeChild(element);
+      getContainer(stack, item?.owner).removeChild(element);
 
       setZIndexOfElements(PopupStack.getElements(stack));
     }

commit 4faa4303a9ad41eac17887255f4df7359b4eb880

fix: Fix Popup ExternalWindow example import (#2841)
Fixes an issue introduced in #2829 where an example imported `Flex` using a [relative path](https://github.com/Workday/canvas-kit/pull/2829/files#diff-eb9d95064ad483df88214fab614969e7f3111bdbdcd06348c6f349d520880d64R19) to `../../../layout` rather than `@workday/canvas-kit-react/layout`. The relative path does not work for consumers of `@workday/canvas-kit-docs` (i.e., the Canvas Site).

[category:Documentation]

modules/react/popup/stories/Popup.stories.mdx
@@ -161,7 +161,7 @@ the popup's stack context when entering/exiting fullscreen.
 
 <ExampleCodeBlock code={FullScreen} />
 
-### Opening an external window
+### Opening an External Window
 
 A popup can open an external window. This isn't supported directly. The `Popup.Popper` subcomponent
 is replaced with a custom subcomponent that connects to the Popup model and controls the lifecycle

modules/react/popup/stories/examples/ExternalWindow.tsx
@@ -16,7 +16,7 @@ import {
 import {Tooltip} from '@workday/canvas-kit-react/tooltip';
 import {Popup, usePopupModel} from '@workday/canvas-kit-react/popup';
 import {SecondaryButton} from '@workday/canvas-kit-react/button';
-import {Flex} from '../../../layout';
+import {Flex} from '@workday/canvas-kit-react/layout';
 
 const mainContentStyles = createStyles({
   padding: system.space.x4,
@@ -105,7 +105,6 @@ const ExternalWindowPortal = ({
     const closeWindow = event => {
       onWindowClose();
     };
-
 
     window.addEventListener('unload', closeWindow);
     newWindow?.addEventListener('unload', closeWindow);