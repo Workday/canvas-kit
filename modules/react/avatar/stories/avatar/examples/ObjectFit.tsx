e41df0cfa5abc2c6650bb5118ca700a174949bb3
krissy.conant <krissy.conant@workday.com>
2024-08-07 11:23:52 -0400

    fix: updated links for objectfit

--- modules/react/avatar/stories/avatar/examples/ObjectFit.tsx
+++ modules/react/avatar/stories/avatar/examples/ObjectFit.tsx
@@ -5,27 +5,21 @@ import {px2rem} from '@workday/canvas-kit-styling';
 export const ObjectFit = () => (
   <div className="story">
     <h3>Original Rectangle Image</h3>
-    <img
-      alt=""
-      src="https://loremflickr.com/cache/resized/65535_53325191273_373deb18ee_n_320_200_nofilter.jpg?lock=1"
-    />
+    <img alt="" src="https://picsum.photos/id/237/300/200" />
     <h3>Object fit on a Rectangle Image</h3>
-    <Avatar
-      as="div"
-      url="https://loremflickr.com/cache/resized/65535_53325191273_373deb18ee_n_320_200_nofilter.jpg?lock=1"
-    />
+    <Avatar as="div" url="https://picsum.photos/id/237/300/200" />
     <h3>Object fit on a Rectangle Image using Dynamic Size</h3>
     <Avatar
       as="div"
       size={px2rem(200)}
-      url="https://placehold.co/450x200/png"
+      url="https://picsum.photos/id/237/300/200"
       objectFit="contain"
     />
     <h3>Original Square Image</h3>
-    <img alt="" src="https://placehold.co/450x450/png" />
+    <img alt="" src="https://picsum.photos/id/237/300/300" />
     <h3>Object fit on a Square Image</h3>
-    <Avatar as="div" url="https://placehold.co/450x450/png" />
+    <Avatar as="div" url="https://picsum.photos/id/237/300/300" />
     <h3>Object fit on a Square Image using Dynamic Size</h3>
-    <Avatar as="div" size={px2rem(200)} url="https://placehold.co/450x450/png" />
+    <Avatar as="div" size={px2rem(200)} url="https://picsum.photos/id/237/300/300" />
   </div>
 );
