845202a5101270424f2c2385666ed0a6daf7dbb7
josh.bagwell <josh.bagwell@workday.com>
2024-08-14 12:18:08 -0600

    chore: Updated deps and jest

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -4,12 +4,16 @@ import {verifyComponent} from './verifyComponent';
 import {jest} from '@jest/globals';
 
 import {setUniqueSeed, resetUniqueIdCount} from '@workday/canvas-kit-react/common';
+import {TextEncoder} from 'util';
 
 expect.extend(toHaveNoViolations);
 
 // add convenience variables to the global context
 (global as any).verifyComponent = verifyComponent;
 (global as any).jest = jest;
+
+// Add globals for TextEncoder that was missing
+(global as any).TextEncoder = TextEncoder;
 
 // Not necessary for our tests, but demonstrate how to have stable ids for jest snapshots
 beforeEach(() => {
