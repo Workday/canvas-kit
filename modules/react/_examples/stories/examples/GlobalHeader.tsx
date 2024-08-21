52818cab8755d5f2a8863eca0ffc850c4960b01d
William Stanton <william.stanton@workday.com>
2024-08-21 13:13:00 -0400

    docs: Autocomplete works for now

--- modules/react/_examples/stories/examples/GlobalHeader.tsx
+++ modules/react/_examples/stories/examples/GlobalHeader.tsx
@@ -14,42 +14,79 @@ import {Avatar} from '@workday/canvas-kit-react/avatar';
 import {Flex, FlexProps} from '@workday/canvas-kit-react/layout';
 import {SearchForm} from '@workday/canvas-kit-labs-react/search-form';
 import {Tooltip} from '@workday/canvas-kit-react/tooltip';
+import {Combobox} from '@workday/canvas-kit-labs-react/combobox';
+import {TextInput} from '@workday/canvas-kit-react/text-input';
+import {StyledMenuItem, MenuItemProps} from '@workday/canvas-kit-react/menu';
 
 interface HeaderItemProps extends FlexProps {}
 
-export const Basic = () => (
-  <GlobalHeader>
-    <GlobalHeader.Item>
-      <Tooltip title="Global Navigation" type="describe">
-        <TertiaryButton aria-label="menu" icon={justifyIcon}>
-          MENU
-        </TertiaryButton>
-      </Tooltip>
-      <Tooltip title="Workday Home">
-        <TertiaryButton>
-          <img src="https://design.workday.com/images/ck-dub-logo-blue.svg" alt="" />
-        </TertiaryButton>
-      </Tooltip>
-    </GlobalHeader.Item>
-    <GlobalHeader.Item margin="auto" width="100%" maxWidth={`calc(${space.xxxl} * 6)`}>
-      <SearchForm onSubmit={() => 1} />
-    </GlobalHeader.Item>
-    <GlobalHeader.Item>
-      <Tooltip title="Assistant">
-        <TertiaryButton icon={assistantIcon} />
-      </Tooltip>
-      <Tooltip title="Notifications">
-        <TertiaryButton icon={notificationsIcon} />
-      </Tooltip>
-      <Tooltip title="My Tasks">
-        <TertiaryButton icon={inboxIcon} />
-      </Tooltip>
-      <Tooltip title="Profile">
-        <Avatar />
-      </Tooltip>
-    </GlobalHeader.Item>
-  </GlobalHeader>
+const tasks = ['Request Time Off', 'Create Expense Report'];
+
+const autocompleteResult = (
+  textModifier: number,
+  disabled: boolean
+): React.ReactElement<MenuItemProps> => (
+  <StyledMenuItem isDisabled={disabled}>
+    Result{' '}
+    <span>
+      num<span>ber</span>
+    </span>{' '}
+    {textModifier}
+  </StyledMenuItem>
 );
+
+const simpleAutoComplete = (count: number, showDisabledItems, total = 5) =>
+  Array.apply(null, Array(count))
+    .map((_, i) => autocompleteResult(i, showDisabledItems && i === 0))
+    .splice(0, total);
+
+export const Basic = () => {
+  const [currentText, setCurrentText] = React.useState('');
+  const textLength = currentText.length;
+
+  const autocompleteCallback = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setCurrentText(event.target.value);
+  };
+
+  return (
+    <GlobalHeader>
+      <GlobalHeader.Item>
+        <Tooltip title="Global Navigation" type="describe">
+          <TertiaryButton aria-label="menu" icon={justifyIcon}>
+            MENU
+          </TertiaryButton>
+        </Tooltip>
+        <Tooltip title="Workday Home">
+          <TertiaryButton>
+            <img src="https://design.workday.com/images/ck-dub-logo-blue.svg" alt="" />
+          </TertiaryButton>
+        </Tooltip>
+      </GlobalHeader.Item>
+      <GlobalHeader.Item margin="auto" width="100%" maxWidth={`calc(${space.xxxl} * 6)`}>
+        <Combobox
+          autocompleteItems={simpleAutoComplete(textLength, false, 10)}
+          onChange={autocompleteCallback}
+        >
+          <TextInput placeholder="Autocomplete" />
+        </Combobox>
+      </GlobalHeader.Item>
+      <GlobalHeader.Item>
+        <Tooltip title="Assistant">
+          <TertiaryButton icon={assistantIcon} />
+        </Tooltip>
+        <Tooltip title="Notifications">
+          <TertiaryButton icon={notificationsIcon} />
+        </Tooltip>
+        <Tooltip title="My Tasks">
+          <TertiaryButton icon={inboxIcon} />
+        </Tooltip>
+        <Tooltip title="Profile">
+          <Avatar />
+        </Tooltip>
+      </GlobalHeader.Item>
+    </GlobalHeader>
+  );
+};
 
 const GlobalHeaderItem = createComponent('div')({
   displayName: 'GlobalHeader.Item',
