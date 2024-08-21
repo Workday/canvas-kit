6534328cc31ac9cf8653d38aa7784466b85d5e31
Nicholas Boll <nicholas.boll@gmail.com>
2024-08-21 16:30:30 -0600

    fix(combobox): Show selected when multiple is enabled

--- modules/react/combobox/lib/ComboboxMenuItem.tsx
+++ modules/react/combobox/lib/ComboboxMenuItem.tsx
@@ -44,7 +44,7 @@ export const useComboboxMenuItem = composeHooks(
       event.preventDefault();
     };
 
-    const selected = model.state.selectedIds[0] === id;
+    const selected = model.state.selectedIds.includes(id);
 
     return {
       role: 'option',
