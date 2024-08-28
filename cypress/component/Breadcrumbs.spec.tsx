6661fc60d9b1a2b28a4c2db88d2c6d30dc1529f6
josh.bagwell <josh.bagwell@workday.com>
2024-08-28 11:24:08 -0600

    chore: Updated Breadcrumbs

--- cypress/component/Breadcrumbs.spec.tsx
+++ cypress/component/Breadcrumbs.spec.tsx
@@ -218,7 +218,7 @@ describe('Breadcrumbs', () => {
             cy.findByRole('menuitem', {name: 'Home'}).should('have.focus');
           });
 
-          it('should contain fifth link as the last menu item', () => {
+          it.only('should contain sixth link as the last menu item', () => {
             cy.focused().realType('{downarrow}');
             cy.focused().realType('{downarrow}');
             cy.focused().realType('{downarrow}');
