6e0e672fce177b34bc292b7b7a61980657634665
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2024-10-07 16:08:15 -0600

    fix: Add visual testing for inputs after form field promotion (#2963)
    Add visual testing back in that were removed by accident during form field promotion
    
    [category:Components]
    
    Co-authored-by: manuel.carrera <manuel.carrera@workday.com>

---
+++ modules/react/text-area/stories/visualTesting.stories.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+
+import {
+  ComponentStatesTable,
+  permutateProps,
+  StaticStates,
+} from '@workday/canvas-kit-react/testing';
+import {customColorTheme} from '../../../../utils/storybook';
+
+import {TextArea} from '@workday/canvas-kit-react/text-area';
+
+export default {
+  title: 'Testing/Inputs/Text Area',
+  component: TextArea,
+  parameters: {
+    chromatic: {
+      disable: false,
+    },
+  },
+};
+
+export const TextAreaStates = () => (
+  <StaticStates>
+    <ComponentStatesTable
+      rowProps={permutateProps(
+        {
+          value: [
+            {value: 'Input value', label: 'With Value'},
+            {value: '', label: 'No Value'},
+          ],
+          placeholder: [{value: 'Placeholder', label: 'Placeholder'}],
+          error: [
+            {value: undefined, label: ''},
+            {value: TextArea.ErrorType.Alert, label: 'Alert'},
+            {value: TextArea.ErrorType.Error, label: 'Error'},
+          ],
+        },
+        props => {
+          if (props.value === '' && !props.placeholder) {
+            return false;
+          }
+          return true;
+        }
+      )}
+      columnProps={permutateProps(
+        {
+          className: [
+            {label: 'Default', value: ''},
+            {label: 'Hover', value: 'hover'},
+            {label: 'Focus', value: 'focus'},
+            {label: 'Focus Hover', value: 'focus hover'},
+            {label: 'Active', value: 'active'},
+            {label: 'Active Hover', value: 'active hover'},
+          ],
+          disabled: [
+            {label: '', value: false},
+            {label: 'Disabled', value: true},
+          ],
+        },
+        props => {
+          if (props.disabled && !['', 'hover'].includes(props.className)) {
+            return false;
+          }
+          return true;
+        }
+      )}
+    >
+      {props => (
+        <TextArea
+          {...props}
+          style={{minWidth: 60, width: 100}}
+          onChange={() => {}} // eslint-disable-line no-empty-function
+        />
+      )}
+    </ComponentStatesTable>
+  </StaticStates>
+);
+
+export const TextAreaThemedStates = () => <TextAreaStates />;
+TextAreaThemedStates.parameters = {
+  canvasProviderDecorator: {
+    theme: customColorTheme,
+  },
+};
