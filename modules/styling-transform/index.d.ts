c8e8cafe1e3ce74cbfb5e0883b22910866a8b667
Nicholas Boll <nicholas.boll@gmail.com>
2024-10-14 12:54:05 -0600

    ci: Fix build script (#2985)
    Fix canary and release builds that broke with TypeScript 5.0: https://github.com/cevek/ttypescript/issues/140
    
    Switched build compiler to `ts-patch` instead.
    
    [category:Infrastructure]

--- modules/styling-transform/index.d.ts
+++ modules/styling-transform/index.d.ts
@@ -1,5 +1,6 @@
 import styleTransformer from './lib/styleTransform';
-export {Config} from './lib/utils/types';
+import {type Config} from './lib/utils/types';
+export {type Config};
 export {parseNodeToStaticValue} from './lib/utils/parseNodeToStaticValue';
 export {parseObjectToStaticValue} from './lib/utils/parseObjectToStaticValue';
 export {createObjectTransform} from './lib/createObjectTransform';
@@ -8,5 +9,7 @@ export {styleTransformer};
 export {withDefaultContext} from './lib/styleTransform';
 export {getClassName} from './lib/utils/handleCreateStencil';
 
-// be compatible with ttypescript which expects a default export
+// be compatible with ts-patch which expects a default export
 export default styleTransformer;
+
+export function createConfig(config: Config): Config;
