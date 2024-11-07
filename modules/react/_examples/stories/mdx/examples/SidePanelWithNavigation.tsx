1699fcff61b0bbb0d7c0c992fe406e5a17c2e0df
William Stanton <william.stanton@workday.com>
2024-11-07 15:07:35 -0500

    docs: Styling compact view

--- modules/react/_examples/stories/mdx/examples/SidePanelWithNavigation.tsx
+++ modules/react/_examples/stories/mdx/examples/SidePanelWithNavigation.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import {styled, StyledType} from '@workday/canvas-kit-react/common';
-import {colors, space, gradients, type} from '@workday/canvas-kit-react/tokens';
+import {colors, gradients, type} from '@workday/canvas-kit-react/tokens';
+import {system} from '@workday/canvas-tokens-web';
 import {SidePanel, useSidePanel} from '@workday/canvas-kit-preview-react/side-panel';
 import {Flex, Box} from '@workday/canvas-kit-react/layout';
-import {BodyText, Subtext} from '@workday/canvas-kit-react/text';
+import {BodyText, Heading, Subtext} from '@workday/canvas-kit-react/text';
 import {Expandable} from '@workday/canvas-kit-labs-react/expandable';
 import {SystemIcon} from '@workday/canvas-kit-react/icon';
 import {birthdayIcon, ribbonIcon} from '@workday/canvas-system-icons-web';
+import {TertiaryButton} from '../../../../button';
+import {createStyles} from '@workday/canvas-kit-styling';
 
 const StyledExpandable = styled(Expandable.Target)({
   borderRadius: 0,
@@ -36,6 +39,112 @@ const bestsellers = [
   {value: 'Tiramisu', linkValue: '#Tiramisu'},
 ];
 
+const stylesOverride = {
+  listContainer: createStyles({
+    listStyle: 'none',
+    padding: system.space.x4,
+    paddingTop: system.space.x16,
+    flexDirection: 'column',
+  }),
+};
+
+const CompactView = () => {
+  return (
+    <Flex as="ul" cs={stylesOverride.listContainer}>
+      <Flex.Item as="li">
+        <TertiaryButton>
+          <SystemIcon icon={ribbonIcon} />
+        </TertiaryButton>
+      </Flex.Item>
+      <Flex.Item as="li">
+        <TertiaryButton>
+          <SystemIcon icon={birthdayIcon} />
+        </TertiaryButton>
+      </Flex.Item>
+    </Flex>
+  );
+};
+
+const ExpandedView = () => {
+  return (
+    <>
+      <Heading size="small">Cake or Death</Heading>
+      <Flex as="ul" flexDirection="column" rowGap="zero" paddingInlineStart="zero">
+        <Flex.Item as="li" listStyle="none">
+          <Expandable padding="zero">
+            <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="m" textAlign="center">
+              <SystemIcon icon={ribbonIcon} paddingRight="s" />
+              <Expandable.Title padding="zero">
+                <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
+                  Bestsellers
+                </BodyText>
+                <Subtext size="medium" color="blackpepper300" marginY="zero">
+                  Award winning sweet treats
+                </Subtext>
+              </Expandable.Title>
+              <Expandable.Icon iconPosition="end" />
+            </StyledExpandable>
+            <Expandable.Content paddingY="zero" paddingX="zero" as="ul">
+              {bestsellers.map(item => {
+                return (
+                  <li>
+                    <StyledLink
+                      size="large"
+                      display="flex"
+                      textDecoration="none"
+                      paddingLeft="xxl"
+                      paddingTop="xxs"
+                      paddingBottom="zero"
+                      href={item.linkValue}
+                    >
+                      {item.value}
+                    </StyledLink>
+                  </li>
+                );
+              })}
+            </Expandable.Content>
+          </Expandable>
+        </Flex.Item>
+        <Flex.Item as="li" listStyle="none">
+          <Expandable padding="zero">
+            <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="m" textAlign="center">
+              <SystemIcon icon={birthdayIcon} paddingRight="s" />
+              <Expandable.Title padding="zero">
+                <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
+                  Custom Cakes
+                </BodyText>
+                <Subtext size="medium" color="blackpepper300" marginY="zero">
+                  To celebrate your life milestones
+                </Subtext>
+              </Expandable.Title>
+              <Expandable.Icon iconPosition="end" />
+            </StyledExpandable>
+            <Expandable.Content paddingY="zero" paddingX="zero" as="ul">
+              {cakes.map(item => {
+                return (
+                  <li>
+                    <StyledLink
+                      size="large"
+                      display="flex"
+                      textDecoration="none"
+                      paddingLeft="xxl"
+                      paddingTop="xxs"
+                      paddingBottom="zero"
+                      href={item.linkValue}
+                    >
+                      {item.value}
+                    </StyledLink>
+                  </li>
+                );
+              })}
+            </Expandable.Content>
+          </Expandable>
+        </Flex.Item>
+      </Flex>
+    </>
+  );
+};
+
 export const WithNavigation = () => {
   const {expanded, panelProps, labelProps, controlProps} = useSidePanel();
 
@@ -43,78 +152,7 @@ export const WithNavigation = () => {
     <Flex height={800} position="relative" backgroundColor="frenchVanilla100">
       <SidePanel {...panelProps}>
         <SidePanel.ToggleButton {...controlProps} />
-        <Flex as="ul" flexDirection="column" rowGap="zero" paddingInlineStart="zero">
-          <Flex.Item as="li" listStyle="none">
-            <Expandable padding="zero">
-              <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="m" textAlign="center">
-                <SystemIcon icon={ribbonIcon} paddingRight="s" />
-                <Expandable.Title padding="zero">
-                  <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
-                    Bestsellers
-                  </BodyText>
-                  <Subtext size="medium" color="blackpepper300" marginY="zero">
-                    Award winning sweet treats
-                  </Subtext>
-                </Expandable.Title>
-                <Expandable.Icon iconPosition="end" />
-              </StyledExpandable>
-              <Expandable.Content paddingY="zero" paddingX="zero" as="ul">
-                {bestsellers.map(item => {
-                  return (
-                    <li>
-                      <StyledLink
-                        size="large"
-                        display="flex"
-                        textDecoration="none"
-                        paddingLeft="xxl"
-                        paddingTop="xxs"
-                        paddingBottom="zero"
-                        href={item.linkValue}
-                      >
-                        {item.value}
-                      </StyledLink>
-                    </li>
-                  );
-                })}
-              </Expandable.Content>
-            </Expandable>
-          </Flex.Item>
-          <Flex.Item as="li" listStyle="none">
-            <Expandable padding="zero">
-              <StyledExpandable paddingTop="m" paddingBottom="xxs" paddingX="m" textAlign="center">
-                <SystemIcon icon={birthdayIcon} paddingRight="s" />
-                <Expandable.Title padding="zero">
-                  <BodyText size="small" fontWeight="bold" color="blackpepper300" marginY="zero">
-                    Custom Cakes
-                  </BodyText>
-                  <Subtext size="medium" color="blackpepper300" marginY="zero">
-                    To celebrate your life milestones
-                  </Subtext>
-                </Expandable.Title>
-                <Expandable.Icon iconPosition="end" />
-              </StyledExpandable>
-              <Expandable.Content paddingY="zero" paddingX="zero" as="ul">
-                {cakes.map(item => {
-                  return (
-                    <li>
-                      <StyledLink
-                        size="large"
-                        display="flex"
-                        textDecoration="none"
-                        paddingLeft="xxl"
-                        paddingTop="xxs"
-                        paddingBottom="zero"
-                        href={item.linkValue}
-                      >
-                        {item.value}
-                      </StyledLink>
-                    </li>
-                  );
-                })}
-              </Expandable.Content>
-            </Expandable>
-          </Flex.Item>
-        </Flex>
+        {expanded ? <ExpandedView /> : <CompactView />}
       </SidePanel>
     </Flex>
   );
