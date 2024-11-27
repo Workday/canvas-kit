89347dde0f5b148e2c0274d698b1d0938f3698a8
Josh <44883293+josh-bagwell@users.noreply.github.com>
2024-11-27 10:39:43 -0700

    test: Resolve Cypress Flaky fixes (#3034)
    Fixes: #2900  
    
    This is adding fixes for the Cypress flaky specs that were caused from the [Webpack Upgrade](https://github.com/Workday/canvas-kit/pull/2674).
    
    This is addressing Flaky results for Cypress. A test is considered to be flaky when it can pass and fail across multiple retry attempts without any code changes. For example, a test is executed and fails, then the test is executed again, without any change to the code, but this time it passes. See more about Flaky [here](https://docs.cypress.io/cloud/features/flaky-test-management).
    
    A Flaky result only happens in the CI and not in local since running Cypress locally only gives `pass` or `fail` results.
    
    We had recently switched from E2E tests (end-to-end) to Component tests when upgrading [Storybook](https://github.com/Workday/canvas-kit/pull/2674) and this had created flaky results on some of our components. We think this may be due to the nature of the Component runner as it is so much quicker than E2E. Due to it's nature of being much quicker, it resulted in flaky results due to our components not being to move as quick as the component runner. There may be times where the Component runner would move quicker than an item being focused in a menu or an item switching focus from one item to the next. 
    
    To address the Flaky issues, there have been updates made to some Component Specs:
    - Be more explicit with contexts (making sure that each line could be something that a user would do)
    - Rely on the use of `findByRole()` instead of using `get()`. This allows us to be more specific about what element it is grabbing
    - Removing deeply nested contexts and moving out to it's own context. This way we are not using more `beforeEach` than is needed and improves legibility.
    
    [category:Test]

--- cypress/component/Menu.spec.tsx
+++ cypress/component/Menu.spec.tsx
@@ -74,17 +74,17 @@ describe('Menu', () => {
 
       context('when down arrow key is pressed', () => {
         beforeEach(() => {
+          cy.findByRole('menuitem', {name: 'First Item'}).should('be.focused');
           cy.focused().realType('{downarrow}');
         });
 
         it('should transfer focus to the second item', () => {
-          cy.findByRole('menuitem', {name: 'Second Item'}).should('have.focus');
+          cy.findByRole('menuitem', {name: 'Second Item'}).should('be.focused');
         });
 
         context('when the enter key is pressed', () => {
           beforeEach(() => {
             cy.focused().realType('{enter}');
-            cy.wait(150);
           });
 
           it('should have aria-expanded set to false', () => {
@@ -100,7 +100,8 @@ describe('Menu', () => {
           });
 
           it('should select the second item', () => {
-            cy.findByTestId('output').should('contain', '1');
+            cy.findByTestId('output').should('exist');
+            cy.findByTestId('output').contains('1');
           });
         });
       });
@@ -123,22 +124,18 @@ describe('Menu', () => {
         });
 
         it('should select the second item', () => {
-          cy.findByTestId('output').should('contain', '1');
+          cy.findByTestId('output').should('exist');
+          cy.findByTestId('output').contains('1');
         });
       });
 
       context('when the fourth item is clicked', () => {
         beforeEach(() => {
-          cy.contains('button', 'Fourth Item').click();
-          cy.wait(150);
+          cy.findByRole('menuitem', {name: 'Fourth Item'}).click();
         });
 
         it('should not close the menu', () => {
           cy.findByRole('menu').should('be.visible');
-        });
-
-        it('should have aria-expanded set to true', () => {
-          cy.findByRole('button', {name: 'Open Menu'}).should('have.attr', 'aria-expanded', 'true');
         });
 
         it('should not select the fourth item', () => {
@@ -170,40 +167,44 @@ describe('Menu', () => {
         });
 
         it.skip('should focus on the third item', () => {
-          cy.findByRole('menuitem', {name: 'Third Item'}).should('have.focus');
+          cy.get('[role="menuitem"]').should('contains', 'Third Item');
         });
       });
 
       context('when up arrow key is pressed', () => {
         beforeEach(() => {
-          cy.focused().realType('{uparrow}');
+          cy.findByRole('menu').should('exist');
+          cy.findByRole('menuitem', {name: 'First Item'})
+            .should('be.focused')
+            .realType('{uparrow}');
         });
 
         it('should focus on the last option', () => {
-          cy.findByRole('menuitem', {name: 'Fourth Item'}).should('have.focus');
+          cy.findByRole('menuitem', {name: 'Fourth Item'}).should('be.focused');
+        });
+      });
+
+      context('when the enter key is pressed', () => {
+        beforeEach(() => {
+          cy.findByRole('menu').should('exist');
+          cy.findByRole('menuitem', {name: 'First Item'})
+            .should('be.focused')
+            .realType('{uparrow}');
+          cy.findByRole('menuitem', {name: 'Fourth Item'}).should('be.visible');
+          cy.focused().realType('{enter}');
         });
 
-        context('when the enter key is pressed', () => {
-          beforeEach(() => {
-            cy.focused().realType('{enter}');
-            cy.wait(150);
-          });
+        it('should not close the menu', () => {
+          cy.findByRole('menu').should('be.visible');
+        });
 
-          it('should not close the menu', () => {
-            cy.findByRole('menu').should('be.visible');
-          });
+        it('should have aria-expanded set to true', () => {
+          cy.findByRole('button').should('have.attr', 'aria-expanded', 'true');
+          cy.findByRole('button').should('contain', 'Open Menu');
+        });
 
-          it('should have aria-expanded set to true', () => {
-            cy.findByRole('button', {name: 'Open Menu'}).should(
-              'have.attr',
-              'aria-expanded',
-              'true'
-            );
-          });
-
-          it('should not select the fourth item', () => {
-            cy.findByTestId('output').should('not.contain', '4');
-          });
+        it('should not select the fourth item', () => {
+          cy.findByTestId('output').should('not.contain', '4');
         });
       });
     });
