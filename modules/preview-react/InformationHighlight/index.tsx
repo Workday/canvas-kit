e6d7c0913b9ec88b9b83700e93c76542a9e905fb
Arianna Malakis <arianna.malakis@workday.com>
2025-01-03 14:00:48 -0800

    export parts

--- modules/preview-react/InformationHighlight/index.tsx
+++ modules/preview-react/InformationHighlight/index.tsx
@@ -1,1 +1,5 @@
 export * from './lib/InformationHighlight';
+export * from './lib/parts/Body';
+export * from './lib/parts/Heading';
+export * from './lib/parts/Icon';
+export * from './lib/parts/Link';
