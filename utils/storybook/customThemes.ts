69077f03699541ae6037f588d8172cad93d34c03
manuel.carrera <manuel.carrera@workday.com>
2025-01-08 12:11:52 -0700

    fix: Add support for action via the canvas provider

--- utils/storybook/customThemes.ts
+++ utils/storybook/customThemes.ts
@@ -6,8 +6,29 @@ export const customColorTheme: PartialCanvasTheme = {
       main: 'purple',
       contrast: 'turquoise',
     },
-    alert: {
-      main: 'coral',
+    error: {
+      main: 'crimson',
+    },
+    success: {
+      main: 'aquamarine',
+    },
+    neutral: {
+      main: 'gray',
+    },
+    common: {
+      focusOutline: 'turquoise',
+    },
+  },
+};
+
+export const customColorThemeWithAction: PartialCanvasTheme = {
+  palette: {
+    primary: {
+      main: 'purple',
+      contrast: 'turquoise',
+    },
+    action: {
+      main: 'red',
     },
     error: {
       main: 'crimson',
