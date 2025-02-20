4c1b8018a26304d15ac5dc5f1c0efbfd162e7a8b
Nicholas Boll <nicholas.boll@gmail.com>
2025-02-20 14:53:32 -0700

    fix(popup): Fix useReturnFocus to respect tabindex=-1 buttons (#3141)
    Separate the concept of focusability between keyboard and mouse. Buttons with a `tabindex` of `-1` are focusable via a pointer device, but not via a keyboard. A user clicking on a button that isn't focusable by keyboard is still a valid target for avoiding focus stealing.
    
    Fixes #3140
    
    Fixes: #1234
    
    [category:Components]

--- modules/react/popup/stories/stories_testing.tsx
+++ modules/react/popup/stories/stories_testing.tsx
@@ -392,9 +392,12 @@ export const ReturnFocusTest = () => {
             <FormField.Label>Name</FormField.Label>
             <FormField.Input as={TextInput} />
           </FormField>
-          <Popup.Target style={{marginBottom: 400, marginLeft: 410}} data-testid="target">
-            Open Popup
-          </Popup.Target>
+          <Flex style={{marginBottom: 400, marginLeft: 410}}>
+            <SecondaryButton id="return-focus-button-tabindex" tabIndex={-1}>
+              Button with TabIndex=-1
+            </SecondaryButton>
+            <Popup.Target data-testid="target">Open Popup</Popup.Target>
+          </Flex>
           <Popup.Popper>
             <Popup.Card>
               <Popup.CloseIcon aria-label="Close" />
