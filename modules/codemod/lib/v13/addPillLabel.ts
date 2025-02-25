dd5cf835d135d53e973245818d280041a8b18cfc
manuel.carrera <manuel.carrera@workday.com>
2025-02-25 08:33:11 -0700

    test: Start adding Pill codemod

---
+++ modules/codemod/lib/v13/addPillLabel.ts
@@ -0,0 +1,65 @@
+import {API, FileInfo, Options, JSXOpeningElement, JSXIdentifier} from 'jscodeshift';
+import {hasImportSpecifiers} from '../v6/utils';
+import {getImportRenameMap} from './utils/getImportRenameMap';
+
+const packages = ['@workday/canvas-kit-preview-react/pill'];
+const packageImports = ['Pill'];
+
+export default function transformer(file: FileInfo, api: API, options: Options) {
+  const j = api.jscodeshift;
+
+  const root = j(file.source);
+
+  // exit if the named imports aren't found
+  if (!hasImportSpecifiers(api, root, packages, packageImports)) {
+    return file.source;
+  }
+
+  // getImportRenameMap utility will tell us if the file containsCanvasImports
+  // and give us an importMap to track what identifiers we need to update
+  const {importMap, styledMap} = getImportRenameMap(j, root, '@workday/canvas-kit-preview-react');
+
+  root
+    .find(j.JSXOpeningElement, (value: JSXOpeningElement) => {
+      const isCorrectImport = Object.entries(importMap).some(
+        ([original, imported]) =>
+          imported === (value.name as JSXIdentifier).name && packageImports.includes(original)
+      );
+
+      const isCorrectStyled = Object.entries(styledMap).some(
+        ([original, styled]) =>
+          styled === (value.name as JSXIdentifier).name && packageImports.includes(original)
+      );
+
+      return isCorrectImport || isCorrectStyled;
+    })
+    .forEach(nodePath => {
+      console.log('node>>>', nodePath.node);
+      nodePath.node.attributes?.forEach(attr => {
+        if (attr.type === 'JSXAttribute') {
+          if (attr.name.name === 'hasError') {
+            attr.name.name = 'error';
+            if (attr.value && attr.value.type === 'JSXExpressionContainer') {
+              const value = attr.value.expression as any;
+
+              if (value.type === 'BooleanLiteral') {
+                if (value.value) {
+                  attr.value = j.stringLiteral('error');
+                } else {
+                  attr.value = j.jsxExpressionContainer(j.identifier('undefined'));
+                }
+              } else if (value.type === 'Identifier') {
+                attr.value.expression = j.conditionalExpression(
+                  j.identifier(value.name),
+                  j.stringLiteral('error'),
+                  j.identifier('undefined')
+                );
+              }
+            }
+          }
+        }
+      });
+    });
+
+  return root.toSource();
+}
---
+++ modules/codemod/lib/v13/spec/addPillLabel.spec.ts
@@ -0,0 +1,173 @@
+import {expectTransformFactory} from './expectTransformFactory';
+import transform from '../addPillLabel';
+import {stripIndent} from 'common-tags';
+
+const expectTransform = expectTransformFactory(transform);
+
+describe('renameHasError', () => {
+  it('should not change non-canvas imports', () => {
+    const input = stripIndent`
+            import {Pill} from '@workday/any-other-package'
+            <>
+                <Pill />
+                <Pill />
+                <Pill />
+            </>
+        `;
+
+    const expected = stripIndent`
+            import {Pill} from '@workday/any-other-package'
+            <>
+                <Pill />
+                <Pill />
+                <Pill />
+            </>
+        `;
+    expectTransform(input, expected);
+  });
+
+  //   it('should rename hasError to error and set the value to "error" for FormField, TextInput and TextArea', () => {
+  //     const input = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         import {TextArea} from '@workday/canvas-kit-preview-react/text-area'
+  //         import {FormField} from '@workday/canvas-kit-preview-react/form-field'
+  //         <>
+  //             <TextInput hasError={true} />
+  //             <TextArea hasError={true} />
+  //             <FormField hasError={true} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         import {TextArea} from '@workday/canvas-kit-preview-react/text-area'
+  //         import {FormField} from '@workday/canvas-kit-preview-react/form-field'
+  //         <>
+  //             <TextInput error="error" />
+  //             <TextArea error="error" />
+  //             <FormField error="error" />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+  //   it('should rename rename hasError to error and handle ternary with variable', () => {
+  //     const input = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         const myBool = true;
+  //         <>
+  //             <TextInput hasError={myBool} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         const myBool = true;
+  //         <>
+  //             <TextInput error={myBool ? "error" : undefined} />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+  //   it('should change main package canvas imports', () => {
+  //     const input = stripIndent`
+  //         import {TextInput, TextArea, FormField} from '@workday/canvas-kit-preview-react'
+  //         <>
+  //             <TextInput hasError={true} />
+  //             <TextArea hasError={true} />
+  //             <FormField hasError={true} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput, TextArea, FormField} from '@workday/canvas-kit-preview-react'
+  //         <>
+  //             <TextInput error="error" />
+  //             <TextArea error="error" />
+  //             <FormField error="error" />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+
+  //   it('should change renamed FormField', () => {
+  //     const input = stripIndent`
+  //         import {TextInput as CanvasTextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         import {TextArea as CanvasTextArea} from '@workday/canvas-kit-preview-react/text-area'
+  //         import {FormField as CanvasForm} from '@workday/canvas-kit-preview-react/form-field'
+
+  //         <>
+  //             <CanvasTextInput hasError={true} />
+  //             <CanvasTextArea hasError={true} />
+  //             <CanvasForm hasError={true} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput as CanvasTextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         import {TextArea as CanvasTextArea} from '@workday/canvas-kit-preview-react/text-area'
+  //         import {FormField as CanvasForm} from '@workday/canvas-kit-preview-react/form-field'
+
+  //         <>
+  //             <CanvasTextInput error="error" />
+  //             <CanvasTextArea error="error" />
+  //             <CanvasForm error="error" />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+
+  //   it('should change styled FormField', () => {
+  //     const input = stripIndent`
+  //         import {FormField} from '@workday/canvas-kit-preview-react/form-field'
+  //         const StyledForm = styled(FormField)({color: "#000"});
+  //         <StyledForm hasError={true} />
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {FormField} from '@workday/canvas-kit-preview-react/form-field'
+  //         const StyledForm = styled(FormField)({color: "#000"});
+  //         <StyledForm error="error" />
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+
+  //   it('should rename rename hasError to error and handle ternary with variable', () => {
+  //     const input = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         const myBool = true;
+  //         <>
+  //             <TextInput hasError={myBool} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput} from '@workday/canvas-kit-preview-react/text-input'
+  //         const myBool = true;
+  //         <>
+  //             <TextInput error={myBool ? "error" : undefined} />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+
+  //   it('should not rename hasError prop for other components', () => {
+  //     const input = stripIndent`
+  //         import {TextInput, MyOtherComponent} from '@workday/canvas-kit-preview-react'
+  //         const myBool = true;
+  //         <>
+  //             <MyOtherComponent hasError={true} />
+  //             <TextInput hasError={myBool} />
+  //         </>
+  //     `;
+
+  //     const expected = stripIndent`
+  //         import {TextInput, MyOtherComponent} from '@workday/canvas-kit-preview-react'
+  //         const myBool = true;
+  //         <>
+  //             <MyOtherComponent hasError={true} />
+  //             <TextInput error={myBool ? "error" : undefined} />
+  //         </>
+  //     `;
+  //     expectTransform(input, expected);
+  //   });
+});
