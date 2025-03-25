796a615f00b02d9b9edf2ee7f8a8ba913af543e0
Neha Ahuja <neha.ahuja@workday.com>
2025-03-25 14:03:40 -0700

    refactor(Banner): Update inline styles to createStyles for BannerLabel

--- modules/react/banner/lib/BannerLabel.tsx
+++ modules/react/banner/lib/BannerLabel.tsx
@@ -2,8 +2,13 @@ import * as React from 'react';
 
 import {ExtractProps, createSubcomponent} from '@workday/canvas-kit-react/common';
 import {Flex} from '@workday/canvas-kit-react/layout';
+import {createStyles} from '@workday/canvas-kit-styling';
 
 import {useBannerLabel, useBannerModel} from './hooks';
+
+const parentContainerStyles = createStyles({
+  flex: '1 1 0%',
+});
 
 export interface BannerLabelProps extends ExtractProps<typeof Flex, never> {
   /**
@@ -18,7 +23,7 @@ export const BannerLabel = createSubcomponent('div')({
   elemPropsHook: useBannerLabel,
 })<BannerLabelProps>(({children, ...elemProps}, Element) => {
   return (
-    <Flex as={Element} flex="1 1 0%" {...elemProps}>
+    <Flex as={Element} cs={parentContainerStyles} {...elemProps}>
       {children}
     </Flex>
   );
