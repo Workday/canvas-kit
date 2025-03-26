91271774bedea1f04b50a689bc4cbc9126b34017
josh.bagwell <josh.bagwell@workday.com>
2025-03-26 11:24:42 -0600

    chore: Removed commented previous logos

--- modules/react/common/lib/parts/_brand-assets.ts
+++ modules/react/common/lib/parts/_brand-assets.ts
@@ -1,15 +1,3 @@
-// const CDN_URI = 'https://design.workday.com/images/';
-
-// export const dubLogoBlue = `<img src="${CDN_URI}ck-dub-logo-blue.svg" alt="">`;
-
-// export const dubLogoWhite = `<img src="${CDN_URI}ck-dub-logo-white.svg" alt="">`;
-
-// export const wdayLogoBlue = `<img src="${CDN_URI}ck-wday-logo-blue.svg" alt="">`;
-
-// export const wdayLogoWhite = `<img src="${CDN_URI}ck-wday-logo-white.svg" alt="">`;
-
-// export const miniWdayLogoBlue = `<img src="${CDN_URI}ck-mini-wday-logo-blue.svg" alt="">`;
-
 const CDN_URI = 'https://static.workday.com/brand-logos/';
 
 export const dubLogoPrimary = `<img src="${CDN_URI}wd-dub-primary.svg" alt="">`;
