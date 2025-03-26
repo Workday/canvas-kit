423e097625d8c1cf8e48dcfc794313aae00ded67
Josh <44883293+josh-bagwell@users.noreply.github.com>
2025-03-26 15:40:26 -0600

    feat!: Promote new Dub logos and add codemod to handle change (#3201)
    Fixes: #3150  
    
    This PR removes the old Dub Logos in Main and promotes the new logos that were previously in our Preview package to Main as part of the brand refresh. It also adds a codemod to update the imports and update the logo names.
    
    [category:Branding]
    
    Release Note:
    We've removed the old Dub Logos in Main and promoted the new logos that were previously in our Preview package to Main as part of the brand refresh. We've also added a codemod to update the import statements as well as the name of the logos used to the new names. 
    
    | Old Logo Name | New Logo Name |
    | --------------- | ----------------- | 
    | dubLogoBlue | dubLogoPrimary | 
    | dubLogoWhite | dubLogoReversed | 
    | wdayLogoBlue | wdayLogoPrimary | 
    | wdayLogoWhite | wdayLogoReversed |
    
    ### BREAKING CHANGES
    There is a codemod that should catch a lot of instances but, if there is an instance that is not caught, users will have to update those names manually. See above table for name references.

--- modules/codemod/lib/v13/index.ts
+++ modules/codemod/lib/v13/index.ts
@@ -1,11 +1,15 @@
 import {Transform} from 'jscodeshift';
 import addPillLabel from './addPillLabel';
+import renameDubLogosImports from './renameDubLogosImports';
+import renameDubLogosReferences from './renameDubLogosReferences';
 
 const transform: Transform = (file, api, options) => {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
   const fixes = [
     // add codemods here
     addPillLabel,
+    renameDubLogosImports,
+    renameDubLogosReferences,
   ];
   return fixes.reduce((source, fix) => fix({...file, source}, api, options) as string, file.source);
 };
