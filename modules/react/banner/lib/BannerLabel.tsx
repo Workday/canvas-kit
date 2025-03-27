82ed57b5524d06d9cb0475b303aed45aa3c737a0
Neha Ahuja <neha.ahuja@workday.com>
2025-03-26 20:56:43 -0700

    refactor(Banner): Used stencil for BannerLabel instead of createStyles

--- modules/react/banner/lib/BannerLabel.tsx
+++ modules/react/banner/lib/BannerLabel.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 import {ExtractProps, createSubcomponent} from '@workday/canvas-kit-react/common';
-import {Flex} from '@workday/canvas-kit-react/layout';
-import {createStyles} from '@workday/canvas-kit-styling';
+import {Flex, mergeStyles} from '@workday/canvas-kit-react/layout';
+import {createStencil} from '@workday/canvas-kit-styling';
 
 import {useBannerLabel, useBannerModel} from './hooks';
 
@@ -13,8 +13,10 @@ export interface BannerLabelProps extends ExtractProps<typeof Flex, never> {
   children: React.ReactNode;
 }
 
-const parentContainerStyles = createStyles({
-  flex: '1 1 0%',
+const bannerLabelStencil = createStencil({
+  base: {
+    flex: '1 1 0%',
+  },
 });
 
 export const BannerLabel = createSubcomponent('div')({
@@ -22,9 +24,5 @@ export const BannerLabel = createSubcomponent('div')({
   modelHook: useBannerModel,
   elemPropsHook: useBannerLabel,
 })<BannerLabelProps>(({children, ...elemProps}, Element) => {
-  return (
-    <Flex as={Element} cs={parentContainerStyles} {...elemProps}>
-      {children}
-    </Flex>
-  );
+  return <Element {...mergeStyles(elemProps, bannerLabelStencil())}>{children}</Element>;
 });
