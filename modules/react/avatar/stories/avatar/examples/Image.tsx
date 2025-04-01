7d758a4995e8c9ab2b54da47fdec5e07d2007dde
Neha Ahuja <neha.ahuja@workday.com>
2025-04-01 16:09:11 -0700

    fix: Add default altText for Avatar in Examples to resolve failing axe tests

--- modules/react/avatar/stories/avatar/examples/Image.tsx
+++ modules/react/avatar/stories/avatar/examples/Image.tsx
@@ -13,13 +13,13 @@ const containerStyles = createStyles({
 export const Image = () => (
   <div className={containerStyles}>
     <h3>Working Images</h3>
-    <Avatar size="extraExtraLarge" as="div" url={testAvatar} />
-    <Avatar size="extraLarge" as="div" url={testAvatar} />
-    <Avatar size="large" as="div" url={testAvatar} />
-    <Avatar size="medium" as="div" url={testAvatar} />
-    <Avatar size="small" as="div" url={testAvatar} />
-    <Avatar size="extraSmall" as="div" url={testAvatar} />
+    <Avatar size="extraExtraLarge" as="div" url={testAvatar} altText="Avatar" />
+    <Avatar size="extraLarge" as="div" url={testAvatar} altText="Avatar" />
+    <Avatar size="large" as="div" url={testAvatar} altText="Avatar" />
+    <Avatar size="medium" as="div" url={testAvatar} altText="Avatar" />
+    <Avatar size="small" as="div" url={testAvatar} altText="Avatar" />
+    <Avatar size="extraSmall" as="div" url={testAvatar} altText="Avatar" />
     <h3>Broken Image</h3>
-    <Avatar url="/fake/path/to/image.png" as="div" />
+    <Avatar url="/fake/path/to/image.png" as="div" altText="Avatar" />
   </div>
 );
