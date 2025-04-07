04e60fcab98599cccf881ab3f334a7ec7984acac
Alan B Smith <a.bax.smith@gmail.com>
2025-04-07 10:45:24 -0600

    fix: Update InformationHighlight.Body to use div (#3222)
    The `InformationHighlight.Body` subcomponent was previously a `p` element, which is _fine_ on its own, but it makes things unintuitive when you need to add multiple blocks of content inside the `Body`. Nesting `p` elements or adding list elements (`ul` or `ol`) is not allowed. To solve this, we're making `Body` a generic `div` element. This is consistent with our other `Body` subcomponents such as `Modal.Body`.
    
    Fixes: #3218
    
    [category:Components]
    
    Release Note:
    There are no visual changes because of this update. However, if you were relying on the `p` tag as an identifier for testing or other purposes, you'll need to adjust your implementation, as the underlying element is now a `div`. Additionally, if you'd like to keep a `p` tag around the `Body` text, you'll need to add it manually. Wrapping text in a `p` is not an accessibility requirement, but it can be helpful when you have multiple, separate content blocks.
    
    ```jsx
    // before
    <InformationHighlight.Body>
      Body text goes here.
      <ul>
        <li>Point One</li>
        <li>Point Two</li>
        <li>Point Three</li>
      </ul>
    </InformationHighlight.Body>
    
    // after
    <InformationHighlight.Body>
      <p style={{ margin: 0 }}>Body text goes here.</p>
      <ul>
        <li>Point One</li>
        <li>Point Two</li>
        <li>Point Three</li>
      </ul>
    </InformationHighlight.Body>
    ```

--- modules/preview-react/information-highlight/stories/examples/Informational.tsx
+++ modules/preview-react/information-highlight/stories/examples/Informational.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import {Flex} from '@workday/canvas-kit-react/layout';
 import {InformationHighlight} from '@workday/canvas-kit-preview-react/information-highlight';
 import {system} from '@workday/canvas-tokens-web';
+import {createStyles} from '@workday/canvas-kit-styling';
+
+const contentTextStyles = createStyles({
+  margin: 0,
+});
+
+const contentListStyles = createStyles({
+  listStyle: 'inside',
+  marginInlineStart: 0,
+  marginBlockStart: system.space.x2,
+  marginBlockEnd: 0,
+  padding: 0,
+});
 
 export const Informational = () => {
   return (
@@ -10,17 +23,33 @@ export const Informational = () => {
         <InformationHighlight.Icon />
         <InformationHighlight.Heading> Informational Highlight </InformationHighlight.Heading>
         <InformationHighlight.Body>
-          If you select the link below it will just reroute you back to this page
+          <p className={contentTextStyles}>
+            This is a low-emphasis, informational callout. You should use this for nice-to-have
+            information, such as:
+          </p>
+          <ul className={contentListStyles}>
+            <li>tangential information or context</li>
+            <li>related features</li>
+            <li>additional opportunities</li>
+          </ul>
         </InformationHighlight.Body>
-        <InformationHighlight.Link href="#hyperlink">View the Docs</InformationHighlight.Link>
+        <InformationHighlight.Link href="#hyperlink">Learn More</InformationHighlight.Link>
       </InformationHighlight>
       <InformationHighlight variant={'informational'} emphasis={'high'}>
         <InformationHighlight.Icon />
         <InformationHighlight.Heading> Informational Highlight </InformationHighlight.Heading>
         <InformationHighlight.Body>
-          If you select the link below it will just reroute you back to this page
+          <p className={contentTextStyles}>
+            This is a high-emphasis, informational callout. You should use this for nice-to-have
+            information, such as:
+          </p>
+          <ul className={contentListStyles}>
+            <li>tangential information or context</li>
+            <li>related features</li>
+            <li>additional opportunities</li>
+          </ul>
         </InformationHighlight.Body>
-        <InformationHighlight.Link href="#hyperlink">View the Docs</InformationHighlight.Link>
+        <InformationHighlight.Link href="#hyperlink">Learn More</InformationHighlight.Link>
       </InformationHighlight>
     </Flex>
   );
