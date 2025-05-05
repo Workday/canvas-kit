6d774d8590ae2575f618c3bebdc52d30872de732
Nicholas Boll <nicholas.boll@gmail.com>
2025-05-05 11:59:13 -0600

    fix(select): Remove excess props from `SelectInputProps` (#3269)
    Removing unintentional props from `SelectInputProps`. Any component that uses `create*` component utility functions and uses `ExtractProps` for the prop interface should use `never` as the second parameter. The `never` means "don't include the default HTML attributes in the prop interface". The `as` prop is used to determine the final HTML attributes. Forgetting to use the `never` means the prop interfaces can be incompatible unintentionally, causing TypeScript issues for developers doing things we say is supported.
    
    [category:Components]
    
    Co-authored-by: manuel.carrera <manuel.carrera@workday.com>
    Co-authored-by: @mannycarrera4 <mannycarrera4@users.noreply.github.com>

--- modules/react/select/lib/SelectInput.tsx
+++ modules/react/select/lib/SelectInput.tsx
@@ -11,7 +11,7 @@ import {useSelectModel} from './hooks/useSelectModel';
 import {createSubcomponent, ExtractProps} from '@workday/canvas-kit-react/common';
 import {system} from '@workday/canvas-tokens-web';
 
-export interface SelectInputProps extends ExtractProps<typeof TextInput>, CSProps {
+export interface SelectInputProps extends ExtractProps<typeof TextInput, never>, CSProps {
   /**
    * The Icon to render at the start of the `input`. Use this prop if your options
    * include icons that you would like to render in the `input` when selected.
@@ -55,37 +55,31 @@ const hiddenSelectInputStencil = createStencil({
 export const SelectInput = createSubcomponent(TextInput)({
   modelHook: useSelectModel,
   elemPropsHook: useSelectInput,
-})<SelectInputProps>(
-  (
-    {placeholder = 'Choose an option', inputStartIcon, formInputProps, ...elemProps},
-    Element,
-    model
-  ) => {
-    return (
-      <InputGroup data-width="ck-formfield-width">
-        {inputStartIcon && model.state.selectedIds.length > 0 && (
-          <InputGroup.InnerStart data-part="select-start-icon-container" {...selectIconsStencil()}>
-            <SystemIcon data-part="select-start-icon" icon={inputStartIcon} />
-          </InputGroup.InnerStart>
-        )}
-        {/* Hidden input to handle ids */}
-        <InputGroup.Input
-          data-part="select-hidden-input"
-          {...formInputProps}
-          {...hiddenSelectInputStencil()}
-        />
-        {/* Visual input */}
-        <InputGroup.Input
-          as={Element}
-          placeholder={placeholder}
-          data-part="select-visual-input"
-          {...elemProps}
-          {...mergeStyles(elemProps, selectInputStencil())}
-        />
-        <InputGroup.InnerEnd data-part="select-caret-container" {...selectIconsStencil()}>
-          <SystemIcon data-part="select-caret-icon" icon={caretDownSmallIcon} />
-        </InputGroup.InnerEnd>
-      </InputGroup>
-    );
-  }
-);
+})<SelectInputProps>(({inputStartIcon, formInputProps, ...elemProps}, Element, model) => {
+  return (
+    <InputGroup data-width="ck-formfield-width">
+      {inputStartIcon && model.state.selectedIds.length > 0 && (
+        <InputGroup.InnerStart data-part="select-start-icon-container" {...selectIconsStencil()}>
+          <SystemIcon data-part="select-start-icon" icon={inputStartIcon} />
+        </InputGroup.InnerStart>
+      )}
+      {/* Hidden input to handle ids */}
+      <InputGroup.Input
+        data-part="select-hidden-input"
+        {...formInputProps}
+        {...hiddenSelectInputStencil()}
+      />
+      {/* Visual input */}
+      <InputGroup.Input
+        as={Element}
+        placeholder="Choose an option" // This will get overridden by the placeholder prop on `Select.Input`
+        data-part="select-visual-input"
+        {...elemProps}
+        {...mergeStyles(elemProps, selectInputStencil())}
+      />
+      <InputGroup.InnerEnd data-part="select-caret-container" {...selectIconsStencil()}>
+        <SystemIcon data-part="select-caret-icon" icon={caretDownSmallIcon} />
+      </InputGroup.InnerEnd>
+    </InputGroup>
+  );
+});
