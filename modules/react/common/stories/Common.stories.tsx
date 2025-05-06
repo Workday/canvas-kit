10b263b1e4642f71fa2557e809babcdf682f024f
manuel.carrera <manuel.carrera@workday.com>
2025-05-06 11:21:00 -0600

    fix: Update file structure

--- modules/react/common/stories/Common.stories.tsx
+++ modules/react/common/stories/Common.stories.tsx
@@ -1,7 +1,7 @@
 import {Meta} from '@storybook/react';
 import mdxDoc from './Common.mdx';
 
-import {BaseExample} from './examples/BaseExample';
+import {BaseExample} from './mdx/examples/BaseExample';
 
 export default {
   title: 'Hooks and Utilities/Common',
