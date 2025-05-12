e9e050e10bdb75f6d256ffef40ea42b55c7ba8f1
Raisa Primerova <raisa.primerova@workday.com>
2025-05-12 14:15:51 -0600

    feat: Refactor Pagination to use stencils

--- modules/react/pagination/lib/Pagination/GoTo/Form.tsx
+++ modules/react/pagination/lib/Pagination/GoTo/Form.tsx
@@ -1,35 +1,40 @@
 import * as React from 'react';
-import {createComponent, useIsRTL} from '@workday/canvas-kit-react/common';
-import {Flex, FlexProps} from '@workday/canvas-kit-react/layout';
 
-import {PaginationContext} from '../usePaginationModel';
+import {createComponent} from '@workday/canvas-kit-react/common';
+import {createStencil} from '@workday/canvas-kit-styling';
+import {FlexProps, mergeStyles} from '@workday/canvas-kit-react/layout';
+import {system} from '@workday/canvas-tokens-web';
+
 import {GoToContext, useGoToForm} from './useGoToForm';
+import {PaginationContext} from '../usePaginationModel';
 
 export interface GoToFormProps extends FlexProps {
   onSubmit?: React.FormEventHandler<HTMLFormElement>;
 }
+
+export const goToFormStencil = createStencil({
+  base: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: system.space.x2,
+    ':dir(rtl)': {
+      paddingInlineStart: system.space.x2,
+      paddingInlineEnd: system.space.x2,
+    },
+  },
+});
 
 export const GoToForm = createComponent('form')({
   Component({children, onSubmit, ...elemProps}: GoToFormProps, ref, Element) {
     const model = React.useContext(PaginationContext);
     const goToContext = useGoToForm({model, onSubmit});
     const {formProps} = goToContext;
-    const shouldUseRTL = useIsRTL();
 
     return (
       <GoToContext.Provider value={goToContext}>
-        <Flex
-          ref={ref}
-          as={Element}
-          alignItems="center"
-          gap="xxs"
-          paddingLeft={shouldUseRTL ? 'xxs' : undefined}
-          paddingRight={shouldUseRTL ? 'xxs' : undefined}
-          {...formProps}
-          {...elemProps}
-        >
+        <Element ref={ref} {...formProps} {...mergeStyles(elemProps, goToFormStencil())}>
           {children}
-        </Flex>
+        </Element>
       </GoToContext.Provider>
     );
   },
