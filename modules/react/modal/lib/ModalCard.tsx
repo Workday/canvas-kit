490ce032361375ff80034b27c6e2409d59e662b0
Raisa Primerova <riyuhide@gmail.com>
2025-06-27 13:32:44 -0600

    fix: Fix responsive modal variant

--- modules/react/modal/lib/ModalCard.tsx
+++ modules/react/modal/lib/ModalCard.tsx
@@ -16,6 +16,7 @@ export const modalCardStencil = createStencil({
     outline: `${px2rem(1)} solid transparent`,
     boxShadow: system.depth[6],
     '@media screen and (max-width: 768px)': {
+      gap: system.space.zero,
       margin: system.space.x4, // 16px all around margin on smaller screen sizes
       padding: system.space.x4, // brings total padding between edge and content to 24px
       borderRadius: system.space.x6, // 24px border radius on smaller devices.
