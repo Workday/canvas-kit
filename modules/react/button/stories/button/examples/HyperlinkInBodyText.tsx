32b508bbdb4a569387aed56d0319d4d855264952
manuel.carrera <manuel.carrera@workday.com>
2025-07-17 13:34:42 -0600

    feat: Add standalone variant to Hyperlink and External Hyperlink

---
+++ modules/react/button/stories/button/examples/HyperlinkInBodyText.tsx
@@ -0,0 +1,14 @@
+import {ExternalHyperlink, Hyperlink} from '@workday/canvas-kit-react/button';
+
+export const InBodyText = () => (
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod{' '}
+    <Hyperlink href="#hyperlink">tempor incididunt</Hyperlink> ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in{' '}
+    <ExternalHyperlink href="#hyperlink">
+      voluptate velit esse cillum dolore eu fugiat
+    </ExternalHyperlink>{' '}
+    nulla pariatur.
+  </p>
+);
---
+++ modules/react/button/stories/button/examples/StandaloneHyperlink.tsx
@@ -0,0 +1,7 @@
+import {Hyperlink} from '@workday/canvas-kit-react/button';
+
+export const StandaloneLink = () => (
+  <Hyperlink variant="standalone" href="#standalone-hyperlink">
+    Standalone Hyperlink
+  </Hyperlink>
+);
