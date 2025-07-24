df34b256f7374650cf3ebfdc58a3575cebc0c85e
manuel.carrera <manuel.carrera@workday.com>
2025-07-24 15:14:17 -0600

    feat: Remove cascade barrier in Canvas Provider

--- modules/react/common/lib/CanvasProvider.tsx
+++ modules/react/common/lib/CanvasProvider.tsx
@@ -2,53 +2,12 @@ import * as React from 'react';
 import {Theme, ThemeProvider, CacheProvider} from '@emotion/react';
 import {InputProvider} from './InputProvider';
 import {defaultCanvasTheme, PartialEmotionCanvasTheme, useTheme} from './theming';
-import {brand, base} from '@workday/canvas-tokens-web';
+import {brand} from '@workday/canvas-tokens-web';
 import {createStyles, getCache} from '@workday/canvas-kit-styling';
 
 export interface CanvasProviderProps {
   theme?: PartialEmotionCanvasTheme;
 }
-
-// copied from brand/_variables.css
-const defaultBranding = createStyles({
-  [brand.error.darkest]: base.red800,
-  [brand.common.alertInner]: base.amber400,
-  [brand.common.alertOuter]: base.amber500,
-  [brand.common.errorInner]: base.red500,
-  [brand.common.focusOutline]: base.blue500,
-  [brand.neutral.accent]: base.neutral0,
-  [brand.neutral.darkest]: base.slate800,
-  [brand.neutral.dark]: base.slate700,
-  [brand.neutral.base]: base.slate600,
-  [brand.neutral.light]: base.slate200,
-  [brand.neutral.lightest]: base.slate50,
-  [brand.success.accent]: base.neutral0,
-  [brand.success.darkest]: base.green800,
-  [brand.success.dark]: base.green700,
-  [brand.success.base]: base.green600,
-  [brand.success.light]: base.green100,
-  [brand.success.lightest]: base.green25,
-  [brand.error.accent]: base.neutral0,
-  [brand.error.dark]: base.red700,
-  [brand.error.darkest]: base.red800,
-  [brand.error.base]: base.red600,
-  [brand.error.light]: base.red100,
-  [brand.error.lightest]: base.red25,
-  [brand.alert.accent]: base.neutral900,
-  [brand.alert.darkest]: base.amber600,
-  [brand.alert.dark]: base.amber500,
-  [brand.alert.base]: base.amber400,
-  [brand.alert.light]: base.amber50,
-  [brand.alert.lightest]: base.amber25,
-  [brand.primary.accent]: base.neutral0,
-  [brand.primary.darkest]: base.blue800,
-  [brand.primary.dark]: base.blue700,
-  [brand.primary.base]: base.blue600,
-  [brand.primary.light]: base.blue200,
-  [brand.primary.lightest]: base.blue25,
-  [brand.gradient
-    .primary]: `linear-gradient(90deg, ${brand.primary.base} 0%, ${brand.primary.dark} 100%)`,
-});
 
 const mappedKeys = {
   lightest: 'lightest',
@@ -64,7 +23,7 @@ export const useCanvasThemeToCssVars = (
   elemProps: React.HTMLAttributes<HTMLElement>
 ) => {
   const filledTheme = useTheme(theme);
-  const className = (elemProps.className || '').split(' ').concat(defaultBranding).join(' ');
+  const className = elemProps.className || '';
   const style = elemProps.style || {};
   const {palette} = filledTheme.canvas;
   (['common', 'primary', 'error', 'alert', 'success', 'neutral'] as const).forEach(color => {
@@ -86,7 +45,7 @@ export const useCanvasThemeToCssVars = (
 
 export const CanvasProvider = ({
   children,
-  theme = {canvas: defaultCanvasTheme},
+  theme,
   ...props
 }: CanvasProviderProps & React.HTMLAttributes<HTMLElement>) => {
   const elemProps = useCanvasThemeToCssVars(theme, props);
