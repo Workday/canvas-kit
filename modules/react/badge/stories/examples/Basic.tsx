97def83a00bdfecc03a076d83d6661f1c1aff2b5
Raisa Primerova <riyuhide@gmail.com>
2025-07-28 09:18:19 -0600

    fix: Update examples

--- modules/react/badge/stories/examples/Basic.tsx
+++ modules/react/badge/stories/examples/Basic.tsx
@@ -1,35 +1,26 @@
 import * as React from 'react';
 import {CountBadge} from '@workday/canvas-kit-react/badge';
-import {createStyles} from '@workday/canvas-kit-styling';
+import {createStyles, cssVar} from '@workday/canvas-kit-styling';
 import {system} from '@workday/canvas-tokens-web';
+import {Text} from '@workday/canvas-kit-react/text';
 
 const containerStyles = createStyles({
-  boxSizing: 'border-box',
   display: 'flex',
-  flexDirection: 'column',
-});
-
-const defaultBackground = createStyles({
-  boxSizing: 'border-box',
-  backgroundColor: system.color.bg.alt.soft,
-  padding: system.space.x4,
-});
-
-const inverseBackground = createStyles({
-  boxSizing: 'border-box',
-  backgroundColor: system.color.bg.primary.default,
+  gap: system.space.x2,
   padding: system.space.x4,
 });
 
 export const Basic = () => {
   return (
-    <div className={containerStyles}>
-      <div className={defaultBackground}>
+    <>
+      <div className={containerStyles}>
+        <Text as="strong">Notifications</Text>
         <CountBadge count={427} />
       </div>
-      <div className={inverseBackground}>
-        <CountBadge count={427} variant="inverse" />
+      <div className={containerStyles}>
+        <Text as="strong">Notifications</Text>
+        <CountBadge count={427} emphasis="low" />
       </div>
-    </div>
+    </>
   );
 };
--- modules/react/badge/stories/examples/Inverse.tsx
+++ modules/react/badge/stories/examples/Inverse.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import {CountBadge} from '@workday/canvas-kit-react/badge';
-import {cssVar} from '@workday/canvas-kit-styling';
+import {createStyles, cssVar} from '@workday/canvas-kit-styling';
 import {system} from '@workday/canvas-tokens-web';
 import {Text} from '@workday/canvas-kit-react/text';
+
+const containerStyles = createStyles({
+  display: 'flex',
+  gap: system.space.x2,
+  padding: system.space.x4,
+  backgroundColor: cssVar(system.color.static.blue.default),
+});
 
 export function Inverse() {
   return (
     <>
-      <div style={{backgroundColor: cssVar(system.color.static.blue.default)}}>
+      <div className={containerStyles}>
+        <Text as="strong" variant="inverse">
+          Notifications
+        </Text>
         <CountBadge count={427} variant="inverse" />
       </div>
-      <br />
-      <div style={{backgroundColor: cssVar(system.color.static.blue.default)}}>
-        <Text fontFamily="monospace" cs={{color: cssVar(system.color.static.white)}}>
-          system.color.static.blue.default
+      <div className={containerStyles}>
+        <Text as="strong" variant="inverse">
+          Notifications
         </Text>
         <CountBadge count={427} variant="inverse" emphasis="low" />
       </div>
