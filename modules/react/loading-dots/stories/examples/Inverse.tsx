562b55928a125a2dd2e0dc5de23bb4b52721aa05
manuel.carrera <manuel.carrera@workday.com>
2025-07-28 13:26:28 -0600

    fix: Add inverse variant

--- modules/react/loading-dots/stories/examples/Inverse.tsx
+++ modules/react/loading-dots/stories/examples/Inverse.tsx
@@ -1,5 +1,41 @@
 import {LoadingDots} from '@workday/canvas-kit-react/loading-dots';
+import {createStencil, createStyles} from '@workday/canvas-kit-styling';
+import {system} from '@workday/canvas-tokens-web';
+import {Graphic} from '@workday/canvas-kit-react/icon';
+
+const loadingStencil = createStencil({
+  base: {
+    background: system.color.bg.overlay,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100%',
+    height: '100%',
+  },
+});
+
+const containerStyles = createStyles({
+  position: 'relative',
+  width: '200px',
+  height: '200px',
+});
 
 export const Inverse = () => {
-  return <LoadingDots variant="inverse" />;
+  return (
+    <div className={containerStyles}>
+      <LoadingDots variant="inverse" cs={loadingStencil()} />
+      <Graphic
+        alt="A magnifying glass"
+        width={200}
+        src={{
+          url: 'https://picsum.photos/200',
+        }}
+        srcset="https://picsum.photos/200 200w, https://picsum.photos/200 200w, https://picsum.photos/800 800w, https://picsum.photos/1200 1200w"
+      />
+    </div>
+  );
 };
