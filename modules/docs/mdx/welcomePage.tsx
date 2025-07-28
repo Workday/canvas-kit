14883d594e45c164f94a76e61fed74357c741259
Jaclyn <jaclyn.sproat@workday.com>
2025-07-28 15:25:41 -0600

    docs: new branding welcome page

--- modules/docs/mdx/welcomePage.tsx
+++ modules/docs/mdx/welcomePage.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore: Cannot find module error
-import headerImage from './CK-banner-temp.jpg';
+import headerImage from './CK-banner-2.png';
 import {Flex, Grid, Box} from '@workday/canvas-kit-react/layout';
 import {InstallBlock} from './installBlock';
 import {Text, Heading} from '@workday/canvas-kit-react/text';
@@ -7,7 +7,7 @@ import {Card} from '@workday/canvas-kit-react/card';
 import {ExternalHyperlink, Hyperlink} from '@workday/canvas-kit-react/button';
 import {rocketIcon, tokensIcon, shapesIcon} from '@workday/canvas-system-icons-web';
 import {SystemIcon} from '@workday/canvas-kit-react/icon';
-import {system} from '@workday/canvas-tokens-web';
+import {system, base} from '@workday/canvas-tokens-web';
 import {createStyles} from '@workday/canvas-kit-styling';
 // @ts-ignore: Cannot find module error
 import {version} from '../../../lerna.json';
@@ -19,7 +19,7 @@ const parentFlexStyles = createStyles({
   marginBottom: system.space.x6,
 });
 
-const textStyles = createStyles({
+const bannerTextStyles = createStyles({
   color: system.color.text.inverse,
   fontSize: '5vmin',
   lineHeight: '5vmin',
@@ -36,7 +36,7 @@ const imageStyles = createStyles({
 });
 
 const borderStyles = createStyles({
-  borderBottom: `1px solid ${system.color.border.primary.default}`,
+  borderBottom: `1px solid ${base.blue900}`,
   marginBottom: system.space.x2,
 });
 
@@ -57,10 +57,10 @@ export const WelcomePage = () => {
       <div className={parentFlexStyles}>
         <Box cs={{overflow: 'hidden', position: 'relative'}}>
           <Flex cs={{flexDirection: 'row', position: 'absolute', top: '45%', left: '10%'}}>
-            <Text typeLevel="title.medium" cs={textStyles}>
+            <Text typeLevel="title.medium" cs={bannerTextStyles}>
               Canvas Kit
             </Text>
-            <Text typeLevel="title.medium" cs={[textStyles, versionStyles]}>
+            <Text typeLevel="title.medium" cs={[bannerTextStyles, versionStyles]}>
               v{version}
             </Text>
           </Flex>
@@ -72,7 +72,7 @@ export const WelcomePage = () => {
         </Box>
         <Text typeLevel="body.medium">
           This project provides a set of components for the Workday Canvas Design System that can be
-          used to implement user experiences consistent with
+          used to implement user experiences consistent with {' '}
           <ExternalHyperlink
             href="https://canvas.workdaydesign.com/"
             iconLabel="Open docs in new window"
@@ -90,8 +90,7 @@ export const WelcomePage = () => {
         >
           <Grid as={Card} className={gridStyles}>
             <SystemIcon
-              color={system.color.static.amber.default}
-              colorHover={system.color.static.amber.default}
+              color={base.pink600}
               icon={rocketIcon}
               size={60}
             ></SystemIcon>
@@ -109,9 +108,8 @@ export const WelcomePage = () => {
           </Grid>
           <Grid as={Card} className={gridStyles}>
             <SystemIcon
-              color={system.color.fg.primary.default}
+              color={base.red300}
               icon={tokensIcon}
-              colorHover={system.color.fg.primary.default}
               size={60}
             ></SystemIcon>
             <Card.Heading>Tokens</Card.Heading>
@@ -122,33 +120,31 @@ export const WelcomePage = () => {
               </Text>
             </Card.Body>
             <Grid.Item
-              as={Hyperlink}
+              as={ExternalHyperlink}
               className={linkStyles}
-              href="https://workday.github.io/canvas-kit/?path=/docs/tokens--overview"
+              href="https://workday.github.io/canvas-tokens/?path=%2Fdocs%2Fdocs-getting-started--docs"
             >
               View Our Tokens
             </Grid.Item>
           </Grid>
           <Grid as={Card} className={gridStyles}>
             <SystemIcon
-              color={system.color.static.green.default}
+              color={base.teal500}
               icon={shapesIcon}
               size={60}
-              colorHover={system.color.static.green.default}
             ></SystemIcon>
-            <Card.Heading>Assets</Card.Heading>
+            <Card.Heading>Styling</Card.Heading>
             <Card.Body>
               <Text>
-                Assets are graphics which help communicate meaning or highlight areas of interaction
-                to our users.
+                Learn how to style Canvas components using tokens, theming, and custom CSS approaches.
               </Text>
             </Card.Body>
             <Grid.Item
               as={Hyperlink}
               className={linkStyles}
-              href="https://workday.github.io/canvas-kit/?path=/docs/assets-icons--overview"
+              href="https://workday.github.io/canvas-kit/?path=/docs/styling-getting-started-overview--docs"
             >
-              View Assets
+              Get Started
             </Grid.Item>
           </Grid>
         </Grid>
