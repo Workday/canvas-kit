c522b639c07bab0daf98354bff6b1030b127bafa
Nicholas Boll <nicholas.boll@gmail.com>
2025-08-06 11:08:08 -0600

    Remove any dev warnings and make sure React 18 and 19 have consistent output

--- modules/react/collection/lib/useOverflowListItemMeasure.tsx
+++ modules/react/collection/lib/useOverflowListItemMeasure.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createElemPropsHook, useLocalRef, useMountLayout} from '@workday/canvas-kit-react/common';
 
 import {useOverflowListModel} from './useOverflowListModel';
