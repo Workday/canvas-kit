5bf564ff018f262ef0f20aa7f21a3ba6e63ff1dc
Nicholas Boll <nicholas.boll@gmail.com>
2025-08-20 14:32:13 -0600

    fix: Export all props to avoid TS using source files

--- modules/react/tabs/index.ts
+++ modules/react/tabs/index.ts
@@ -1,8 +1,8 @@
 export * from './lib/Tabs';
 export * from './lib/useTabsModel';
 
-export {StyledTabItem, useTabsItem} from './lib/TabsItem';
-export {useTabsList} from './lib/TabsList';
-export {useTabsMenuPopper} from './lib/TabsMenuPopper';
-export {useTabsOverflowButton} from './lib/TabsOverflowButton';
-export {useTabsPanel} from './lib/TabsPanel';
+export * from './lib/TabsItem';
+export * from './lib/TabsList';
+export * from './lib/TabsMenuPopper';
+export * from './lib/TabsOverflowButton';
+export * from './lib/TabsPanel';
