62af211cff5498447595381ae6ca1e4e44af5cab
manuel.carrera <manuel.carrera@workday.com>
2025-08-22 09:11:46 -0600

    fix: Make theming work in Popups

--- modules/react/common/lib/CanvasProvider.tsx
+++ modules/react/common/lib/CanvasProvider.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import {Theme, ThemeProvider, CacheProvider} from '@emotion/react';
 import {defaultCanvasTheme, PartialEmotionCanvasTheme, useTheme} from './theming';
 import {InputProvider} from './InputProvider';
-import {brand} from '@workday/canvas-tokens-web';
-import {getCache, maybeWrapCSSVariables} from '@workday/canvas-kit-styling';
+import {brand, base} from '@workday/canvas-tokens-web';
+import {getCache, maybeWrapCSSVariables, createStyles} from '@workday/canvas-kit-styling';
 
 export interface CanvasProviderProps {
   theme?: PartialEmotionCanvasTheme;
@@ -19,24 +19,72 @@ const mappedKeys = {
   contrast: 'accent',
 };
 
+const defaultBranding = createStyles({
+  [brand.common.alertInner]: base.amber400,
+  [brand.common.alertOuter]: base.amber500,
+  [brand.common.errorInner]: base.red500,
+  [brand.common.focusOutline]: base.blue500,
+  [brand.neutral.accent]: base.neutral0,
+  [brand.neutral.darkest]: base.slate800,
+  [brand.neutral.dark]: base.slate700,
+  [brand.neutral.base]: base.slate600,
+  [brand.neutral.light]: base.slate200,
+  [brand.neutral.lighter]: base.slate50,
+  [brand.neutral.lightest]: base.slate25,
+  [brand.success.accent]: base.neutral0,
+  [brand.success.darkest]: base.green800,
+  [brand.success.dark]: base.green700,
+  [brand.success.base]: base.green600,
+  [brand.success.light]: base.green200,
+  [brand.success.lighter]: base.green50,
+  [brand.success.lightest]: base.green25,
+  [brand.error.accent]: base.neutral0,
+  [brand.error.darkest]: base.red800,
+  [brand.error.dark]: base.red700,
+  [brand.error.base]: base.red600,
+  [brand.error.light]: base.red200,
+  [brand.error.lighter]: base.red50,
+  [brand.error.lightest]: base.red25,
+  [brand.alert.accent]: base.neutral900,
+  [brand.alert.darkest]: base.amber600,
+  [brand.alert.dark]: base.amber500,
+  [brand.alert.base]: base.amber400,
+  [brand.alert.light]: base.amber200,
+  [brand.alert.lighter]: base.amber50,
+  [brand.alert.lightest]: base.amber25,
+  [brand.primary.accent]: base.neutral0,
+  [brand.primary.darkest]: base.blue800,
+  [brand.primary.dark]: base.blue700,
+  [brand.primary.base]: base.blue600,
+  [brand.primary.light]: base.blue200,
+  [brand.primary.lighter]: base.blue50,
+  [brand.primary.lightest]: base.blue25,
+  [brand.gradient
+    .primary]: `linear-gradient(90deg, ${brand.primary.base} 0%, ${brand.primary.dark} 100%)`,
+});
+
 export const useCanvasThemeToCssVars = (
   theme: PartialEmotionCanvasTheme | undefined,
   elemProps: React.HTMLAttributes<HTMLElement>
 ) => {
   const filledTheme = useTheme(theme);
-  const className = elemProps.className || '';
+  const className = (elemProps.className || '').split(' ').concat(defaultBranding).join(' ');
   const style = elemProps.style || {};
   const {palette} = filledTheme.canvas;
   (['common', 'primary', 'error', 'alert', 'success', 'neutral'] as const).forEach(color => {
     if (color === 'common') {
-      //@ts-ignore
-      style[brand.common.focusOutline] = maybeWrapCSSVariables(palette.common.focusOutline);
-      //@ts-ignore
-      style[brand.common.alertInner] = maybeWrapCSSVariables(palette.common.alertInner);
-      //@ts-ignore
-      style[brand.common.alertOuter] = maybeWrapCSSVariables(palette.common.alertOuter);
-      //@ts-ignore
-      style[brand.common.errorInner] = maybeWrapCSSVariables(palette.common.errorInner);
+      (['focusOutline', 'alertInner', 'alertOuter', 'errorInner'] as const).forEach(key => {
+        if (palette.common[key] !== defaultCanvasTheme.palette.common[key]) {
+          //@ts-ignore
+          style[brand.common.focusOutline] = maybeWrapCSSVariables(palette.common.focusOutline);
+          //@ts-ignore
+          style[brand.common.alertInner] = maybeWrapCSSVariables(palette.common.alertInner);
+          //@ts-ignore
+          style[brand.common.alertOuter] = maybeWrapCSSVariables(palette.common.alertOuter);
+          //@ts-ignore
+          style[brand.common.errorInner] = maybeWrapCSSVariables(palette.common.errorInner);
+        }
+      });
     }
     (['lightest', 'lighter', 'light', 'main', 'dark', 'darkest', 'contrast'] as const).forEach(
       key => {
@@ -49,6 +97,7 @@ export const useCanvasThemeToCssVars = (
       }
     );
   });
+
   return {...elemProps, className, style};
 };
 
@@ -57,15 +106,17 @@ export const CanvasProvider = ({
   theme,
   ...props
 }: CanvasProviderProps & React.HTMLAttributes<HTMLElement>) => {
-  const elemProps = useCanvasThemeToCssVars(theme, props);
+  const {className, style, ...rest} = useCanvasThemeToCssVars(theme, props);
   const cache = getCache();
+
   return (
     <CacheProvider value={cache}>
       <ThemeProvider theme={theme as Theme}>
         <InputProvider />
         <div
           dir={theme?.canvas?.direction || defaultCanvasTheme.direction}
-          {...(elemProps as React.HTMLAttributes<HTMLDivElement>)}
+          style={style}
+          {...(rest as React.HTMLAttributes<HTMLDivElement>)}
         >
           {children}
         </div>
