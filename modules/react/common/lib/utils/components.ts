b483ce8171244ef3aab86bb7105d2fda149c4748
Alan B Smith <a.bax.smith@gmail.com>
2025-08-28 12:01:52 -0600

    chore: Update insights to omit subcomponents

--- modules/react/common/lib/utils/components.ts
+++ modules/react/common/lib/utils/components.ts
@@ -542,7 +542,7 @@ export const createComponent =
       ({as: asOverride, ...props}, ref) => {
         return Component(
           {
-            ...setCanvasKitTags({displayName}),
+            ...setCanvasKitTags(displayName),
             ...props,
           } as any,
           ref as ExtractRef<E>,
