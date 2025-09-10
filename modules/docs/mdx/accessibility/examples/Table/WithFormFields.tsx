9f668fd0a46407e479c3e4e77caa02da7d787651
William Stanton <william.stanton@workday.com>
2025-09-10 15:18:16 -0400

    docs: Adding formatting to the Amount field

--- modules/docs/mdx/accessibility/examples/Table/WithFormFields.tsx
+++ modules/docs/mdx/accessibility/examples/Table/WithFormFields.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useUniqueId} from '@workday/canvas-kit-react/common';
 import {createStyles} from '@workday/canvas-kit-styling';
+import {system} from '@workday/canvas-tokens-web';
 import {trashIcon} from '@workday/canvas-system-icons-web';
 import {TextInput} from '@workday/canvas-kit-react/text-input';
 import {Table} from '@workday/canvas-kit-react/table';
@@ -16,6 +17,26 @@ const centerStyles = createStyles({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+});
+
+const currencyInputContainer = createStyles({
+  position: 'relative',
+  display: 'inline-block',
+});
+
+const dollarPrefix = createStyles({
+  position: 'absolute',
+  left: system.space.x2,
+  top: '50%',
+  transform: 'translateY(-50%)',
+  color: system.color.text.default,
+  pointerEvents: 'none',
+  zIndex: 1,
+  ...system.type.subtext.large,
+});
+
+const currencyInput = createStyles({
+  paddingLeft: system.space.x4, // Make room for dollar sign
 });
 
 const expensesData = [
@@ -38,9 +59,28 @@ interface LineItemProps {
 
 const LineItem = ({itemName, labelIds}: LineItemProps) => {
   const [name, setName] = React.useState(itemName);
+  const [amount, setAmount] = React.useState('');
 
   function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
+  }
+
+  function handleAmountChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+
+    // For type="number", the browser handles most validation
+    // We just need to limit decimal places to 2 for currency
+    if (value.includes('.')) {
+      const parts = value.split('.');
+      if (parts[1] && parts[1].length > 2) {
+        // Limit to 2 decimal places
+        const limitedValue = parts[0] + '.' + parts[1].substring(0, 2);
+        setAmount(limitedValue);
+        return;
+      }
+    }
+
+    setAmount(value);
   }
 
   return (
@@ -49,7 +89,22 @@ const LineItem = ({itemName, labelIds}: LineItemProps) => {
         <TextInput onChange={handleNameChange} value={name} aria-labelledby={labelIds.name} />
       </Table.Header>
       <Table.Cell>
-        <TextInput type="number" width="11rem" aria-labelledby={labelIds.amount} />
+        <div className={currencyInputContainer}>
+          <span className={dollarPrefix} aria-hidden="true">
+            $
+          </span>
+          <TextInput
+            type="number"
+            value={amount}
+            onChange={handleAmountChange}
+            placeholder="0.00"
+            min="0"
+            step="0.01"
+            width="10.5rem"
+            aria-labelledby={labelIds.amount}
+            cs={currencyInput}
+          />
+        </div>
       </Table.Cell>
       <Table.Cell cs={centerStyles}>
         <Checkbox aria-labelledby={labelIds.recon} />
@@ -79,7 +134,7 @@ export const WithFormFields = () => {
             Expense Name{' '}
           </Table.Header>
           <Table.Header scope="col" id={colHeadIds.amount}>
-            Budgeted Amount{' '}
+            Budgeted Amount (USD){' '}
           </Table.Header>
           <Table.Header scope="col" id={colHeadIds.recon} cs={centerStyles}>
             Reconciled
