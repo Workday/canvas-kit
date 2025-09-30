e66ed6a7cecf81a3b5c7512bb0b9761fda7df1dc
WorkdayHenry <henry.mason@workday.com>
2025-09-29 17:07:10 -0700

    docs: Adding documentation for useModalModel hook (#3525)
    Added documentation to useModalModel.
    
    Could possibly add more documentation but I am going on leave so any further issues I think I will just record and come back to later.
    
    I think the main second piece of documentation would be on usePopupModel.
    
    Also, note, I didn't add the direct type for the prop passed into useModalModel because I think it is possible it will shift and the docgen was already creating it.
    
    [category:Documentation]

--- modules/react/modal/lib/hooks/useModalModel.ts
+++ modules/react/modal/lib/hooks/useModalModel.ts
@@ -11,6 +11,35 @@ import {createModelHook} from '@workday/canvas-kit-react/common';
 
 import {useCloseOnOverlayClick} from './useCloseOnOverlayClick';
 
+/**
+ * This model hook uses {@link usePopupModel} and pre-configures behaviors that are required for an
+ * accessible modal. `useModalModel` should be used in most cases, but if you require custom
+ * behaviors, you can use `usePopupModel` directly. Be sure to add proper popup behaviors to ensure
+ * the modal is accessible.
+ *
+ * The following behaviors are added to the `PopupModel`:
+ * - {@link useInitialFocus}
+ * - {@link useReturnFocus}
+ * - {@link useCloseOnOverlayClick}
+ * - {@link useCloseOnEscape}
+ * - {@link useFocusTrap}
+ * - {@link useAssistiveHideSiblings}
+ * - {@link useDisableBodyScroll}
+ *
+ * You can pass the Modal model config either directly to the `Modal` component or to the
+ * `useModalModel` hook, but not both. A `model` prop always takes precedence over the config passed
+ * to the `useModalModel` hook. If no `model` is passed to a `Modal` component, a `ModalModel` will
+ * be created for you. Creating your own model hoists the modal's state to the level of your
+ * component and allows you to access the model's state and events.
+ *
+ * ```tsx
+ * const model = useModalModel(config);
+ *
+ * <Modal model={model}>
+ *   // ...
+ * </Modal>
+ * ```
+ */
 export const useModalModel = createModelHook({
   defaultConfig: usePopupModel.defaultConfig,
   requiredConfig: usePopupModel.requiredConfig,
