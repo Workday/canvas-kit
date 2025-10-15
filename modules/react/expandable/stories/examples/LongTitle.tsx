eaae6e2f860e7b9bf6335bb7e14c6e5e47e73c0e
William Stanton <william.stanton@workday.com>
2025-10-15 16:58:14 -0400

    docs: Updating accessibility for Expandable

--- modules/react/expandable/stories/examples/LongTitle.tsx
+++ modules/react/expandable/stories/examples/LongTitle.tsx
@@ -6,7 +6,7 @@ export const LongTitle = () => (
   <Expandable>
     <Expandable.Target headingLevel="h4">
       <Expandable.Icon iconPosition="start" />
-      <Expandable.Avatar name="Avatar" url={testAvatar} />
+      <Expandable.Avatar name="Avatar" isDecorative url={testAvatar} />
       <Expandable.Title>
         Our house special supreme pizza includes pepperoni, sausage, bell peppers, mushrooms,
         onions, and oregano.
