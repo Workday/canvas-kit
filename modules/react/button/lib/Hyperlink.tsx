1e382b86f19f973936bfc0e9c153b6c67c0ffd58
manuel.carrera <manuel.carrera@workday.com>
2025-11-19 15:48:30 -0700

    fix: Update hyperlink

--- modules/react/button/lib/Hyperlink.tsx
+++ modules/react/button/lib/Hyperlink.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {createComponent, forwardFitTokens} from '@workday/canvas-kit-react/common';
+import {createComponent} from '@workday/canvas-kit-react/common';
 import {createStencil, CSProps, cssVar, handleCsProp, px2rem} from '@workday/canvas-kit-styling';
 import {system, brand} from '@workday/canvas-tokens-web';
 
