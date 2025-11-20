8e88127532132b65c92ee26103751824972ad915
William Stanton <william.stanton@workday.com>
2025-11-20 14:30:23 -0500

    docs: Creating new TextArea example

---
+++ modules/docs/mdx/accessibility/examples/Forms/CommentBoxWithCharLimit.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {FormField} from '@workday/canvas-kit-react/form-field';
+import {TextArea} from '@workday/canvas-kit-react/text-area';
+import {AriaLiveRegion, AccessibleHide} from '@workday/canvas-kit-react/common';
+
+const MAX_CHARACTERS = 200;
+const DEBOUNCE_DELAY = 2000; // 2 seconds after user stops typing
+
+export default function CommentBoxWithCharLimit() {
+  const [value, setValue] = React.useState('');
+  const [liveUpdateStr, setLiveUpdateStr] = React.useState('');
+  const hintTextStr = `${value.length} of ${MAX_CHARACTERS} characters`;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleBlur = () => {
+    setLiveUpdateStr('');
+  };
+
+  React.useEffect(() => {
+    // Immediately announce when limit is reached (bypass debounce)
+    if (value.length === MAX_CHARACTERS) {
+      setLiveUpdateStr(`Character limit reached. ${value.length} of ${MAX_CHARACTERS} characters`);
+      return;
+    }
+
+    // Otherwise, debounce the updates
+    const timer = setTimeout(() => {
+      setLiveUpdateStr(hintTextStr);
+    }, DEBOUNCE_DELAY);
+    return () => clearTimeout(timer);
+  }, [hintTextStr]);
+
+  return (
+    <FormField>
+      <FormField.Label>Comments</FormField.Label>
+      <FormField.Field>
+        <FormField.Input
+          as={TextArea}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={value}
+          maxLength={MAX_CHARACTERS}
+        />
+        <FormField.Hint>
+          {hintTextStr}
+          <AriaLiveRegion as={AccessibleHide}>{liveUpdateStr}</AriaLiveRegion>
+        </FormField.Hint>
+      </FormField.Field>
+    </FormField>
+  );
+}
