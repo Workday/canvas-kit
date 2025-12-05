2646d46415ff8d344d98211d97c52c682bfcb98c
manuel.carrera <manuel.carrera@workday.com>
2025-12-05 12:08:28 -0700

    feat: Promote segmented control from Preview to Main

---
+++ modules/codemod/lib/v15/spec/promoteSegmentedControl.spec.ts
@@ -0,0 +1,107 @@
+import {expectTransformFactory} from './expectTransformFactory';
+import transform from '../promoteSegmentedControl';
+import {stripIndent} from 'common-tags';
+
+const expectTransform = expectTransformFactory(transform);
+
+describe('Promote SegmentedControl to main package', () => {
+  describe('Component package import', () => {
+    it('should not transform SegmentedControl from other imports', () => {
+      const input = stripIndent`
+          import { SegmentedControl } from "@other-package";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl } from "@other-package";
+        `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should not transform other preview imports', () => {
+      const input = stripIndent`
+          import { Avatar } from "@workday/canvas-kit-preview-react/avatar";
+          import { Pill } from "@workday/canvas-kit-preview-react/pill";
+        `;
+
+      const expected = input;
+
+      expectTransform(input, expected);
+    });
+
+    it('should  properly transform preview import for SegmentedControl to react SegmentedControl', () => {
+      const input = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-preview-react/segmented-control";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+        `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should properly transform preview import for SegmentedControl to react SegmentedControl from main packages', () => {
+      const input = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-preview-react";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+        `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should properly transform named preview import for SegmentedControl only to react SegmentedControl', () => {
+      const input = stripIndent`
+          import { SegmentedControl as CanvasSegmentedControl } from "@workday/canvas-kit-preview-react/segmented-control";
+        `;
+
+      const expected = stripIndent`
+            import { SegmentedControl as CanvasSegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+        `;
+
+      expectTransform(input, expected);
+    });
+  });
+
+  describe('Main package import', () => {
+    it('should properly transform main preeview import for SegmentedControl to react SegmentedControl', () => {
+      const input = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-preview-react";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+        `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should properly transform named main prewview import for SegmentedControl only to react SegmentedControl', () => {
+      const input = stripIndent`
+          import { SegmentedControl as CanvasSegmentedControl } from "@workday/canvas-kit-preview-react";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl as CanvasSegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+        `;
+
+      expectTransform(input, expected);
+    });
+
+    it('should properly transform main preview import for SegmentedControl only to react SegmentedControl', () => {
+      const input = stripIndent`
+          import {SegmentedControl, Pill} from "@workday/canvas-kit-preview-react";
+        `;
+
+      const expected = stripIndent`
+          import { SegmentedControl } from "@workday/canvas-kit-react/segmented-control";
+          import { Pill } from "@workday/canvas-kit-preview-react";
+        `;
+
+      expectTransform(input, expected);
+    });
+  });
+});
