94496410c1129b19ea88e5056683248f62347f35
William Stanton <william.stanton@workday.com>
2025-12-22 18:16:32 -0500

    docs: Refactored Basic example

--- modules/labs-react/side-panel/stories/examples/Basic.tsx
+++ modules/labs-react/side-panel/stories/examples/Basic.tsx
@@ -1,55 +1,32 @@
-import * as React from 'react';
-import {SecondaryButton} from '@workday/canvas-kit-react/button';
 import {SidePanel, useSidePanelModel} from '@workday/canvas-kit-labs-react/side-panel';
 import {Flex} from '@workday/canvas-kit-react/layout';
-import {Heading, Text} from '@workday/canvas-kit-react/text';
 import {AccentIcon} from '@workday/canvas-kit-react/icon';
 import {rocketIcon} from '@workday/canvas-accent-icons-web';
 import {createStyles, px2rem} from '@workday/canvas-kit-styling';
 import {system} from '@workday/canvas-tokens-web';
 
-const stylesOverride = {
-  viewPortContainer: createStyles({
-    height: px2rem(320),
-  }),
-  panel: createStyles({
-    alignItems: 'center',
-    padding: system.space.x4,
-  }),
-  accentIcon: createStyles({
-    marginInlineEnd: system.space.x4,
-  }),
-  mainContent: createStyles({
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    flex: 1,
-    flexBasis: 'auto',
-  }),
-};
+const flexHeadingStyles = createStyles({
+  alignItems: 'center',
+  gap: system.space.x2,
+});
+
+const viewPortStyles = createStyles({
+  height: px2rem(320),
+});
 
 export const Basic = () => {
   const model = useSidePanelModel();
 
   return (
-    <Flex cs={stylesOverride.viewPortContainer}>
+    <Flex cs={viewPortStyles}>
       <SidePanel model={model}>
-        <SidePanel.Heading size="small">Tasks Panel</SidePanel.Heading>
-        <SidePanel.ToggleButton />
-        {/* <Flex cs={stylesOverride.panel}>
-          {model.state.transitionState === 'expanded' && (
-            <Flex cs={stylesOverride.accentIcon}>
-              <AccentIcon icon={rocketIcon} />
-            </Flex>
-          )}
-          <Heading
-            size="small"
-            id={model.state.labelId}
-            hidden={model.state.transitionState === 'collapsed' ? true : undefined}
-          >
+        <SidePanel.Heading size="small">
+          <Flex cs={flexHeadingStyles}>
+            <AccentIcon icon={rocketIcon} />
             Tasks Panel
-          </Heading>
-        </Flex> */}
+          </Flex>
+        </SidePanel.Heading>
+        <SidePanel.ToggleButton />
       </SidePanel>
     </Flex>
   );
