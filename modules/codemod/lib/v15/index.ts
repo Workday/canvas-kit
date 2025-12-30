dae86002be5b63437fc5c02aa9fde21fb66ad7f4
manuel.carrera <manuel.carrera@workday.com>
2025-12-30 12:48:33 -0700

    fix: Add codemod to export index

--- modules/codemod/lib/v15/index.ts
+++ modules/codemod/lib/v15/index.ts
@@ -1,10 +1,11 @@
 import {Transform} from 'jscodeshift';
 import promoteInformationHighlight from './promoteInformationHighlight';
 import promoteSegmentedControl from './promoteSegmentedControl';
+import promoteAvatar from './promoteAvatar';
 
 const transform: Transform = (file, api, options) => {
   // These will run in order. If your transform depends on others, place yours after dependent transforms
-  const fixes: Transform[] = [promoteSegmentedControl, promoteInformationHighlight];
+  const fixes: Transform[] = [promoteSegmentedControl, promoteInformationHighlight, promoteAvatar];
   return fixes.reduce((source, fix) => fix({...file, source}, api, options) as string, file.source);
 };
 
