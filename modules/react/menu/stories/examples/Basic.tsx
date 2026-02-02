d748bc782b26bc213aac8d8d4259f69d88222848
manuel.carrera <manuel.carrera@workday.com>
2026-02-02 10:03:38 -0700

    fix: Undo storybook change

--- modules/react/menu/stories/examples/Basic.tsx
+++ modules/react/menu/stories/examples/Basic.tsx
@@ -6,24 +6,22 @@ import {BodyText} from '@workday/canvas-kit-react/text';
 export const Basic = () => {
   const [selected, setSelected] = React.useState('');
   return (
-    <div style={{width: '400px', margin: '0 auto'}} dir="rtl">
-      <Menu onSelect={data => setSelected(data.id)}>
-        <Menu.Target>Open Menu</Menu.Target>
-        <Menu.Popper>
-          <Menu.Card>
-            <Menu.List>
-              <Menu.Item>First Item</Menu.Item>
-              <Menu.Item>Second Item</Menu.Item>
-              <Menu.Divider />
-              <Menu.Item>Third Item (with a really, really, really long label)</Menu.Item>
-              <Menu.Item aria-disabled>Fourth Item</Menu.Item>
-            </Menu.List>
-          </Menu.Card>
-        </Menu.Popper>
-        <BodyText size="small" marginTop="s">
-          Selected: <span data-testid="output">{selected}</span>
-        </BodyText>
-      </Menu>
-    </div>
+    <Menu onSelect={data => setSelected(data.id)}>
+      <Menu.Target>Open Menu</Menu.Target>
+      <Menu.Popper>
+        <Menu.Card>
+          <Menu.List>
+            <Menu.Item>First Item</Menu.Item>
+            <Menu.Item>Second Item</Menu.Item>
+            <Menu.Divider />
+            <Menu.Item>Third Item (with a really, really, really long label)</Menu.Item>
+            <Menu.Item aria-disabled>Fourth Item</Menu.Item>
+          </Menu.List>
+        </Menu.Card>
+      </Menu.Popper>
+      <BodyText size="small" marginTop="s">
+        Selected: <span data-testid="output">{selected}</span>
+      </BodyText>
+    </Menu>
   );
 };
