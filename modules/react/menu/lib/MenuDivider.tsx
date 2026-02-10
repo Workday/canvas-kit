ad9360795c04f33e98ad6394b8a90afd74747aca
manuel.carrera <manuel.carrera@workday.com>
2026-02-10 12:25:39 -0700

    fix: Update divider

--- modules/react/menu/lib/MenuDivider.tsx
+++ modules/react/menu/lib/MenuDivider.tsx
@@ -13,7 +13,7 @@ export const menuDividerStencil = createStencil({
     // TODO (forwardfit token): Revisit token, using v4 token and fallback to v3 token
     borderTop: `${px2rem(1)} solid ${cssVar(system.color.border.default, system.color.border.divider)}`,
     // TODO (forwardfit token): Revisit token, using v4 token and fallback to v3 token
-    margin: `${cssVar(system.padding.xs, system.space.x2)} ${px2rem(16)}`,
+    margin: `${cssVar(system.padding.xs, system.space.x2)} ${cssVar(system.padding.md, system.space.zero)}`,
   },
 });
 
