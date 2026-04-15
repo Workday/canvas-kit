28451b173501c2ab57f3df212d092ccba183e40d
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2026-04-15 08:05:02 -0600

    feat: Add codemod and docs for icons (#3865)
    Fixes: #3864 
    
    Codemods and docs for icon migration have been added.
    
    [category:Infrastructure]

--- modules/codemod/index.js
+++ modules/codemod/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const {spawn} = require('child_process');
-const chalk = require('chalk');
+const { spawn } = require("child_process");
+const chalk = require("chalk");
 
 const {
   _: commands,
@@ -10,124 +10,212 @@ const {
   ignoreConfig,
   ignorePattern,
   verbose,
-} = require('yargs')
-  .scriptName('canvas-kit-codemod')
-  .usage(chalk.bold.blueBright('$0 <transform> [path]'))
+} = require("yargs")
+  .scriptName("canvas-kit-codemod")
+  .usage(chalk.bold.blueBright("$0 <transform> [path]"))
   .options({
-    'ignore-config': {
-      type: 'string',
+    "ignore-config": {
+      type: "string",
       describe:
-        'Ignore files if they match patterns sourced from a configuration file (e.g. a .gitignore)',
-      default: '',
+        "Ignore files if they match patterns sourced from a configuration file (e.g. a .gitignore)",
+      default: "",
     },
-    'ignore-pattern': {
-      type: 'string',
-      describe: 'Ignore files that match a provided glob expression (e.g. **/dist/**)',
-      default: '**/node_modules/**',
+    "ignore-pattern": {
+      type: "string",
+      describe:
+        "Ignore files that match a provided glob expression (e.g. **/dist/**)",
+      default: "**/node_modules/**",
     },
     verbose: {
-      type: 'number',
-      describe: 'Show more information about the transform process',
+      type: "number",
+      describe: "Show more information about the transform process",
       default: 0,
       choices: [0, 1, 2],
     },
   })
-  .command('v5 [path]', chalk.gray('Canvas Kit v4 > v5 migration transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v6 [path]', chalk.gray('Canvas Kit v5 > v6 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v7 [path]', chalk.gray('Canvas Kit v6 > v7 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v8 [path]', chalk.gray('Canvas Kit v7 > v8 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v9 [path]', chalk.gray('Canvas Kit v8 > v9 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v11 [path]', chalk.gray('Canvas Kit v10 > v11 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v12 [path]', chalk.gray('Canvas Kit v11 > v12 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v13 [path]', chalk.gray('Canvas Kit v12 > v13 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v13.2 [path]', chalk.gray('Canvas React Tokens > Canvas Tokens Web v2'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v14 [path]', chalk.gray('Canvas Kit v13 > v14 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .command('v14-tokens [path]', chalk.gray('Canvas Kit v13 > v14 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
   .command(
-    'v14.1 [path]',
-    chalk.gray('Canvas Kit v14.1 upgrade transform (style props deprecation)'),
-    yargs => {
-      yargs.positional('path', {
-        type: 'string',
-        default: '.',
-        describe: chalk.gray('The path to execute the transform in (recursively).'),
+    "v5 [path]",
+    chalk.gray("Canvas Kit v4 > v5 migration transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
       });
     }
   )
-  .command('v15 [path]', chalk.gray('Canvas Kit v14 > v15 upgrade transform'), yargs => {
-    yargs.positional('path', {
-      type: 'string',
-      default: '.',
-      describe: chalk.gray('The path to execute the transform in (recursively).'),
-    });
-  })
-  .demandCommand(1, chalk.red.bold('You must provide a transform to apply.'))
+  .command(
+    "v6 [path]",
+    chalk.gray("Canvas Kit v5 > v6 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v7 [path]",
+    chalk.gray("Canvas Kit v6 > v7 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v8 [path]",
+    chalk.gray("Canvas Kit v7 > v8 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v9 [path]",
+    chalk.gray("Canvas Kit v8 > v9 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v11 [path]",
+    chalk.gray("Canvas Kit v10 > v11 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v12 [path]",
+    chalk.gray("Canvas Kit v11 > v12 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v13 [path]",
+    chalk.gray("Canvas Kit v12 > v13 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v13.2 [path]",
+    chalk.gray("Canvas React Tokens > Canvas Tokens Web v2"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v14 [path]",
+    chalk.gray("Canvas Kit v13 > v14 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v14-tokens [path]",
+    chalk.gray("Canvas Kit v13 > v14 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v14.1 [path]",
+    chalk.gray("Canvas Kit v14.1 upgrade transform (style props deprecation)"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v15 [path]",
+    chalk.gray("Canvas Kit v14 > v15 upgrade transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .command(
+    "v15-icons [path]",
+    chalk.gray("New system and expressive icons migration transform"),
+    (yargs) => {
+      yargs.positional("path", {
+        type: "string",
+        default: ".",
+        describe: chalk.gray(
+          "The path to execute the transform in (recursively)."
+        ),
+      });
+    }
+  )
+  .demandCommand(1, chalk.red.bold("You must provide a transform to apply."))
   .strictCommands()
   .fail((msg, err, yargs) => {
     if (err) {
@@ -138,32 +226,34 @@ const {
     process.exit(1);
   })
   .updateStrings({
-    'Commands:': 'Commands (transforms):',
+    "Commands:": "Commands (transforms):",
   })
-  .alias('v', 'version')
-  .alias('h', 'help')
+  .alias("v", "version")
+  .alias("h", "help")
   .help().argv;
 
 const transform = commands[0];
 // Only add an ignore-config if one is provided
-const ignoreConfigArg = ignoreConfig ? `--ignore-config=${ignoreConfig}` : '';
+const ignoreConfigArg = ignoreConfig ? `--ignore-config=${ignoreConfig}` : "";
 console.log(ignorePattern);
 
-console.log(chalk.blueBright(`\nApplying ${transform} transform to '${path}'\n`));
+console.log(
+  chalk.blueBright(`\nApplying ${transform} transform to '${path}'\n`)
+);
 const args =
   `-t ${__dirname}/dist/es6/${transform} ${path} --parser tsx --extensions js,jsx,ts,tsx ${ignoreConfigArg} --ignore-pattern=${ignorePattern} --verbose=${verbose}`.split(
-    ' '
+    " "
   );
 
 const proc = spawn(`jscodeshift`, args);
 
 let errCode = 0;
-proc.stdout.on('data', data => console.log(data.toString()));
-proc.stderr.on('data', data => {
+proc.stdout.on("data", (data) => console.log(data.toString()));
+proc.stderr.on("data", (data) => {
   errCode = 1;
   console.error(data.toString());
 });
 
-proc.on('close', code => {
+proc.on("close", (code) => {
   process.exit(code || errCode);
 });
