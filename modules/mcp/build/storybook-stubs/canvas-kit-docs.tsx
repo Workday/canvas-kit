146aca2569fc59bcfb5802fa1f1e5eda18ff3daf
manuel.carrera <manuel.carrera@workday.com>
2026-04-16 09:36:41 -0600

    fix: Revert changes

--- modules/mcp/build/storybook-stubs/canvas-kit-docs.tsx
+++ modules/mcp/build/storybook-stubs/canvas-kit-docs.tsx
@@ -131,7 +131,6 @@ export function ExampleCodeBlock({code}: {code: ExampleComponent}) {
 }
 
 export function SymbolDoc(_props: {name?: string; fileName?: string}) {
-  console.log('SymbolDoc', _props);
   return null;
 }
 
