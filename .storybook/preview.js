d8d64d5517714e917d52b1cef40f3a5ffdc4a6c2
Sheelah Brennan <sheelah@workday.com>
2026-06-02 13:50:46 -0600

    feat!: Update type to use Sana Sans

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,8 @@ import '@workday/canvas-tokens-web/css/base/_variables.css';
 import '@workday/canvas-tokens-web/css/brand/_variables.css';
 import '@workday/canvas-tokens-web/css/component/_variables.css';
 import '@workday/canvas-tokens-web/css/system/_variables.css';
+// After tokens so Sana Sans overrides token font-family defaults in the preview iframe
+import './updated-type.css';
 
 import {CanvasProviderDecorator} from '../utils/storybook';
 import routes from './routes';
