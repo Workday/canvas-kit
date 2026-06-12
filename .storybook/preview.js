c70e97007d15aa49879a1afcc0be1404eaf47f79
Raisa Primerova <riyuhide@gmail.com>
2026-06-12 12:37:35 -0600

    chore: Attach sana theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,7 @@ import {defaultCanvasTheme} from '@workday/canvas-kit-react/common';
 import '@workday/canvas-tokens-web/css/base/_variables.css';
 import '@workday/canvas-tokens-web/css/brand/_variables.css';
 import '@workday/canvas-tokens-web/css/component/_variables.css';
+import '@workday/canvas-tokens-web/css/sana/_variables.css';
 import '@workday/canvas-tokens-web/css/system/_variables.css';
 
 import {CanvasProviderDecorator} from '../utils/storybook';
