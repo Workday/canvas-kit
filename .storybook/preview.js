87166a5c6b7763a45e7d4c90dade1167472c9abe
Sheelah Brennan <sheelah@workday.com>
2026-06-12 13:51:32 -0600

    docs: add Sana theme toggle to Storybook toolbar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,6 +14,22 @@ import theme from './theme';
 // set routes on window for testing the validity of the routes
 window.__routes = routes;
 
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Token theme',
+    defaultValue: 'canvas',
+    toolbar: {
+      icon: 'paintbrush',
+      items: [
+        {value: 'canvas', title: 'Canvas'},
+        {value: 'sana', title: 'Sana'},
+      ],
+      showName: true,
+    },
+  },
+};
+
 export const decorators = [CanvasProviderDecorator];
 
 export const parameters = {
