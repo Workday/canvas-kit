7efb15b3604fe01f219136257ee19c74c87608d3
josh.bagwell <josh.bagwell@workday.com>
2026-07-08 12:46:03 -0600

    chore: Updated spec

--- modules/styling/spec/colorSpace.spec.ts
+++ modules/styling/spec/colorSpace.spec.ts
@@ -23,7 +23,7 @@ describe('CSS color-mix Functions', () => {
       const expected = colorSpace.hover({color, fallback, colorType});
 
       expect(expected).toBe(
-        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-hover, black) calc(var(--cnvs-sys-opacity-${colorType}-hover, 0.18) * 100%))`
+        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-hover, oklch(0.0847 0 0 / 0.17)) calc(var(--cnvs-sys-opacity-${colorType}-hover, 0.18) * 100%))`
       );
     });
     it('should return color-mix of hover state using surface tokens', () => {
@@ -34,7 +34,7 @@ describe('CSS color-mix Functions', () => {
       const expected = colorSpace.hover({color, fallback, colorType});
 
       expect(expected).toBe(
-        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-hover-default, black) calc(var(--cnvs-sys-opacity-${colorType}-hover, 0.08) * 100%))`
+        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-hover-default, oklch(0.3343 0.0951 253.3 / 0.05)) calc(var(--cnvs-sys-opacity-${colorType}-hover, 0.08) * 100%))`
       );
     });
   });
@@ -47,7 +47,7 @@ describe('CSS color-mix Functions', () => {
       const expected = colorSpace.pressed({color, fallback, colorType});
 
       expect(expected).toBe(
-        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-pressed, black) calc(var(--cnvs-sys-opacity-${colorType}-pressed, 0.36) * 100%))`
+        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-pressed, oklch(0.0847 0 0 / 0.36)) calc(var(--cnvs-sys-opacity-${colorType}-pressed, 0.36) * 100%))`
       );
     });
     it('should return color-mix of pressed state using surface tokens', () => {
@@ -58,7 +58,7 @@ describe('CSS color-mix Functions', () => {
       const expected = colorSpace.pressed({color, fallback, colorType});
 
       expect(expected).toBe(
-        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-pressed-default, black) calc(var(--cnvs-sys-opacity-${colorType}-pressed, 0.18) * 100%))`
+        `color-mix(in srgb, var(${color}, var(${fallback})) , var(--cnvs-sys-color-${colorType}-overlay-pressed-default, oklch(0.3337 0.0688 250.79 / 0.09)) calc(var(--cnvs-sys-opacity-${colorType}-pressed, 0.18) * 100%))`
       );
     });
   });
