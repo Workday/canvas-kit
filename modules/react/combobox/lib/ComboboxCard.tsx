63e84ebf2338dafd90c86596ff639f93d5fcd2b5
manuel.carrera <manuel.carrera@workday.com>
2026-07-23 15:30:29 -0600

    fix: Update upgrade guides

--- modules/react/combobox/lib/ComboboxCard.tsx
+++ modules/react/combobox/lib/ComboboxCard.tsx
@@ -4,8 +4,7 @@ import {
   createSubcomponent,
 } from '@workday/canvas-kit-react/common';
 import {Menu} from '@workday/canvas-kit-react/menu';
-import {createStencil, handleCsProp} from '@workday/canvas-kit-styling';
-import {system} from '@workday/canvas-tokens-web';
+import {handleCsProp} from '@workday/canvas-kit-styling';
 
 import {useComboboxModel} from './hooks/useComboboxModel';
 
