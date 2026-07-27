0744e4146f16f75090280918c42219680735239d
Sheelah Brennan <sheelah@workday.com>
2026-07-27 11:12:38 -0600

    docs: Add story for ctaPlacementEnd on RTL

--- modules/react/information-highlight/stories/examples/RTL.tsx
+++ modules/react/information-highlight/stories/examples/RTL.tsx
@@ -32,6 +32,26 @@ export const RTL = () => {
           <InformationHighlight.Link href="#hyperlink">وثائق</InformationHighlight.Link>
         </InformationHighlight>
       </CanvasProvider>
+      <CanvasProvider dir="rtl">
+        <InformationHighlight variant="informational" ctaPlacement="end">
+          <InformationHighlight.Icon />
+          <InformationHighlight.Heading>انتباه! من اليمين إلى اليسار</InformationHighlight.Heading>
+          <InformationHighlight.Body>
+            مع `ctaPlacement="end"`، يظهر الرابط بجانب المحتوى، متمركزًا عموديًا عند نهاية السطر.
+          </InformationHighlight.Body>
+          <InformationHighlight.Link href="#hyperlink">وثائق</InformationHighlight.Link>
+        </InformationHighlight>
+      </CanvasProvider>
+      <CanvasProvider dir="rtl">
+        <InformationHighlight variant="caution" emphasis="high" ctaPlacement="end">
+          <InformationHighlight.Icon />
+          <InformationHighlight.Heading>انتباه! من اليمين إلى اليسار</InformationHighlight.Heading>
+          <InformationHighlight.Body>
+            يعمل وضع CTA عبر الأنماط ومستويات التأكيد.
+          </InformationHighlight.Body>
+          <InformationHighlight.Link href="#hyperlink">وثائق</InformationHighlight.Link>
+        </InformationHighlight>
+      </CanvasProvider>
     </Flex>
   );
 };
