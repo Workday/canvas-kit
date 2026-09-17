e4d7a6a59aa50f20a7f66db8752eaf19d3d14eda
Raisa Primerova <raisa.primerova@workday.com>
2026-09-17 09:58:14 -0600

    fix: Fix issue with story naming

--- modules/preview-react/tabs/stories/Tabs.stories.ts
+++ modules/preview-react/tabs/stories/Tabs.stories.ts
@@ -7,7 +7,7 @@ import {AlternativeTabStop as AlternativeTabStopExample} from './examples/Altern
 import {Basic as BasicExample} from './examples/Basic';
 import {DisabledTab as DisabledTabExample} from './examples/DisabledTab';
 import {DynamicTabs as DynamicTabsExample} from './examples/DynamicTabs';
-import {FilledTabs as OutlinedTabsExample} from './examples/FilledTabs';
+import {FilledTabs as FilledTabsExample} from './examples/FilledTabs';
 import {HoistedModel as HoistedModelExample} from './examples/HoistedModel';
 import {Icons as IconsExample} from './examples/Icons';
 import {NamedTabs as NamedTabsExample} from './examples/NamedTabs';
@@ -31,8 +31,8 @@ type Story = StoryObj<typeof Tabs>;
 export const Basic: Story = {
   render: BasicExample,
 };
-export const OutlinedTabs: Story = {
-  render: OutlinedTabsExample,
+export const FilledTabs: Story = {
+  render: FilledTabsExample,
 };
 export const NamedTabs: Story = {
   render: NamedTabsExample,
